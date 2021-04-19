20441aaadbba75a051be99e3f468fcbfe64e9840
pappeste <pappeste@amazon.com>
2021-04-19 14:50:05 -0700

    Typo

--- Gems/AWSClientAuth/Code/Source/Authentication/AuthenticationProviderManager.cpp
+++ Gems/AWSClientAuth/Code/Source/Authentication/AuthenticationProviderManager.cpp
@@ -42,7 +42,7 @@ namespace AWSClientAuth
     {
         ResetProviders();
         AZ::IO::FileIOBase* fileIO = AZ::IO::FileIOBase::GetInstance();
-        AZ_Assert(fileIO), "File IO is not initialized.");
+        AZ_Assert(fileIO, "File IO is not initialized.");
         
         m_settingsRegistry.reset();
         m_settingsRegistry = AZStd::make_shared<AZ::SettingsRegistryImpl>();
