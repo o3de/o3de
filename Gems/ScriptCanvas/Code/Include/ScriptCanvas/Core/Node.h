f0fb56c9faa11a5281710ab5e17596a36c012dfd
chcurran <82187351+carlitosan@users.noreply.github.com>
2021-04-20 12:04:57 -0700

    Replace string/hash based execution out look up for indices, 10-50% speed up in lookup code. Fix bugs calling dependent functions.

--- Gems/ScriptCanvas/Code/Include/ScriptCanvas/Core/Node.h
+++ Gems/ScriptCanvas/Code/Include/ScriptCanvas/Core/Node.h
@@ -700,6 +700,8 @@ namespace ScriptCanvas
         // override if necessary, usually only when the node's execution topology dramatically alters at edit-time in a way that is not generally parseable 
         ConstSlotsOutcome GetSlotsInExecutionThreadByType(const Slot& executionSlot, CombinedSlotType targetSlotType, const Slot* executionChildSlot = nullptr) const;
 
+        size_t GetOutIndex(const Slot& slot) const;
+
         // override if necessary, only used by NodeableNodes which can hide branched outs and rename them later
         virtual AZ::Outcome<AZStd::string> GetInternalOutKey(const Slot& slot) const;
 
@@ -750,6 +752,8 @@ namespace ScriptCanvas
         virtual const Slot* GetEBusDisconnectSlot() const;
 
         virtual AZStd::string GetEBusName() const;
+
+        virtual AZStd::optional<size_t> GetEventIndex(AZStd::string eventName) const;
 
         virtual AZStd::vector<SlotId> GetEventSlotIds() const;
 
