c933811ecc54e2e0aa0901755371b91aad016eb1
Jonny Galloway <gallowj@amazon.com>
2021-04-22 10:20:30 -0500

    config update
    Was using engineroot.txt as a search slug, that file no longer exists, switched to engine.json

--- Gems/AtomLyIntegration/TechnicalArt/DccScriptingInterface/azpy/__init__.py
+++ Gems/AtomLyIntegration/TechnicalArt/DccScriptingInterface/azpy/__init__.py
@@ -36,11 +36,10 @@ _TYPE_TAG = 'module'
 
 _PACKAGENAME = _TOOL_TAG
 
-__all__ = ['initialize_logger', 
-           'config_utils', 'render',
+__all__ = ['config_utils', 'render',
            'constants', 'return_stub', 'synthetic_env',
            'env_base', 'env_bool', 'test', 'dev',
-           'lumberyard', 'marmoset'] #'blender', 'maya', 'substance', 'houdini']
+           'lumberyard', 'marmoset']  # 'blender', 'maya', 'substance', 'houdini']
 # -------------------------------------------------------------------------
 
 
