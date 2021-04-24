e1ef16c8a2f4bcbdd7881a4adda270ed056bc257
dmcdiar <dmcdiar@amazon.com>
2021-04-23 19:24:16 -0700

    Chenged DynamicInputAssembly back to InputAssembly since that change is not in this branch.

--- Gems/Atom/RPI/Code/Source/RPI.Public/Buffer/BufferSystem.cpp
+++ Gems/Atom/RPI/Code/Source/RPI.Public/Buffer/BufferSystem.cpp
@@ -105,7 +105,7 @@ namespace AZ
                 bufferPoolDesc.m_hostMemoryAccess = RHI::HostMemoryAccess::Write;
                 break;
             case CommonBufferPoolType::DynamicInputAssembly:
-                bufferPoolDesc.m_bindFlags = RHI::BufferBindFlags::DynamicInputAssembly | RHI::BufferBindFlags::ShaderRead;
+                bufferPoolDesc.m_bindFlags = RHI::BufferBindFlags::InputAssembly | RHI::BufferBindFlags::ShaderRead;
                 bufferPoolDesc.m_heapMemoryLevel = RHI::HeapMemoryLevel::Host;
                 bufferPoolDesc.m_hostMemoryAccess = RHI::HostMemoryAccess::Write;
                 break;
