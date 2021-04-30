ec6f1ef0edc42bbcaeeffd9e344193f5df4156e4
Tommy Walton <waltont@amazon.com>
2021-04-30 12:08:39 -0700

    Added a comment clarifying why we still want to use a concurrency_checker even though the underlying IVisibilityScene is thread safe

--- Gems/Atom/RPI/Code/Source/RPI.Public/Culling.cpp
+++ Gems/Atom/RPI/Code/Source/RPI.Public/Culling.cpp
@@ -242,6 +242,8 @@ namespace AZ
             // Multiple threads can call RegisterOrUpdateCullable at the same time
             // since the underlying visScene is thread safe, but we don't want any threads
             // to call RegisterOrUpdateCullable between BeginCulling and EndCulling,
+            // because that could cause a mis-match between the result of
+            // IVisibilityScene::GetEntryCount and the number of cullables actually in the scene
             // so use soft_lock_shared here
             m_cullDataConcurrencyCheck.soft_lock_shared();
             m_visScene->InsertOrUpdateEntry(cullable.m_cullData.m_visibilityEntry);
@@ -253,6 +255,8 @@ namespace AZ
             // Multiple threads can call UnregisterCullable at the same time
             // since the underlying visScene is thread safe, but we don't want any threads
             // to call UnregisterCullable between BeginCulling and EndCulling,
+            // because that could cause a mis-match between the result of
+            // IVisibilityScene::GetEntryCount and the number of cullables actually in the scene
             // so use soft_lock_shared here
             m_cullDataConcurrencyCheck.soft_lock_shared();
             m_visScene->RemoveEntry(cullable.m_cullData.m_visibilityEntry);
