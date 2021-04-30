025f4b342b4bdceaa5d116f196655ee18421a816
Tommy Walton <waltont@amazon.com>
2021-04-30 11:23:55 -0700

    Added unit tests, fixed a bug caught by the tests, and fixed the release build of the ArrayView.cpp tests

--- Code/Framework/AtomCore/AtomCore/std/parallel/concurrency_checker.h
+++ Code/Framework/AtomCore/AtomCore/std/parallel/concurrency_checker.h
@@ -20,7 +20,7 @@
 
 namespace AZStd
 {
-    //! Simple class for verifying that no concurrent access is occuring.
+    //! Simple class for verifying that no concurrent access is occurring.
     //! This is *not* a synchronization primitive, and is intended simply for checking that no concurrency issues exist.
     //! It will be compiled out in release builds.
     //! Use concurrency_checker like a mutex (i.e. call soft_lock() and soft_unlock() around all instances of your data access).
@@ -51,16 +51,16 @@ namespace AZStd
         AZ_FORCE_INLINE void soft_lock_shared()
         {
 #ifdef AZ_CONCURRENCY_CHECKER_ENABLED
+            AZ_Assert(m_concurrencyCounter == 0, "Concurrency check failed. A soft_lock_shared was attempted when there was already a soft_lock.");
             ++m_sharedConcurrencyCounter;
-            AZ_Assert(m_concurrencyCounter == 0, "Concurrency check failed. Multiple threads are trying to access data at the same time, or there is a lock/unlock mismatch.");
 #endif
         }
 
         AZ_FORCE_INLINE void soft_unlock_shared()
         {
 #ifdef AZ_CONCURRENCY_CHECKER_ENABLED
+            AZ_Assert(m_sharedConcurrencyCounter != 0, "Concurrency check failed. There is a shared_lock/shared_unlock mismatch.");
             --m_sharedConcurrencyCounter;
-            AZ_Assert(m_concurrencyCounter == 0, "Concurrency check failed. If the assert in soft_lock() has not triggered already, then most likely there is a lock/unlock mismatch.");
 #endif
         }
 
