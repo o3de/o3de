6dcc75ed78a6478796888c273f0ddb15fe8fb3ac
Aristo7 <5432499+Aristo7@users.noreply.github.com>
2021-05-05 14:43:41 -0500

    AtomBridge contains all runtime deps now

--- Gems/AtomLyIntegration/AtomBridge/Code/Source/Platform/Mac/additional_windows_tool_deps.cmake
+++ Gems/AtomLyIntegration/AtomBridge/Code/Source/Platform/Mac/additional_windows_tool_deps.cmake
@@ -9,6 +9,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #
 
-set(GEM_DEPENDENCIES
-    Gem::QtForPython.Editor
+set(LY_RUNTIME_DEPENDENCIES
+    Gem::Atom_RHI_Metal.Private
+    Gem::Atom_RHI_Metal.Builders
+    Gem::Atom_RHI_Vulkan.Builders
+    Gem::Atom_RHI_DX12.Builders
+    Gem::Atom_RHI_Null.Private
+    Gem::Atom_RHI_Null.Builders
 )
