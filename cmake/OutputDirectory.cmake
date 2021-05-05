412bf6777239da2a30378a481b47a5c07d8e4572
pappeste <pappeste@amazon.com>
2021-05-05 12:08:55 -0700

    changing the default prefix (install path) to be <source_dir>/install to workaround the "build/*" filter in the AP This is also better since we support installing different configuraitons/platforms in the same prefix

--- cmake/OutputDirectory.cmake
+++ cmake/OutputDirectory.cmake
@@ -13,4 +13,8 @@
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib CACHE PATH "Build directory for static libraries and import libraries")
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin CACHE PATH "Build directory for shared libraries")
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin CACHE PATH "Build directory for executables")
-set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR}/install CACHE PATH "Installation prefix")
+
+# We install outside of the binary dir because our install support muliple platforms to 
+# be installed together. We also have an exclusion rule in the AP that filters out the 
+# "build" folder which is a common binary dir
+set(CMAKE_INSTALL_PREFIX ${CMAKE_SOURCE_DIR}/install CACHE PATH "Installation prefix")
