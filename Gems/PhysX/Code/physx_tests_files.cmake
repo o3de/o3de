52d2998a06cf5230ab05a2493631de95123996b5
jonbeer <jonbeer@amazon.com>
2021-05-06 08:14:41 -0700

    Removing physics test and aztoolsframework dependency

--- Gems/PhysX/Code/physx_tests_files.cmake
+++ Gems/PhysX/Code/physx_tests_files.cmake
@@ -25,7 +25,6 @@ set(FILES
     Tests/PhysXForceRegionTest.cpp
     Tests/PhysXMaterialLibraryTest.cpp
     Tests/PhysXCollisionFilteringTest.cpp
-    Tests/PhysXColliderPrefabTests.cpp
     Tests/PhysXJointsTest.cpp
     Tests/PhysXSceneTests.cpp
     Tests/PhysXSceneQueryTests.cpp
