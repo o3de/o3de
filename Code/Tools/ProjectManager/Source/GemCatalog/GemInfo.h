91bf7b773dd802faa5152e94e7dcc43787bf2a72
Benjamin Jillich <jillich@amazon.com>
2021-05-06 07:50:13 +0200

    Changed the feature enum into a string list

--- Code/Tools/ProjectManager/Source/GemCatalog/GemInfo.h
+++ Code/Tools/ProjectManager/Source/GemCatalog/GemInfo.h
@@ -15,6 +15,7 @@
 #if !defined(Q_MOC_RUN)
 #include <AzCore/Math/Uuid.h>
 #include <QString>
+#include <QStringList>
 #include <QVector>
 #endif
 
@@ -33,40 +34,6 @@ namespace O3DE::ProjectManager
         };
         Q_DECLARE_FLAGS(Platforms, Platform)
 
-        enum Feature
-        {
-            Animation = 0x0,
-            Assets = 0x1,
-            Audio = 0x2,
-            Camera = 0x3,
-            CloudServices = 0x4,
-            Debug = 0x5,
-            Environment = 0x6,
-            Framework = 0x7,
-            Gameplay = 0x8,
-            Input = 0x9,
-            Material = 0x10,
-            Multiplayer = 0x11,
-            Physics = 0x12,
-            Prototyping = 0x13,
-            Rendering = 0x14,
-            Required = 0x15,
-            Sample = 0x16,
-            Scripting = 0x17,
-            SDK = 0x18,
-            Security = 0x19,
-            Social = 0x20,
-            Tools = 0x21,
-            UI = 0x22,
-            Utilities = 0x23,
-            UX = 0x24,
-            Video = 0x25,
-            VR = 0x26,
-            Weather = 0x27,
-            Core = 0x28
-        };
-        Q_DECLARE_FLAGS(Features, Feature)
-
         GemInfo(const QString& name, const QString& creator, const QString& summary, Platforms platforms, bool isAdded);
 
         QString m_name;
@@ -76,7 +43,7 @@ namespace O3DE::ProjectManager
         bool m_isAdded = false; //! Is the gem currently added and enabled in the project?
         QString m_summary;
         Platforms m_platforms;
-        Features m_features;
+        QStringList m_features;
         QString m_version;
         QString m_lastUpdatedDate;
         QString m_documentationUrl;
@@ -86,4 +53,3 @@ namespace O3DE::ProjectManager
 } // namespace O3DE::ProjectManager
 
 Q_DECLARE_OPERATORS_FOR_FLAGS(O3DE::ProjectManager::GemInfo::Platforms)
-Q_DECLARE_OPERATORS_FOR_FLAGS(O3DE::ProjectManager::GemInfo::Features)
