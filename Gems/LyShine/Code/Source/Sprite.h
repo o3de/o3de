4c8f2d68fb69ee8dd6b62b61237c4217c76179b7
michabr <82236305+michabr@users.noreply.github.com>
2021-05-07 11:25:57 -0700

    Fix up particle emitter to work with Atom (#626)
    * Fix up particle emitter component to work with Atom
    
    * PR feedback and disable render target tests until supported

--- Gems/LyShine/Code/Source/Sprite.h
+++ Gems/LyShine/Code/Source/Sprite.h
@@ -41,7 +41,6 @@ public: // member functions
     Borders GetBorders() const override;
     void SetBorders(Borders borders) override;
     void SetCellBorders(int cellIndex, Borders borders) override;
-    ITexture* GetTexture() override;
     void Serialize(TSerialize ser) override;
     bool SaveToXml(const string& pathname) override;
     bool AreBordersZeroWidth() const override;
@@ -71,7 +70,7 @@ public: // member functions
 
     // ~TextureAtlasNotifications
 
-    AZ::Data::Instance<AZ::RPI::Image> GetImage() { return m_image; }
+    AZ::Data::Instance<AZ::RPI::Image> GetImage();
 
 public: // static member functions
 
@@ -93,9 +92,6 @@ public: // static member functions
     static AZStd::string GetImageSourcePathFromProductPath(const AZStd::string& productPathname);
 
 private:
-    static bool LoadTexture(const string& texturePathname, const string& pathname, ITexture*& texture);
-    static void ReleaseTexture(ITexture*& texture);
-
     static bool LoadImage(const AZStd::string& nameTex, AZ::Data::Instance<AZ::RPI::Image>& image);
     static void ReleaseImage(AZ::Data::Instance<AZ::RPI::Image>& image);
 
@@ -120,7 +116,6 @@ private: // data
     string m_pathname;
     string m_texturePathname;
     Borders m_borders;
-    ITexture* m_texture;
     AZ::Data::Instance<AZ::RPI::Image> m_image;
     int m_numSpriteSheetCellTags;                       //!< Number of Cell child-tags in sprite XML; unfortunately needed to help with serialization.
 
--- Gems/LyShineExamples/Code/Source/UiCustomImageComponent.cpp
+++ Gems/LyShineExamples/Code/Source/UiCustomImageComponent.cpp
@@ -78,8 +78,9 @@ namespace LyShineExamples
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////
-    void UiCustomImageComponent::Render(LyShine::IRenderGraph* renderGraph)
+    void UiCustomImageComponent::Render([[maybe_unused]] LyShine::IRenderGraph* renderGraph)
     {
+#ifdef LYSHINE_ATOM_TODO // [LYN-3635] convert to use Atom
         // get fade value (tracked by UiRenderer) and compute the desired alpha for the image
         float fade = renderGraph->GetAlphaFade();
         float desiredAlpha = m_overrideAlpha * fade;
@@ -125,6 +126,7 @@ namespace LyShineExamples
         bool isTexturePremultipliedAlpha = false; // we are not rendering from a render target with alpha in it
         LyShine::BlendMode blendMode = LyShine::BlendMode::Normal;
         renderGraph->AddPrimitive(&m_cachedPrimitive, texture, m_clamp, isTextureSRGB, isTexturePremultipliedAlpha, blendMode);
+#endif
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////
