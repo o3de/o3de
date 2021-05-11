93ef3acb504ca8b978e12b414d09cc5c835c4ac3
Aristo7 <5432499+Aristo7@users.noreply.github.com>
2021-05-11 18:43:09 -0400

    Removing non-tool modules from tools runtime deps

--- Gems/AtomLyIntegration/AtomBridge/Code/Source/Platform/Mac/additional_mac_tool_deps.cmake
+++ Gems/AtomLyIntegration/AtomBridge/Code/Source/Platform/Mac/additional_mac_tool_deps.cmake
@@ -10,10 +10,7 @@
 #
 
 set(LY_RUNTIME_DEPENDENCIES
-    Gem::Atom_RHI_Metal.Private
     Gem::Atom_RHI_Metal.Builders
     Gem::Atom_RHI_Vulkan.Builders
     Gem::Atom_RHI_DX12.Builders
-    Gem::Atom_RHI_Null.Private
-    Gem::Atom_RHI_Null.Builders
 )
--- Gems/AtomLyIntegration/AtomBridge/Code/Source/Platform/Windows/additional_windows_tool_deps.cmake
+++ Gems/AtomLyIntegration/AtomBridge/Code/Source/Platform/Windows/additional_windows_tool_deps.cmake
@@ -10,11 +10,7 @@
 #
 
 set(LY_RUNTIME_DEPENDENCIES
-    Gem::Atom_RHI_Vulkan.Private
     Gem::Atom_RHI_Vulkan.Builders
-    Gem::Atom_RHI_DX12.Private
     Gem::Atom_RHI_DX12.Builders
-    Gem::Atom_RHI_Null.Private
-    Gem::Atom_RHI_Null.Builders
     Gem::Atom_RHI_Metal.Builders
 )
