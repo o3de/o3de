8e90e87bc83774d767cda5124871dd40df8c8642
greerdv <greerdv@amazon.com>
2021-05-11 16:17:23 +0100

    fixing bug with shape collider editor body not being deleted when it is recreated

--- Gems/PhysX/Code/Source/EditorShapeColliderComponent.cpp
+++ Gems/PhysX/Code/Source/EditorShapeColliderComponent.cpp
@@ -267,8 +267,14 @@ namespace PhysX
 
         if (m_sceneInterface)
         {
+            //remove the previous body if any
+            if (m_editorBodyHandle != AzPhysics::InvalidSimulatedBodyHandle)
+            {
+                m_sceneInterface->RemoveSimulatedBody(m_editorSceneHandle, m_editorBodyHandle);
+                m_editorBodyHandle = AzPhysics::InvalidSimulatedBodyHandle;
+            }
+
             m_editorBodyHandle = m_sceneInterface->AddSimulatedBody(m_editorSceneHandle, &configuration);
-            m_editorBody = azdynamic_cast<StaticRigidBody*>(m_sceneInterface->GetSimulatedBodyFromHandle(m_editorSceneHandle, m_editorBodyHandle));
         }
 
         AzPhysics::SimulatedBodyComponentRequestsBus::Handler::BusConnect(GetEntityId());
@@ -676,7 +682,6 @@ namespace PhysX
         {
             m_sceneInterface->RemoveSimulatedBody(m_editorSceneHandle, m_editorBodyHandle);
             m_editorBodyHandle = AzPhysics::InvalidSimulatedBodyHandle;
-            m_editorBody = nullptr;
         }
     }
 
@@ -747,21 +752,31 @@ namespace PhysX
 
     bool EditorShapeColliderComponent::IsPhysicsEnabled() const
     {
-        return m_editorBody != nullptr && m_editorBody->m_simulating;
+        return m_editorBodyHandle != AzPhysics::InvalidSimulatedBodyHandle;
     }
 
     AZ::Aabb EditorShapeColliderComponent::GetAabb() const
     {
-        if (m_editorBody)
+        if (m_sceneInterface && m_editorBodyHandle != AzPhysics::InvalidSimulatedBodyHandle)
         {
-            return m_editorBody->GetAabb();
+            if (auto* body = m_sceneInterface->GetSimulatedBodyFromHandle(m_editorSceneHandle, m_editorBodyHandle))
+            {
+                return body->GetAabb();
+            }
         }
         return AZ::Aabb::CreateNull();
     }
 
     AzPhysics::SimulatedBody* EditorShapeColliderComponent::GetSimulatedBody()
     {
-        return m_editorBody;
+        if (m_sceneInterface && m_editorBodyHandle != AzPhysics::InvalidSimulatedBodyHandle)
+        {
+            if (auto* body = m_sceneInterface->GetSimulatedBodyFromHandle(m_editorSceneHandle, m_editorBodyHandle))
+            {
+                return body;
+            }
+        }
+        return nullptr;
     }
 
     AzPhysics::SimulatedBodyHandle EditorShapeColliderComponent::GetSimulatedBodyHandle() const
@@ -771,9 +786,12 @@ namespace PhysX
 
     AzPhysics::SceneQueryHit EditorShapeColliderComponent::RayCast(const AzPhysics::RayCastRequest& request)
     {
-        if (m_editorBody)
+        if (m_sceneInterface && m_editorBodyHandle != AzPhysics::InvalidSimulatedBodyHandle)
         {
-            return m_editorBody->RayCast(request);
+            if (auto* body = m_sceneInterface->GetSimulatedBodyFromHandle(m_editorSceneHandle, m_editorBodyHandle))
+            {
+                return body->RayCast(request);
+            }
         }
         return AzPhysics::SceneQueryHit();
     }
