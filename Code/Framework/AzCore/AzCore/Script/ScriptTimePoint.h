fe2931829338c9e6805050533056417a2f2d2686
nvsickle <nvsickle@amazon.com>
2021-05-11 12:18:49 -0700

    Make ScriptTimePoint::Get const correct

--- Code/Framework/AzCore/AzCore/Script/ScriptTimePoint.h
+++ Code/Framework/AzCore/AzCore/Script/ScriptTimePoint.h
@@ -43,7 +43,7 @@ namespace AZ
             return AZStd::string::format("Time %llu", m_timePoint.time_since_epoch().count());
         }
 
-        const AZStd::chrono::system_clock::time_point& Get() { return m_timePoint; }
+        const AZStd::chrono::system_clock::time_point& Get() const { return m_timePoint; }
 
         // Returns the time point in seconds
         double GetSeconds() const
