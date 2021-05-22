b6bfd825d8c8e95db5f92f80b0314a01a8ff821e
dhrudesh <dhrudesh@amazon.com>
2021-05-21 17:37:17 -0700

    Adding null renderer param to AWS automation tests launchers

--- AutomatedTesting/Gem/PythonTests/AWS/Windows/client_auth/test_password_signin.py
+++ AutomatedTesting/Gem/PythonTests/AWS/Windows/client_auth/test_password_signin.py
@@ -66,7 +66,7 @@ class TestAWSClientAuthPasswordSignIn(object):
         file_to_monitor = os.path.join(launcher.workspace.paths.project_log(), GAME_LOG_NAME)
         log_monitor = ly_test_tools.log.log_monitor.LogMonitor(launcher=launcher, log_file_path=file_to_monitor)
 
-        launcher.args = ['+LoadLevel', 'AWS/ClientAuthPasswordSignUp']
+        launcher.args = ['+LoadLevel', 'AWS/ClientAuthPasswordSignUp', '-NullRenderer', '-cl_streamerDevMode=true']
 
         with launcher.start(launch_ap=False):
             result = log_monitor.monitor_log_for_lines(
