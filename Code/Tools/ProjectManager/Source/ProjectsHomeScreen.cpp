21ab3c5394106d9861843a55a90a4863e9570084
alexpete <alexpete@amazon.com>
2021-05-25 15:36:48 -0700

    Fixed editor executable name case

--- Code/Tools/ProjectManager/Source/ProjectsHomeScreen.cpp
+++ Code/Tools/ProjectManager/Source/ProjectsHomeScreen.cpp
@@ -138,7 +138,7 @@ namespace O3DE::ProjectManager
         if (!projectPath.isEmpty())
         {
             AZ::IO::FixedMaxPath executableDirectory = AZ::Utils::GetExecutableDirectory();
-            AZStd::string executableFilename = "editor";
+            AZStd::string executableFilename = "Editor";
             AZ::IO::FixedMaxPath editorExecutablePath = executableDirectory / (executableFilename + AZ_TRAIT_OS_EXECUTABLE_EXTENSION);
             auto cmdPath = AZ::IO::FixedMaxPathString::format("%s -regset=\"/Amazon/AzCore/Bootstrap/project_path=%s\"", editorExecutablePath.c_str(), projectPath.toStdString().c_str());
 
