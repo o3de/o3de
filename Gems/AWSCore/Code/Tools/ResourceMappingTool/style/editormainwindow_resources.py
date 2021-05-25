87d9c55c0e3f052599890ae8b8e90e35072c866b
onecent1101 <liug@amazon.com>
2021-05-24 22:39:26 -0700

    Fix missing legal header

--- Gems/AWSCore/Code/Tools/ResourceMappingTool/style/editormainwindow_resources.py
+++ Gems/AWSCore/Code/Tools/ResourceMappingTool/style/editormainwindow_resources.py
@@ -1,7 +1,13 @@
-# Resource object code (Python 3)
-# Created by: object code
-# Created by: The Resource Compiler for Qt version 5.15.2
-# WARNING! All changes made in this file will be lost!
+"""
+All or portions of this file Copyright (c) Amazon.com, Inc. or its affiliates or
+its licensors.
+
+For complete copyright and license terms please see the LICENSE at the root of this
+distribution (the "License"). All use of this software is governed by the License,
+or, if provided, by the license below or the license accompanying this file. Do not
+remove or modify any license notices. This file is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+"""
 
 from PySide2 import QtCore
 
