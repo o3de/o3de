51448865919b2ecaccdfaf98f58c1195b8709a11
Aaron Ruiz Mora <moraaar@amazon.com>
2021-05-28 18:50:20 +0100

    Reenable automated tests test_C4044459_Material_DynamicFriction in main test suite

--- AutomatedTesting/Gem/PythonTests/physics/TestSuite_Main.py
+++ AutomatedTesting/Gem/PythonTests/physics/TestSuite_Main.py
@@ -41,8 +41,8 @@ class TestAutomation(TestAutomationBase):
         from . import C5932041_PhysXForceRegion_LocalSpaceForceOnRigidBodies as test_module
         self._run_test(request, workspace, editor, test_module)
 
-    @pytest.mark.xfail(reason="Fails until level is updated using material library from PhysX Configuration (LYN-3961)")
     @revert_physics_config
+    @fm.file_override('physxsystemconfiguration.setreg','C4044459_Material_DynamicFriction.setreg_override', 'AutomatedTesting/Registry')
     def test_C4044459_Material_DynamicFriction(self, request, workspace, editor, launcher_platform):
         from . import C4044459_Material_DynamicFriction as test_module
         self._run_test(request, workspace, editor, test_module)
