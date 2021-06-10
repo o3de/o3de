549f80d6f755639b146eedecdbe54ae5f5f7a4de
onecent1101 <liug@amazon.com>
2021-06-09 18:45:16 -0700

    update error message

--- Gems/AWSCore/Code/Tools/ResourceMappingTool/model/error_messages.py
+++ Gems/AWSCore/Code/Tools/ResourceMappingTool/model/error_messages.py
@@ -10,10 +10,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 """
 
 ERROR_PAGE_TOOL_SETUP_ERROR_MESSAGE: str = \
-    "It appears you have not setup " \
+    "AWS credentials are missing or invalid. See " \
     "<a href=\"https://docs.o3de.org/docs/user-guide/gems/reference/aws/aws-core/configuring-credentials/\">"\
-    "<span style=\"color:#4A90E2;\">AWS credential</span></a> " \
-    "or it is invalid.<br>Please check log file under Gems/AWSCore/Code/Tool/ResourceMappingTool for more details."
+    "<span style=\"color:#4A90E2;\">documentation</span></a> for details." \
+    "<br>Check log file under Gems/AWSCore/Code/Tool/ResourceMappingTool for further information."
 
 VIEW_EDIT_PAGE_SAVING_FAILED_WITH_INVALID_ROW_ERROR_MESSAGE: str = \
     "Row {} have errors. Please correct errors or delete the row to proceed."
