f340189075b990fb925b5b83a00b29eb8ec469e4
mriegger <mriegger@amazon.com>
2021-06-10 15:48:49 -0700

    remove tabs

--- Gems/Atom/Feature/Common/Code/Include/Atom/Feature/CoreLights/DiskLightFeatureProcessorInterface.h
+++ Gems/Atom/Feature/Common/Code/Include/Atom/Feature/CoreLights/DiskLightFeatureProcessorInterface.h
@@ -101,7 +101,7 @@ namespace AZ
             //! Sets the shadowmap Pcf (percentage closer filtering) method.
             virtual void SetPcfMethod(LightHandle handle, PcfMethod method) = 0;
             //! Sets the Esm exponent to use. Higher values produce a steeper falloff in the border areas between light and shadow.
-			virtual void SetEsmExponent(LightHandle handle, float exponent) = 0;
+            virtual void SetEsmExponent(LightHandle handle, float exponent) = 0;
 
             //! Sets all of the the disk data for the provided LightHandle.
             virtual void SetDiskData(LightHandle handle, const DiskLightData& data) = 0;
