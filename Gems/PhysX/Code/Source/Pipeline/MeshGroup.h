ef7f940fd28ed338f85190972386b47d5e51b869
moraaar <moraaar@amazon.com>
2021-06-10 16:45:10 +0100

    Use dropdown comboboxes in Physics Materials fields' of PhysX Groups in FBX Settings

--- Gems/PhysX/Code/Source/Pipeline/MeshGroup.h
+++ Gems/PhysX/Code/Source/Pipeline/MeshGroup.h
@@ -177,7 +177,7 @@ namespace PhysX
             AZ_CLASS_ALLOCATOR_DECL
 
             MeshGroup();
-            ~MeshGroup() override = default;
+            ~MeshGroup() override;
 
             static void Reflect(AZ::ReflectContext* context);
 
@@ -224,6 +224,9 @@ namespace PhysX
             bool GetDecomposeMeshesVisibility() const;
 
             AZStd::string GetMaterialSlotLabel(int index) const;
+            AZStd::vector<AZStd::string> GetPhysicsMaterialNames() const;
+
+            void OnMaterialLibraryChanged(const AZ::Data::AssetId& materialLibraryAssetId);
 
             AZ::Uuid m_id{};
             AZStd::string m_name{};
@@ -239,6 +242,8 @@ namespace PhysX
             AZStd::vector<AZStd::string> m_physicsMaterials;
 
             const AZ::SceneAPI::Containers::SceneGraph* m_graph = nullptr;
+            
+            AzPhysics::SystemEvents::OnMaterialLibraryChangedEvent::Handler m_materialLibraryChangedHandler;
         };
     }
 }
