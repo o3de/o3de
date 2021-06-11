49d5dfb38ff377e76c578381e145e0d04b637cdc
alexpete <alexpete@amazon.com>
2021-06-11 11:27:33 -0700

    Reverting fixed string usage to simpler types

--- Code/Tools/ProjectManager/tests/main.cpp
+++ Code/Tools/ProjectManager/tests/main.cpp
@@ -11,8 +11,6 @@
 */
 
 #include <AzTest/AzTest.h>
-#include <AzCore/std/string/fixed_string.h>
-#include <AzCore/std/containers/fixed_vector.h>
 
 DECLARE_AZ_UNIT_TEST_MAIN();
 
@@ -27,13 +25,10 @@ int main(int argc, char* argv[])
     if (argc == 1)
     {
         // if no parameters are provided, add the --unittests parameter
-        constexpr size_t MaxCommandArgsCount = 128;
-        using ArgumentContainer = AZStd::fixed_vector<char*, MaxCommandArgsCount>;
-
-        constexpr AZStd::basic_fixed_string unittestArg{ "--unittests" };
-        ArgumentContainer argContainer{ argv[0], const_cast<char*>(unittestArg.data()) };
-
-        return runDefaultRunner(aznumeric_cast<int>(argContainer.size()), argContainer.data());
+        constexpr int defaultArgc = 2;
+        char unittest_arg[] = "--unittests"; // Conversion from string literal to char* is not allowed per ISO C++11
+        char* defaultArgv[defaultArgc] = { argv[0], unittest_arg };
+        return runDefaultRunner(defaultArgc, defaultArgv);
     }
     INVOKE_AZ_UNIT_TEST_MAIN(nullptr); 
     return 0;
