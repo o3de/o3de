19d4670b86e3b67acff03cbe7c4bf5fd0f81ac90
lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
2021-06-15 11:46:37 -0500

    Updated the comment in the EditorModule constructor
    The comment indicates that all component descriptors should be added to the m_descriptors list

--- Templates/DefaultGem/Template/Code/Source/${Name}EditorModule.cpp
+++ Templates/DefaultGem/Template/Code/Source/${Name}EditorModule.cpp
@@ -27,6 +27,9 @@ namespace ${SanitizedCppName}
         ${SanitizedCppName}EditorModule()
         {
             // Push results of [MyComponent]::CreateDescriptor() into m_descriptors here.
+            // Add ALL components descriptors associated with this gem to m_descriptors.
+            // This will associate the AzTypeInfo information for the components with the the SerializeContext, BehaviorContext and EditContext.
+            // This happens through the [MyComponent]::Reflect() function.
             m_descriptors.insert(m_descriptors.end(), {
                 ${SanitizedCppName}EditorSystemComponent::CreateDescriptor(),
             });
@@ -34,6 +37,7 @@ namespace ${SanitizedCppName}
 
         /**
          * Add required SystemComponents to the SystemEntity.
+         * Non-SystemComponents should not be added here
          */
         AZ::ComponentTypeList GetRequiredSystemComponents() const override
         {
