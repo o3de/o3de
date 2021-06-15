1de651006e4662ef125f8d518b51537149728f22
lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
2021-06-15 11:47:37 -0500

    Added more detail to the ModuleInterface constructor about the registering the Component Descriptors

--- Templates/DefaultGem/Template/Code/Source/${Name}ModuleInterface.h
+++ Templates/DefaultGem/Template/Code/Source/${Name}ModuleInterface.h
@@ -28,6 +28,9 @@ namespace ${SanitizedCppName}
         ${SanitizedCppName}ModuleInterface()
         {
             // Push results of [MyComponent]::CreateDescriptor() into m_descriptors here.
+            // Add ALL components descriptors associated with this gem to m_descriptors.
+            // This will associate the AzTypeInfo information for the components with the the SerializeContext, BehaviorContext and EditContext.
+            // This happens through the [MyComponent]::Reflect() function.
             m_descriptors.insert(m_descriptors.end(), {
                 ${SanitizedCppName}SystemComponent::CreateDescriptor(),
                 });
