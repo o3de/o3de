02c8cba664906889f367ad83bfbf9fac85a217f1
Chris Santora <santorac@amazon.com>
2021-06-15 19:26:11 -0700

    Added new shader reinitialization signaling.
    This was done while working on "ATOM-15728 Shader Hot Reload Fails in Debug Build", but it turned out these changes did not actually fix the issue (or any other known hot-reload issue). Still, these improvements are appropriate as they correct logical oversights.
    
    ShaderVariant was not listening to asset reloads. It needs to know when the ShaderVariantAsset reload happens so it can reinitialize it's members as well as propagate reinitialization messages. I added a member for the ShaderAsset as the class needs this to reinitialize itself. So now the class listens for reloads of both the ShaderVariantAsset and the ShaderAsset.
    
    Shader was not listening for ShaderAsset reinitialization events.
    
    Updated the API for ShaderReloadNotificationBus's OnShaderVariantReinitialized to include the ShaderVariant which is the most relevant information (the other information wasn't really being used anyway).

--- Gems/Atom/RPI/Code/Include/Atom/RPI.Public/Shader/ShaderVariant.h
+++ Gems/Atom/RPI/Code/Include/Atom/RPI.Public/Shader/ShaderVariant.h
@@ -46,6 +46,9 @@ namespace AZ
             bool IsRootVariant() const { return m_shaderVariantAsset->IsRootVariant(); }
 
             ShaderVariantStableId GetStableId() const { return m_shaderVariantAsset->GetStableId(); }
+            
+            const Data::Asset<ShaderAsset>& GetShaderAsset() const { return m_shaderAsset; }
+            const Data::Asset<ShaderVariantAsset>& GetShaderVariantAsset() const { return m_shaderVariantAsset; }
 
             const Data::Asset<ShaderAsset>& GetShaderAsset() const { return m_shaderAsset; }
             const Data::Asset<ShaderVariantAsset>& GetShaderVariantAsset() const { return m_shaderVariantAsset; }
