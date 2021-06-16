4014cacff8b9c05071fcd203fd43d8ed19e243b5
lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
2021-06-15 20:44:23 -0500

    Added a ${GemName}.Builders variant to Asset-Only Gems (#1324)
    * Creating Builder variant aliases for all Asset Gems
    
    * Added an AssetGem template for creating an Asset Gem
    
    The AssetGem template is meant for non-code Gems
    
    * Updated the engine_template.py commands to use pathlib
    
    Each of the four commands: create-template, create-from-template,
    create-project and create-gem have been updated to use pathlib.Path or
    any path manipulation
    
    Fix several python errors when instantiating a template with missing
    o3de objects.
    For example attempting to use the create-project command with the
    "DefaultGem" template would result in a python exception instaed of a
    error log and a return value 0..
    
    Added a **force** option to the engine_template commands
    to force overriding of existing files
    
    * Optimized search through text files when the create-template command is
    run when attempting to replace the license text section
    
    * Adding a Minimal Project Template that contains the minimal set of gems
    as stated by UX
    
    * Minor changes to the DefaultProject template. Added a license section to the EngineFinder.cmake
    
    * Adding the MinimalProject to the engine.json file

---
+++ Templates/MinimalProject/Template/Code/Include/${Name}/${Name}Bus.h
@@ -0,0 +1,44 @@
+// {BEGIN_LICENSE}
+/*
+ * All or portions of this file Copyright (c) Amazon.com, Inc. or its affiliates or
+ * its licensors.
+ *
+ * For complete copyright and license terms please see the LICENSE at the root of this
+ * distribution (the "License"). All use of this software is governed by the License,
+ * or, if provided, by the license below or the license accompanying this file. Do not
+ * remove or modify any license notices. This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ */
+ // {END_LICENSE}
+
+#pragma once
+
+#include <AzCore/EBus/EBus.h>
+#include <AzCore/Interface/Interface.h>
+
+namespace ${SanitizedCppName}
+{
+    class ${SanitizedCppName}Requests
+    {
+    public:
+        AZ_RTTI(${SanitizedCppName}Requests, "${Random_Uuid}");
+        virtual ~${SanitizedCppName}Requests() = default;
+        // Put your public methods here
+    };
+    
+    class ${SanitizedCppName}BusTraits
+        : public AZ::EBusTraits
+    {
+    public:
+        //////////////////////////////////////////////////////////////////////////
+        // EBusTraits overrides
+        static constexpr AZ::EBusHandlerPolicy HandlerPolicy = AZ::EBusHandlerPolicy::Single;
+        static constexpr AZ::EBusAddressPolicy AddressPolicy = AZ::EBusAddressPolicy::Single;
+        //////////////////////////////////////////////////////////////////////////
+    };
+
+    using ${SanitizedCppName}RequestBus = AZ::EBus<${SanitizedCppName}Requests, ${SanitizedCppName}BusTraits>;
+    using ${SanitizedCppName}Interface = AZ::Interface<${SanitizedCppName}Requests>;
+
+} // namespace ${SanitizedCppName}
