6cf3a2657484f2c7fd7ce9dfbbb607b6d247966b
jckand-amzn <jckand@amazon.com>
2021-06-16 16:41:50 -0500

    Duplicating test to run on GPU node

--- AutomatedTesting/Gem/PythonTests/editor/test_BasicEditorWorkflows.py
+++ AutomatedTesting/Gem/PythonTests/editor/test_BasicEditorWorkflows.py
@@ -66,3 +66,35 @@ class TestBasicEditorWorkflows(object):
             timeout=log_monitor_timeout,
             auto_test_mode=False
         )
+
+    @pytest.mark.test_case_id("C6351273", "C6384955", "C16929880", "C15167490", "C15167491")
+    @pytest.mark.SUITE_main
+    @pytest.mark.REQUIRES_gpu
+    def test_BasicEditorWorkflows_GPU_LevelEntityComponentCRUD(self, request, editor, level, launcher_platform):
+
+        # Skip test if running against Debug build
+        if "debug" in internal_plugin.build_directory:
+            pytest.skip("Does not execute against debug builds.")
+
+        expected_lines = [
+            "Create and load new level: True",
+            "New entity creation: True",
+            "Create entity hierarchy: True",
+            "Add component: True",
+            "Component update: True",
+            "Remove component: True",
+            "Save and Export: True",
+            "BasicEditorWorkflows_LevelEntityComponent:  result=SUCCESS",
+        ]
+
+        hydra.launch_and_validate_results(
+            request,
+            test_directory,
+            editor,
+            "BasicEditorWorkflows_LevelEntityComponentCRUD.py",
+            expected_lines,
+            cfg_args=[level],
+            timeout=log_monitor_timeout,
+            auto_test_mode=False,
+            null_renderer=False
+        )
