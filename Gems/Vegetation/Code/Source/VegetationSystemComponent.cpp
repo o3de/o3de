13bf685661f1f15096e8d354ebefa3b228286f5f
Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
2021-06-18 09:28:31 -0500

    Cleaned up vegetation_precompiled.h (#1418)
    * Removed vegetation_precompiled.h
    * Removed CryCommon dependency

--- Gems/Vegetation/Code/Source/VegetationSystemComponent.cpp
+++ Gems/Vegetation/Code/Source/VegetationSystemComponent.cpp
@@ -9,7 +9,6 @@
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 *
 */
-#include "Vegetation_precompiled.h"
 #include "VegetationSystemComponent.h"
 
 #include <AzCore/RTTI/BehaviorContext.h>
@@ -26,8 +25,6 @@
 #include <Vegetation/EmptyInstanceSpawner.h>
 #include <Vegetation/DynamicSliceInstanceSpawner.h>
 #include <Vegetation/PrefabInstanceSpawner.h>
-
-#include <CrySystemBus.h>
 
 namespace Vegetation
 {
