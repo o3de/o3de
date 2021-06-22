9e4d6a1d85ae86cd7b11831ae83db5f209080c9c
nvsickle <nvsickle@amazon.com>
2021-06-21 20:58:00 -0700

    Scale the viewport debug info overlay by the DPI scale factor
    Signed-off-by: nvsickle <nvsickle@amazon.com>

--- Gems/AtomLyIntegration/AtomViewportDisplayInfo/Code/Source/AtomViewportDisplayInfoSystemComponent.cpp
+++ Gems/AtomLyIntegration/AtomViewportDisplayInfo/Code/Source/AtomViewportDisplayInfoSystemComponent.cpp
@@ -157,7 +157,7 @@ namespace AZ::Render
         auto viewportSize = viewportContext->GetViewportSize();
         m_drawParams.m_position = AZ::Vector3(viewportSize.m_width, 0.0f, 1.0f) + AZ::Vector3(r_topRightBorderPadding);
         m_drawParams.m_color = AZ::Colors::White;
-        m_drawParams.m_scale = AZ::Vector2(0.7f);
+        m_drawParams.m_scale = AZ::Vector2(0.7f * viewportContext->GetDpiScalingFactor());
         m_drawParams.m_hAlign = AzFramework::TextHorizontalAlignment::Right;
         m_drawParams.m_monospace = false;
         m_drawParams.m_depthTest = false;
