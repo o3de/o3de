b9e0d7b4152b1a74d03c4fc009db481f7a38b59b
John <jonawals@amazon.com>
2021-06-22 15:15:24 +0100

    missing pycov stuff

--- Code/Framework/AzToolsFramework/AzToolsFramework/aztoolsframework_files.cmake
+++ Code/Framework/AzToolsFramework/AzToolsFramework/aztoolsframework_files.cmake
@@ -41,7 +41,7 @@ set(FILES
     API/EditorVegetationRequestsBus.h
     API/EditorPythonConsoleBus.h
     API/EditorPythonRunnerRequestsBus.h
-    API/EditorPythonRunnerNotificationBus.h
+    API/EditorPythonScriptNotificationsBus.h
     API/EntityPropertyEditorRequestsBus.h
     API/EditorWindowRequestBus.h
     API/EntityCompositionRequestBus.h
