3ea3615d75c46d495a9d60bb7ab6558e96bdeef5
spham <spham@amazon.com>
2021-06-22 20:43:55 -0700

    - Update the copyright validation script to exclude known files that only have the original copyright embedded - Update to fail on any 'modifications by Amazon.com' etc

--- scripts/commit_validation/commit_validation/validators/copyright_header_validator.py
+++ scripts/commit_validation/commit_validation/validators/copyright_header_validator.py
@@ -16,6 +16,16 @@ AMAZON_ORIGINAL_COPYRIGHT_PATTERN = re.compile(r'.*?this\sfile\sCopyright\s*\(c\
 AMAZON_MODIFICATION_COPYRIGHT_PATTERN = re.compile(r'.*?Modifications\scopyright\sAmazon\.com', re.IGNORECASE | re.DOTALL)
 CRYTEK_COPYRIGHT_PATTERN = re.compile(r'Copyright Crytek', re.MULTILINE)
 
+EXCLUDED_COPYRIGHT_VALIDATION_PATTERNS = [
+    '*/Code/Framework/AzCore/AzCore/Math/Sha1.h',                                       # Copyright 2007 Andy Tompkins.
+    '*/Code/Framework/AzCore/AzCore/std/string/utf8/core.h',                            # Copyright 2006 Nemanja Trifunovic
+    '*/Code/Framework/AzCore/AzCore/std/string/utf8/unchecked.h',                       # Copyright 2006 Nemanja Trifunovic
+    '*/Gems/Atom/Feature/Common/Assets/ShaderLib/Atom/Features/PBR/Lights/Ltc.azsli',   # Copyright (c) 2017, Eric Heitz, Jonathan Dupuy, Stephen Hill and David Neubelt.
+    '*/Code/CryEngine/CryCommon/MTPseudoRandom.h',                                      # Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura
+    '*/Code/CryEngine/CryCommon/PNoise3.h',                                             # Copyright(c) Ken Perlin
+    '*/Code/Framework/AzQtComponents/AzQtComponents/Components/FlowLayout.*'            # Copyright (C) 2015 The Qt Company Ltd.
+] + EXCLUDED_VALIDATION_PATTERNS
+
 THIS_FILE = os.path.normcase(__file__)
 
 class CopyrightHeaderValidator(CommitValidator):
@@ -26,7 +36,7 @@ class CopyrightHeaderValidator(CommitValidator):
             if os.path.normcase(file_name) == THIS_FILE:
                 # Skip this validator file
                 continue
-            for pattern in EXCLUDED_VALIDATION_PATTERNS:
+            for pattern in EXCLUDED_COPYRIGHT_VALIDATION_PATTERNS:
                 if fnmatch.fnmatch(file_name, pattern):
                     if VERBOSE: print(f'{file_name}::{self.__class__.__name__} SKIPPED - Validation pattern excluded on path.')
                     break
@@ -58,7 +68,13 @@ class CopyrightHeaderValidator(CommitValidator):
                     if VERBOSE: print(error_message)
                     errors.append(error_message)
 
-                if not has_o3de_pattern and not has_amazon_mod_pattern:
+                if has_amazon_mod_pattern:
+                    # Has the Modifications Amazon notice
+                    error_message = str(f'{file_name}::{self.__class__.__name__} FAILED - Source file contains Modifications copyright Amazon.com.')
+                    if VERBOSE: print(error_message)
+                    errors.append(error_message)
+
+                if not has_o3de_pattern:
                     # Missing the O3DE copyright AND does not have the Amazon Modifications copyright, assuming that this file is missing valid copyrights in general
                     error_message = str(f'{file_name}::{self.__class__.__name__} FAILED - Source file missing O3DE copyright header.')
                     if VERBOSE: print(error_message)
