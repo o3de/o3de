1221b0f74fc74142c2ea83a77dbd850a644473b7
Doug McDiarmid <dmcdiar@amazon.com>
2021-06-23 02:06:31 -0700

    Minor formatting change.

--- Gems/Atom/RPI/Code/Source/RPI.Public/Pass/FullscreenTrianglePass.cpp
+++ Gems/Atom/RPI/Code/Source/RPI.Public/Pass/FullscreenTrianglePass.cpp
@@ -182,11 +182,13 @@ namespace AZ
 
             RHI::Size targetImageSize = outputAttachment->m_descriptor.m_image.m_size;
 
-            m_viewportState.m_minX = m_viewportState.m_minY = 0;
+            m_viewportState.m_minX = 0.0f;
+            m_viewportState.m_minY = 0.0f;
             m_viewportState.m_maxX = AZStd::min(static_cast<uint32_t>(params.m_viewportState.m_maxX), targetImageSize.m_width);
             m_viewportState.m_maxY = AZStd::min(static_cast<uint32_t>(params.m_viewportState.m_maxY), targetImageSize.m_height);
 
-            m_scissorState.m_minX = m_scissorState.m_minY = 0;
+            m_scissorState.m_minX = 0.0f;
+            m_scissorState.m_minY = 0.0f;
             m_scissorState.m_maxX = AZStd::min(static_cast<uint32_t>(params.m_scissorState.m_maxX), targetImageSize.m_width);
             m_scissorState.m_maxY = AZStd::min(static_cast<uint32_t>(params.m_scissorState.m_maxY), targetImageSize.m_height);
 
