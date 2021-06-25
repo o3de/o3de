ae121eef133063877d7f1cc9f083632170fdbf6b
Benjamin Jillich <jillich@amazon.com>
2021-06-25 16:18:12 +0200

    [LYN-4727] Memory management improvements and clear ownership for motion meta data
    Signed-off-by: rhhong <rhhong@amazon.com>

--- Gems/EMotionFX/Code/EMotionFX/Pipeline/SceneAPIExt/Rules/MotionMetaDataRule.h
+++ Gems/EMotionFX/Code/EMotionFX/Pipeline/SceneAPIExt/Rules/MotionMetaDataRule.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <AzCore/Memory/SystemAllocator.h>
+#include <AzCore/std/smart_ptr/unique_ptr.h>
 #include <EMotionFX/Source/MotionEventTable.h>
 #include <SceneAPI/SceneCore/DataTypes/Rules/IRule.h>
 #include <SceneAPI/SceneData/SceneDataConfiguration.h>
@@ -20,32 +21,39 @@ namespace EMotionFX::Pipeline::Rule
         AZ_RTTI(EMotionFX::Pipeline::Rule::MotionMetaData, "{A381A915-3CB3-4F60-82B3-70865CFA1F4F}");
         AZ_CLASS_ALLOCATOR(MotionMetaData, AZ::SystemAllocator, 0)
 
-        MotionMetaData() = default;
+        MotionMetaData();
+        MotionMetaData(EMotionFX::EMotionExtractionFlags extractionFlags, EMotionFX::MotionEventTable* eventTable);
         virtual ~MotionMetaData() = default;
+
+        EMotionFX::EMotionExtractionFlags GetMotionExtractionFlags() const { return m_motionExtractionFlags; }
+        AZStd::unique_ptr<EMotionFX::MotionEventTable> GetClonedEventTable(EMotionFX::Motion* targetMotion) const;
 
         static void Reflect(AZ::ReflectContext* context);
 
-        EMotionFX::MotionEventTable* m_motionEventTable = nullptr;
+    private:
+        static AZStd::unique_ptr<EMotionFX::MotionEventTable> CloneMotionEventTable(EMotionFX::MotionEventTable* sourceEventTable);
+
         EMotionFX::EMotionExtractionFlags m_motionExtractionFlags;
+        AZStd::unique_ptr<EMotionFX::MotionEventTable> m_motionEventTable;
     };
 
     class MotionMetaDataRule
-        : public ExternalToolRule<MotionMetaData>
+        : public ExternalToolRule<AZStd::shared_ptr<MotionMetaData>>
     {
     public:
         AZ_RTTI(EMotionFX::Pipeline::Rule::MotionMetaDataRule, "{E68D0C3D-CBFF-4536-95C1-676474B351A5}", AZ::SceneAPI::DataTypes::IRule);
         AZ_CLASS_ALLOCATOR(MotionMetaDataRule, AZ::SystemAllocator, 0)
 
         MotionMetaDataRule();
-        MotionMetaDataRule(const MotionMetaData& data);
+        MotionMetaDataRule(const AZStd::shared_ptr<MotionMetaData>& data);
         ~MotionMetaDataRule() final = default;
 
-        const MotionMetaData& GetData() const override      { return m_data; }
-        void SetData(const MotionMetaData& data) override   { m_data = data; }
+        const AZStd::shared_ptr<MotionMetaData>& GetData() const override      { return m_data; }
+        void SetData(const AZStd::shared_ptr<MotionMetaData>& data) override   { m_data = data; }
 
         static void Reflect(AZ::ReflectContext* context);
 
     private:
-        MotionMetaData m_data;
+        AZStd::shared_ptr<MotionMetaData> m_data;
     };
 } // EMotionFX::Pipeline::Rule
