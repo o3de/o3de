8530e783cab7d461a900c1362e04bc3e99c3ec40
lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
2021-06-24 22:23:51 -0500

    Setting the build timeout for the ProjectBuilder CMake commands to -1 (#1583)
    This prevents Project build step from timing out
    
    Moving the project build directory to be under the <project-path>/build/ folder to prevent two issues
    1. The AssetProcessor from scanning that folder for assets.
       [Bb]uild is part of the excluded folders
    2. To prevent git from seeing modified files in the build directory as
       the default .gitignore file ignores [Bb]uild

--- Code/Tools/ProjectManager/Source/ProjectManagerDefs.h
+++ Code/Tools/ProjectManager/Source/ProjectManagerDefs.h
@@ -13,7 +13,7 @@ namespace O3DE::ProjectManager
     inline constexpr static int ProjectPreviewImageWidth = 210;
     inline constexpr static int ProjectPreviewImageHeight = 280;
 
-    static const QString ProjectBuildPathPostfix = "Windows_VS2019";
+    static const QString ProjectBuildPathPostfix = "build/windows_vs2019";
     static const QString ProjectBuildErrorLogPathPostfix = "CMakeFiles/CMakeProjectBuildError.log";
     static const QString ProjectPreviewImagePath = "preview.png";
 } // namespace O3DE::ProjectManager
