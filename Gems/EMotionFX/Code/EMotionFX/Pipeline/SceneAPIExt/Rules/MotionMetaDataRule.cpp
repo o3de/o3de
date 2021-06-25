ba3b1dede34e128908dddc3f560e1500784bebc5
Benjamin Jillich <jillich@amazon.com>
2021-06-25 16:18:12 +0200

    [LYN-4727] Memory management improvements and clear ownership for motion meta data
    Signed-off-by: moudgils <moudgils@amazon.com>

--- Gems/EMotionFX/Code/EMotionFX/Pipeline/SceneAPIExt/Rules/MotionMetaDataRule.cpp
+++ Gems/EMotionFX/Code/EMotionFX/Pipeline/SceneAPIExt/Rules/MotionMetaDataRule.cpp
@@ -28,12 +28,44 @@ namespace EMotionFX::Pipeline::Rule
             ;
     }
 
-    MotionMetaDataRule::MotionMetaDataRule()
-        : ExternalToolRule<MotionMetaData>()
+    MotionMetaData::MotionMetaData(EMotionFX::EMotionExtractionFlags extractionFlags, EMotionFX::MotionEventTable* eventTable)
+        : m_motionExtractionFlags(extractionFlags)
+    {
+        m_motionEventTable = CloneMotionEventTable(eventTable);
+    }
+
+    MotionMetaData::MotionMetaData()
+        : m_motionExtractionFlags(static_cast<EMotionFX::EMotionExtractionFlags>(0))
     {
     }
 
-    MotionMetaDataRule::MotionMetaDataRule(const MotionMetaData& data)
+    AZStd::unique_ptr<EMotionFX::MotionEventTable> MotionMetaData::GetClonedEventTable(EMotionFX::Motion* targetMotion) const
+    {
+        AZStd::unique_ptr<EMotionFX::MotionEventTable> clonedEventTable = AZStd::move(CloneMotionEventTable(m_motionEventTable.get()));
+        clonedEventTable->InitAfterLoading(targetMotion);
+        return AZStd::move(clonedEventTable);
+    }
+
+    AZStd::unique_ptr<EMotionFX::MotionEventTable> MotionMetaData::CloneMotionEventTable(EMotionFX::MotionEventTable* sourceEventTable)
+    {
+        AZ::SerializeContext* serializeContext = nullptr;
+        AZ::ComponentApplicationBus::BroadcastResult(serializeContext, &AZ::ComponentApplicationBus::Events::GetSerializeContext);
+        if (!serializeContext)
+        {
+            AZ_Error("EMotionFX", false, "Cannot clone motion event table for motion meta data. Can't get serialize context from component application.");
+            return {};
+        }
+
+        AZStd::unique_ptr<EMotionFX::MotionEventTable> clonedEventTable(serializeContext->CloneObject<EMotionFX::MotionEventTable>(sourceEventTable));
+        return AZStd::move(clonedEventTable);
+    }
+
+    MotionMetaDataRule::MotionMetaDataRule()
+        : ExternalToolRule<AZStd::shared_ptr<MotionMetaData>>()
+    {
+    }
+
+    MotionMetaDataRule::MotionMetaDataRule(const AZStd::shared_ptr<MotionMetaData>& data)
         : MotionMetaDataRule()
     {
         m_data = data;
