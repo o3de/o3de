7ed0fd7b5781fc207132d84a55dcfc88af6949ad
Terry Michaels <miterenc@amazon.com>
2021-06-29 14:00:11 -0500

    Removed Amazon as Company for QSettings (#1649)
    * Removed Amazon as Company for QSettings
    
    Signed-off-by: Terry Michaels <miterenc@amazon.com>
    Signed-off-by: Alex Montgomery <alexmont@amazon.com>

--- Gems/EMotionFX/Code/EMotionFX/Tools/EMotionStudio/Plugins/RenderPlugins/Source/OpenGLRender/OpenGLRenderPlugin.h
+++ Gems/EMotionFX/Code/EMotionFX/Tools/EMotionStudio/Plugins/RenderPlugins/Source/OpenGLRender/OpenGLRenderPlugin.h
@@ -35,7 +35,7 @@ namespace EMStudio
         const char* GetCompileDate() const override         { return MCORE_DATE; }
         const char* GetName() const override                { return "OpenGL Render Window"; }
         uint32 GetClassID() const override                  { return static_cast<uint32>(RenderPlugin::CLASS_ID); }
-        const char* GetCreatorName() const override         { return "Amazon"; }
+        const char* GetCreatorName() const override         { return "O3DE"; }
         float GetVersion() const override                   { return 1.0f;  }
         bool GetIsClosable() const override                 { return true;  }
         bool GetIsFloatable() const override                { return true;  }
--- Gems/EMotionFX/Code/EMotionFX/Tools/EMotionStudio/Plugins/StandardPlugins/Source/MotionEvents/MotionEventsPlugin.h
+++ Gems/EMotionFX/Code/EMotionFX/Tools/EMotionStudio/Plugins/StandardPlugins/Source/MotionEvents/MotionEventsPlugin.h
@@ -43,7 +43,7 @@ namespace EMStudio
         const char* GetCompileDate() const override     { return MCORE_DATE; }
         const char* GetName() const override            { return "Motion Events"; }
         uint32 GetClassID() const override              { return MotionEventsPlugin::CLASS_ID; }
-        const char* GetCreatorName() const override     { return "Amazon"; }
+        const char* GetCreatorName() const override     { return "O3DE"; }
         float GetVersion() const override               { return 1.0f;  }
         bool GetIsClosable() const override             { return true;  }
         bool GetIsFloatable() const override            { return true;  }
