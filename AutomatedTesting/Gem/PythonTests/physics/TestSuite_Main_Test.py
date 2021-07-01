5fc5a7e1d8948db839fb4d576d88c268d70be931
Garcia Ruiz <aljanru@amazon.co.uk>
2021-07-01 12:23:20 +0200

    Physics test suite with new paralellization

---
+++ AutomatedTesting/Gem/PythonTests/physics/TestSuite_Main_Test.py
@@ -0,0 +1,16 @@
+import pytest
+import os
+import sys
+
+from ly_test_tools import LAUNCHERS
+from ly_test_tools.o3de.editor_test import EditorSingleTest, EditorSharedTest, EditorTestSuite
+
+@pytest.mark.parametrize("launcher_platform", ['windows_editor'])
+@pytest.mark.parametrize("project", ["AutomatedTesting"])
+class TestAutomation(EditorTestSuite):
+    
+    class test_C100000(EditorSharedTest):
+        from . import C100000_RigidBody_EnablingGravityWorksPoC as test_module
+
+    class test_C111111(EditorSharedTest):
+        from . import C111111_RigidBody_EnablingGravityWorksUsingNotificationsPoC as test_module
