bedeb789947be53a1cd28a536019c78cce945b51
igarri <igarri@amazon.com>
2021-07-05 13:35:14 +0100

    Added Button Icon
    Signed-off-by: igarri <igarri@amazon.com>

--- Code/Editor/AzAssetBrowser/AzAssetBrowserWindow.h
+++ Code/Editor/AzAssetBrowser/AzAssetBrowserWindow.h
@@ -1,6 +1,7 @@
 /*
- * Copyright (c) Contributors to the Open 3D Engine Project. For complete copyright and license terms please see the LICENSE at the root of this distribution.
- * 
+ * Copyright (c) Contributors to the Open 3D Engine Project. For complete copyright and license terms please see the LICENSE at the root of
+ * this distribution.
+ *
  * SPDX-License-Identifier: Apache-2.0 OR MIT
  *
  */
@@ -30,15 +31,14 @@ namespace AzToolsFramework
 
         enum class AssetBrowserDisplayState : int
         {
-            StandardMode,
-            SearchViewMode,
+            ExpandedMode,
+            DefaultMode,
             Invalid
         };
-    }
-}
+    } // namespace AssetBrowser
+} // namespace AzToolsFramework
 
-class AzAssetBrowserWindow
-    : public QWidget
+class AzAssetBrowserWindow : public QWidget
 {
     Q_OBJECT
 public:
@@ -58,23 +58,22 @@ private:
     void UpdateDisplayInfo();
 protected slots:
     void CreateSwitchViewMenu();
-    void SetStandardAssetBrowserMode();
-    void SetSearchViewAssetBrowserMode();
+    void SetExpandedAssetBrowserMode();
+    void SetDefaultAssetBrowserMode();
     void UpdateTableModelAfterFilter();
     void SetTableViewVisibleAfterFilter();
 
 private:
-
     QScopedPointer<Ui::AzAssetBrowserWindowClass> m_ui;
     QScopedPointer<AzToolsFramework::AssetBrowser::AssetBrowserFilterModel> m_filterModel;
     QScopedPointer<AzToolsFramework::AssetBrowser::AssetBrowserTableModel> m_tableModel;
     AzToolsFramework::AssetBrowser::AssetBrowserModel* m_assetBrowserModel;
     QMenu* m_viewSwitchMenu = nullptr;
-    QAction* m_standardAssetBrowserMode = nullptr;
-    QAction* m_searchViewAssetBrowserMode = nullptr;
+    QAction* m_expandedAssetBrowserMode = nullptr;
+    QAction* m_defaultAssetBrowserMode = nullptr;
     AzToolsFramework::AssetBrowser::AssetBrowserDisplayState m_assetBrowserDisplayState =
-        AzToolsFramework::AssetBrowser::AssetBrowserDisplayState::SearchViewMode;
-    
+        AzToolsFramework::AssetBrowser::AssetBrowserDisplayState::DefaultMode;
+
     void UpdatePreview() const;
 
 private Q_SLOTS:
