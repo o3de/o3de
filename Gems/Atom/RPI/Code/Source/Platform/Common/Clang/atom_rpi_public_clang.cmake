537229aa13daaa6543a87483c99ca6490b5c01b4
Chris Burel <burelc@amazon.com>
2021-07-06 08:55:19 -0700

    Fix flags used to build MaskedOcclusionCulling with clang
    Signed-off-by: Chris Burel <burelc@amazon.com>

---
+++ Gems/Atom/RPI/Code/Source/Platform/Common/Clang/atom_rpi_public_clang.cmake
@@ -0,0 +1,18 @@
+#
+# Copyright (c) Contributors to the Open 3D Engine Project. For complete copyright and license terms please see the LICENSE at the root of this distribution.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+#
+
+ly_add_source_properties(
+    SOURCES External/MaskedOcclusionCulling/MaskedOcclusionCullingAVX2.cpp
+    PROPERTY COMPILE_OPTIONS
+    VALUES -mavx2 -mfma -msse4.1
+)
+
+ly_add_source_properties(
+    SOURCES External/MaskedOcclusionCulling/MaskedOcclusionCulling.cpp
+    PROPERTY COMPILE_OPTIONS
+    VALUES -mno-avx
+)
