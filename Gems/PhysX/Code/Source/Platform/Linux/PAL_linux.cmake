a8667ccef6a7a1a3fc8d0774252dbd21d84341ff
moraaar <moraaar@amazon.com>
2021-07-06 18:19:08 +0100

    Updating linux version of NvCloth, poly2tri and v-hacd.
    Signed-off-by: moraaar <moraaar@amazon.com>

--- Gems/PhysX/Code/Source/Platform/Linux/PAL_linux.cmake
+++ Gems/PhysX/Code/Source/Platform/Linux/PAL_linux.cmake
@@ -8,6 +8,6 @@
 set(PAL_TRAIT_PHYSX_SUPPORTED TRUE)
 
 if(PAL_TRAIT_BUILD_HOST_TOOLS)
-     ly_associate_package(PACKAGE_NAME poly2tri-0.3.3-rev2-multiplatform TARGETS poly2tri PACKAGE_HASH 04092d06716f59b936b61906eaf3647db23b685d81d8b66131eb53e0aeaa1a38) 
-     ly_associate_package(PACKAGE_NAME v-hacd-2.0-rev1-multiplatform TARGETS v-hacd PACKAGE_HASH 5c71aef19cc9787d018d64eec076e9f51ea5a3e0dc6b6e22e57c898f6cc4afe3)
+     ly_associate_package(PACKAGE_NAME poly2tri-7f0487a-rev1-linux TARGETS poly2tri PACKAGE_HASH b16eef8f0bc469de0e3056d28d7484cf42659667e39b68b239f0d3a4cbb533d0) 
+     ly_associate_package(PACKAGE_NAME v-hacd-2.3-1a49edf-rev1-linux TARGETS v-hacd PACKAGE_HASH 777bed3c7805a63446ddfea51c505b35398744f9245fa7ddc58e6a25d034e682)
 endif()
