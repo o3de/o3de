c73ff7e26a379a77ed649b46109356adce29da50
Benjamin Jillich <jillich@amazon.com>
2021-07-14 16:41:29 +0200

    Transform node animations ignored
    Enabled the ability to export animations for transform nodes.

--- Gems/EMotionFX/Code/EMotionFX/Pipeline/RCExt/Motion/MotionDataBuilder.h
+++ Gems/EMotionFX/Code/EMotionFX/Pipeline/RCExt/Motion/MotionDataBuilder.h
@@ -9,6 +9,9 @@
 
 #include <SceneAPI/SceneCore/Components/ExportingComponent.h>
 #include <SceneAPI/SceneCore/Containers/Scene.h>
+#include <SceneAPI/SceneCore/Containers/SceneGraph.h>
+#include <SceneAPI/SceneCore/DataTypes/GraphData/IBoneData.h>
+#include <SceneAPI/SceneCore/DataTypes/GraphData/ITransform.h>
 
 namespace EMotionFX
 {
@@ -28,6 +31,14 @@ namespace EMotionFX
             static void Reflect(AZ::ReflectContext* context);
 
             AZ::SceneAPI::Events::ProcessingResult BuildMotionData(MotionDataBuilderContext& context);
+
+        private:
+            //! Get the bind pose transform in local space.
+            AZ::SceneAPI::DataTypes::MatrixType GetLocalSpaceBindPose(const AZ::SceneAPI::Containers::SceneGraph& sceneGraph,
+                const AZ::SceneAPI::Containers::SceneGraph::NodeIndex rootBoneNodeIndex,
+                const AZ::SceneAPI::Containers::SceneGraph::NodeIndex nodeIndex,
+                const AZ::SceneAPI::DataTypes::ITransform* transform,
+                const AZ::SceneAPI::DataTypes::IBoneData* bone) const;
         };
     } // namespace Pipeline
 } // namespace EMotionFX
