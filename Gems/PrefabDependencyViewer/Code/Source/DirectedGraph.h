213bf1c8526d7897b297e58c5d368040e0417852
Parshant Juneja <parshaj@amazon.com>
2021-07-23 15:26:38 -0700

    Collapsed the nested namespace
    Signed-off-by: Parshant Juneja <parshaj@amazon.com>

--- Gems/PrefabDependencyViewer/Code/Source/DirectedGraph.h
+++ Gems/PrefabDependencyViewer/Code/Source/DirectedGraph.h
@@ -18,198 +18,195 @@
 #include <AzCore/std/containers/queue.h>
 #include <AzCore/std/containers/stack.h>
 
-namespace PrefabDependencyViewer
+namespace PrefabDependencyViewer::Utils
 {
-    namespace Utils
+    class Node;
+    using NodeSet = AZStd::unordered_set<Node*>;
+    using ChildrenMap = AZStd::unordered_map<Node*, NodeSet>;
+    using TemplateId = AzToolsFramework::Prefab::TemplateId;
+
+    struct MetaData
     {
-        class Node;
-        using NodeSet = AZStd::unordered_set<Node*>;
-        using ChildrenMap = AZStd::unordered_map<Node*, NodeSet>;
-        using TemplateId = AzToolsFramework::Prefab::TemplateId;
+    public:
+        AZ_CLASS_ALLOCATOR(MetaData, AZ::SystemAllocator, 0);
 
-        struct MetaData
+        MetaData() = default;
+
+        MetaData(TemplateId tid, const char* source)
+            : m_tid(tid)
+            , m_source(source)
         {
-        public:
-            AZ_CLASS_ALLOCATOR(MetaData, AZ::SystemAllocator, 0);
+        }
 
-            MetaData() = default;
-
-            MetaData(TemplateId tid, const char* source)
-                : m_tid(tid)
-                , m_source(source)
-            {
-            }
-
-            TemplateId GetTemplateId()
-            {
-                return m_tid;
-            }
-
-            const char* GetSource()
-            {
-                return m_source;
-            }
-
-        private:
-            TemplateId m_tid;
-            const char* m_source;
-        };
-
-        class Node
+        TemplateId GetTemplateId()
         {
-        public:
-            AZ_CLASS_ALLOCATOR(Node, AZ::SystemAllocator, 0);
+            return m_tid;
+        }
 
-            Node(TemplateId tid, const char* source, Node* parent = nullptr)
-                : m_metaData(tid, source)
-                , m_parent(parent)
-            {
-            }
-
-            MetaData GetMetaData()
-            {
-                return m_metaData;
-            }
-
-            Node* GetParent()
-            {
-                return m_parent;
-            }
-
-            // In the future want to be able to edit the tree.
-            void SetParent(Node* parent)
-            {
-                m_parent = parent;
-            }
-
-        private:
-            MetaData m_metaData;
-            Node* m_parent;
-        };
-
-        class DirectedGraph
+        const char* GetSource()
         {
-        public:
-            DirectedGraph() {}
+            return m_source;
+        }
 
-            void AddNode(Node* node)
+    private:
+        TemplateId m_tid;
+        const char* m_source;
+    };
+
+    class Node
+    {
+    public:
+        AZ_CLASS_ALLOCATOR(Node, AZ::SystemAllocator, 0);
+
+        Node(TemplateId tid, const char* source, Node* parent = nullptr)
+            : m_metaData(tid, source)
+            , m_parent(parent)
+        {
+        }
+
+        MetaData GetMetaData()
+        {
+            return m_metaData;
+        }
+
+        Node* GetParent()
+        {
+            return m_parent;
+        }
+
+        // In the future want to be able to edit the tree.
+        void SetParent(Node* parent)
+        {
+            m_parent = parent;
+        }
+
+    private:
+        MetaData m_metaData;
+        Node* m_parent;
+    };
+
+    class DirectedGraph
+    {
+    public:
+        DirectedGraph() {}
+
+        void AddNode(Node* node)
+        {
+            m_nodes.insert(node);
+        }
+
+        void AddChild(Node* parent, Node* child)
+        {
+            if (parent)
             {
-                m_nodes.insert(node);
+                m_children[parent].insert(child);
+                child->SetParent(parent);
+            }
+            else
+            {
+                m_root = child;
+            }
+        }
+
+        Node* GetRoot() const
+        {
+            return m_root;
+        }
+
+        NodeSet GetChildren(Node* parent) const
+        {
+            AZ_Assert(parent, "Nullptr can't have children");
+
+            auto iter = m_children.find(parent);
+            if (iter != m_children.end())
+            {
+                return iter->second;
             }
 
-            void AddChild(Node* parent, Node* child)
+            return NodeSet();
+        }
+
+        DirectedGraph(const DirectedGraph& rhs)
+        {
+            AZStd::stack<AZStd::pair<Node*, Node*>> stack;
+            stack.push(AZStd::make_pair(rhs.m_root, nullptr));
+
+            while (!stack.empty())
             {
-                if (parent)
+                AZStd::pair<Node*, Node*> pair = stack.top();
+                Node* rhsNode = pair.first;
+                Node* parent = pair.second;
+
+                stack.pop();
+
+                MetaData cpyMetaData = rhsNode->GetMetaData();
+                Node* copy = aznew Node(cpyMetaData.GetTemplateId(),
+                                        cpyMetaData.GetSource(),
+                                        parent);
+
+                AddNode(copy);
+                AddChild(parent, copy);
+
+                for (Node* child : rhs.GetChildren(rhsNode))
                 {
-                    m_children[parent].insert(child);
-                    child->SetParent(parent);
+                    stack.push(AZStd::make_pair(child, copy));
+                }
+            }
+        }
+
+        ~DirectedGraph()
+        {
+            auto delete_node = [](Node* node) { delete node; };
+            AZStd::for_each(m_nodes.begin(), m_nodes.end(), delete_node);
+        }
+
+        AZStd::vector<int> countNodesAtEachLevel(int& widestLevelSize) const
+        {
+            /** Directed Graph can't have cycles because of the
+            non-circular nature of the Prefab. */
+            AZStd::vector<int> count;
+            using pair = AZStd::pair<int, Node*>;
+            AZStd::queue<pair> queue;
+            queue.push(AZStd::make_pair(0, m_root));
+
+
+            while (!queue.empty())
+            {
+                pair p = queue.front();
+                int level = p.first;
+                Node* currNode = p.second;
+
+                queue.pop();
+
+                if (count.size() <= level)
+                {
+                    // Started a new level so check if the previous level
+                    // was any bigger then the widest level size seen so far.
+                    int prevLevelCount = level != 0 ? count[level - 1] : 0;
+                    widestLevelSize = AZStd::max(widestLevelSize, prevLevelCount);
+                    count.push_back(1);
                 }
                 else
                 {
-                    m_root = child;
+                    ++count[level];
                 }
-            }
 
-            Node* GetRoot() const
-            {
-                return m_root;
-            }
-
-            NodeSet GetChildren(Node* parent) const
-            {
-                AZ_Assert(parent, "Nullptr can't have children");
-
-                auto iter = m_children.find(parent);
-                if (iter != m_children.end())
+                auto it = m_children.find(currNode);
+                if (it != m_children.end())
                 {
-                    return iter->second;
-                }
-
-                return NodeSet();
-            }
-
-            DirectedGraph(const DirectedGraph& rhs)
-            {
-                AZStd::stack<AZStd::pair<Node*, Node*>> stack;
-                stack.push(AZStd::make_pair(rhs.m_root, nullptr));
-
-                while (!stack.empty())
-                {
-                    AZStd::pair<Node*, Node*> pair = stack.top();
-                    Node* rhsNode = pair.first;
-                    Node* parent = pair.second;
-
-                    stack.pop();
-
-                    MetaData cpyMetaData = rhsNode->GetMetaData();
-                    Node* copy = aznew Node(cpyMetaData.GetTemplateId(),
-                                            cpyMetaData.GetSource(),
-                                            parent);
-
-                    AddNode(copy);
-                    AddChild(parent, copy);
-
-                    for (Node* child : rhs.GetChildren(rhsNode))
-                    {
-                        stack.push(AZStd::make_pair(child, copy));
-                    }
+                    NodeSet children = it->second;
+                    for (Node* node : children)
+                        queue.push(AZStd::make_pair(level + 1, node));
                 }
             }
 
-            ~DirectedGraph()
-            {
-                auto delete_node = [](Node* node) { delete node; };
-                AZStd::for_each(m_nodes.begin(), m_nodes.end(), delete_node);
-            }
-
-            AZStd::vector<int> countNodesAtEachLevel(int& widestLevelSize) const
-            {
-                /** Directed Graph can't have cycles because of the
-                non-circular nature of the Prefab. */
-                AZStd::vector<int> count;
-                using pair = AZStd::pair<int, Node*>;
-                AZStd::queue<pair> queue;
-                queue.push(AZStd::make_pair(0, m_root));
-
-
-                while (!queue.empty())
-                {
-                    pair p = queue.front();
-                    int level = p.first;
-                    Node* currNode = p.second;
-
-                    queue.pop();
-
-                    if (count.size() <= level)
-                    {
-                        // Started a new level so check if the previous level
-                        // was any bigger then the widest level size seen so far.
-                        int prevLevelCount = level != 0 ? count[level - 1] : 0;
-                        widestLevelSize = AZStd::max(widestLevelSize, prevLevelCount);
-                        count.push_back(1);
-                    }
-                    else
-                    {
-                        ++count[level];
-                    }
-
-                    auto it = m_children.find(currNode);
-                    if (it != m_children.end())
-                    {
-                        NodeSet children = it->second;
-                        for (Node* node : children)
-                            queue.push(AZStd::make_pair(level + 1, node));
-                    }
-                }
-
-                // Check if the last level was the widest.
-                widestLevelSize = AZStd::max(widestLevelSize, count[count.size() - 1]);
-                return count;
-            }
-        private:
-            NodeSet m_nodes {};
-            ChildrenMap m_children {};
-            Node* m_root = nullptr;
-        };
-    } // namespace Utils
-} // namespace PrefabDependencyViewer
+            // Check if the last level was the widest.
+            widestLevelSize = AZStd::max(widestLevelSize, count[count.size() - 1]);
+            return count;
+        }
+    private:
+        NodeSet m_nodes {};
+        ChildrenMap m_children {};
+        Node* m_root = nullptr;
+    };
+}
