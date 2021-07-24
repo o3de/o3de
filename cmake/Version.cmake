ecd27eeab8b53811863ed4841e1d5c20a60f83d3
rbnpontes <rbnpontes@gmail.com>
2021-07-24 04:25:01 -0300

    Update version names

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -6,7 +6,7 @@
 #
 
 string(TIMESTAMP current_year "%Y")
-set(LY_VERSION_COPYRIGHT_YEAR ${current_year} CACHE STRING "Open 3D Engine's copyright year")
-set(LY_VERSION_STRING "0.0.0.0" CACHE STRING "Open 3D Engine's version")
-set(LY_VERSION_BUILD_NUMBER 0 CACHE STRING "Open 3D Engine's build number")
-set(LY_VERSION_ENGINE_NAME "o3de" CACHE STRING "Open 3D Engine's engine name")
+set(LY_VERSION_COPYRIGHT_YEAR ${current_year} CACHE STRING "REngine copyright year")
+set(LY_VERSION_STRING "0.0.0.0" CACHE STRING "REngine version")
+set(LY_VERSION_BUILD_NUMBER 0 CACHE STRING "REngine build number")
+set(LY_VERSION_ENGINE_NAME "REngine" CACHE STRING "REngine engine name")
