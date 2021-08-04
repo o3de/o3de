24e1967953b28a64a6e29e07f6d154ca226e8803
lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
2021-08-04 13:28:21 -0500

    Converted the AZStd implementations of unintialized construct to use std
    The uninitialized_default_construct and uninitialized_value_construct
    functions implementations have been removed and the Standard library
    implementations have been brought into the AZStd namespace scope
    
    Signed-off-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>

--- Code/Framework/AzCore/Tests/AZStd/CreateDestroy.cpp
+++ Code/Framework/AzCore/Tests/AZStd/CreateDestroy.cpp
@@ -134,4 +134,46 @@ namespace UnitTest
         EXPECT_FLOAT_EQ(4.0f, resultAddress->m_floatValue);
         AZStd::destroy_at(resultAddress);
     }
+
+    TEST(CreateDestroy, UninitializedDefaultConstruct_IsAbleToConstructMultipleElements_Succeeds)
+    {
+        struct RefWrapper
+        {
+            int m_intRef{ 2 };
+        };
+        constexpr size_t ArraySize = 2;
+        AZStd::aligned_storage_for_t<RefWrapper> testArray[ArraySize];
+        RefWrapper(&uninitializedAddress)[2] = reinterpret_cast<RefWrapper(&)[2]>(testArray);
+        AZStd::uninitialized_default_construct(AZStd::begin(uninitializedAddress), AZStd::end(uninitializedAddress));
+
+
+        EXPECT_EQ(2, uninitializedAddress[0].m_intRef);
+        EXPECT_EQ(2, uninitializedAddress[1].m_intRef);
+        // Reset uninitializedAddress to Debug pattern
+        memset(uninitializedAddress, 0xCD, ArraySize * sizeof(RefWrapper));
+        AZStd::uninitialized_default_construct_n(AZStd::data(uninitializedAddress), AZStd::size(uninitializedAddress));
+        EXPECT_EQ(2, uninitializedAddress[0].m_intRef);
+        EXPECT_EQ(2, uninitializedAddress[1].m_intRef);
+    }
+
+    TEST(CreateDestroy, UninitializedValueConstruct_IsAbleToConstructMultipleElements_Succeeds)
+    {
+        struct RefWrapper
+        {
+            int m_intRef{ 2 };
+        };
+        constexpr size_t ArraySize = 2;
+        AZStd::aligned_storage_for_t<RefWrapper> testArray[ArraySize];
+        RefWrapper(&uninitializedAddress)[2] = reinterpret_cast<RefWrapper(&)[2]>(testArray);
+        AZStd::uninitialized_value_construct(AZStd::begin(uninitializedAddress), AZStd::end(uninitializedAddress));
+
+
+        EXPECT_EQ(2, uninitializedAddress[0].m_intRef);
+        EXPECT_EQ(2, uninitializedAddress[1].m_intRef);
+        // Reset uninitializedAddress to Debug pattern
+        memset(uninitializedAddress, 0xCD, ArraySize * sizeof(RefWrapper));
+        AZStd::uninitialized_value_construct_n(AZStd::data(uninitializedAddress), AZStd::size(uninitializedAddress));
+        EXPECT_EQ(2, uninitializedAddress[0].m_intRef);
+        EXPECT_EQ(2, uninitializedAddress[1].m_intRef);
+    }
 }
