a068f1f8589976b716dabeae4569165e2f4d4966
John <jonawals@amazon.com>
2021-08-04 18:54:39 +0100

    fix validation errors
    Signed-off-by: John <jonawals@amazon.com>

--- Code/Tools/TestImpactFramework/Runtime/Code/Tests/TestImpactMicroRepo.h
+++ Code/Tools/TestImpactFramework/Runtime/Code/Tests/TestImpactMicroRepo.h
@@ -1,12 +1,8 @@
 /*
- * All or portions of this file Copyright (c) Amazon.com, Inc. or its affiliates or
- * its licensors.
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
  *
- * For complete copyright and license terms please see the LICENSE at the root of this
- * distribution (the "License"). All use of this software is governed by the License,
- * or, if provided, by the license below or the license accompanying this file. Do not
- * remove or modify any license notices. This file is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
  *
  */
 
@@ -28,45 +24,45 @@ namespace UnitTest
     // Build target dependency graphs
     // ==============================
     // 
-    //       ┌────────┐     ┌───────┐   ┌────────────┐
-    //       │Lib Core│     │Lib Aux│   │ Lib Shared │*
-    //       └──▲──▲──┘     └──▲─▲──┘   └──────▲─────┘
-    //          │  │           │ │             │
-    //    ┌─────┘  └────┐ ┌────┘ └─────┐┌──────┘
-    //    │             │ │            ││
-    // ┌──┴──┐        ┌─┴─┴─┐      ┌───┴┴───┐
-    // │Lib A│        │Lib B│      │Lib Misc│
-    // └──▲──┘        └──▲──┘      └────────┘
-    //    │              │            
-    //    └─────┐ ┌──────┘            
-    //          │ │                   
-    //        ┌─┴─┴─┐             
-    //        │Lib C│             
-    //        └─────┘             
+    //       #--------#     #-------#   #------------#
+    //       |Lib Core|     |Lib Aux|   | Lib Shared |*
+    //       #--^--^--#     #--^-^--#   #------^-----#
+    //          |  |           | |             |
+    //    #-----#  #----# #----# #-----##------#
+    //    |             | |            ||
+    // #-----#        #-----#      #--------#
+    // |Lib A|        |Lib B|      |Lib Misc|
+    // #--^--#        #--^--#      #--------#
+    //    |              |            
+    //    #-----# #------#            
+    //          | |                   
+    //        #-----#             
+    //        |Lib C|             
+    //        #-----#             
     // 
     // =============================
     // Test target dependency graphs
     // =============================
-    //                                            ┌────────┐
-    //                                            │Lib Core│
-    //                                            └──▲──▲──┘
-    //                                               │  │
-    //                                         ┌─────┘  └────┐
-    //                                         │             │
-    // ┌────────┐ ┌────────┐      ┌───────┐ ┌──┴──┐        ┌─┴───┐ ┌───────┐ ┌────────────┐
-    // │Lib Core│ │Lib Core│      │Lib Aux│ │Lib A│        │Lib B│ │Lib Aux│ │ Lib Shared │*
-    // └────▲───┘ └────▲───┘      └───▲───┘ └──▲──┘        └──┬──┘ └───▲───┘ └──────▲─────┘
-    //     │          │              │         │              │        │            │   
-    //     │          └─────┐ ┌──────┘         └─────┐ ┌──────┘        └────┐┌──────┘   
-    //     │                │ │                      │ │                    ││
-    //  ┌──┴──┐           ┌─┴─┴─┐                  ┌─┴─┴─┐               ┌──┴┴────┐  ┌──────────┐    ┌────────┐ ┌───────┐
-    //  │Lib A│           │Lib B│                  │Lib C│               │Lib Misc│  │Lib Shared│    │Lib Core│ │Lib Aux│
-    //  └──▲──┘           └──▲──┘                  └──▲──┘               └───▲────┘  └────▲─────┘    └───▲────┘ └───▲───┘
-    //     │                 │                        │                      │            │              │          │
-    //     │                 │                        │                      │            │              │          │
-    // ┌───┴───┐         ┌───┴───┐                ┌───┴───┐              ┌───┴─────┐ ┌────┴──────┐  ┌────┴────┐ ┌───┴────┐
-    // │Test A │         │Test B │                │Test C │              │Test Misc│ │Test Shared│* │Test Core│ │Test Aux│
-    // └───────┘         └───────┘                └───────┘              └─────────┘ └───────────┘  └─────────┘ └────────┘
+    //                                            #--------#
+    //                                            |Lib Core|
+    //                                            #--^--^--#
+    //                                               |  |
+    //                                         #-----#  #----#
+    //                                         |             |
+    // #--------# #--------#      #-------# #-----#        #-----# #-------# #------------#
+    // |Lib Core| |Lib Core|      |Lib Aux| |Lib A|        |Lib B| |Lib Aux| | Lib Shared |*
+    // #----^---# #----^---#      #---^---# #--^--#        #-----# #---^---# #------^-----#
+    //     |          |              |         |              |        |            |   
+    //     |          #-----# #------#         #-----# #------#        #----##------#   
+    //     |                | |                      | |                    ||
+    //  #-----#           #-----#                  #-----#               #--------#  #----------#    #--------# #-------#
+    //  |Lib A|           |Lib B|                  |Lib C|               |Lib Misc|  |Lib Shared|    |Lib Core| |Lib Aux|
+    //  #--^--#           #--^--#                  #--^--#               #---^----#  #----^-----#    #---^----# #---^---#
+    //     |                 |                        |                      |            |              |          |
+    //     |                 |                        |                      |            |              |          |
+    // #-------#         #-------#                #-------#              #---------# #-----------#  #---------# #--------#
+    // |Test A |         |Test B |                |Test C |              |Test Misc| |Test Shared|* |Test Core| |Test Aux|
+    // #-------#         #-------#                #-------#              #---------# #-----------#  #---------# #--------#
     //
     // =============
     // Test Coverage
