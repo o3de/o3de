baab204c0bf79a7932ad5ddbb57931e68c47802c
Kyle B <kylebirnbaum@gmail.com>
2021-08-05 00:07:54 -0700

    updated Mock with new interface values
    Signed-off-by: Kyle B <kylebirnbaum@gmail.com>

--- Gems/Atom/Feature/Common/Code/Mocks/MockMeshFeatureProcessor.h
+++ Gems/Atom/Feature/Common/Code/Mocks/MockMeshFeatureProcessor.h
@@ -32,9 +32,13 @@ namespace UnitTest
         MOCK_METHOD2(SetLocalAabb, void(const MeshHandle&, const AZ::Aabb&));
         MOCK_CONST_METHOD1(GetLocalAabb, AZ::Aabb(const MeshHandle&));
         MOCK_METHOD2(SetSortKey, void (const MeshHandle&, AZ::RHI::DrawItemSortKey));
-        MOCK_METHOD1(GetSortKey, AZ::RHI::DrawItemSortKey(const MeshHandle&));
+        MOCK_CONST_METHOD1(GetSortKey, AZ::RHI::DrawItemSortKey(const MeshHandle&));
         MOCK_METHOD2(SetLodOverride, void(const MeshHandle&, AZ::RPI::Cullable::LodOverride));
-        MOCK_METHOD1(GetLodOverride, AZ::RPI::Cullable::LodOverride(const MeshHandle&));
+        MOCK_CONST_METHOD1(GetLodOverride, AZ::RPI::Cullable::LodOverride(const MeshHandle&));
+        MOCK_METHOD2(SetMinimumScreenCoverage, void(const MeshHandle&, float));
+        MOCK_CONST_METHOD1(GetMinimumScreenCoverage, float(const MeshHandle&));
+        MOCK_METHOD2(SetQualityDecayRate, void(const MeshHandle&, float));
+        MOCK_CONST_METHOD1(GetQualityDecayRate, float(const MeshHandle&));
         MOCK_METHOD2(AcquireMesh, MeshHandle (const AZ::Render::MeshHandleDescriptor&, const AZ::Render::MaterialAssignmentMap&));
         MOCK_METHOD2(AcquireMesh, MeshHandle (const AZ::Render::MeshHandleDescriptor&, const AZ::Data::Instance<AZ::RPI::Material>&));
         MOCK_METHOD2(SetRayTracingEnabled, void (const MeshHandle&, bool));
