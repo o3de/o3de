196ce44545c7e38df114836d039e34fc4c6ad4d0
John <jonawals@amazon.com>
2021-08-11 20:44:32 +0100

    Fix test selection efficiency for MARS.
    Signed-off-by: John <jonawals@amazon.com>

--- scripts/build/TestImpactAnalysis/mars_utils.py
+++ scripts/build/TestImpactAnalysis/mars_utils.py
@@ -318,7 +318,7 @@ def generate_mars_sequence(sequence_report: dict, mars_job: dict, change_list:di
     test_run_selection = {}
     test_run_selection[SELECTED_KEY] = generate_mars_test_run_selections(sequence_report[SELECTED_TEST_RUNS_KEY], sequence_report[SELECTED_TEST_RUN_REPORT_KEY], t0_timestamp)
     if sequence_report[SEQUENCE_TYPE_KEY] == IMPACT_ANALYSIS_SEQUENCE_TYPE_KEY or sequence_report[SEQUENCE_TYPE_KEY] == SAFE_IMPACT_ANALYSIS_SEQUENCE_TYPE_KEY:
-        total_test_runs = sequence_report[TOTAL_NUM_TEST_RUNS_KEY]
+        total_test_runs = sequence_report[TOTAL_NUM_TEST_RUNS_KEY] + len(sequence_report[DISCARDED_TEST_RUNS_KEY])
         if total_test_runs > 0:
             test_run_selection[SELECTED_KEY][EFFICIENCY_KEY] = (1.0 - (test_run_selection[SELECTED_KEY][TOTAL_NUM_TEST_RUNS_KEY] / total_test_runs)) * 100
         else:
