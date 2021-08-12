3423676d1abc3f61093b90285d9c4b57e8d8a730
junbo <junbo@amazon.com>
2021-08-12 09:59:35 -0700

    Update the automation tests to use existing CloudFormation stacks
    Signed-off-by: junbo <junbo@amazon.com>

--- AutomatedTesting/Gem/PythonTests/AWS/Windows/core/test_aws_resource_interaction.py
+++ AutomatedTesting/Gem/PythonTests/AWS/Windows/core/test_aws_resource_interaction.py
@@ -3,12 +3,27 @@ Copyright (c) Contributors to the Open 3D Engine Project.
 For complete copyright and license terms please see the LICENSE at the root of this distribution.
 
 SPDX-License-Identifier: Apache-2.0 OR MIT
+
+Prerequisites for running AWS automation tests on your local Windows machine:
+1) Go to the AWS IAM console and create an IAM role called o3de-automation-tests with admin permissions
+2) Grant your AWS account permission to assume the o3de-automation-tests IAM role following:
+   https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_use_permissions-to-switch.html
+3) Copy o3de\scripts\build\Platform\Windows\deploy_cdk_applications.cmd to your engine root folder
+4) Open a Command Prompt window and set the following environment variables:
+   Set O3DE_AWS_PROJECT_NAME=AWSAUTO
+   Set O3DE_AWS_DEPLOY_REGION=us-west-2
+   Set ASSUME_ROLE_ARN="arn:aws:iam::{your_aws_account_id}:role/o3de-automation-tests"
+   Set COMMIT_ID=HEAD
+5) Deploy the CDK applications for AWS gems by running deploy_cdk_applications.cmd in the same Command Prompt window
+6) Edit AWS/common/constants.py to replace the assume role ARN with your own:
+   arn:aws:iam::{your_aws_account_id}:role/o3de-automation-tests
 """
 
-import os
 import logging
-import typing
+import os
 import shutil
+import typing
+from botocore.exceptions import ClientError
 
 import pytest
 import ly_test_tools
@@ -16,15 +31,14 @@ import ly_test_tools.log.log_monitor
 import ly_test_tools.environment.process_utils as process_utils
 import ly_test_tools.o3de.asset_processor_utils as asset_processor_utils
 
-from botocore.exceptions import ClientError
+from AWS.common import constants
+
+# fixture imports
 from assetpipeline.ap_fixtures.asset_processor_fixture import asset_processor
 
 AWS_CORE_FEATURE_NAME = 'AWSCore'
-AWS_RESOURCE_MAPPING_FILE_NAME = 'default_aws_resource_mappings.json'
 
 process_utils.kill_processes_named("o3de", ignore_extensions=True)  # Kill ProjectManager windows
-
-GAME_LOG_NAME = 'Game.log'
 
 logger = logging.getLogger(__name__)
 
@@ -46,7 +60,7 @@ def setup(launcher: pytest.fixture, asset_processor: pytest.fixture) -> typing.T
     asset_processor.start()
     asset_processor.wait_for_idle()
 
-    file_to_monitor = os.path.join(launcher.workspace.paths.project_log(), GAME_LOG_NAME)
+    file_to_monitor = os.path.join(launcher.workspace.paths.project_log(), constants.GAME_LOG_NAME)
     log_monitor = ly_test_tools.log.log_monitor.LogMonitor(launcher=launcher, log_file_path=file_to_monitor)
 
     return log_monitor, s3_download_dir
@@ -58,7 +72,7 @@ def write_test_data_to_dynamodb_table(resource_mappings: pytest.fixture, aws_uti
     :param resource_mappings: resource_mappings fixture.
     :param aws_utils: aws_utils fixture.
     """
-    table_name = resource_mappings.get_resource_name_id("AWSCore.ExampleDynamoTableOutput")
+    table_name = resource_mappings.get_resource_name_id(f'{AWS_CORE_FEATURE_NAME}.ExampleDynamoTableOutput')
     try:
         aws_utils.client('dynamodb').put_item(
             TableName=table_name,
@@ -77,21 +91,19 @@ def write_test_data_to_dynamodb_table(resource_mappings: pytest.fixture, aws_uti
 @pytest.mark.SUITE_periodic
 @pytest.mark.usefixtures('automatic_process_killer')
 @pytest.mark.usefixtures('asset_processor')
-@pytest.mark.usefixtures('cdk')
 @pytest.mark.parametrize('feature_name', [AWS_CORE_FEATURE_NAME])
-@pytest.mark.parametrize('region_name', ['us-west-2'])
-@pytest.mark.parametrize('assume_role_arn', ['arn:aws:iam::645075835648:role/o3de-automation-tests'])
-@pytest.mark.parametrize('session_name', ['o3de-Automation-session'])
+@pytest.mark.parametrize('region_name', [constants.AWS_REGION])
+@pytest.mark.parametrize('assume_role_arn', [constants.ASSUME_ROLE_ARN])
+@pytest.mark.parametrize('session_name', [constants.SESSION_NAME])
 @pytest.mark.usefixtures('workspace')
 @pytest.mark.parametrize('project', ['AutomatedTesting'])
 @pytest.mark.parametrize('level', ['AWS/Core'])
 @pytest.mark.usefixtures('resource_mappings')
-@pytest.mark.parametrize('resource_mappings_filename', [AWS_RESOURCE_MAPPING_FILE_NAME])
+@pytest.mark.parametrize('resource_mappings_filename', [constants.AWS_RESOURCE_MAPPING_FILE_NAME])
+@pytest.mark.parametrize('stacks', [[f'{constants.AWS_PROJECT_NAME}-{AWS_CORE_FEATURE_NAME}',
+                                     f'{constants.AWS_PROJECT_NAME}-{AWS_CORE_FEATURE_NAME}-Example-{constants.AWS_REGION}']])
 @pytest.mark.usefixtures('aws_credentials')
 @pytest.mark.parametrize('profile_name', ['AWSAutomationTest'])
-@pytest.mark.usefixtures('cdk')
-@pytest.mark.parametrize('deployment_params', [['--all']])
-@pytest.mark.parametrize('destroy_stacks_on_teardown', [True])
 class TestAWSCoreAWSResourceInteraction(object):
     """
     Test class to verify the scripting behavior for the AWSCore gem.
@@ -119,7 +131,7 @@ class TestAWSCoreAWSResourceInteraction(object):
                                 expected_lines: typing.List[str],
                                 unexpected_lines: typing.List[str]):
         """
-        Setup: Deploys cdk and updates resource mapping file.
+        Setup: Updates resource mapping file using existing CloudFormation stacks.
         Tests: Interact with AWS S3, DynamoDB and Lambda services.
         Verification: Script canvas nodes can communicate with AWS services successfully.
         """
---
+++ AutomatedTesting/Gem/PythonTests/AWS/common/constants.py
@@ -0,0 +1,13 @@
+"""
+Copyright (c) Contributors to the Open 3D Engine Project.
+For complete copyright and license terms please see the LICENSE at the root of this distribution.
+
+SPDX-License-Identifier: Apache-2.0 OR MIT
+"""
+
+ASSUME_ROLE_ARN = 'arn:aws:iam::645075835648:role/o3de-automation-tests'
+AWS_PROJECT_NAME = 'AWSAUTO'
+AWS_REGION = 'us-west-2'
+AWS_RESOURCE_MAPPING_FILE_NAME = 'default_aws_resource_mappings.json'
+GAME_LOG_NAME = 'Game.log'
+SESSION_NAME = 'o3de-Automation-session'
