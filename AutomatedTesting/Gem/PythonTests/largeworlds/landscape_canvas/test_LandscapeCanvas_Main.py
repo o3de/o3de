4b5c70acd00958342ae08979024a122c449ab091
jckand-amzn <jckand@amazon.com>
2021-08-13 11:22:44 -0500

    Updating main Landscape Canvas test suite to batch run tests
    Signed-off-by: jckand-amzn <jckand@amazon.com>

--- AutomatedTesting/Gem/PythonTests/largeworlds/landscape_canvas/test_LandscapeCanvas_Main.py
+++ AutomatedTesting/Gem/PythonTests/largeworlds/landscape_canvas/test_LandscapeCanvas_Main.py
@@ -5,23 +5,18 @@ For complete copyright and license terms please see the LICENSE at the root of t
 SPDX-License-Identifier: Apache-2.0 OR MIT
 """
 
-import os
 import pytest
-import sys
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../../automatedtesting_shared')
-from base import TestAutomationBase
+from ly_test_tools.o3de.editor_test import EditorSingleTest, EditorSharedTest, EditorParallelTest, EditorTestSuite
 
 
-@pytest.mark.SUITE_main
+@pytest.mark.SUITE_periodic
 @pytest.mark.parametrize("launcher_platform", ['windows_editor'])
 @pytest.mark.parametrize("project", ["AutomatedTesting"])
-class TestAutomation(TestAutomationBase):
+class TestAutomation(EditorTestSuite):
 
-    def test_LandscapeCanvas_SlotConnections_UpdateComponentReferences(self, request, workspace, editor, launcher_platform):
+    class test_LandscapeCanvas_SlotConnections_UpdateComponentReferences(EditorSharedTest):
         from .EditorScripts import SlotConnections_UpdateComponentReferences as test_module
-        self._run_test(request, workspace, editor, test_module)
 
-    def test_LandscapeCanvas_GradientMixer_NodeConstruction(self, request, workspace, editor, launcher_platform):
+    class test_LandscapeCanvas_GradientMixer_NodeConstruction(EditorSharedTest):
         from .EditorScripts import GradientMixer_NodeConstruction as test_module
-        self._run_test(request, workspace, editor, test_module)
