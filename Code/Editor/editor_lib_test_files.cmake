80e08dd9475aa9b4b29db60e2c9d10a4d14d4e71
hultonha <82228511+hultonha@users.noreply.github.com>
2021-08-19 09:06:24 +0100

    Fix issue with mouse input for viewport camera (#3210)
    * fix for drift accumulating in the viewport camera
    
    Signed-off-by: hultonha <hultonha@amazon.co.uk>
    
    * fix typo and update how events are stored
    
    Signed-off-by: hultonha <hultonha@amazon.co.uk>
    
    * respond to PR feedback and fix linux and windows build issues
    
    Signed-off-by: hultonha <hultonha@amazon.co.uk>
    
    * fix failing unit tests in camera input
    
    Signed-off-by: hultonha <hultonha@amazon.co.uk>

--- Code/Editor/editor_lib_test_files.cmake
+++ Code/Editor/editor_lib_test_files.cmake
@@ -21,6 +21,7 @@ set(FILES
     Lib/Tests/test_ViewportTitleDlgPythonBindings.cpp
     Lib/Tests/test_DisplaySettingsPythonBindings.cpp
     Lib/Tests/test_ViewportManipulatorController.cpp
+    Lib/Tests/test_ModularViewportCameraController.cpp
     DisplaySettingsPythonFuncs.cpp
     DisplaySettingsPythonFuncs.h
 )
