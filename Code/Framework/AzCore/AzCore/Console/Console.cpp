85310a5135fc86d8c2147d579d18b969931c49bf
lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
2021-08-18 19:41:56 -0500

    Moved the execution of deferred console commands into CreateModuleClass hook
    Any module that loads using the ModuleManager system will attempt to
    execute any deferred console commands to allow newly registered commands
    from that module to be dispatched.
    
    Signed-off-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>

--- Code/Framework/AzCore/AzCore/Console/Console.cpp
+++ Code/Framework/AzCore/AzCore/Console/Console.cpp
@@ -178,7 +178,7 @@ namespace AZ
     {
         auto DeferredCommandCallable = [this](const DeferredCommand& deferredCommand)
         {
-            return DispatchCommand(deferredCommand.m_command, deferredCommand.m_arguments, deferredCommand.m_silentMode,
+            return this->DispatchCommand(deferredCommand.m_command, deferredCommand.m_arguments, deferredCommand.m_silentMode,
                 deferredCommand.m_invokedFrom, deferredCommand.m_requiredSet, deferredCommand.m_requiredClear);
         };
         // Attempt to invoke the deferred command and remove it from the queue if successful
@@ -355,12 +355,6 @@ namespace AZ
         }
 
         deferredHead = nullptr;
-
-        // Execute any deferred console commands after linking any new deferred functors
-        // LinkDeferredFunctors is invoked after a module has loaded
-        // So this allows console commands dispatched before a module is loaded
-        // to now execute each time a module loads and registers new functors
-        ExecuteDeferredConsoleCommands();
     }
 
     void Console::MoveFunctorsToDeferredHead(ConsoleFunctorBase*& deferredHead)
--- Code/Framework/AzCore/AzCore/Module/Module.h
+++ Code/Framework/AzCore/AzCore/Module/Module.h
@@ -98,19 +98,26 @@ namespace AZ
 ///
 /// \param MODULE_NAME      Name of module.
 /// \param MODULE_CLASSNAME Name of AZ::Module class (include namespace).
+///
+/// Execute any deferred console commands after linking any new deferred functors
+/// This allows deferred console commands defined within the module to now execute
+/// at this point now that the module has been loaded
 #if defined(AZ_MONOLITHIC_BUILD)
 #   define AZ_DECLARE_MODULE_CLASS(MODULE_NAME, MODULE_CLASSNAME) \
     extern "C" AZ::Module * CreateModuleClass_##MODULE_NAME() { return aznew MODULE_CLASSNAME; }
 #else
 #   define AZ_DECLARE_MODULE_CLASS(MODULE_NAME, MODULE_CLASSNAME)                                \
     AZ_DECLARE_MODULE_INITIALIZATION                                                             \
-    extern "C" AZ_DLL_EXPORT AZ::Module * CreateModuleClass()                                    \
+    extern "C" AZ_DLL_EXPORT AZ::Module* CreateModuleClass()                                     \
     {                                                                                            \
-        AZ::ConsoleFunctorBase*& deferredHead = AZ::ConsoleFunctorBase::GetDeferredHead();       \
-        AZ::Interface<AZ::IConsole>::Get()->LinkDeferredFunctors(deferredHead);                  \
+        if (auto console = AZ::Interface<AZ::IConsole>::Get(); console != nullptr)               \
+        {                                                                                        \
+             console->LinkDeferredFunctors(AZ::ConsoleFunctorBase::GetDeferredHead());           \
+             console->ExecuteDeferredConsoleCommands();                                          \
+        }                                                                                        \
         return aznew MODULE_CLASSNAME;                                                           \
     }                                                                                            \
-    extern "C" AZ_DLL_EXPORT void DestroyModuleClass(AZ::Module * module) { delete module; }
+    extern "C" AZ_DLL_EXPORT void DestroyModuleClass(AZ::Module* module) { delete module; }
 #endif
 
 #endif // AZCORE_MODULE_INCLUDE_H
