47758ab7a6f3c0867b9d760863417f8a1e8e543f
santorac <55155825+santorac@users.noreply.github.com>
2021-08-19 17:42:41 -0700

    Updated RapidJSON package name and hashes.
    Signed-off-by: santorac <55155825+santorac@users.noreply.github.com>

--- cmake/3rdParty/Platform/Android/BuiltInPackages_android.cmake
+++ cmake/3rdParty/Platform/Android/BuiltInPackages_android.cmake
@@ -8,7 +8,7 @@
 
 # shared by other platforms:
 ly_associate_package(PACKAGE_NAME md5-2.0-multiplatform              TARGETS md5       PACKAGE_HASH 29e52ad22c78051551f78a40c2709594f0378762ae03b417adca3f4b700affdf)
-ly_associate_package(PACKAGE_NAME RapidJSON-1.1.0-multiplatform      TARGETS RapidJSON PACKAGE_HASH 630d0e17e6cddce4dabe9bbd6c9835b7fc58be5e56508e6ef1a21ef191e90136)
+ly_associate_package(PACKAGE_NAME RapidJSON-1.1.0-rev1-multiplatform TARGETS RapidJSON PACKAGE_HASH 2f5e26ecf86c3b7a262753e7da69ac59928e78e9534361f3d00c1ad5879e4023)
 ly_associate_package(PACKAGE_NAME RapidXML-1.13-multiplatform        TARGETS RapidXML  PACKAGE_HASH 510b3c12f8872c54b34733e34f2f69dd21837feafa55bfefa445c98318d96ebf)
 ly_associate_package(PACKAGE_NAME cityhash-1.1-multiplatform         TARGETS cityhash  PACKAGE_HASH 0ace9e6f0b2438c5837510032d2d4109125845c0efd7d807f4561ec905512dd2)
 ly_associate_package(PACKAGE_NAME lz4-r128-multiplatform             TARGETS lz4       PACKAGE_HASH d7b1d5651191db2c339827ad24f669d9d37754143e9173abc986184532f57c9d)
