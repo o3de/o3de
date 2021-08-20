5cd6e8df8627582ab5b094bf44633f3b6d88555f
amzn-phist <52085794+amzn-phist@users.noreply.github.com>
2021-08-20 16:27:58 -0500

    Update to use bracket argument
    Signed-off-by: amzn-phist <52085794+amzn-phist@users.noreply.github.com>

--- cmake/Platform/Linux/Install_linux.cmake
+++ cmake/Platform/Linux/Install_linux.cmake
@@ -7,18 +7,21 @@
 #
 
 #! ly_install_code_function_override: Linux-specific copy function to handle RPATH fixes
-function(ly_install_code_function_override)
-    install(CODE
-"function(ly_copy source_file target_directory)
-    file(COPY \"\${source_file}\" DESTINATION \"\${target_directory}\" FILE_PERMISSIONS ${LY_COPY_PERMISSIONS} FOLLOW_SYMLINK_CHAIN)
-
-    get_filename_component(target_filename_ext \"\${source_file}\" LAST_EXT)
-    if(\"\${source_file}\" MATCHES \"qt/plugins\" AND \"\${target_filename_ext}\" STREQUAL \".so\")
-        get_filename_component(target_filename \"\${source_file}\" NAME)
-        file(RPATH_CHANGE FILE \"\${target_directory}/\${target_filename}\" OLD_RPATH \"\\$ORIGIN/../../lib\" NEW_RPATH \"\\$ORIGIN/..\")
+set(ly_copy_template [[
+function(ly_copy source_file target_directory)
+    file(COPY "${source_file}" DESTINATION "${target_directory}" FILE_PERMISSIONS @LY_COPY_PERMISSIONS@ FOLLOW_SYMLINK_CHAIN)
+    get_filename_component(target_filename_ext "${source_file}" LAST_EXT)
+    if("${source_file}" MATCHES "qt/plugins" AND "${target_filename_ext}" STREQUAL ".so")
+        get_filename_component(target_filename "${source_file}" NAME)
+        file(RPATH_CHANGE FILE "${target_directory}/${target_filename}" OLD_RPATH "\$ORIGIN/../../lib" NEW_RPATH "\$ORIGIN/..")
     endif()
-endfunction()"
-    )
+endfunction()
+]])
+
+string(CONFIGURE "${ly_copy_template}" ly_copy_function_linux @ONLY)
+
+function(ly_install_code_function_override)
+    install(CODE "${ly_copy_function_linux}")
 endfunction()
 
 include(cmake/Platform/Common/Install_common.cmake)
