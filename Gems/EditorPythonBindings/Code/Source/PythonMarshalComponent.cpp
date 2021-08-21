62439e03fb90e919687b2a05d89d617ef14b7e77
Esteban Papp <81431996+amznestebanpapp@users.noreply.github.com>
2021-08-20 17:58:32 -0700

    Gems/EditorPythonBindings
    Signed-off-by: Esteban Papp <81431996+amznestebanpapp@users.noreply.github.com>

--- Gems/EditorPythonBindings/Code/Source/PythonMarshalComponent.cpp
+++ Gems/EditorPythonBindings/Code/Source/PythonMarshalComponent.cpp
@@ -697,14 +697,12 @@ namespace EditorPythonBindings
         class TypeConverterDictionary final
             : public PythonMarshalComponent::TypeConverter
         {
-            AZ::GenericClassInfo* m_genericClassInfo = nullptr;
             const AZ::SerializeContext::ClassData* m_classData = nullptr;
             const AZ::TypeId m_typeId = {};
 
         public:
-            TypeConverterDictionary(AZ::GenericClassInfo* genericClassInfo, const AZ::SerializeContext::ClassData* classData, const AZ::TypeId& typeId)
-                : m_genericClassInfo(genericClassInfo)
-                , m_classData(classData)
+            TypeConverterDictionary([[maybe_unused]] AZ::GenericClassInfo* genericClassInfo, const AZ::SerializeContext::ClassData* classData, const AZ::TypeId& typeId)
+                : m_classData(classData)
                 , m_typeId(typeId)
             {
             }
