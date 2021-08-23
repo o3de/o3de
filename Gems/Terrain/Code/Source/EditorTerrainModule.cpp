97a053a540f21f48413eb55e8b3c25e9cebeba0d
Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
2021-08-23 15:41:57 -0500

    Initial stub terrain gem (#3368)
    This is the initial Terrain Gem. In this PR, it doesn't do anything, but it contains all of the initial code, icons, and build scripts for the gem to compile and build successfully. The follow-up PR will contain the first round of functioning code.
    
    Also, with the creation of the gem, the TerrainSurfaceDataSystemComponent is getting relocated from the SurfaceData Gem to the Terrain Gem. This should have no impact, as that component has provided no active functionality in o3de. (It will start working again with the initial terrain system code in the next PR)
    
    This also adds a couple of null guards to prefab code to prevent AP crashes, and fixed an incorrect service dependency in the VegetationSystemComponent that was exposed by moving the TerrainSurfaceDataSystemComponent.
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>

---
+++ Gems/Terrain/Code/Source/EditorTerrainModule.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+
+#include <EditorTerrainModule.h>
+#include <EditorComponents/EditorTerrainSystemComponent.h>
+
+namespace Terrain
+{
+    EditorTerrainModule::EditorTerrainModule()
+    {
+        m_descriptors.insert(
+            m_descriptors.end(),
+            {
+                Terrain::EditorTerrainSystemComponent::CreateDescriptor(),
+            });
+    }
+
+    AZ::ComponentTypeList EditorTerrainModule::GetRequiredSystemComponents() const
+    {
+        AZ::ComponentTypeList requiredComponents = TerrainModule::GetRequiredSystemComponents();
+        requiredComponents.insert(
+            requiredComponents.end(),
+            {
+                azrtti_typeid<EditorTerrainSystemComponent>(),
+            });
+
+        return requiredComponents;
+    }
+}
+
+AZ_DECLARE_MODULE_CLASS(Gem_TerrainEditor, Terrain::EditorTerrainModule)
