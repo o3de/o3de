fc7642a7930ac186b2110496111c0201d84ebe4e
Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
2021-08-23 12:58:53 -0500

    Added null checks to prefab system to prevent AP crashes.
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>

--- Code/Framework/AzToolsFramework/AzToolsFramework/Prefab/Instance/Instance.cpp
+++ Code/Framework/AzToolsFramework/AzToolsFramework/Prefab/Instance/Instance.cpp
@@ -650,7 +650,10 @@ namespace AzToolsFramework
 
         AZStd::unique_ptr<AZ::Entity> Instance::DetachContainerEntity()
         {
-            m_instanceEntityMapper->UnregisterEntity(m_containerEntity->GetId());
+            if (m_containerEntity)
+            {
+                m_instanceEntityMapper->UnregisterEntity(m_containerEntity->GetId());
+            }
             return AZStd::move(m_containerEntity);
         }
     }
