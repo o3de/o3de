fc44063a0aed144337064f5316cb60c1d1c4a004
jckand-amzn <jckand@amazon.com>
2021-08-24 16:00:45 -0500

    Renaming test files for standardization. Keeping old, unoptimized files temporarily.
    Signed-off-by: jckand-amzn <jckand@amazon.com>

--- AutomatedTesting/Gem/PythonTests/editor/TestSuite_Main.py
+++ AutomatedTesting/Gem/PythonTests/editor/TestSuite_Main.py
@@ -31,10 +31,49 @@ class TestAutomationNoAutoTestMode(EditorTestSuite):
     @pytest.mark.REQUIRES_gpu
     class test_BasicEditorWorkflows_GPU_LevelEntityComponentCRUD(EditorSingleTest):
         # Disable null renderer
-        EditorTestSuite.use_null_renderer = False
+        use_null_renderer = False
 
         # Custom teardown to remove slice asset created during test
         def teardown(self, request, workspace, editor, editor_test_results, launcher_platform):
             file_system.delete([os.path.join(workspace.paths.engine_root(), "AutomatedTesting", "Levels", "tmp_level")],
                                True, True)
         from .EditorScripts import BasicEditorWorkflows_LevelEntityComponentCRUD as test_module
+
+    class test_InputBindings_Add_Remove_Input_Events(EditorSharedTest):
+        from .EditorScripts import InputBindings_Add_Remove_Input_Events as test_module
+
+    @pytest.mark.skip(reason="Crashes Editor: ATOM-15493")
+    class test_AssetPicker_UI_UX(EditorSharedTest):
+        from .EditorScripts import AssetPicker_UI_UX as test_module
+
+
+@pytest.mark.SUITE_main
+@pytest.mark.parametrize("launcher_platform", ['windows_editor'])
+@pytest.mark.parametrize("project", ["AutomatedTesting"])
+class TestAutomationAutoTestMode(EditorTestSuite):
+
+    # Enable only -autotest_mode for these tests. Tests cannot run in -BatchMode due to UI interactions
+    global_extra_cmdline_args = ["-autotest_mode"]
+
+    class test_AssetBrowser_TreeNavigation(EditorSharedTest):
+        from .EditorScripts import AssetBrowser_TreeNavigation as test_module
+
+    @pytest.mark.skip(reason="Crashes Editor: ATOM-15493")
+    class test_AssetBrowser_SearchFiltering(EditorSharedTest):
+        from .EditorScripts import AssetBrowser_SearchFiltering as test_module
+
+    class test_ComponentCRUD_Add_Delete_Components(EditorSharedTest):
+        from .EditorScripts import ComponentCRUD_Add_Delete_Components as test_module
+
+    class test_Docking_BasicDockedTools(EditorSharedTest):
+        from .EditorScripts import Docking_BasicDockedTools as test_module
+
+    class test_Menus_EditMenuOptions_Work(EditorSharedTest):
+        from .EditorScripts import Menus_EditMenuOptions as test_module
+
+    class test_Menus_ViewMenuOptions_Work(EditorSharedTest):
+        from .EditorScripts import Menus_ViewMenuOptions as test_module
+
+    @pytest.mark.skip(reason="Times out due to dialogs failing to dismiss: LYN-4208")
+    class test_Menus_FileMenuOptions_Work(EditorSharedTest):
+        from .EditorScripts import Menus_FileMenuOptions as test_module
