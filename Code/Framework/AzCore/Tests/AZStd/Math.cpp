e68f25acf8d78a046bec8c49c6a3d7c56467e62c
nemerle <96597+nemerle@users.noreply.github.com>
2021-08-26 21:14:29 +0200

    written a reduced set of lerp tests, but now the license is correct
    Signed-off-by: nemerle <96597+nemerle@users.noreply.github.com>

--- Code/Framework/AzCore/Tests/AZStd/Math.cpp
+++ Code/Framework/AzCore/Tests/AZStd/Math.cpp
@@ -9,43 +9,35 @@
 #include <AzCore/UnitTest/TestTypes.h>
 #include <AzCore/std/math.h>
 
-// Unittest code copied from LLVM's libcxx under the following license:
-//===----------------------------------------------------------------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-
 namespace UnitTest
 {
     template<typename T>
     static void performTests()
     {
         using ::testing::Eq;
+        using AZStd::lerp;
 
-        constexpr T maxV = std::numeric_limits<T>::max();
         constexpr T inf = std::numeric_limits<T>::infinity();
+        constexpr T maxNumber = std::numeric_limits<T>::max();
+        constexpr T eps = std::numeric_limits<T>::epsilon();
+        constexpr T a = T(42);
 
-        //  Things that can be compared exactly
-        EXPECT_THAT(AZStd::lerp(T(0), T(12), T(0)), Eq(T(0)));
-        EXPECT_THAT(AZStd::lerp(T(0), T(12), T(1)), Eq(T(12)));
-        EXPECT_THAT(AZStd::lerp(T(12), T(0), T(0)), Eq(T(12)));
-        EXPECT_THAT(AZStd::lerp(T(12), T(0), T(1)), Eq(T(0)));
+        // Following the properties defined in the c++ standard p0811r3 proposal
 
-        EXPECT_THAT(AZStd::lerp(T(0), T(12), T(0.5)), Eq(T(6)));
-        EXPECT_THAT(AZStd::lerp(T(12), T(0), T(0.5)), Eq(T(6)));
-        EXPECT_THAT(AZStd::lerp(T(0), T(12), T(2)), Eq(T(24)));
-        EXPECT_THAT(AZStd::lerp(T(12), T(0), T(2)), Eq(T(-12)));
+        //exactness: lerp(a,b,0)==a && lerp(a,b,1)==b
+        EXPECT_THAT(lerp(eps, maxNumber, T(0)), Eq(eps));
+        EXPECT_THAT(lerp(eps, maxNumber, T(1)), Eq(maxNumber));
 
-        EXPECT_THAT(AZStd::lerp(maxV, maxV / 10, T(0)), Eq(maxV));
-        EXPECT_THAT(AZStd::lerp(maxV / 10, maxV, T(1)), Eq(maxV));
+        //monotonicity: cmp(lerp(a,b,t2),lerp(a,b,t1)) * cmp(t2,t1) * cmp(b,a) >= 0, where cmp is an arithmetic three-way comparison function
 
-        EXPECT_THAT(AZStd::lerp(T(2.3), T(2.3), inf), Eq(T(2.3)));
+        //determinacy: result is NaN only for lerp(a,a,INFINITY)
+        //TODO: do we care ?
+        //boundedness: t<0 || t>1 || isfinite(lerp(a,b,t))
+        //TODO: ?
 
-        EXPECT_THAT(AZStd::lerp(T(0), T(0), T(23)), Eq(T(0)));
-        EXPECT_THAT(std::isnan(AZStd::lerp(T(0), T(0), inf)), Eq(true));
+        //consistency: lerp(a,a,t)==a
+        EXPECT_THAT(lerp(a, a, T(0.5)), Eq(a));
+        EXPECT_THAT(lerp(eps, eps, T(0.5)), Eq(eps));
     }
     TEST(StdMath, LerpOperations)
     {
