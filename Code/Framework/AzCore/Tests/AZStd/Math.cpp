76720699e16632640bab1346dfddef42e24e5d8f
nemerle <96597+nemerle@users.noreply.github.com>
2021-08-27 18:09:54 +0200

    Cleanup lerp test cases
    Remove comments that suggested very heavy tests that required things like `for every t1..`
    
    Signed-off-by: nemerle <96597+nemerle@users.noreply.github.com>

--- Code/Framework/AzCore/Tests/AZStd/Math.cpp
+++ Code/Framework/AzCore/Tests/AZStd/Math.cpp
@@ -12,42 +12,32 @@
 namespace UnitTest
 {
     template<typename T>
-    class StdMathTest
-        : public ::testing::Test
+    class StdMathTest : public ::testing::Test
     {
     };
-    
+
     using MathTestConfigs = ::testing::Types<float, double, long double>;
     TYPED_TEST_CASE(StdMathTest, MathTestConfigs);
 
     TYPED_TEST(StdMathTest, LerpOperations)
     {
-        using ::testing::Eq;
         using AZStd::lerp;
+        using ::testing::Eq;
 
         constexpr T inf = AZtd::numeric_limits<T>::infinity();
         constexpr T maxNumber = AZtd::numeric_limits<T>::max();
         constexpr T eps = AZtd::numeric_limits<T>::epsilon();
         constexpr T a{ 42 };
 
-        // Following the properties defined in the c++ standard p0811r3 proposal
-
-        //exactness: lerp(a,b,0)==a && lerp(a,b,1)==b
+        // exactness: lerp(a,b,0)==a && lerp(a,b,1)==b
         EXPECT_THAT(lerp(eps, maxNumber, T(0)), Eq(eps));
         EXPECT_THAT(lerp(eps, maxNumber, T(1)), Eq(maxNumber));
 
-        //monotonicity: cmp(lerp(a,b,t2),lerp(a,b,t1)) * cmp(t2,t1) * cmp(b,a) >= 0, where cmp is an arithmetic three-way comparison function
-
-        //determinacy: result is NaN only for lerp(a,a,INFINITY)
-        //TODO: do we care ?
-        //boundedness: t<0 || t>1 || isfinite(lerp(a,b,t))
-        //TODO: ?
-
-        //consistency: lerp(a,a,t)==a
+        // consistency: lerp(a,a,t)==a
         EXPECT_THAT(lerp(a, a, T(0.5)), Eq(a));
         EXPECT_THAT(lerp(eps, eps, T(0.5)), Eq(eps));
 
-	//a few generic tests taken from MathUtilTests.cpp
+        // a few generic tests taken from MathUtilTests.cpp
         EXPECT_EQ(T(2.5), lerp(T(2), T(4), T(0.25)));
         EXPECT_EQ(T(6.0), lerp(T(2), T(4), T(2.0)));
         EXPECT_EQ(T(3.5), lerp(T(2), T(4), T(0.75)));
