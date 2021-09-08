4911a56e0a78d2f0a777383a71585f5c6651bc7d
nemerle <96597+nemerle@users.noreply.github.com>
2021-09-08 11:04:31 +0200

    Clang format on ClientAuthAWSCredentials.cpp
    Signed-off-by: nemerle <96597+nemerle@users.noreply.github.com>

--- Gems/AWSClientAuth/Code/Source/Authorization/ClientAuthAWSCredentials.cpp
+++ Gems/AWSClientAuth/Code/Source/Authorization/ClientAuthAWSCredentials.cpp
@@ -8,38 +8,42 @@
 
 #include <Authorization/ClientAuthAWSCredentials.h>
 
-#include <AzCore/RTTI/BehaviorContext.h>
 #include <AzCore/RTTI/AzStdOnDemandReflection.inl>
-#include <AzCore/Serialization/SerializeContext.h>
+#include <AzCore/RTTI/BehaviorContext.h>
 #include <AzCore/Serialization/AZStdContainers.inl>
+#include <AzCore/Serialization/SerializeContext.h>
 
 namespace AWSClientAuth
 {
-void ClientAuthAWSCredentials::Reflect(AZ::ReflectContext *context)
-{
-    auto serializeContext = azrtti_cast<AZ::SerializeContext*>(context);
-    if (serializeContext)
+    void ClientAuthAWSCredentials::Reflect(AZ::ReflectContext* context)
     {
-        serializeContext->Class<ClientAuthAWSCredentials>()
+        auto serializeContext = azrtti_cast<AZ::SerializeContext*>(context);
+        if (serializeContext)
+        {
+            serializeContext->Class<ClientAuthAWSCredentials>()
                 ->Field("AWSAccessKeyId", &ClientAuthAWSCredentials::m_accessKeyId)
                 ->Field("AWSSecretKey", &ClientAuthAWSCredentials::m_secretKey)
                 ->Field("AWSSessionToken", &ClientAuthAWSCredentials::m_sessionToken);
-    }
+        }
 
-
-    AZ::BehaviorContext* behaviorContext = azrtti_cast<AZ::BehaviorContext*>(context);
-    if (behaviorContext)
-    {
-        behaviorContext->Class<ClientAuthAWSCredentials>()
+        AZ::BehaviorContext* behaviorContext = azrtti_cast<AZ::BehaviorContext*>(context);
+        if (behaviorContext)
+        {
+            behaviorContext->Class<ClientAuthAWSCredentials>()
                 ->Attribute(AZ::Script::Attributes::Category, "AWSClientAuth")
                 ->Attribute(AZ::Script::Attributes::Storage, AZ::Script::Attributes::StorageType::Value)
                 ->Attribute(AZ::Script::Attributes::Scope, AZ::Script::Attributes::ScopeFlags::Common)
                 ->Constructor()
                 ->Constructor<const ClientAuthAWSCredentials&>()
-                ->Property("AWSAccessKeyId", BehaviorValueGetter(&ClientAuthAWSCredentials::m_accessKeyId), BehaviorValueSetter(&ClientAuthAWSCredentials::m_accessKeyId))
-                ->Property("AWSSecretKey", BehaviorValueGetter(&ClientAuthAWSCredentials::m_secretKey), BehaviorValueSetter(&ClientAuthAWSCredentials::m_secretKey))
-                ->Property("AWSSessionToken", BehaviorValueGetter(&ClientAuthAWSCredentials::m_sessionToken), BehaviorValueSetter(&ClientAuthAWSCredentials::m_sessionToken));
+                ->Property(
+                    "AWSAccessKeyId", BehaviorValueGetter(&ClientAuthAWSCredentials::m_accessKeyId),
+                    BehaviorValueSetter(&ClientAuthAWSCredentials::m_accessKeyId))
+                ->Property(
+                    "AWSSecretKey", BehaviorValueGetter(&ClientAuthAWSCredentials::m_secretKey),
+                    BehaviorValueSetter(&ClientAuthAWSCredentials::m_secretKey))
+                ->Property(
+                    "AWSSessionToken", BehaviorValueGetter(&ClientAuthAWSCredentials::m_sessionToken),
+                    BehaviorValueSetter(&ClientAuthAWSCredentials::m_sessionToken));
+        }
     }
-}
-}
-
+} // namespace AWSClientAuth
