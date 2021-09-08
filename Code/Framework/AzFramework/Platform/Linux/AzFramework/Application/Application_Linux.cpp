11a03d1d815082b4bf09ae78bb4b1a0d5cfa33d2
Steve Pham <spham@amazon.com>
2021-09-07 19:55:20 -0700

    Split out xcb implementation into _xcb specific files
    Signed-off-by: Steve Pham <spham@amazon.com>

--- Code/Framework/AzFramework/Platform/Linux/AzFramework/Application/Application_Linux.cpp
+++ Code/Framework/AzFramework/Platform/Linux/AzFramework/Application/Application_Linux.cpp
@@ -6,113 +6,13 @@
  *
  */
 
-#include <AzFramework/API/ApplicationAPI_Platform.h>
 #include <AzFramework/Application/Application.h>
+
+#include "Application_Linux_xcb.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 namespace AzFramework
 {
-#if PAL_TRAIT_LINUX_WINDOW_MANAGER_XCB
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    class LinuxXcbConnectionManagerImpl
-        : public LinuxXcbConnectionManagerBus::Handler
-    {
-    public:
-        LinuxXcbConnectionManagerImpl()
-        {
-            m_xcbConnection = xcb_connect(nullptr, nullptr);
-            AZ_Error("ApplicationLinux", m_xcbConnection != nullptr, "Unable to connect to X11 Server.");
-            LinuxXcbConnectionManagerBus::Handler::BusConnect();
-        }
-
-        ~LinuxXcbConnectionManagerImpl()
-        {
-            LinuxXcbConnectionManagerBus::Handler::BusDisconnect();
-            xcb_disconnect(m_xcbConnection);   
-        }
-
-        xcb_connection_t* GetXcbConnection() const override
-        {
-            return m_xcbConnection;
-        }
-
-    private:
-    
-        xcb_connection_t*   m_xcbConnection = nullptr;
-    };
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    class ApplicationLinux_xcb
-        : public Application::Implementation
-        , public LinuxLifecycleEvents::Bus::Handler
-    {
-    public:
-        ////////////////////////////////////////////////////////////////////////////////////////////
-        AZ_CLASS_ALLOCATOR(ApplicationLinux_xcb, AZ::SystemAllocator, 0);
-        ApplicationLinux_xcb();
-        ~ApplicationLinux_xcb() override;
-
-        ////////////////////////////////////////////////////////////////////////////////////////////
-        // Application::Implementation
-        void PumpSystemEventLoopOnce() override;
-        void PumpSystemEventLoopUntilEmpty() override;
-
-    private:
-    
-        AZStd::unique_ptr<LinuxXcbConnectionManager>    m_xcbConnectionManager;
-    };
-
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    ApplicationLinux_xcb::ApplicationLinux_xcb()
-    {
-        LinuxLifecycleEvents::Bus::Handler::BusConnect();
-        m_xcbConnectionManager = AZStd::make_unique<LinuxXcbConnectionManagerImpl>();
-        if (LinuxXcbConnectionManagerInterface::Get() == nullptr)
-        {
-            LinuxXcbConnectionManagerInterface::Register(m_xcbConnectionManager.get());
-        }
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    ApplicationLinux_xcb::~ApplicationLinux_xcb()
-    {
-        if (LinuxXcbConnectionManagerInterface::Get() == m_xcbConnectionManager.get())
-        {
-            LinuxXcbConnectionManagerInterface::Unregister(m_xcbConnectionManager.get());
-        }
-        m_xcbConnectionManager.reset();
-        LinuxLifecycleEvents::Bus::Handler::BusDisconnect();
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    void ApplicationLinux_xcb::PumpSystemEventLoopOnce()
-    {
-        if (xcb_connection_t* xcbConnection = m_xcbConnectionManager->GetXcbConnection())
-        {
-            if (xcb_generic_event_t *event = xcb_poll_for_event(xcbConnection))
-            {
-                LinuxXcbEventHandlerBus::Broadcast(&LinuxXcbEventHandlerBus::Events::HandleXcbEvent, event);
-                free(event);
-            }
-        }
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    void ApplicationLinux_xcb::PumpSystemEventLoopUntilEmpty()
-    {
-        if (xcb_connection_t* xcbConnection = m_xcbConnectionManager->GetXcbConnection())
-        {
-            while (xcb_generic_event_t* event = xcb_poll_for_event(xcbConnection))
-            {
-                LinuxXcbEventHandlerBus::Broadcast(&LinuxXcbEventHandlerBus::Events::HandleXcbEvent, event);
-                free(event);
-            }
-        }
-    }
-
-#endif // PAL_TRAIT_LINUX_WINDOW_MANAGER_XCB        
-
     ////////////////////////////////////////////////////////////////////////////////////////////////
     Application::Implementation* Application::Implementation::Create()
     {
