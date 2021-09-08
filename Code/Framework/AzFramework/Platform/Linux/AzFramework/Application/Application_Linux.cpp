4e2c6ded5370cbb2721fb8cb6b7aed8d4146b915
Steve Pham <spham@amazon.com>
2021-09-07 17:40:45 -0700

    - Renamed Application_Linux and NativeWindow_Linux classes to have _xcb extensions - Implemented handling of window resize and close events
    Signed-off-by: Steve Pham <spham@amazon.com>

--- Code/Framework/AzFramework/Platform/Linux/AzFramework/Application/Application_Linux.cpp
+++ Code/Framework/AzFramework/Platform/Linux/AzFramework/Application/Application_Linux.cpp
@@ -13,6 +13,7 @@
 namespace AzFramework
 {
 #if PAL_TRAIT_LINUX_WINDOW_MANAGER_XCB
+    ////////////////////////////////////////////////////////////////////////////////////////////////
     class LinuxXcbConnectionManagerImpl
         : public LinuxXcbConnectionManagerBus::Handler
     {
@@ -29,78 +30,104 @@ namespace AzFramework
             LinuxXcbConnectionManagerBus::Handler::BusDisconnect();
             xcb_disconnect(m_xcbConnection);   
         }
+
         xcb_connection_t* GetXcbConnection() const override
         {
             return m_xcbConnection;
         }
+
     private:
+    
         xcb_connection_t*   m_xcbConnection = nullptr;
     };
-#endif // PAL_TRAIT_LINUX_WINDOW_MANAGER_XCB
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    class ApplicationLinux
+    class ApplicationLinux_xcb
         : public Application::Implementation
         , public LinuxLifecycleEvents::Bus::Handler
     {
     public:
         ////////////////////////////////////////////////////////////////////////////////////////////
-        AZ_CLASS_ALLOCATOR(ApplicationLinux, AZ::SystemAllocator, 0);
-        ApplicationLinux();
-        ~ApplicationLinux() override;
+        AZ_CLASS_ALLOCATOR(ApplicationLinux_xcb, AZ::SystemAllocator, 0);
+        ApplicationLinux_xcb();
+        ~ApplicationLinux_xcb() override;
 
         ////////////////////////////////////////////////////////////////////////////////////////////
         // Application::Implementation
         void PumpSystemEventLoopOnce() override;
         void PumpSystemEventLoopUntilEmpty() override;
+
     private:
-
-#if PAL_TRAIT_LINUX_WINDOW_MANAGER_XCB
+    
         AZStd::unique_ptr<LinuxXcbConnectionManager>    m_xcbConnectionManager;
-#endif // PAL_TRAIT_LINUX_WINDOW_MANAGER_XCB
-
     };
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    Application::Implementation* Application::Implementation::Create()
-    {
-        return aznew ApplicationLinux();
-    }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    ApplicationLinux::ApplicationLinux()
+    ApplicationLinux_xcb::ApplicationLinux_xcb()
     {
         LinuxLifecycleEvents::Bus::Handler::BusConnect();
-
-#if PAL_TRAIT_LINUX_WINDOW_MANAGER_XCB
         m_xcbConnectionManager = AZStd::make_unique<LinuxXcbConnectionManagerImpl>();
         if (LinuxXcbConnectionManagerInterface::Get() == nullptr)
         {
             LinuxXcbConnectionManagerInterface::Register(m_xcbConnectionManager.get());
         }
-#endif // PAL_TRAIT_LINUX_WINDOW_MANAGER_XCB
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    ApplicationLinux::~ApplicationLinux()
+    ApplicationLinux_xcb::~ApplicationLinux_xcb()
     {
-#if PAL_TRAIT_LINUX_WINDOW_MANAGER_XCB
         if (LinuxXcbConnectionManagerInterface::Get() == m_xcbConnectionManager.get())
         {
             LinuxXcbConnectionManagerInterface::Unregister(m_xcbConnectionManager.get());
         }
         m_xcbConnectionManager.reset();
-#endif // PAL_TRAIT_LINUX_WINDOW_MANAGER_XCB        
         LinuxLifecycleEvents::Bus::Handler::BusDisconnect();
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    void ApplicationLinux::PumpSystemEventLoopOnce()
+    void ApplicationLinux_xcb::PumpSystemEventLoopOnce()
     {
+        if (xcb_connection_t* xcbConnection = m_xcbConnectionManager->GetXcbConnection())
+        {
+            if (xcb_generic_event_t *event = xcb_poll_for_event(xcbConnection))
+            {
+                LinuxXcbEventHandlerBus::Broadcast(&LinuxXcbEventHandlerBus::Events::HandleXcbEvent, event);
+                free(event);
+            }
+        }
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    void ApplicationLinux::PumpSystemEventLoopUntilEmpty()
+    void ApplicationLinux_xcb::PumpSystemEventLoopUntilEmpty()
     {
+        if (xcb_connection_t* xcbConnection = m_xcbConnectionManager->GetXcbConnection())
+        {
+            while (xcb_generic_event_t* event = xcb_poll_for_event(xcbConnection))
+            {
+                LinuxXcbEventHandlerBus::Broadcast(&LinuxXcbEventHandlerBus::Events::HandleXcbEvent, event);
+                free(event);
+            }
+        }
     }
+
+#endif // PAL_TRAIT_LINUX_WINDOW_MANAGER_XCB        
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    Application::Implementation* Application::Implementation::Create()
+    {
+#if PAL_TRAIT_LINUX_WINDOW_MANAGER_XCB
+        return aznew ApplicationLinux_xcb();
+#elif PAL_TRAIT_LINUX_WINDOW_MANAGER_WAYLAND
+        #error "Linux Window Manager Wayland not supported."
+        return nullptr;
+#elif PAL_TRAIT_LINUX_WINDOW_MANAGER_XLIB
+        #error "Linux Window Manager XLIB not supported."
+        return nullptr;
+#else
+        #error "Linux Window Manager not recognized."
+        return nullptr;
+#endif // PAL_TRAIT_LINUX_WINDOW_MANAGER_XCB
+    }
+
 } // namespace AzFramework
--- Code/Framework/AzFramework/Platform/Linux/AzFramework/Windowing/NativeWindow_Linux.cpp
+++ Code/Framework/AzFramework/Platform/Linux/AzFramework/Windowing/NativeWindow_Linux.cpp
@@ -7,20 +7,28 @@
  */
 
 #include <AzFramework/API/ApplicationAPI_Platform.h>
+#include <AzFramework/Application/Application.h>
 #include <AzFramework/Windowing/NativeWindow.h>
 #include <xcb/xcb.h>
 
 namespace AzFramework
 {
-    class NativeWindowImpl_Linux final
+
+#if PAL_TRAIT_LINUX_WINDOW_MANAGER_XCB
+    static constexpr uint8_t s_XcbFormatDataSize = 32;
+    static constexpr uint16_t s_DefaultXcbWindowBorderWidth = 4;
+
+    class NativeWindowImpl_Linux_xcb final
         : public NativeWindow::Implementation
+        , public LinuxXcbEventHandlerBus::Handler
     {
     public:
-        AZ_CLASS_ALLOCATOR(NativeWindowImpl_Linux, AZ::SystemAllocator, 0);
-        NativeWindowImpl_Linux();
-        ~NativeWindowImpl_Linux() override;
+        AZ_CLASS_ALLOCATOR(NativeWindowImpl_Linux_xcb, AZ::SystemAllocator, 0);
+        NativeWindowImpl_Linux_xcb();
+        ~NativeWindowImpl_Linux_xcb() override;
 
-        // NativeWindow::Implementation overrides...
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        // NativeWindow::Implementation
         void InitWindow(const AZStd::string& title,
                         const WindowGeometry& geometry,
                         const WindowStyleMasks& styleMasks) override;
@@ -30,57 +38,49 @@ namespace AzFramework
         void SetWindowTitle(const AZStd::string& title) override;
         void ResizeClientArea(WindowSize clientAreaSize) override;
         uint32_t GetDisplayRefreshRate() const override;        
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        // LinuxXcbEventHandlerBus::Handler
+        void HandleXcbEvent(xcb_generic_event_t* event) override;
+
     private:
-        xcb_connection_t*     m_xcbConnection = NULL;
-        xcb_window_t          m_xcbWindow = 0;
+        bool ValidateXcbResult(xcb_void_cookie_t cookie);
+        void WindowSizeChanged(const uint32_t width, const uint32_t height);
+
+        xcb_connection_t*   m_xcbConnection = nullptr;
+        xcb_window_t        m_xcbWindow = 0;
+        xcb_atom_t          m_xcbAtomProtocols;
+        xcb_atom_t          m_xcbAtomDeleteWindow;
     };
     
-    NativeWindowImpl_Linux::NativeWindowImpl_Linux() 
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    NativeWindowImpl_Linux_xcb::NativeWindowImpl_Linux_xcb() 
         : NativeWindow::Implementation()
     {
-#if PAL_TRAIT_LINUX_WINDOW_MANAGER_XCB
-            if (auto xcbConnectionManager = AzFramework::LinuxXcbConnectionManagerInterface::Get();
-                xcbConnectionManager != nullptr)
-            {
-                m_xcbConnection = xcbConnectionManager->GetXcbConnection();
-            }
-            AZ_Error("AtomVulkan_RHI", m_xcbConnection!=nullptr, "Unable to get XCB Connection");
-
-#elif PAL_TRAIT_LINUX_WINDOW_MANAGER_WAYLAND
-            #error "Linux Window Manager Wayland not supported."
-            return RHI::ResultCode::Unimplemented;
-#elif PAL_TRAIT_LINUX_WINDOW_MANAGER_XLIB
-            #error "Linux Window Manager XLIB not supported."
-            return RHI::ResultCode::Unimplemented;
-#else
-            #error "Linux Window Manager not recognized."
-            return RHI::ResultCode::Unimplemented;
-#endif // PAL_TRAIT_LINUX_WINDOW_MANAGER_XCB
-    }
-
-    NativeWindowImpl_Linux::~NativeWindowImpl_Linux()
-    {
-    }
-
-
-    NativeWindow::Implementation* NativeWindow::Implementation::Create()
-    {
-        return aznew NativeWindowImpl_Linux();
-    }
-
-    void NativeWindowImpl_Linux::InitWindow([[maybe_unused]]const AZStd::string& title,
-                                            const WindowGeometry& geometry,
-                                            [[maybe_unused]]const WindowStyleMasks& styleMasks)
-    {
-        if (m_xcbConnection == NULL)
+        if (auto xcbConnectionManager = AzFramework::LinuxXcbConnectionManagerInterface::Get();
+            xcbConnectionManager != nullptr)
         {
-            return;
+            m_xcbConnection = xcbConnectionManager->GetXcbConnection();
         }
+        AZ_Error("AtomVulkan_RHI", m_xcbConnection!=nullptr, "Unable to get XCB Connection");
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    NativeWindowImpl_Linux_xcb::~NativeWindowImpl_Linux_xcb()
+    {
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    void NativeWindowImpl_Linux_xcb::InitWindow([[maybe_unused]]const AZStd::string& title,
+                                                const WindowGeometry& geometry,
+                                                [[maybe_unused]]const WindowStyleMasks& styleMasks)
+    {
         // Get the parent window 
         const xcb_setup_t* xcbSetup = xcb_get_setup(m_xcbConnection);
         xcb_screen_t * xcbRootScreen = xcb_setup_roots_iterator(xcbSetup).data;
         xcb_window_t xcbParentWindow = xcbRootScreen->root;
 
+        // Create an XCB window from the connection
         m_xcbWindow = xcb_generate_id(m_xcbConnection);
 
         uint16_t borderWidth = 0;
@@ -88,38 +88,68 @@ namespace AzFramework
         if ((mask & WindowStyleMasks::WINDOW_STYLE_BORDERED) ||
             (mask & WindowStyleMasks::WINDOW_STYLE_RESIZEABLE))
         {
-            borderWidth = 4;
+            borderWidth = s_DefaultXcbWindowBorderWidth;
         }
 
         uint32_t eventMask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
-        uint32_t valueList[] = { xcbRootScreen->black_pixel, 0 };
+        
+        uint32_t valueList[] = { xcbRootScreen->black_pixel, 
+                                 XCB_EVENT_MASK_STRUCTURE_NOTIFY };
 
-        xcb_create_window(m_xcbConnection,
-                          XCB_COPY_FROM_PARENT, //depth
-                          m_xcbWindow, // Window ID
-                          xcbParentWindow,  // parent.
-                          aznumeric_cast<int16_t>(geometry.m_posX),     // X
-                          aznumeric_cast<int16_t>(geometry.m_posY),     // Y
-                          aznumeric_cast<int16_t>(geometry.m_width),    // Width
-                          aznumeric_cast<int16_t>(geometry.m_height),   // Height
-                          borderWidth,                                  // Border Width
-                          XCB_WINDOW_CLASS_INPUT_OUTPUT,        // Class
-                          xcbRootScreen->root_visual,
-                          eventMask,
-                          valueList);
+        xcb_void_cookie_t xcb_check_result;
+
+        xcb_check_result = xcb_create_window_checked(m_xcbConnection,
+                                                     XCB_COPY_FROM_PARENT,                         //depth
+                                                     m_xcbWindow,                                  // Window ID
+                                                     xcbParentWindow,                              // parent.
+                                                     aznumeric_cast<int16_t>(geometry.m_posX),     // X
+                                                     aznumeric_cast<int16_t>(geometry.m_posY),     // Y
+                                                     aznumeric_cast<int16_t>(geometry.m_width),    // Width
+                                                     aznumeric_cast<int16_t>(geometry.m_height),   // Height
+                                                     borderWidth,                                  // Border Width
+                                                     XCB_WINDOW_CLASS_INPUT_OUTPUT,                // Class
+                                                     xcbRootScreen->root_visual,
+                                                     eventMask,
+                                                     valueList);
+
+        AZ_Assert(ValidateXcbResult(xcb_check_result), "Failed to create xcb window.");
 
         SetWindowTitle(title);
+
+        // Setup the window close event 
+        const static char* wmProtocolString = "WM_PROTOCOLS";
+        
+        xcb_intern_atom_cookie_t cookie_protocol = xcb_intern_atom(m_xcbConnection, 1, strlen(wmProtocolString), wmProtocolString);
+        xcb_intern_atom_reply_t* reply_protocol = xcb_intern_atom_reply(m_xcbConnection, cookie_protocol, nullptr);
+        AZ_Error("AtomVulkan_RHI", reply_protocol!=NULL, "Unable to query xcb '%s' atom", wmProtocolString);
+        m_xcbAtomProtocols = reply_protocol->atom;
+
+        const static char* wmDeleteWindowString = "WM_DELETE_WINDOW";
+        xcb_intern_atom_cookie_t cookie_delete_window = xcb_intern_atom(m_xcbConnection, 0, strlen(wmDeleteWindowString), wmDeleteWindowString);
+        xcb_intern_atom_reply_t* reply_delete_window = xcb_intern_atom_reply(m_xcbConnection, cookie_delete_window, nullptr);
+        AZ_Error("AtomVulkan_RHI", reply_delete_window!=NULL, "Unable to query xcb '%s' atom", wmDeleteWindowString);
+        m_xcbAtomDeleteWindow = reply_delete_window->atom;
+
+        xcb_check_result = xcb_change_property_checked(m_xcbConnection, 
+                                                       XCB_PROP_MODE_REPLACE, 
+                                                       m_xcbWindow,
+			                                           m_xcbAtomProtocols, 
+                                                       XCB_ATOM_ATOM, 
+                                                       s_XcbFormatDataSize, 
+                                                       1,
+			                                           &m_xcbAtomDeleteWindow);
+                                                    
+        AZ_Assert(ValidateXcbResult(xcb_check_result), "Failed to change atom property for WM_CLOSE event");
 
         m_width = geometry.m_width;
         m_height = geometry.m_height;
     }
 
-    void NativeWindowImpl_Linux::Activate()
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    void NativeWindowImpl_Linux_xcb::Activate()
     {
-        if (m_xcbConnection==NULL)
-        {
-            return;
-        }
+        LinuxXcbEventHandlerBus::Handler::BusConnect();
+
         if (!m_activated) // nothing to do if window was already activated
         {
             m_activated = true;
@@ -129,48 +159,123 @@ namespace AzFramework
         }
     }
 
-    void NativeWindowImpl_Linux::Deactivate()
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    void NativeWindowImpl_Linux_xcb::Deactivate()
     {
-        if (m_xcbConnection==NULL)
-        {
-            return;
-        }
         if (m_activated) // nothing to do if window was already deactivated
         {
             m_activated = false;
 
+            WindowNotificationBus::Event(reinterpret_cast<NativeWindowHandle>(m_xcbWindow), &WindowNotificationBus::Events::OnWindowClosed);
+
             xcb_unmap_window(m_xcbConnection, m_xcbWindow);
             xcb_flush(m_xcbConnection);
         }
+        LinuxXcbEventHandlerBus::Handler::BusDisconnect();
     }    
 
-    NativeWindowHandle NativeWindowImpl_Linux::GetWindowHandle() const
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    NativeWindowHandle NativeWindowImpl_Linux_xcb::GetWindowHandle() const
     {
         return reinterpret_cast<NativeWindowHandle>(m_xcbWindow);
     }
 
-    void NativeWindowImpl_Linux::SetWindowTitle(const AZStd::string& title)
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    void NativeWindowImpl_Linux_xcb::SetWindowTitle(const AZStd::string& title)
     {
-        xcb_change_property(m_xcbConnection,
-                            XCB_PROP_MODE_REPLACE,
-                            m_xcbWindow,
-                            XCB_ATOM_WM_NAME,
-                            XCB_ATOM_STRING,
-                            8,
-                            static_cast<uint32_t>(title.size()),
-                            title.c_str());
+        xcb_void_cookie_t xcb_check_result;
+        xcb_check_result = xcb_change_property(m_xcbConnection,
+                                               XCB_PROP_MODE_REPLACE,
+                                               m_xcbWindow,
+                                               XCB_ATOM_WM_NAME,
+                                               XCB_ATOM_STRING,
+                                               8,
+                                               static_cast<uint32_t>(title.size()),
+                                               title.c_str());
+        AZ_Assert(ValidateXcbResult(xcb_check_result), "Failed to set window title.");
     }
 
-    void NativeWindowImpl_Linux::ResizeClientArea(WindowSize clientAreaSize)
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    void NativeWindowImpl_Linux_xcb::ResizeClientArea(WindowSize clientAreaSize)
     {
         const static uint32_t values[] = { clientAreaSize.m_width, clientAreaSize.m_height };
 
         xcb_configure_window (m_xcbConnection, m_xcbWindow, XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT, values);
+        
+        m_width = clientAreaSize.m_width;
+        m_height = clientAreaSize.m_height;
     }
 
-    uint32_t NativeWindowImpl_Linux::GetDisplayRefreshRate() const
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    uint32_t NativeWindowImpl_Linux_xcb::GetDisplayRefreshRate() const
     {
         //Using 60 for now until proper support is added
         return 60;
     }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    bool NativeWindowImpl_Linux_xcb::ValidateXcbResult(xcb_void_cookie_t cookie)
+    {
+        bool result = true;
+        if (xcb_generic_error_t* error = xcb_request_check(m_xcbConnection, cookie))
+        {
+            AZ_TracePrintf("Error","Error code %d", error->error_code);
+            result = false;
+        }
+        return result;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    void NativeWindowImpl_Linux_xcb::HandleXcbEvent(xcb_generic_event_t* event)
+    {
+        switch (event->response_type & 0x7f)
+        {
+            case XCB_CONFIGURE_NOTIFY:
+            {
+                xcb_configure_notify_event_t *cne = reinterpret_cast<xcb_configure_notify_event_t *>(event);
+                if ((cne->width != m_width) ||
+                    (cne->height != m_height))
+                {
+                    WindowSizeChanged(static_cast<uint32_t>(cne->width), 
+                                      static_cast<uint32_t>(cne->height));
+                }
+                break;
+            }
+            case XCB_CLIENT_MESSAGE:
+            {
+                xcb_client_message_event_t *cme = reinterpret_cast<xcb_client_message_event_t *>(event);
+	            if ((cme->type == m_xcbAtomProtocols) && 
+                    (cme->format == s_XcbFormatDataSize) &&
+			        (cme->data.data32[0] == m_xcbAtomDeleteWindow))
+                {
+                    Deactivate();
+                    ApplicationRequests::Bus::Broadcast(&ApplicationRequests::ExitMainLoop);
+                }
+                break;
+            }
+        }
+        AZ_UNUSED(event);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    void NativeWindowImpl_Linux_xcb::WindowSizeChanged(const uint32_t width, const uint32_t height)
+    {
+        if (m_width != width || m_height != height)
+        {
+            m_width = width;
+            m_height = height;
+
+            if (m_activated)
+            {
+                WindowNotificationBus::Event(reinterpret_cast<NativeWindowHandle>(m_xcbWindow), &WindowNotificationBus::Events::OnWindowResized, width, height);
+            }
+        }
+    }
+#endif // PAL_TRAIT_LINUX_WINDOW_MANAGER_XCB
+
+    NativeWindow::Implementation* NativeWindow::Implementation::Create()
+    {
+        return aznew NativeWindowImpl_Linux_xcb();
+    }
+
 } // namespace AzFramework
