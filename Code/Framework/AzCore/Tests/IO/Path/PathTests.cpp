c2e4143407986b92c2c847ede521778a114d3788
lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
2021-09-14 18:39:36 -0500

    MS Build Tools 14.29 workaround around suppressing warnings using the external header feature
    Signed-off-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>

--- Code/Framework/AzCore/Tests/IO/Path/PathTests.cpp
+++ Code/Framework/AzCore/Tests/IO/Path/PathTests.cpp
@@ -235,7 +235,9 @@ namespace UnitTest
         EXPECT_THAT(testPath1, compareMatcher);
         // Compare hash using parameterized Matcher
         const size_t testPath1Hash = AZStd::hash<AZ::IO::PathView>{}(testPath1);
+AZ_PUSH_DISABLE_WARNING(4296, "-Wunknown-warning-option")
         EXPECT_THAT(testPath1Hash, hashMatcher);
+AZ_POP_DISABLE_WARNING
     }
 
     INSTANTIATE_TEST_CASE_P(
