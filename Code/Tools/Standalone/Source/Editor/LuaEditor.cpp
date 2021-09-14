e96b75127882bb7cb4305761af1c1db2f58f425d
Steve Pham <spham@amazon.com>
2021-09-14 16:12:39 -0700

    Adding missing destroy for SystemAllocator
    Signed-off-by: Steve Pham <spham@amazon.com>

--- Code/Tools/Standalone/Source/Editor/LuaEditor.cpp
+++ Code/Tools/Standalone/Source/Editor/LuaEditor.cpp
@@ -74,6 +74,10 @@ int main(int argc, char* argv[])
         // if its in GUI mode or not.
     }
 
+    if (AZ::AllocatorInstance<AZ::SystemAllocator>::IsReady())
+    {
+        AZ::AllocatorInstance<AZ::SystemAllocator>::Destroy();
+    }
     if (AZ::AllocatorInstance<AZ::OSAllocator>::IsReady())
     {
         AZ::AllocatorInstance<AZ::OSAllocator>::Destroy();
