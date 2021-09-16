1f3744d85d3d637c90929c2fe91e711f8a0296ba
Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
2021-09-16 13:54:04 -0500

    Add unit test to verify terrain layers define terrain regions.
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>

--- Gems/Terrain/Code/Tests/TerrainSystemTest.cpp
+++ Gems/Terrain/Code/Tests/TerrainSystemTest.cpp
@@ -9,20 +9,21 @@
 #include <AzCore/Component/ComponentApplication.h>
 #include <AzCore/Memory/MemoryComponent.h>
 
-#include <TerrainSystem/TerrainSystem.h>
-
 #include <AzTest/AzTest.h>
+
+#include <TerrainSystem/TerrainSystem.h>
+#include <Components/TerrainLayerSpawnerComponent.h>
+
 #include <Terrain/MockTerrain.h>
 
 using ::testing::AtLeast;
 using ::testing::NiceMock;
+using ::testing::Return;
 
 class TerrainSystemTest : public ::testing::Test
 {
 protected:
     AZ::ComponentApplication m_app;
-
-    AZStd::unique_ptr<AZ::Entity> m_entity;
     AZStd::unique_ptr<Terrain::TerrainSystem> m_terrainSystem;
 
     void SetUp() override
@@ -113,6 +114,8 @@ TEST_F(TerrainSystemTest, DestroyEventsCalledOnDeactivation)
 
 TEST_F(TerrainSystemTest, TerrainDoesNotExistWhenNoTerrainLayerSpawnersAreRegistered)
 {
+    // For the terrain system, terrain should only exist where terrain layer spawners are present.
+
     // Verify that in the active terrain system, if there are no terrain layer spawners, any arbitrary point
     // will return false for terrainExists, returns a height equal to the min world bounds of the terrain system, and returns
     // a normal facing up the Z axis.
@@ -136,11 +139,104 @@ TEST_F(TerrainSystemTest, TerrainDoesNotExistWhenNoTerrainLayerSpawnersAreRegist
             EXPECT_FALSE(terrainExists);
             EXPECT_EQ(height, worldBounds.GetMin().GetZ());
 
+            terrainExists = true;
             AZ::Vector3 normal = m_terrainSystem->GetNormal(
                 position, AzFramework::Terrain::TerrainDataRequests::Sampler::EXACT, &terrainExists);
             EXPECT_FALSE(terrainExists);
             EXPECT_EQ(normal, AZ::Vector3::CreateAxisZ());
+
+            bool isHole = m_terrainSystem->GetIsHoleFromFloats(
+                position.GetX(), position.GetY(), AzFramework::Terrain::TerrainDataRequests::Sampler::EXACT);
+            EXPECT_TRUE(isHole);
         }
     }
 }
 
+TEST_F(TerrainSystemTest, TerrainExistsOnlyWithinTerrainLayerSpawnerBounds)
+{
+    // Verify that the presence of a TerrainLayerSpawner causes terrain to exist in (and *only* in) the box where the TerrainLayerSpawner
+    // is defined.
+
+    // The terrain system should only query Heights and Normals from the TerrainAreaHeightRequest bus within the
+    // TerrainLayerSpawner region, and so those values should only get returned from GetHeight/GetNormal for queries inside that region.
+
+    // Create the base entity with a mock Box Shape and a Terrain Layer Spawner.
+    auto entity = CreateEntity();
+    CreateComponent<UnitTest::MockBoxShapeComponent>(entity.get());
+    CreateComponent<Terrain::TerrainLayerSpawnerComponent>(entity.get());
+
+    // Set up the box shape to return a box from (0,0,5) to (10, 10, 15)
+    AZ::Aabb spawnerBox = AZ::Aabb::CreateFromMinMaxValues(0.0f, 0.0f, 5.0f, 10.0f, 10.0f, 15.0f);
+    NiceMock<UnitTest::MockBoxShapeComponentRequests> boxShapeRequests(entity->GetId());
+    NiceMock<UnitTest::MockShapeComponentRequests> shapeRequests(entity->GetId());
+    ON_CALL(shapeRequests, GetEncompassingAabb).WillByDefault(Return(spawnerBox));
+
+    // Set up a mock height provider that always returns 5.0 and a normal of Y-up.
+    const float spawnerHeight = 5.0f;
+    const AZ::Vector3 spawnerNormal = AZ::Vector3::CreateAxisY();
+    NiceMock<UnitTest::MockTerrainAreaHeightRequests> terrainAreaHeightRequests(entity->GetId());
+    ON_CALL(terrainAreaHeightRequests, GetHeight)
+        .WillByDefault(
+            [spawnerHeight](const AZ::Vector3& inPosition, AZ::Vector3& outPosition,
+                [[maybe_unused]] AzFramework::Terrain::TerrainDataRequests::Sampler sampleFilter)
+            {
+                outPosition = inPosition;
+                outPosition.SetZ(spawnerHeight);
+            });
+    ON_CALL(terrainAreaHeightRequests, GetNormal)
+        .WillByDefault(
+            [spawnerNormal](
+                [[maybe_unused]] const AZ::Vector3& inPosition, AZ::Vector3& outNormal,
+                [[maybe_unused]] AzFramework::Terrain::TerrainDataRequests::Sampler sampleFilter)
+            {
+                outNormal = spawnerNormal;
+            });
+
+    ActivateEntity(entity.get());
+
+    // Verify that terrain exists within the layer spawner bounds, and doesn't exist outside of it.
+
+    // Create the terrain system and give it one tick to fully initialize itself.
+    m_terrainSystem = AZStd::make_unique<Terrain::TerrainSystem>();
+    m_terrainSystem->Activate();
+    AZ::TickBus::Broadcast(&AZ::TickBus::Events::OnTick, 0.f, AZ::ScriptTimePoint{});
+
+    AZ::Aabb worldBounds = m_terrainSystem->GetTerrainAabb();
+
+    // Create a box that's twice as big as the layer spawner box.  Loop through it and verify that points within the layer box contain
+    // terrain and the expected height & normal values, and points outside the layer box don't contain terrain.
+    const AZ::Aabb encompassingBox =
+        AZ::Aabb::CreateFromMinMax(spawnerBox.GetMin() - (spawnerBox.GetExtents() / 2.0f),
+            spawnerBox.GetMax() + (spawnerBox.GetExtents() / 2.0f));
+
+    for (float y = encompassingBox.GetMin().GetY(); y < encompassingBox.GetMax().GetY(); y += 1.0f)  
+    {
+        for (float x = encompassingBox.GetMin().GetX(); x < encompassingBox.GetMax().GetX(); x += 1.0f)
+        {
+            AZ::Vector3 position(x, y, 0.0f);
+            bool heightQueryTerrainExists = false;
+            bool normalQueryTerrainExists = false;
+            float height =
+                m_terrainSystem->GetHeight(position, AzFramework::Terrain::TerrainDataRequests::Sampler::EXACT, &heightQueryTerrainExists);
+            AZ::Vector3 normal =
+                m_terrainSystem->GetNormal(position, AzFramework::Terrain::TerrainDataRequests::Sampler::EXACT, &normalQueryTerrainExists);
+            bool isHole = m_terrainSystem->GetIsHoleFromFloats(
+                position.GetX(), position.GetY(), AzFramework::Terrain::TerrainDataRequests::Sampler::EXACT);
+
+            if (spawnerBox.Contains(AZ::Vector3(position.GetX(), position.GetY(), spawnerBox.GetMin().GetZ())))
+            {
+                EXPECT_TRUE(heightQueryTerrainExists);
+                EXPECT_TRUE(normalQueryTerrainExists);
+                EXPECT_FALSE(isHole);
+                EXPECT_EQ(height, spawnerHeight);
+                EXPECT_EQ(normal, spawnerNormal);
+            }
+            else
+            {
+                EXPECT_FALSE(heightQueryTerrainExists);
+                EXPECT_FALSE(normalQueryTerrainExists);
+                EXPECT_TRUE(isHole);
+            }
+        }
+    }
+}
