dbfe843903bf6dfa9441db62ce4b43d1d98f86c0
sphrose <82213493+sphrose@users.noreply.github.com>
2021-09-21 15:27:25 +0100

    TerrainHeightGradientList Tests
    Signed-off-by: sphrose <82213493+sphrose@users.noreply.github.com>

--- Gems/GradientSignal/Code/gradientsignal_mocks_files.cmake
+++ Gems/GradientSignal/Code/gradientsignal_mocks_files.cmake
@@ -7,9 +7,5 @@
 #
 
 set(FILES
-    Tests/TerrainTest.cpp
-    Tests/TerrainSystemTest.cpp
-    Tests/LayerSpawnerTests.cpp
-    Tests/MockAxisAlignedBoxShapeComponent.h
-    Tests/TerrainHeightGradientListTests.cpp
+    Mocks/GradientSignal/Ebuses/MockGradientRequestBus.h
 )
