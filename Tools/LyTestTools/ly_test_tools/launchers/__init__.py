09ce73aa44303cfd4622c578f311bc41c6fef65b
sweeneys <sweeneys@amazon.com>
2021-09-22 17:50:12 -0700

    Initial update for this module's unit tests on Linux
    Signed-off-by: sweeneys <sweeneys@amazon.com>

--- Tools/LyTestTools/ly_test_tools/launchers/__init__.py
+++ Tools/LyTestTools/ly_test_tools/launchers/__init__.py
@@ -6,7 +6,7 @@ SPDX-License-Identifier: Apache-2.0 OR MIT
 """
 
 from ly_test_tools.launchers.platforms.base import Launcher
+from ly_test_tools.launchers.platforms.linux.launcher import LinuxLauncher, LinuxEditor, DedicatedLinuxLauncher
 from ly_test_tools.launchers.platforms.mac.launcher import MacLauncher
-from ly_test_tools.launchers.platforms.win.launcher import (
-    WinLauncher, DedicatedWinLauncher, WinEditor, WinGenericLauncher)
+from ly_test_tools.launchers.platforms.win.launcher import WinLauncher, DedicatedWinLauncher, WinEditor, WinGenericLauncher
 from ly_test_tools.launchers.platforms.android.launcher import AndroidLauncher
