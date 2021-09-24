24bc554d54e288025c71582b374eee054bb4d79d
nemerle <96597+nemerle@users.noreply.github.com>
2021-09-24 13:43:33 +0200

    Missing license headers
    Signed-off-by: nemerle <96597+nemerle@users.noreply.github.com>

--- Gems/ScriptEvents/Code/Source/ScriptEventMethod.cpp
+++ Gems/ScriptEvents/Code/Source/ScriptEventMethod.cpp
@@ -1,4 +1,12 @@
-#include "ScriptEvents/ScriptEventMethod.h"
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+ 
+ #include "ScriptEvents/ScriptEventMethod.h"
 
 #include <AzCore/RTTI/BehaviorContext.h>
 #include <AzCore/Serialization/SerializeContext.h>
