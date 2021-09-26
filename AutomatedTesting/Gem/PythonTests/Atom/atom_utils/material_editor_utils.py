616eab8ff0203f84400d614f8603ca9652d0ce43
lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
2021-09-25 22:04:24 -0500

    Replaced references that assumes the project path is <engroot>/AutomatedTesting with <projectroot> in the AutomatedTesting python test
    Signed-off-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>

--- AutomatedTesting/Gem/PythonTests/Atom/atom_utils/material_editor_utils.py
+++ AutomatedTesting/Gem/PythonTests/Atom/atom_utils/material_editor_utils.py
@@ -211,7 +211,7 @@ class Timeout:
         return time.time() > self.die_after
 
 
-screenshotsFolder = os.path.join(azlmbr.paths.engroot, "AtomTest", "Cache" "pc", "Screenshots")
+screenshotsFolder = os.path.join(azlmbr.paths.projectroot, "Cache" "pc", "Screenshots")
 
 
 class ScreenshotHelper:
