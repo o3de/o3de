589dae331a568985123650260a35619b8da2b038
Mike Chang <62353586+amzn-changml@users.noreply.github.com>
2021-09-28 00:23:31 -0700

    Remove conditionals
    Signed-off-by: Mike Chang <changml@amazon.com>

--- cmake/Platform/Windows/PackagingPostBuild.cmake
+++ cmake/Platform/Windows/PackagingPostBuild.cmake
@@ -55,10 +55,8 @@ set(_light_command
     -o "${_bootstrap_output_file}"
 )
 
-if(EXISTS ${CPACK_SOURCE_DIR}\\scripts\\signer\\signer.ps1)
-    message(STATUS "Signing base files in ${_cpack_wix_out_dir}")
-    execute_process(COMMAND "powershell.exe -ExecutionPolicy Bypass -File ${CPACK_SOURCE_DIR}\\scripts\\signer\\signer.ps1 -basePath ${_cpack_wix_out_dir}")
-endif()
+message(STATUS "Signing base files in ${_cpack_wix_out_dir}")
+execute_process(COMMAND "powershell.exe -ExecutionPolicy Bypass -File ${CPACK_SOURCE_DIR}\\scripts\\signer\\signer.ps1 -basePath ${_cpack_wix_out_dir}")
 
 message(STATUS "Creating Bootstrap Installer...")
 execute_process(
@@ -85,10 +83,8 @@ file(COPY ${_bootstrap_output_file}
 
 message(STATUS "Bootstrap installer generated to ${CPACK_PACKAGE_DIRECTORY}/${_bootstrap_filename}")
 
-if(EXISTS ${CPACK_SOURCE_DIR}\\scripts\\signer\\signer.ps1)
-    message(STATUS "Signing bootstrap installer in ${CPACK_PACKAGE_DIRECTORY}")
-    execute_process(COMMAND "powershell.exe -ExecutionPolicy Bypass -File ${CPACK_SOURCE_DIR}\\scripts\\signer\\signer.ps1 -bootstrapPath ${CPACK_PACKAGE_DIRECTORY}\\${_bootstrap_filename}")
-endif()
+message(STATUS "Signing bootstrap installer in ${CPACK_PACKAGE_DIRECTORY}")
+execute_process(COMMAND "powershell.exe -ExecutionPolicy Bypass -File ${CPACK_SOURCE_DIR}\\scripts\\signer\\signer.ps1 -bootstrapPath ${CPACK_PACKAGE_DIRECTORY}\\${_bootstrap_filename}")
 
 # use the internal default path if somehow not specified from cpack_configure_downloads
 if(NOT CPACK_UPLOAD_DIRECTORY)
