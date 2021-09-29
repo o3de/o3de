9f6f048cb4bb2523bc86cd8bc9e77d3f44108869
chcurran <82187351+carlitosan@users.noreply.github.com>
2021-09-29 16:26:14 -0700

    fix bad unused variable component property check
    Signed-off-by: chcurran <82187351+carlitosan@users.noreply.github.com>

--- Gems/ScriptCanvas/Code/Builder/ScriptCanvasBuilder.cpp
+++ Gems/ScriptCanvas/Code/Builder/ScriptCanvasBuilder.cpp
@@ -182,12 +182,15 @@ namespace ScriptCanvasBuilder
                 continue;
             }
 
-            // copy to override unused list for editor display
-            m_overridesUnused.push_back(*graphVariable);
-            auto& overrideValue = m_overridesUnused.back();
-            overrideValue.DeepCopy(*graphVariable);
-            overrideValue.SetScriptInputControlVisibility(AZ::Edit::PropertyVisibility::Hide);
-            overrideValue.SetAllowSignalOnChange(false);
+            if (graphVariable->IsComponentProperty())
+            {
+                // copy to override unused list for editor display
+                m_overridesUnused.push_back(*graphVariable);
+                auto& overrideValue = m_overridesUnused.back();
+                overrideValue.DeepCopy(*graphVariable);
+                overrideValue.SetScriptInputControlVisibility(AZ::Edit::PropertyVisibility::Hide);
+                overrideValue.SetAllowSignalOnChange(false);
+            }
         }
     }
 
