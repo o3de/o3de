d26e213905c0302d3dc9c527dfd50fc790844da8
lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
2021-09-28 19:00:07 -0500

    Changed the AssetManager::DispatchEvents function to only execute the AssetBus queued events once
    Changed the AssetJobsFloodTest.AssetWithNoLoadReference_LoadDependencies_BehaviorObeyed test to dispatch events until the OnAssetContainerReady callback is signaled.
    
    This happens after every asset load to make sure that the
    expiring AssetContainer instances are removed from `AssetManager::m_ownedAssetContainer` container before retrying to load the same asset.
    
    Signed-off-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>

--- Code/Framework/AzCore/AzCore/Asset/AssetManager.cpp
+++ Code/Framework/AzCore/AzCore/Asset/AssetManager.cpp
@@ -340,6 +340,14 @@ namespace AZ
                     // (Load jobs will attempt to reuse blocked threads before spinning off new job threads)
                     ProcessLoadJob();
                 }
+
+                // Pump the AssetBus function queue once more after the load has completed in case additional
+                // functions have been queued between the last call to DispatchEvents and the completion
+                // of the current load job
+                if (m_shouldDispatchEvents)
+                {
+                    AssetManager::Instance().DispatchEvents();
+                }
             }
 
             void Finish()
@@ -567,10 +575,7 @@ namespace AZ
         {
             AZ_PROFILE_FUNCTION(AzCore);
             AssetManagerNotificationBus::Broadcast(&AssetManagerNotificationBus::Events::OnAssetEventsDispatchBegin);
-            while (AssetBus::QueuedEventCount())
-            {
-                AssetBus::ExecuteQueuedEvents();
-            }
+            AssetBus::ExecuteQueuedEvents();
             AssetManagerNotificationBus::Broadcast(&AssetManagerNotificationBus::Events::OnAssetEventsDispatchEnd);
         }
 
