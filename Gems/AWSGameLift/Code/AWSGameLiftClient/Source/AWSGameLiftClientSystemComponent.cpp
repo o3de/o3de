a534fccc9b1b9ba1c928cccb2d65027ece496b59
Vincent Liu <5900509+onecent1101@users.noreply.github.com>
2021-10-18 12:47:55 -0700

    Expose matchmaking event polling APIs and add required session notifications (#4636)
    * Expose matchmaking event polling APIs and add required session notifications
    
    Signed-off-by: onecent1101 <liug@amazon.com>

--- Gems/AWSGameLift/Code/AWSGameLiftClient/Source/AWSGameLiftClientSystemComponent.cpp
+++ Gems/AWSGameLift/Code/AWSGameLiftClient/Source/AWSGameLiftClientSystemComponent.cpp
@@ -12,6 +12,7 @@
 #include <AzCore/Serialization/EditContextConstants.inl>
 #include <AzFramework/Session/SessionConfig.h>
 
+#include <AWSGameLiftClientLocalTicketTracker.h>
 #include <AWSGameLiftClientManager.h>
 #include <AWSGameLiftClientSystemComponent.h>
 #include <Request/AWSGameLiftAcceptMatchRequest.h>
@@ -59,10 +60,17 @@ namespace AWSGameLift
             behaviorContext->EBus<AWSGameLiftRequestBus>("AWSGameLiftRequestBus")
                 ->Attribute(AZ::Script::Attributes::Category, "AWSGameLift")
                 ->Event("ConfigureGameLiftClient", &AWSGameLiftRequestBus::Events::ConfigureGameLiftClient,
-                    {{{"Region", ""}}})
+                    { { { "Region", "" } } })
                 ->Event("CreatePlayerId", &AWSGameLiftRequestBus::Events::CreatePlayerId,
-                    {{{"IncludeBrackets", ""},
-                      {"IncludeDashes", ""}}});
+                    { { { "IncludeBrackets", "" },
+                        { "IncludeDashes", "" } } });
+
+            behaviorContext->EBus<AWSGameLiftMatchmakingEventRequestBus>("AWSGameLiftMatchmakingEventRequestBus")
+                ->Attribute(AZ::Script::Attributes::Category, "AWSGameLift")
+                ->Event("StartPolling", &AWSGameLiftMatchmakingEventRequestBus::Events::StartPolling,
+                    { { { "TicketId", "" },
+                        { "PlayerId", "" } } })
+                ->Event("StopPolling", &AWSGameLiftMatchmakingEventRequestBus::Events::StopPolling);
         }
     }
 
