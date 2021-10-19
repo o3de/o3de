34ec8cfc4706b4e7fa2e26e9e894ad239021783b
Junbo Liang <68558268+junbo75@users.noreply.github.com>
2021-10-19 10:30:02 -0700

    Add GameLift to the AWS menu (#4716)
    * Add GameLift to the AWS menu
    
    Signed-off-by: Junbo Liang <junbo@amazon.com>

--- Gems/AWSCore/Code/Include/Private/Editor/Constants/AWSCoreEditorMenuNames.h
+++ Gems/AWSCore/Code/Include/Private/Editor/Constants/AWSCoreEditorMenuNames.h
@@ -38,4 +38,14 @@ namespace AWSCore
     static constexpr const char AWSMetricsAPIReferenceActionText[] = "API reference";
     static constexpr const char AWSMetricsAdvancedTopicsActionText[] = "Advanced topics";
     static constexpr const char AWSMetricsSettingsActionText[] = "Metrics settings";
+
+    static constexpr const char AWSGameLiftActionText[] = "GameLift Gem";
+    static constexpr const char AWSGameLiftGemOverviewActionText[] = "Gem overview";
+    static constexpr const char AWSGameLiftGemSetupActionText[] = "Setup";
+    static constexpr const char AWSMGameLiftScriptingActionText[] = "Scripting reference";
+    static constexpr const char AWSGameLiftAPIReferenceActionText[] = "API reference";
+    static constexpr const char AWSGameLiftAdvancedTopicsActionText[] = "Advanced topics";
+    static constexpr const char AWSGameLiftLocalTestingActionText[] = "Local testing";
+    static constexpr const char AWSGameLiftBuildPackagingActionText[] = "Build packaging (Windows)";
+    static constexpr const char AWSGameLiftResourceManagementActionText[] = "Resource Management";
 } // namespace AWSCore
