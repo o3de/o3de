8e797982a5fe5792b39ec6e80c52cdc905796799
Vincent Liu <5900509+onecent1101@users.noreply.github.com>
2021-10-20 13:11:12 -0700

    [LYN-7530] Fix matchmaking request type typo and add more matchmaking notifications (#4774)
    * [LYN-7530] Fix matchmaking request type typo and add more matchmaking notifications
    
    Signed-off-by: onecent1101 <liug@amazon.com>

--- Code/Framework/AzFramework/AzFramework/Matchmaking/IMatchmakingRequests.h
+++ Code/Framework/AzFramework/AzFramework/Matchmaking/IMatchmakingRequests.h
@@ -43,7 +43,7 @@ namespace AzFramework
     class IMatchmakingAsyncRequests
     {
     public:
-        AZ_RTTI(ISessionAsyncRequests, "{53513480-2D02-493C-B44E-96AA27F42429}");
+        AZ_RTTI(IMatchmakingAsyncRequests, "{53513480-2D02-493C-B44E-96AA27F42429}");
 
         IMatchmakingAsyncRequests() = default;
         virtual ~IMatchmakingAsyncRequests() = default;
@@ -60,4 +60,31 @@ namespace AzFramework
         // @param  stopMatchmakingRequest The request of StopMatchmaking operation
         virtual void StopMatchmakingAsync(const StopMatchmakingRequest& stopMatchmakingRequest) = 0;
     };
+
+    //! MatchmakingAsyncRequestNotifications
+    //! The notifications correspond to matchmaking async requests
+    class MatchmakingAsyncRequestNotifications
+        : public AZ::EBusTraits
+    {
+    public:
+        // Safeguard handler for multi-threaded use case
+        using MutexType = AZStd::recursive_mutex;
+
+        //////////////////////////////////////////////////////////////////////////
+        // EBusTraits overrides
+        static const AZ::EBusHandlerPolicy HandlerPolicy = AZ::EBusHandlerPolicy::Multiple;
+        static const AZ::EBusAddressPolicy AddressPolicy = AZ::EBusAddressPolicy::Single;
+        //////////////////////////////////////////////////////////////////////////
+
+        // OnAcceptMatchAsyncComplete is fired once AcceptMatchAsync completes
+        virtual void OnAcceptMatchAsyncComplete() = 0;
+
+        // OnStartMatchmakingAsyncComplete is fired once StartMatchmakingAsync completes
+        // @param matchmakingTicketId The unique identifier for the matchmaking ticket
+        virtual void OnStartMatchmakingAsyncComplete(const AZStd::string& matchmakingTicketId) = 0;
+
+        // OnStopMatchmakingAsyncComplete is fired once StopMatchmakingAsync completes
+        virtual void OnStopMatchmakingAsyncComplete() = 0;
+    };
+    using MatchmakingAsyncRequestNotificationBus = AZ::EBus<MatchmakingAsyncRequestNotifications>;
 } // namespace AzFramework
--- Gems/AWSGameLift/Code/AWSGameLiftClient/Source/AWSGameLiftClientManager.h
+++ Gems/AWSGameLift/Code/AWSGameLiftClient/Source/AWSGameLiftClientManager.h
@@ -8,10 +8,13 @@
 
 #pragma once
 
+#include <AzCore/RTTI/BehaviorContext.h>
 #include <AzCore/std/smart_ptr/shared_ptr.h>
 #include <AzFramework/Matchmaking/MatchmakingNotifications.h>
 
-#include <Request/IAWSGameLiftRequests.h>
+#include <Request/AWSGameLiftRequestBus.h>
+#include <Request/AWSGameLiftSessionRequestBus.h>
+#include <Request/AWSGameLiftMatchmakingRequestBus.h>
 
 namespace AWSGameLift
 {
@@ -23,21 +26,36 @@ namespace AWSGameLift
     struct AWSGameLiftStartMatchmakingRequest;
     struct AWSGameLiftStopMatchmakingRequest;
 
-    // MatchAcceptanceNotificationBus EBus handler for scripting
-    class AWSGameLiftMatchAcceptanceNotificationBusHandler
-        : public AzFramework::MatchAcceptanceNotificationBus::Handler
+    // MatchmakingNotificationBus EBus handler for scripting
+    class AWSGameLiftMatchmakingNotificationBusHandler
+        : public AzFramework::MatchmakingNotificationBus::Handler
         , public AZ::BehaviorEBusHandler
     {
     public:
         AZ_EBUS_BEHAVIOR_BINDER(
-            AWSGameLiftMatchAcceptanceNotificationBusHandler,
+            AWSGameLiftMatchmakingNotificationBusHandler,
             "{CBE057D3-F5CE-46D3-B02D-8A6A1446B169}",
             AZ::SystemAllocator,
-            OnMatchAcceptance);
+            OnMatchAcceptance, OnMatchComplete, OnMatchError, OnMatchFailure);
 
         void OnMatchAcceptance() override
         {
             Call(FN_OnMatchAcceptance);
+        }
+
+        void OnMatchComplete() override
+        {
+            Call(FN_OnMatchComplete);
+        }
+
+        void OnMatchError() override
+        {
+            Call(FN_OnMatchError);
+        }
+
+        void OnMatchFailure() override
+        {
+            Call(FN_OnMatchFailure);
         }
     };
 
