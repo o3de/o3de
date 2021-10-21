2b51ba62eea16572b564593e75fd01398c5f750f
Adi-Amazon <Adi Bar-Lev 82479970+Adi-Amazon@users.noreply.github.com>
2021-10-21 10:10:11 -0400

    Hair - ShortCut rendering technique and pipeline with Marschner lighting model
    - This rendering technique reduces the memory required per pipeline by 750MB compared to the PPLL technique!!
    - Using 3 screen buffers representing the closest 3 hair gragments depths.
    - Going through second geometry pass that disqualify any fragment further than the third depth, the technique blends the three closes shaders hair fragments.
    - Supports the Marschner lighting model (big change from the original TressFX 4.1 implementation)
    - The current technique is almost twice the performance of the PPLL but not quite as advacned when come to final visual quality.
    
    Remarks:
    Unlike the PPLL, this technique is still lacking some of the advanced features added to the PPLL such as
    1. Back lobe (TT) conseal by depth comparison
    2. Thickness dependency in light transfer (mainly TT)
    3. Allowing TT transfer for thin separated hair strands (might be supported by default with no distinction)
    
    Signed-off-by: Adi-Amazon <Adi Bar-Lev 82479970+Adi-Amazon@users.noreply.github.com>
    Signed-off-by: Adi-Amazon <Adi Bar-Lev barlev@amazon.com>
    Signed-off-by: Adi-Amazon <Adi Bar-Lev 82479970+Adi-Amazon@users.noreply.github.com>
    Signed-off-by: Adi-Amazon <Adi Bar-Lev barlev@amazon.com>

---
+++ Gems/AtomTressFX/Code/Passes/HairShortCutGeometryShadingPass.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+#pragma once
+
+#include <Passes/HairGeometryRasterPass.h>
+#include <Rendering/HairGlobalSettings.h>
+
+namespace AZ
+{
+    namespace RHI
+    {
+        struct DrawItem;
+    }
+
+    namespace Render
+    {
+        namespace Hair
+        {
+            //! This geometry pass uses the following Srgs:
+            //!  - PerPassSrg shared by all hair passes for the shared dynamic buffer 
+            //!  - PerMaterialSrg - used solely by this pass to alter the vertices and apply the visual
+            //!     hair properties to each fragment.
+            //!  - HairDynamicDataSrg (PerObjectSrg) - shared buffers views for this hair object only.
+            //!  - PerViewSrg and PerSceneSrg - as per the data from Atom.
+            class HairShortCutGeometryShadingPass
+                : public HairGeometryRasterPass
+            {
+                AZ_RPI_PASS(HairShortCutGeometryShadingPass);
+
+            public:
+                AZ_RTTI(HairShortCutGeometryShadingPass, "{11BA673D-0788-4B25-978D-9737BF4E48FE}", HairGeometryRasterPass);
+                AZ_CLASS_ALLOCATOR(HairShortCutGeometryShadingPass, SystemAllocator, 0);
+
+                static RPI::Ptr<HairShortCutGeometryShadingPass> Create(const RPI::PassDescriptor& descriptor);
+
+                void CompileResources(const RHI::FrameGraphCompileContext& context) override;
+
+            protected:
+                AZ::Name o_enableShadows;
+                AZ::Name o_enableDirectionalLights;
+                AZ::Name o_enablePunctualLights;
+                AZ::Name o_enableAreaLights;
+                AZ::Name o_enableIBL;
+                AZ::Name o_hairLightingModel;
+                AZ::Name o_enableMarschner_R;
+                AZ::Name o_enableMarschner_TRT;
+                AZ::Name o_enableMarschner_TT;
+                AZ::Name o_enableLongtitudeCoeff;
+                AZ::Name o_enableAzimuthCoeff;
+
+                explicit HairShortCutGeometryShadingPass(const RPI::PassDescriptor& descriptor);
+
+                void UpdateGlobalShaderOptions();
+
+                // Pass behavior overrides
+                void BuildInternal() override;
+
+                HairGlobalSettings m_hairGlobalSettings;
+                AZ::RPI::ShaderVariantKey m_shaderOptions;
+            };
+
+        } // namespace Hair
+    } // namespace Render
+}   // namespace AZ
