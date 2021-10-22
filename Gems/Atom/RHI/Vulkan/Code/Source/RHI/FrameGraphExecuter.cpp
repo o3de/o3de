6544a2bc0c37b2e6ba7c104e6c2656ccaa26cc4e
moudgils <moudgils@amazon.com>
2021-10-22 10:52:58 -0700

    Minor perf improvement
    Signed-off-by: moudgils <moudgils@amazon.com>

--- Gems/Atom/RHI/Vulkan/Code/Source/RHI/FrameGraphExecuter.cpp
+++ Gems/Atom/RHI/Vulkan/Code/Source/RHI/FrameGraphExecuter.cpp
@@ -178,7 +178,7 @@ namespace AZ
             AZ_Assert(findIter != m_groupHandlers.end(), "Could not find group handler for groupId %d", group.GetGroupId().GetIndex());
             FrameGraphExecuteGroupHandlerBase* handler = findIter->second.get();
             // Wait until all execute groups of the handler has finished and also make sure that the handler itself hasn't executed already (which is possible for parallel encoding).
-            if (handler->IsComplete() && !handler->IsExecuted())
+            if (!handler->IsExecuted() && handler->IsComplete())
             {
                 // This will execute the recorded work into the queue.
                 handler->End();
