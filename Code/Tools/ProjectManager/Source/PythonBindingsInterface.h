63c3c2570d176ad93c7b622cd845a3c6346b0327
AMZN-alexpete <26804013+AMZN-alexpete@users.noreply.github.com>
2021-10-21 19:04:29 -0700

    Fix comment
    Signed-off-by: AMZN-alexpete <26804013+AMZN-alexpete@users.noreply.github.com>

--- Code/Tools/ProjectManager/Source/PythonBindingsInterface.h
+++ Code/Tools/ProjectManager/Source/PythonBindingsInterface.h
@@ -92,7 +92,7 @@ namespace O3DE::ProjectManager
         virtual AZ::Outcome<QVector<AZStd::string>, AZStd::string> GetEnabledGemNames(const QString& projectPath) = 0;
 
         /**
-         * Registers the gem to the specified project, or to the engine if no project path is given
+         * Registers the gem to the specified project, or to the o3de_manifest.json if no project path is given
          * @param gemPath the path to the gem
          * @param projectPath the path to the project. If empty, will register the external path in o3de_manifest.json 
          * @return An outcome with the success flag as well as an error message in case of a failure.
