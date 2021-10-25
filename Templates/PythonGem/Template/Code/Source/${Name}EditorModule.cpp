f162ae2dc0f49a56d9c2a7c397de77d396121857
Jonny Galloway <gallowj@amazon.com>
2021-10-25 18:18:27 -0500

    Scaffold PythonGem template (#4888)
    * Renamed ctest_pytest.ini to pytest.ini so it is used by default, added TestSuite_ as collection file (#4822)
    
    * Fixed warnings of unused marks, renamed ctest_pytest.ini to pytest.ini to better consistency on runs
    
    * Fixed some test suites to run propertly
    
    * Fix missing arguments
    
    * Fixed missing cmakelists and renamed missing file
    
    * Temp disable editor_testing_tests as timeout in jenkins
    
    Signed-off-by: Jonny Gallowy <gallowj@amazon.com>
    
    * scaffold PythonGem template
    
    Signed-off-by: Jonny Gallowy <gallowj@amazon.com>
    
    * CustomTool was not added to CMakeLists
    
    Signed-off-by: Jonny Gallowy <gallowj@amazon.com>
    
    * Update Templates/PythonGem/Template/Code/CMakeLists.txt
    
    makes sense thank you for the help
    
    Co-authored-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
    Signed-off-by: Jonny Gallowy <gallowj@amazon.com>
    
    * Update Templates/PythonGem/Template/Code/CMakeLists.txt
    
    Co-authored-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
    Signed-off-by: Jonny Gallowy <gallowj@amazon.com>
    
    * Delete  ${NameLower}_tests_files.cmake
    
    Signed-off-by: Jonny Gallowy <gallowj@amazon.com>
    
    * suggested changes made
    
    Signed-off-by: Jonny Gallowy <gallowj@amazon.com>
    
    * cleanup not needed files
    
    Signed-off-by: Jonny Gallowy <gallowj@amazon.com>
    
    * fix up template.json
    
    Signed-off-by: Jonny Gallowy <gallowj@amazon.com>
    
    * made a correction
    
    Signed-off-by: Jonny Gallowy <gallowj@amazon.com>
    
    * made a correction
    
    Signed-off-by: Jonny Gallowy <gallowj@amazon.com>
    
    * fixes
    
    Signed-off-by: Jonny Gallowy <gallowj@amazon.com>
    
    * fixes
    
    Signed-off-by: Jonny Gallowy <gallowj@amazon.com>
    
    * more fixes, fixed a file name
    
    Signed-off-by: Jonny Gallowy <gallowj@amazon.com>
    
    * Added helper method to az_qt_helpers for retrieving the main window instance.
    
    Signed-off-by: Chris Galvan <chgalvan@amazon.com>
    
    * additional fixes
    
    Signed-off-by: Jonny Gallowy <gallowj@amazon.com>
    
    * Fix for AP trowing errors at boot (can not get mainwindow)
    
    Signed-off-by: Jonny Gallowy <gallowj@amazon.com>
    
    * Added missing EBus connect/disconnect calls in the EditorSystemComponent
    
    Signed-off-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
    
    * additional fix up
    
    Signed-off-by: Jonny Gallowy <gallowj@amazon.com>
    
    * adding dialog internals
    
    Signed-off-by: Jonny Gallowy <gallowj@amazon.com>
    
    Co-authored-by: AMZN-AlexOteiza <82234181+AMZN-AlexOteiza@users.noreply.github.com>
    Co-authored-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
    Co-authored-by: Chris Galvan <chgalvan@amazon.com>
    Signed-off-by: Gene Walters <genewalt@amazon.com>

---
+++ Templates/PythonGem/Template/Code/Source/${Name}EditorModule.cpp
@@ -0,0 +1,47 @@
+// {BEGIN_LICENSE}
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+// {END_LICENSE}
+
+#include <${Name}ModuleInterface.h>
+#include <${Name}EditorSystemComponent.h>
+
+namespace ${SanitizedCppName}
+{
+    class ${SanitizedCppName}EditorModule
+        : public ${SanitizedCppName}ModuleInterface
+    {
+    public:
+        AZ_RTTI(${SanitizedCppName}EditorModule, "${ModuleClassId}", ${SanitizedCppName}ModuleInterface);
+        AZ_CLASS_ALLOCATOR(${SanitizedCppName}EditorModule, AZ::SystemAllocator, 0);
+
+        ${SanitizedCppName}EditorModule()
+        {
+            // Push results of [MyComponent]::CreateDescriptor() into m_descriptors here.
+            // Add ALL components descriptors associated with this gem to m_descriptors.
+            // This will associate the AzTypeInfo information for the components with the the SerializeContext, BehaviorContext and EditContext.
+            // This happens through the [MyComponent]::Reflect() function.
+            m_descriptors.insert(m_descriptors.end(), {
+                ${SanitizedCppName}EditorSystemComponent::CreateDescriptor(),
+            });
+        }
+
+        /**
+         * Add required SystemComponents to the SystemEntity.
+         * Non-SystemComponents should not be added here
+         */
+        AZ::ComponentTypeList GetRequiredSystemComponents() const override
+        {
+            return AZ::ComponentTypeList {
+                azrtti_typeid<${SanitizedCppName}EditorSystemComponent>(),
+            };
+        }
+    };
+}// namespace ${SanitizedCppName}
+
+AZ_DECLARE_MODULE_CLASS(Gem_${SanitizedCppName}, ${SanitizedCppName}::${SanitizedCppName}EditorModule)
