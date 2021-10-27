c0395f61e4d3006507a2d6827f48011665fdaac3
puvvadar <puvvadar@amazon.com>
2021-10-27 11:25:25 -0700

    Switch AzGenericTypeInfo to auto and add specialization for QuantizedValues
    Signed-off-by: puvvadar <puvvadar@amazon.com>

--- Code/Framework/AzCore/AzCore/RTTI/TypeInfo.h
+++ Code/Framework/AzCore/AzCore/RTTI/TypeInfo.h
@@ -149,11 +149,12 @@ namespace AZ
     {
         /// Needs to match declared parameter type.
         template <template <typename...> class> constexpr bool false_v1 = false;
-        template <template <AZStd::size_t...> class> constexpr bool false_v2 = false;
-        template <template <typename, AZStd::size_t> class> constexpr bool false_v3 = false;
-        template <template <typename, typename, AZStd::size_t> class> constexpr bool false_v4 = false;
-        template <template <typename, typename, typename, AZStd::size_t> class> constexpr bool false_v5 = false;
-        template <template <typename, AZStd::size_t, typename> class> constexpr bool false_v6 = false;
+        template <template <auto...> class> constexpr bool false_v2 = false;
+        template <template <typename, auto> class> constexpr bool false_v3 = false;
+        template <template <typename, typename, auto> class> constexpr bool false_v4 = false;
+        template <template <typename, typename, typename, auto> class> constexpr bool false_v5 = false;
+        template <template <typename, auto, typename> class> constexpr bool false_v6 = false;
+        template <template <AZStd::size_t, AZStd::size_t, AZStd::ptrdiff_t, AZStd::ptrdiff_t> class> constexpr bool false_v7 = false;
 
         template<typename T>
         inline const AZ::TypeId& Uuid()
@@ -169,7 +170,7 @@ namespace AZ
             return s_uuid;
         }
 
-        template<template<AZStd::size_t...> class T>
+        template<template<auto...> class T>
         inline const AZ::TypeId& Uuid()
         {
             static_assert(false_v2<T>, "Missing specialization for this template. Make sure it's registered for type info support.");
@@ -177,7 +178,7 @@ namespace AZ
             return s_uuid;
         }
 
-        template<template<typename, AZStd::size_t> class T>
+        template<template<typename, auto> class T>
         inline const AZ::TypeId& Uuid()
         {
             static_assert(false_v3<T>, "Missing specialization for this template. Make sure it's registered for type info support.");
@@ -185,7 +186,7 @@ namespace AZ
             return s_uuid;
         }
 
-        template<template<typename, typename, AZStd::size_t> class T>
+        template<template<typename, typename, auto> class T>
         inline const AZ::TypeId& Uuid()
         {
             static_assert(false_v4<T>, "Missing specialization for this template. Make sure it's registered for type info support.");
@@ -193,7 +194,7 @@ namespace AZ
             return s_uuid;
         }
 
-        template<template<typename, typename, typename, AZStd::size_t> class T>
+        template<template<typename, typename, typename, auto> class T>
         inline const AZ::TypeId& Uuid()
         {
             static_assert(false_v5<T>, "Missing specialization for this template. Make sure it's registered for type info support.");
@@ -201,13 +202,22 @@ namespace AZ
             return s_uuid;
         }
 
-        template<template<typename, AZStd::size_t, typename> class T>
+        template<template<typename, auto, typename> class T>
         inline const AZ::TypeId& Uuid()
         {
             static_assert(false_v6<T>, "Missing specialization for this template. Make sure it's registered for type info support.");
             static const AZ::TypeId s_uuid = AZ::TypeId::CreateNull();
             return s_uuid;
         }
+
+        template<template<AZStd::size_t, AZStd::size_t, AZStd::ptrdiff_t, AZStd::ptrdiff_t> class T>
+        inline const AZ::TypeId& Uuid()
+        {
+            static_assert(false_v7<T>, "Missing specialization for this template. Make sure it's registered for type info support.");
+            static const AZ::TypeId s_uuid = AZ::TypeId::CreateNull();
+            return s_uuid;
+        }
+
     }
 
     namespace Internal
