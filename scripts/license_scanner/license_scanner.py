932c29c58d473340f47dc4813ca24b54993e276a
Mike Chang <changml@amazon.com>
2021-11-01 01:43:34 -0700

    Added package creation function and config file. Generalized variables in scan method
    Signed-off-by: Mike Chang <changml@amazon.com>

--- scripts/license_scanner/license_scanner.py
+++ scripts/license_scanner/license_scanner.py
@@ -28,7 +28,7 @@ class LicenseScanner:
     def __init__(self, config_file=None):
         self.config_file = config_file
         self.config_data = self._load_config()
-        self.license_regex = self._load_license_regex()
+        self.file_regex = self._load_file_regex()
 
     def _load_config(self):
         """Load config from the provided file. Sets default file if one is not provided."""
@@ -43,45 +43,53 @@ class LicenseScanner:
             print('Config file cannot be found')
             raise
 
-    def _load_license_regex(self):
+    def _load_file_regex(self):
         """Returns regex object with case-insensitive matching from the list of filename patterns."""
         regex_patterns = []
-        for pattern in self.config_data['license_patterns']:
-            regex_patterns.append(fnmatch.translate(pattern))
+        if 'license_patterns' in self.config_data:
+            for pattern in self.config_data['license_patterns']:
+                regex_patterns.append(fnmatch.translate(pattern))
+        if 'package_patterns' in self.config_data:
+            for pattern in self.config_data['package_patterns']:
+                regex_patterns.append(fnmatch.translate(pattern))
+        else:
+            print('Invalid key values found in config file! Use "license_patterns" for licenses, "package_patterns" for 3p packages')
+            raise KeyError
+
         return re.compile('|'.join(regex_patterns), re.IGNORECASE)
 
     def scan(self, path=os.curdir):
-        """Scan directory tree for filenames matching license_regex.
+        """Scan directory tree for filenames matching file_regex.
 
         :param path: Path of the directory to run scanner
-        :return: Package paths and their corresponding license file contents
+        :return: Package paths and their corresponding file contents
         :rtype: dict
         """
-        licenses = 0
-        license_files = {}
+        files = 0
+        matching_files = {}
 
         for dirpath, dirnames, filenames in os.walk(path):
             for file in filenames:
-                if self.license_regex.match(file):
-                    license_file_content = self._get_license_file_contents(os.path.join(dirpath, file))
+                if self.file_regex.match(file):
+                    matching_file_content = self._get_file_contents(os.path.join(dirpath, file))
                     rel_dirpath = os.path.relpath(dirpath, path)  # Limit path inside scanned directory
-                    license_files[rel_dirpath] = license_file_content
-                    licenses += 1
-                    print(f'License file: {os.path.join(dirpath, file)}')
+                    matching_files[rel_dirpath] = matching_file_content
+                    files += 1
+                    print(f'Matching file: {os.path.join(dirpath, file)}')
 
             # Remove directories that should not be scanned
             for dir in self.config_data['excluded_directories']:
                 if dir in dirnames:
                     dirnames.remove(dir)
-        print(f'{licenses} license files found.')
-        return license_files
+        print(f'{files} files found.')
+        return matching_files
 
-    def _get_license_file_contents(self, filepath):
+    def _get_file_contents(self, filepath):
         try:
             with open(filepath, encoding='utf8') as f:
                 return f.read()
         except UnicodeDecodeError:
-            print(f'Unable to read license file: {filepath}')
+            print(f'Unable to read file: {filepath}')
             pass
 
     def create_license_file(self, licenses, filepath='NOTICES.txt'):
@@ -89,18 +97,43 @@ class LicenseScanner:
 
         :param licenses: Dict with package paths and their corresponding license file contents
         :param filepath: Path to write the file
-       """
-        package_separator = '------------------------------------'
-        with open(filepath, 'w', encoding='utf8') as f:
+        """
+        license_separator = '------------------------------------'
+        with open(filepath, 'w', encoding='utf8') as lf:
             for directory, license in licenses.items():
                 license_output = '\n\n'.join([
-                    f'{package_separator}',
+                    f'{license_separator}',
                     f'Package path: {directory}',
                     'License:',
                     f'{license}\n'
                 ])
-                f.write(license_output)
+                lf.write(license_output)
         return None
+    
+    def create_package_file(self, packages, filepath='SPDX-Licenses.json', dirpath=None):
+        """Creates file with all the provided SPDX package info summaries.
+
+        :param licenses: Dict with package paths and their corresponding license file contents
+        :param filepath: Path to write the file
+        :param dirpath: Root path for packages
+        :rtype: dict
+        """
+        licenses = {}
+        package_separator = ','
+        with open(filepath, 'w', encoding='utf8') as pf:
+            for directory, package in packages.items():
+                package_obj = json.loads(package)
+                package_json = json.dumps(package_obj, indent=4)
+                package_output = ''.join([
+                    f'{package_json}',
+                    f'{package_separator}\n'
+                ])
+                pf.write(package_output)
+                if dirpath:
+                    license_path = os.path.join(directory, pathlib.Path(package_obj['LicenseFile'])) # Gets relative path
+                    license_content = self._get_file_contents(os.path.join(dirpath, license_path))
+                    licenses[license_path] = license_content
+        return licenses
 
 
 def parse_args():
@@ -108,6 +141,7 @@ def parse_args():
         description='Script to run LicenseScanner and generate license file')
     parser.add_argument('--config-file', '-c', type=pathlib.Path, help='Config file for LicenseScanner')
     parser.add_argument('--license-file-path', '-l', type=pathlib.Path, help='Create license file in the provided path')
+    parser.add_argument('--package-file-path', '-p', type=pathlib.Path, help='Create package summary file in the provided path')
     parser.add_argument('--scan-path', '-s', default=os.curdir, type=pathlib.Path, help='Path to scan')
     return parser.parse_args()
 
@@ -116,10 +150,15 @@ def main():
     try:
         args = parse_args()
         ls = LicenseScanner(args.config_file)
-        licenses = ls.scan(args.scan_path)
+        scanned_path_data = ls.scan(args.scan_path)
 
         if args.license_file_path:
-            ls.create_license_file(licenses, args.license_file_path)
+            ls.create_license_file(scanned_path_data, args.license_file_path)
+        if args.package_file_path:
+            ls.create_package_file(scanned_path_data, args.package_file_path)
+        if args.license_file_path and args.package_file_path:
+            license_files = ls.create_package_file(scanned_path_data, args.package_file_path, args.scan_path)
+            ls.create_license_file(license_files, args.license_file_path)
     except FileNotFoundError as e:
         print(f'Type: {type(e).__name__}, Error: {e}')
         return 1
