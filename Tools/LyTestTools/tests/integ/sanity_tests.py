2a96a8ac7fa10ab07f00091844c1865b991235be
sweeneys <sweeneys@amazon.com>
2021-11-10 15:16:15 -0800

    Revert changes to pipeline and sanity tests, update unit test
    Signed-off-by: sweeneys <sweeneys@amazon.com>

--- Tools/LyTestTools/tests/integ/sanity_tests.py
+++ Tools/LyTestTools/tests/integ/sanity_tests.py
@@ -9,8 +9,6 @@ Launch the windows launcher attached to the currently installed instance.
 """
 # Import any dependencies for the test.
 import logging
-import subprocess
-
 import pytest
 
 # Import any desired LTT modules from the package `ly_test_tools`. All LTT modules can be viewed at `Tools/LyTestTools/ly_test_tools`.
@@ -22,7 +20,6 @@ import ly_test_tools.builtin.helpers as helpers
 # The `environment` module contains tools that involve the system's environment such as processes or timed waiters.
 import ly_test_tools.environment.process_utils as process_utils
 import ly_test_tools.environment.waiter as waiter
-import ly_test_tools.o3de.asset_processor as ap
 
 # Initialize a logger instance to hook all test logs together. The sub-logger pattern below makes it easy to track which file creates a log line.
 logger = logging.getLogger(__name__)
@@ -47,7 +44,6 @@ class TestAutomatedTestingProject(object):
         test to run on instances without a GPU. We launch the game client executable and wait for the process to exist.
         A try/finally block ensures proper test cleanup if issues occur during the test.
         """
-        ap_pre_exists = process_utils.process_exists("AssetProcessor", ignore_extensions=True)
 
         # Kill processes that may interfere with the test
         process_utils.kill_processes_named(names=process_utils.LY_PROCESS_KILL_LIST, ignore_extensions=True)
@@ -68,8 +64,6 @@ class TestAutomatedTestingProject(object):
             # Clean up processes after the test is finished
             process_utils.kill_processes_named(names=process_utils.LY_PROCESS_KILL_LIST, ignore_extensions=True)
 
-        assert not ap_pre_exists
-
     def test_StartEditor_Sanity(self, project):
         """
         The `test_StartEditor_Sanity` test function is similar to the previous example with minor adjustments. A
@@ -78,7 +72,6 @@ class TestAutomatedTestingProject(object):
         `-autotest_mode` arg supresses modal dialogs from interfering with our test. We launch the Editor executable and
         wait for the process to exist.
         """
-        ap_pre_exists = process_utils.process_exists("AssetProcessor", ignore_extensions=True)
 
         # Kill processes that may interfere with the test
         process_utils.kill_processes_named(names=process_utils.LY_PROCESS_KILL_LIST, ignore_extensions=True)
@@ -98,129 +91,3 @@ class TestAutomatedTestingProject(object):
         finally:
             # Clean up processes after the test is finished
             process_utils.kill_processes_named(names=process_utils.LY_PROCESS_KILL_LIST, ignore_extensions=True)
-
-        assert not ap_pre_exists
-
-    def test_StartAP_Sanity(self, project):
-
-        ap_pre_exists = process_utils.process_exists("AssetProcessor", ignore_extensions=True)
-
-        # Kill processes that may interfere with the test
-        process_utils.kill_processes_named(names=process_utils.LY_PROCESS_KILL_LIST, ignore_extensions=True)
-
-        try:
-            # Create the Workspace object
-            workspace = helpers.create_builtin_workspace(project=project)
-
-            ap_manager = ap.AssetProcessor(workspace)
-
-            # start
-            ap_manager.create_temp_log_root()
-
-            import os
-            import time
-
-            """
-            
-            def start(self, connection_timeout=30, quitonidle=False,
-                      add_gem_scan_folders=None,
-                      add_config_scan_folders=None,
-                      accept_input=True, run_until_idle=False,
-                      scan_folder_pattern=None, connect_to_ap=False):
-            def gui_process(self, timeout=30, fastscan=True,
-                            capture_output=False, platforms=None,
-                            extra_params=None,
-                            decode=True,
-                            expect_failure=False, quitonidle=False,
-                            accept_input=True):
-            """
-
-            command = ap_manager.build_ap_command(
-                ap_path=os.path.abspath(workspace.paths.asset_processor()),
-                fastscan=True,
-                platforms=None,  # but this apparently adds the params in extra params
-                extra_params=["--acceptInput", "--platforms", "linux", "--logDir", ap_manager._temp_log_root],
-                add_gem_scan_folders=None,
-                add_config_scan_folders=None,
-                scan_folder_pattern=None)
-
-            ap_exe_path = os.path.dirname(workspace.paths.asset_processor())
-
-            ap_proc = subprocess.Popen(command, cwd=ap_exe_path, env=process_utils.get_display_env(), stdout=subprocess.PIPE)
-            time.sleep(1)
-            if ap_proc.poll() is not None:
-                raise RuntimeError(f"AssetProcessor immediately quit with errorcode {ap_proc.returncode}")
-
-            port = None
-
-            def _get_port_from_log():
-                nonlocal port
-                if not os.path.exists(workspace.paths.ap_gui_log()):
-                    logger.error("No GUI log at expected path")
-                    return False
-
-                log = ap.APLogParser(workspace.paths.ap_gui_log())
-                if len(log.runs):
-                    try:
-                        port = log.runs[-1]["Control Port"]
-                        logger.error(f"Successfully read Control Port {port}")
-                        return True
-                    except Exception as ex:  # intentionally broad
-                        logger.error("Failed to read port from file", exc_info=ex)
-                return False
-
-            waiter.wait_for(_get_port_from_log, timeout=10)
-
-            import socket
-            connection_socket = None
-
-            def _attempt_connection():
-                nonlocal port, connection_socket
-                if ap_proc.poll() is not None:
-                    raise RuntimeError(
-                        f"Asset processor exited early with errorcode: {ap_proc.returncode}")
-
-                connection_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                connection_socket.settimeout(60)
-                try:
-                    connection_socket.connect(('127.0.0.1', port))
-                    return True
-                except Exception as ex:  # Purposefully broad
-                    logger.error(f"Failed to connect", exc_info=ex)
-                return False
-
-            waiter.wait_for(_attempt_connection, timeout=60)
-
-            # false default, so ignoring self.connect_listen()
-
-            # wait for idle
-            #self.send_message("waitforidle")
-            connection_socket.sendall("waitforidle".encode())
-            logger.error("Mesage sent")
-            connection_socket.settimeout(300)
-            logger.error("Waiting for result")
-            result = connection_socket.recv(4096).decode()
-            assert result == "idle", f"Did not get idle state from AP, message was instead: {result}"
-
-        except Exception:
-            if ap_proc.poll() is not None:
-                raise RuntimeError("Unexpectedly exited early")
-
-            ret = subprocess.run("sudo netstat -tulpn | grep LISTEN", capture_output=True, shell=True, timeout=10)
-            logger.error(f"Current port status:\n{ret.stdout}")
-
-            output = ""
-            linecount = 0
-            for line in iter(ap_proc.stdout.readline, ''):
-                output += f"{line.rstrip()}\n"
-                logger.error(line.rstrip())
-                linecount += 1
-                if linecount > 200:
-                    break
-            raise RuntimeError(f"Error during AP test, with output:\n{output}")
-
-        finally:
-            # Clean up processes after the test is finished
-            process_utils.kill_processes_named(names=process_utils.LY_PROCESS_KILL_LIST, ignore_extensions=True)
-
-        assert not ap_pre_exists
