7820e46be1a53e5c3e80e941c76af4198857eb01
changml <changml@amazon.com>
2021-11-12 03:24:28 -0800

    Add test for 3p path
    Signed-off-by: changml <changml@amazon.com>

--- cmake/Packaging.cmake
+++ cmake/Packaging.cmake
@@ -54,9 +54,7 @@ set(DEFAULT_LICENSE_FILE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE.txt")
 set(CPACK_RESOURCE_FILE_LICENSE ${DEFAULT_LICENSE_FILE})
 set(CPACK_LICENSE_URL ${LY_INSTALLER_LICENSE_URL})
 
-file(REAL_PATH "${CMAKE_CURRENT_SOURCE_DIR}/.." _root_path)
 set(CPACK_PACKAGE_INSTALL_DIRECTORY "${CPACK_PACKAGE_NAME}/${CPACK_PACKAGE_VERSION}")
-set(CPACK_LY_3P_PACKAGE_DIRECTORY "${_root_path}/3rdParty/packages")
 
 # neither of the SOURCE_DIR variables equate to anything during execution of pre/post build scripts
 set(CPACK_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/cmake)
@@ -141,10 +139,15 @@ ly_install(FILES ${_cmake_package_dest}
 )
 
 # Scan the engine and 3rd Party folders for licenses
+if (EXISTS ${LY_3RDPARTY_PATH}/packages)
+    set(CPACK_LY_3P_PACKAGE_DIRECTORY ${LY_3RDPARTY_PATH}/packages)
+else()
+    file(REAL_PATH "${CMAKE_CURRENT_SOURCE_DIR}/.." _root_path)
+    set(CPACK_LY_3P_PACKAGE_DIRECTORY "${_root_path}/3rdParty/packages")
 file(TO_NATIVE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/python/python.cmd" _python_cmd)
 file(TO_NATIVE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/scripts/license_scanner/license_scanner.py" _license_script)
 file(TO_NATIVE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/scripts/license_scanner/scanner_config.json" _license_config_script)
-set(_license_scan_path "${CMAKE_CURRENT_SOURCE_DIR} ${CPACK_LY_3P_PACKAGE_DIRECTORY}")
+set(_license_scan_path "${CMAKE_CURRENT_SOURCE_DIR}" "${CPACK_LY_3P_PACKAGE_DIRECTORY}")
 set(CPACK_3P_LICENSE_FILE "${CPACK_BINARY_DIR}/NOTICES.txt")
 set(CPACK_3P_MANIFEST_FILE "${CPACK_BINARY_DIR}/SPDX-License.json")
 
@@ -158,7 +161,7 @@ set(_license_command
 
 message(STATUS "Scanning for license files in ${_license_scan_path}")
 execute_process(
-    COMMAND ${_license_command} --scan-path "${CMAKE_CURRENT_SOURCE_DIR}" "${CPACK_LY_3P_PACKAGE_DIRECTORY}"
+    COMMAND ${_license_command} --scan-path ${_license_scan_path}
     RESULT_VARIABLE _license_result
     ERROR_VARIABLE _license_errors
     OUTPUT_VARIABLE _license_output
