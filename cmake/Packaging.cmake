8da3f0299945e7c5b3476debeb7dd3df8b1d73a7
changml <changml@amazon.com>
2021-11-12 02:06:13 -0800

    Fix typo
    Signed-off-by: changml <changml@amazon.com>

--- cmake/Packaging.cmake
+++ cmake/Packaging.cmake
@@ -166,7 +166,7 @@ execute_process(
 
 if(NOT ${_license_result} EQUAL 0)
     message(FATAL_ERROR "An error occurred during license scan.  ${_license_errors}")
-else
+else()
     set(CPACK_RESOURCE_FILE_LICENSE ${CPACK_3P_LICENSE_FILE})
 endif()
 
