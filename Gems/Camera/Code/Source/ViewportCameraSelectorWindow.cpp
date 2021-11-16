33c8d5677d8a860f9e2f2626eca0edfbca94be97
AMZN-Igarri <82394219+AMZN-Igarri@users.noreply.github.com>
2021-11-16 15:06:08 +0000

    fixed case formatting
    Signed-off-by: AMZN-Igarri <82394219+AMZN-Igarri@users.noreply.github.com>

--- Gems/Camera/Code/Source/ViewportCameraSelectorWindow.cpp
+++ Gems/Camera/Code/Source/ViewportCameraSelectorWindow.cpp
@@ -264,24 +264,24 @@ namespace Camera
         {
             switch (cursorAction)
             {
-                case CursorAction::MoveUp:
-                {
-                    return GetPreviousIndex();
-                }
-                case CursorAction::MoveDown:
-                {
-                    return GetNextIndex();
-                }
-                case CursorAction::MoveNext:
-                {
-                    return GetNextIndex();
-                }
-                case CursorAction::MovePrevious:
-                {
-                    return GetPreviousIndex();
-                }
-                default:
-                    return currentIndex();
+            case CursorAction::MoveUp:
+            {
+                return GetPreviousIndex();
+            }
+            case CursorAction::MoveDown:
+            {
+                return GetNextIndex();
+            }
+            case CursorAction::MoveNext:
+            {
+                return GetNextIndex();
+            }
+            case CursorAction::MovePrevious:
+            {
+                return GetPreviousIndex();
+            }
+            default:
+                return currentIndex();
             }
         }
 
