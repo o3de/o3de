0e239881cbb4f09278abff85c08a6f43989cef68
Vincent Liu <5900509+onecent1101@users.noreply.github.com>
2021-11-16 12:14:05 -0800

    Add cvar for aws log level (#5507)
    * Add cvar for aws log level
    
    Signed-off-by: onecent1101 <liug@amazon.com>
    
    * Remove old crysystem registered cmd and add safeguard to get cvar from console
    
    Signed-off-by: onecent1101 <liug@amazon.com>
    
    * Suppress error for client auth unit test as logging was routed to warning before
    
    Signed-off-by: onecent1101 <liug@amazon.com>

--- Gems/AWSGameLift/Code/AWSGameLiftClient/Source/AWSGameLiftClientManager.cpp
+++ Gems/AWSGameLift/Code/AWSGameLiftClient/Source/AWSGameLiftClientManager.cpp
@@ -33,7 +33,7 @@
 
 namespace AWSGameLift
 {
-#if defined(AWSGAMELIFT_DEV)
+#if defined(AZ_DEBUG_BUILD) || defined(AZ_PROFILE_BUILD)
     AZ_CVAR(AZ::CVarFixedString, cl_gameliftLocalEndpoint, "", nullptr, AZ::ConsoleFunctorFlags::Null, "The local endpoint to test with GameLiftLocal SDK.");
 #endif
 
@@ -87,7 +87,7 @@ namespace AWSGameLift
 
         // Set up client endpoint or region
         AZStd::string localEndpoint = "";
-#if defined(AWSGAMELIFT_DEV)
+#if defined(AZ_DEBUG_BUILD) || defined(AZ_PROFILE_BUILD)
         localEndpoint = static_cast<AZ::CVarFixedString>(cl_gameliftLocalEndpoint);
 #endif
         if (!localEndpoint.empty())
