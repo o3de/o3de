b45ae57636eff5486c41f6c65fe80ac3e115ceeb
Esteban Papp <81431996+amznestebanpapp@users.noreply.github.com>
2021-11-16 11:24:44 -0800

    uploads to S3 from the upload directory and fixes the latest upload
    Signed-off-by: Esteban Papp <81431996+amznestebanpapp@users.noreply.github.com>

--- cmake/Platform/Linux/PackagingPostBuild_linux.cmake
+++ cmake/Platform/Linux/PackagingPostBuild_linux.cmake
@@ -13,21 +13,32 @@ include(${LY_ROOT_FOLDER}/cmake/Platform/Common/PackagingPostBuild_common.cmake)
 
 if(CPACK_UPLOAD_URL)
 
+    # use the internal default path if somehow not specified from cpack_configure_downloads
+    if(NOT CPACK_UPLOAD_DIRECTORY)
+        set(CPACK_UPLOAD_DIRECTORY ${CPACK_PACKAGE_DIRECTORY}/CPackUploads)
+    endif()
+
     ly_upload_to_url(
         ${CPACK_UPLOAD_URL}
-        ${CPACK_TOPLEVEL_DIRECTORY}
+        ${CPACK_UPLOAD_DIRECTORY}
         ".*(deb|asc|.md5)$"
     )
 
     # for auto tagged builds, we will also upload a second copy of just the boostrapper
     # to a special "Latest" folder under the branch in place of the commit date/hash
     if(CPACK_AUTO_GEN_TAG)
-        ly_upload_to_latest(${CPACK_UPLOAD_URL} ${_bootstrap_output_file})
+
+        set(latest_deb_package "${CPACK_UPLOAD_DIRECTORY}/CPackUploads/${CPACK_PACKAGE_NAME}_latest.deb")
+        file(COPY_FILE ${CPACK_UPLOAD_DIRECTORY}/CPackUploads/${CPACK_PACKAGE_FILE_NAME}.deb
+            DESTINATION ${latest_deb_package}
+        )
+        ly_upload_to_latest(${CPACK_UPLOAD_URL} ${latest_deb_package})
         
         # replace the name fo the binary inside the generated checksum
-        file(READ "${CPACK_TOPLEVEL_DIRECTORY}/${CPACK_PACKAGE_FILE_NAME}.md5" _checksum_contents)
+        file(READ "${CPACK_UPLOAD_DIRECTORY}/CPackUploads/${CPACK_PACKAGE_FILE_NAME}.deb.md5" _checksum_contents)
         string(REPLACE "_${CPACK_PACKAGE_VERSION}" "" non_versioned_checksum_contents ${_checksum_contents})
-        file(WRITE "${CPACK_BINARY_DIR}/${CPACK_PACKAGE_NAME}.md5" "${non_versioned_checksum_contents}")
-        ly_upload_to_latest(${CPACK_UPLOAD_URL} "${CPACK_BINARY_DIR}/${CPACK_PACKAGE_NAME}.md5")
+        set(latest_md5_file "${CPACK_UPLOAD_DIRECTORY}/CPackUploads/${CPACK_PACKAGE_NAME}_latest.deb.md5")
+        file(WRITE "${latest_md5_file}" "${non_versioned_checksum_contents}")
+        ly_upload_to_latest(${CPACK_UPLOAD_URL} "${latest_md5_file}")
     endif()
 endif()
