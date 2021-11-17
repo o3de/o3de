cd7c069fbbbdf5089435751b578a19d051a5a4dd
Esteban Papp <81431996+amznestebanpapp@users.noreply.github.com>
2021-11-16 17:30:33 -0800

    Removes FilteredComponentList from Code/Editor/Plugins/ComponentEntityEditorPlugin
    Signed-off-by: Esteban Papp <81431996+amznestebanpapp@users.noreply.github.com>

--- Code/Editor/Plugins/ComponentEntityEditorPlugin/componententityeditorplugin_files.cmake
+++ Code/Editor/Plugins/ComponentEntityEditorPlugin/componententityeditorplugin_files.cmake
@@ -25,8 +25,6 @@ set(FILES
     UI/ComponentPalette/ComponentDataModel.h
     UI/ComponentPalette/ComponentDataModel.cpp
     UI/ComponentPalette/ComponentPaletteSettings.h
-    UI/ComponentPalette/FilteredComponentList.h
-    UI/ComponentPalette/FilteredComponentList.cpp
     UI/Outliner/OutlinerDisplayOptionsMenu.h
     UI/Outliner/OutlinerDisplayOptionsMenu.cpp
     UI/Outliner/OutlinerTreeView.hxx
