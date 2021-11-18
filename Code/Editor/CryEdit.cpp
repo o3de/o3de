e8576acbb0510717d57f168096a9cc6ad0c2fbf9
Danilo Aimini <82231674+AMZN-daimini@users.noreply.github.com>
2021-11-18 14:16:56 -0800

    Replace old Developer Preview release name with Stable 21.11 (#5759)
    Signed-off-by: Danilo Aimini <82231674+AMZN-daimini@users.noreply.github.com>

--- Code/Editor/CryEdit.cpp
+++ Code/Editor/CryEdit.cpp
@@ -3834,7 +3834,7 @@ void CCryEditApp::SetEditorWindowTitle(QString sTitleStr, QString sPreTitleStr, 
     {
         if (sTitleStr.isEmpty())
         {
-            sTitleStr = QObject::tr("O3DE Editor [Developer Preview]");
+            sTitleStr = QObject::tr("O3DE Editor [Stable 21.11]");
         }
 
         if (!sPreTitleStr.isEmpty())
