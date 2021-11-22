778a039d0cbb3ed672f3feab2a301cb215f9f2b6
Mike Chang <changml@amazon.com>
2021-11-22 15:19:20 -0800

    Add signer script and new gpg signing cert
    Signed-off-by: Mike Chang <changml@amazon.com>

---
+++ scripts/signer/Platform/Linux/signer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+#
+
+set +x
+
+export GPG_TTY=$(tty) # Required to pass valid tty during ssh sessions
+file=$1
+
+fingerprint=$(gpg --list-keys --with-colons  | awk -F: '/fpr:/ {print $10}' | tail -n1) #Get the last certificate in the list, which is the signing cert
+echo "Signing with $fingerprint"
+dpkg-sig -k $fingerprint --sign builder $file
+dpkg-sig --verify $file && echo "Signing $file complete!"
