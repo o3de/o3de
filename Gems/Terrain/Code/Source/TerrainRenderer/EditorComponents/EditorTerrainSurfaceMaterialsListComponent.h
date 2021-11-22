4af3c15165a6b45201e988141af074dac729c427
Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
2021-11-22 13:43:08 -0600

    Added missing doc links.
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>

--- Gems/Terrain/Code/Source/TerrainRenderer/EditorComponents/EditorTerrainSurfaceMaterialsListComponent.h
+++ Gems/Terrain/Code/Source/TerrainRenderer/EditorComponents/EditorTerrainSurfaceMaterialsListComponent.h
@@ -27,6 +27,6 @@ namespace Terrain
         static constexpr const char* const s_componentDescription = "Provides a mapping between surface tags and render materials.";
         static constexpr const char* const s_icon = "Editor/Icons/Components/TerrainSurfaceMaterials.svg";
         static constexpr const char* const s_viewportIcon = "Editor/Icons/Components/Viewport/TerrainSurfaceMaterials.svg";
-        static constexpr const char* const s_helpUrl = "";
+        static constexpr const char* const s_helpUrl = "https://o3de.org/docs/user-guide/components/reference/terrain/surface-material-list/";
     };
 }
