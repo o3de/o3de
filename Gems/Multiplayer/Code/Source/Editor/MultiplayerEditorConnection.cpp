b2629091172cd42a5f9852a6bad84c9e11318dcf
Gene Walters <genewalt@amazon.com>
2021-12-01 10:06:04 -0800

    Misc whitespace edit
    Signed-off-by: Gene Walters <genewalt@amazon.com>

--- Gems/Multiplayer/Code/Source/Editor/MultiplayerEditorConnection.cpp
+++ Gems/Multiplayer/Code/Source/Editor/MultiplayerEditorConnection.cpp
@@ -160,7 +160,7 @@ namespace Multiplayer
             }
             
             networkInterface->Listen(sv_port);
-            
+
             AZLOG_INFO("Editor Server completed receiving the editor's level assets, responding to Editor...\n")
             return connection->SendReliablePacket(MultiplayerEditorPackets::EditorServerReady());
         }
