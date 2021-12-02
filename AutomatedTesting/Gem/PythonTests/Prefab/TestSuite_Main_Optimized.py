ab76f8cb6289ad9a10c25e46694b929a09785288
jckand-amzn <82226555+jckand-amzn@users.noreply.github.com>
2021-12-02 12:28:58 -0600

    Combined tests under NoAutoTestMode test class. Updated prefab name in test due to conflict running in parallel.
    Signed-off-by: jckand-amzn <82226555+jckand-amzn@users.noreply.github.com>

--- AutomatedTesting/Gem/PythonTests/Prefab/TestSuite_Main_Optimized.py
+++ AutomatedTesting/Gem/PythonTests/Prefab/TestSuite_Main_Optimized.py
@@ -13,30 +13,9 @@ from ly_test_tools.o3de.editor_test import EditorSingleTest, EditorSharedTest, E
 @pytest.mark.SUITE_main
 @pytest.mark.parametrize("launcher_platform", ['windows_editor'])
 @pytest.mark.parametrize("project", ["AutomatedTesting"])
-class TestAutomationAutoTestMode(EditorTestSuite):
-
-    class test_OpenLevel_ContainingTwoEntities(EditorSharedTest):
-        from .tests.open_level import OpenLevel_ContainingTwoEntities as test_module
-
-    class test_CreatePrefab_WithSingleEntity(EditorSharedTest):
-        from .tests.create_prefab import CreatePrefab_WithSingleEntity as test_module
-
-    class test_InstantiatePrefab_ContainingASingleEntity(EditorSharedTest):
-        from .tests.instantiate_prefab import InstantiatePrefab_ContainingASingleEntity as test_module
-
-    class test_DeletePrefab_ContainingASingleEntity(EditorSharedTest):
-        from .tests.delete_prefab import DeletePrefab_ContainingASingleEntity as test_module
-
-    class test_DuplicatePrefab_ContainingASingleEntity(EditorSharedTest):
-        from .tests.duplicate_prefab import DuplicatePrefab_ContainingASingleEntity as test_module
-
-
-@pytest.mark.SUITE_main
-@pytest.mark.parametrize("launcher_platform", ['windows_editor'])
-@pytest.mark.parametrize("project", ["AutomatedTesting"])
 class TestAutomationNoAutoTestMode(EditorTestSuite):
 
-    # Enable only -BatchMode for these tests. Tests cannot run in -autotest_mode due to UI interactions
+    # Enable only -BatchMode for these tests. Some tests cannot run in -autotest_mode due to UI interactions
     global_extra_cmdline_args = ["-BatchMode"]
 
     class test_CreatePrefab_UnderAnEntity(EditorSharedTest):
@@ -56,3 +35,18 @@ class TestAutomationNoAutoTestMode(EditorTestSuite):
 
     class test_DetachPrefab_UnderAnotherPrefab(EditorSharedTest):
         from .tests.detach_prefab import DetachPrefab_UnderAnotherPrefab as test_module
+
+    class test_OpenLevel_ContainingTwoEntities(EditorSharedTest):
+        from .tests.open_level import OpenLevel_ContainingTwoEntities as test_module
+
+    class test_CreatePrefab_WithSingleEntity(EditorSharedTest):
+        from .tests.create_prefab import CreatePrefab_WithSingleEntity as test_module
+
+    class test_InstantiatePrefab_ContainingASingleEntity(EditorSharedTest):
+        from .tests.instantiate_prefab import InstantiatePrefab_ContainingASingleEntity as test_module
+
+    class test_DeletePrefab_ContainingASingleEntity(EditorSharedTest):
+        from .tests.delete_prefab import DeletePrefab_ContainingASingleEntity as test_module
+
+    class test_DuplicatePrefab_ContainingASingleEntity(EditorSharedTest):
+        from .tests.duplicate_prefab import DuplicatePrefab_ContainingASingleEntity as test_module
