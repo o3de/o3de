c5bfd8c440d785301aa04700020ea6aac37b63b8
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2021-12-03 13:29:47 -0800

    Update to double escape quotes when reconstructing a command line (on windows)
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- Code/Framework/AzFramework/Tests/ProcessLaunchParseTests.cpp
+++ Code/Framework/AzFramework/Tests/ProcessLaunchParseTests.cpp
@@ -165,12 +165,8 @@ namespace UnitTest
         AzFramework::ProcessOutput processOutput;
         AzFramework::ProcessLauncher::ProcessLaunchInfo processLaunchInfo;
 
-        processLaunchInfo.m_commandlineParameters.emplace<AZStd::vector<AZStd::string>>(
-            AZStd::vector<AZStd::string>{ AZStd::string(AZ_TRAIT_TEST_ROOT_FOLDER "ProcessLaunchTest"),
-                                            "-param1",
-                                            R"("\"param 1val\"")",
-                                            R"(-param2="\"param2v al\"")" }
-        );
+        processLaunchInfo.m_commandlineParameters.emplace<AZStd::vector<AZStd::string>>(AZStd::vector<AZStd::string>{
+            AZStd::string(AZ_TRAIT_TEST_ROOT_FOLDER "ProcessLaunchTest"), "-param1", R"("param 1val")", R"(-param2="param2v al")" });
 
         processLaunchInfo.m_workingDirectory = AZ::Test::GetCurrentExecutablePath();
         processLaunchInfo.m_showWindow = false;
@@ -205,12 +201,8 @@ namespace UnitTest
         AzFramework::ProcessOutput processOutput;
         AzFramework::ProcessLauncher::ProcessLaunchInfo processLaunchInfo;
 
-        processLaunchInfo.m_commandlineParameters.emplace<AZStd::vector<AZStd::string>>(
-            AZStd::vector<AZStd::string>{ AZStd::string(AZ_TRAIT_TEST_ROOT_FOLDER "ProcessLaunchTest"),
-                                            "-param1",
-                                            R"("param, 1val")",
-                                            R"(-param2="param,2v al")" }
-        );
+        processLaunchInfo.m_commandlineParameters.emplace<AZStd::vector<AZStd::string>>(AZStd::vector<AZStd::string>{
+            AZStd::string(AZ_TRAIT_TEST_ROOT_FOLDER "ProcessLaunchTest"), "-param1", R"("param, 1val")", R"(-param2="param,2v al")" });
 
         processLaunchInfo.m_workingDirectory = AZ::Test::GetCurrentExecutablePath();
         processLaunchInfo.m_showWindow = false;
