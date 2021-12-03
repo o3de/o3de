5260425d0ac36eed481c00295e09903de5e1a97e
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2021-12-03 13:38:47 -0800

    Revert "Update to double escape quotes when reconstructing a command line (on windows)"
    This reverts commit 1e13a45fa9a8cd7ed16163b3370e0ac07633b225.
    
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- Code/Framework/AzFramework/Tests/ProcessLaunchParseTests.cpp
+++ Code/Framework/AzFramework/Tests/ProcessLaunchParseTests.cpp
@@ -201,8 +201,20 @@ namespace UnitTest
         AzFramework::ProcessOutput processOutput;
         AzFramework::ProcessLauncher::ProcessLaunchInfo processLaunchInfo;
 
-        processLaunchInfo.m_commandlineParameters.emplace<AZStd::vector<AZStd::string>>(AZStd::vector<AZStd::string>{
-            AZStd::string(AZ_TRAIT_TEST_ROOT_FOLDER "ProcessLaunchTest"), "-param1", R"("param, 1val")", R"(-param2="param,2v al")" });
+        processLaunchInfo.m_commandlineParameters.emplace<AZStd::vector<AZStd::string>>(
+#if defined(AZ_PLATFORM_WINDOWS)
+            // On Windows, double escapement of double-quotes is needed
+            AZStd::vector<AZStd::string>{ AZStd::string(AZ_TRAIT_TEST_ROOT_FOLDER "ProcessLaunchTest"),
+                                            "-param1",
+                                            R"("\"param, 1val\"")",
+                                            R"(-param2="\"param,2v al\"")" }
+#else
+            AZStd::vector<AZStd::string>{ AZStd::string(AZ_TRAIT_TEST_ROOT_FOLDER "ProcessLaunchTest"),
+                                            "-param1",
+                                            R"("param, 1val")",
+                                            R"(-param2="param,2v al")" }
+#endif // defined(AZ_PLATFORM_WINDOWS)
+        );
 
         processLaunchInfo.m_workingDirectory = AZ::Test::GetCurrentExecutablePath();
         processLaunchInfo.m_showWindow = false;
