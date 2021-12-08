e3f47071ce7a474dcd0eebbb4cd157e54661c177
galibzon <66021303+galibzon@users.noreply.github.com>
2021-12-08 14:56:16 -0600

    Make math classes & EntityId visible in Lua Documentation.
    Small change in behavior context attribute for a few
    classes so they become visible when extracting Lua symbols
    from the ScriptContextDebug() API.
    
    From:
    Attribute(AZ::Script::Attributes::ExcludeFrom,
        AZ::Script::Attributes::ExcludeFlags::All)
    
    To:
    Attribute(AZ::Script::Attributes::ExcludeFrom,
        AZ::Script::Attributes::ExcludeFlags::ListOnly)
    
    It seems the previous code was simply trying to hide those symbols from
    the Script Canvas node palette. But it was also, seemingly unintentional,
    hiding those symbol from Lua Documentation.
    
    Signed-off-by: galibzon <66021303+galibzon@users.noreply.github.com>

--- Code/Framework/AzCore/AzCore/Math/Vector2.cpp
+++ Code/Framework/AzCore/AzCore/Math/Vector2.cpp
@@ -191,7 +191,7 @@ namespace AZ
             behaviorContext->Class<Vector2>()->
                 Attribute(Script::Attributes::Scope, Script::Attributes::ScopeFlags::Common)->
                 Attribute(Script::Attributes::Module, "math")->
-                Attribute(Script::Attributes::ExcludeFrom, Script::Attributes::ExcludeFlags::All)->
+                Attribute(Script::Attributes::ExcludeFrom, Script::Attributes::ExcludeFlags::ListOnly)->
                 Constructor<float>()->
                 Constructor<float, float>()->
                 Attribute(Script::Attributes::Storage, Script::Attributes::StorageType::Value)->
