ba5c1a43850ef87ce81f60c0a223fb4b2165268f
amzn-mike <80125227+amzn-mike@users.noreply.github.com>
2021-12-09 14:44:17 -0600

    Update comments to make ebus event behavior more clear
    Signed-off-by: amzn-mike <80125227+amzn-mike@users.noreply.github.com>

--- Code/Framework/AzToolsFramework/AzToolsFramework/Prefab/PrefabPublicNotificationBus.h
+++ Code/Framework/AzToolsFramework/AzToolsFramework/Prefab/PrefabPublicNotificationBus.h
@@ -27,10 +27,12 @@ namespace AzToolsFramework
             virtual void OnPrefabTemplateDirtyFlagUpdated(
                 [[maybe_unused]] TemplateId templateId, [[maybe_unused]] bool status) {}
 
-            // Sent after a template has been removed
+            // Sent after a single template has been removed
+            // Does not get sent when all templates are being removed at once.  Be sure to handle OnAllTemplatesRemoved as well.
             virtual void OnTemplateRemoved([[maybe_unused]] TemplateId templateId) {}
 
             // Sent after all templates have been removed
+            // Does not trigger individual OnTemplateRemoved events
             virtual void OnAllTemplatesRemoved() {}
         };
 
