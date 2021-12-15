069eb6710d38fa67fbd94010e30a2794cf0ff75d
chiyenteng <82238204+chiyenteng@users.noreply.github.com>
2021-12-15 08:25:18 -0800

    Make python automated test 'test_WindowsMacPlatforms_MoveCorruptedSliceFile_MoveSuccess' to use prefab instead
    Signed-off-by: chiyenteng <82238204+chiyenteng@users.noreply.github.com>

--- AutomatedTesting/Gem/PythonTests/assetpipeline/ap_fixtures/clear_moveoutput_fixture.py
+++ AutomatedTesting/Gem/PythonTests/assetpipeline/ap_fixtures/clear_moveoutput_fixture.py
@@ -4,7 +4,7 @@ For complete copyright and license terms please see the LICENSE at the root of t
 
 SPDX-License-Identifier: Apache-2.0 OR MIT
 
-Fixture for clearing out 'MoveOutput' folders from \dev and \dev\PROJECT
+Fixture for clearing out 'MoveOutput' folders from \\dev and \\dev\\PROJECT
 """
 
 # Import builtin libraries
