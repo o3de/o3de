c9766eed4dddbb41ca0886635a5ae0e848ef8ef5
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2021-12-15 14:49:37 -0800

    Fix commit 5f489da60f8110b00297c507e89dba3235d5294d with the correct GCC version compare operator '>='
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- Code/Framework/AzCore/AzCore/std/string/string_view.h
+++ Code/Framework/AzCore/AzCore/std/string/string_view.h
@@ -291,7 +291,7 @@ namespace AZStd
         static constexpr int compare(const char_type* s1, const char_type* s2, size_t count) noexcept
         {
             // In GCC versions prior to major version 10, __builtin_memcmp fails in valid checks in constexpr evaluation
-#if !defined(AZ_COMPILER_GCC) || AZ_COMPILER_GCC < 100000
+#if !defined(AZ_COMPILER_GCC) || AZ_COMPILER_GCC >= 100000
             if constexpr (AZStd::is_same_v<char_type, char>)
             {
                 return __builtin_memcmp(s1, s2, count);
