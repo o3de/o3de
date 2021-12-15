99488bc805e69129e194a81784bf8d30ef286378
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2021-12-15 15:37:12 -0800

    Fix for GCC compile error related to not findind AZStd::is_same
      In file included from Code/Framework/AzCore/Tests/AZStd/Pair.cpp:9:
    Code/Framework/AzCore/./AzCore/std/containers/compressed_pair.h:48:69: error: ‘is_same’ is not a member of ‘AZStd’; did you mean ‘std::is_same’?
       48 |         template <typename U, typename = AZStd::enable_if_t<!AZStd::is_same<AZStd::remove_cvref_t<U>, compressed_pair_element>::value>>
          |                                                                     ^~~~~~~
    
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- Code/Framework/AzCore/AzCore/std/containers/compressed_pair.h
+++ Code/Framework/AzCore/AzCore/std/containers/compressed_pair.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <AzCore/std/utils.h>
+#include <AzCore/std/typetraits/is_same.h>
 #include <AzCore/std/typetraits/remove_cvref.h>
 
 /* Microsoft C++ ABI puts 1 byte of padding between each empty base class when multiple inheritance is being used
