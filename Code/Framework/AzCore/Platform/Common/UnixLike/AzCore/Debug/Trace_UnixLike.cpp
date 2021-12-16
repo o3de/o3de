db150b4495505e3a7737df511f8a64c663f56809
Michael Pollind <mpollind@gmail.com>
2021-12-15 19:18:24 -0800

    feature: add Exception Handler support for unix
    REF: https://github.com/o3de/o3de/issues/5886
    
    Signed-off-by: Michael Pollind <mpollind@gmail.com>
    Signed-off-by: santorac <55155825+santorac@users.noreply.github.com>

--- Code/Framework/AzCore/Platform/Common/UnixLike/AzCore/Debug/Trace_UnixLike.cpp
+++ Code/Framework/AzCore/Platform/Common/UnixLike/AzCore/Debug/Trace_UnixLike.cpp
@@ -6,74 +6,129 @@
  *
  */
 
+#include <AzCore/Debug/TraceMessageBus.h>
 #include <AzCore/IO/SystemFile.h>
 #include <AzCore/std/string/string_view.h>
 
 #include <ctype.h>
+#include <execinfo.h>
 #include <signal.h>
 #include <unistd.h>
 
-namespace AZ::Debug::Platform
+namespace AZ::Debug
 {
 #if defined(AZ_ENABLE_DEBUG_TOOLS)
-    bool performDebuggerDetection()
+    void ExceptionHandler(int signal);
+#endif
+
+    constexpr int MaxMessageLength = 4096;
+    constexpr int MaxStackLines = 100;
+
+    namespace Platform
     {
-        AZ::IO::SystemFile processStatusFile;
-        if (!processStatusFile.Open("/proc/self/status", AZ::IO::SystemFile::SF_OPEN_READ_ONLY))
+#if defined(AZ_ENABLE_DEBUG_TOOLS)
+        bool performDebuggerDetection()
         {
-            return false;
-        }
-
-        char buffer[4096];
-        AZ::IO::SystemFile::SizeType numRead = processStatusFile.Read(sizeof(buffer), buffer);
-
-        const AZStd::string_view processStatusView(buffer, buffer + numRead);
-        constexpr AZStd::string_view tracerPidString = "TracerPid:";
-        const size_t tracerPidOffset = processStatusView.find(tracerPidString);
-        if (tracerPidOffset == AZStd::string_view::npos)
-        {
-            return false;
-        }
-        for (size_t i = tracerPidOffset + tracerPidString.length(); i < numRead; ++i)
-        {
-            if (!::isspace(processStatusView[i]))
+            AZ::IO::SystemFile processStatusFile;
+            if (!processStatusFile.Open("/proc/self/status", AZ::IO::SystemFile::SF_OPEN_READ_ONLY))
             {
-                return processStatusView[i] != '0';
+                return false;
+            }
+
+            char buffer[4096];
+            AZ::IO::SystemFile::SizeType numRead = processStatusFile.Read(sizeof(buffer), buffer);
+
+            const AZStd::string_view processStatusView(buffer, buffer + numRead);
+            constexpr AZStd::string_view tracerPidString = "TracerPid:";
+            const size_t tracerPidOffset = processStatusView.find(tracerPidString);
+            if (tracerPidOffset == AZStd::string_view::npos)
+            {
+                return false;
+            }
+            for (size_t i = tracerPidOffset + tracerPidString.length(); i < numRead; ++i)
+            {
+                if (!::isspace(processStatusView[i]))
+                {
+                    return processStatusView[i] != '0';
+                }
+            }
+            return false;
+        }
+
+        bool IsDebuggerPresent()
+        {
+            static bool s_detectionPerformed = false;
+            static bool s_debuggerDetected = false;
+            if (!s_detectionPerformed)
+            {
+                s_debuggerDetected = performDebuggerDetection();
+                s_detectionPerformed = true;
+            }
+            return s_debuggerDetected;
+        }
+
+        bool AttachDebugger()
+        {
+            // Not supported yet
+            AZ_Assert(false, "AttachDebugger() is not supported for Unix platform yet");
+            return false;
+        }
+
+        void SignalHandler(int handler)
+        {
+        }
+
+        void HandleExceptions(bool isEnabled)
+        {
+            if (isEnabled)
+            {
+                signal(SIGSEGV, ExceptionHandler);
+                signal(SIGTRAP, ExceptionHandler);
+                signal(SIGILL, ExceptionHandler);
+            }
+            else
+            {
+                signal(SIGSEGV, SIG_DFL);
+                signal(SIGTRAP, SIG_DFL);
+                signal(SIGILL, SIG_DFL);
             }
         }
-        return false;
-    }
 
-    bool IsDebuggerPresent()
-    {
-        static bool s_detectionPerformed = false;
-        static bool s_debuggerDetected = false;
-        if (!s_detectionPerformed)
+        void DebugBreak()
         {
-            s_debuggerDetected = performDebuggerDetection();
-            s_detectionPerformed = true;
+            raise(SIGINT);
         }
-        return s_debuggerDetected;
-    }
-
-    bool AttachDebugger()
-    {
-        // Not supported yet
-        AZ_Assert(false, "AttachDebugger() is not supported for Unix platform yet");
-        return false;
-    }
-
-    void HandleExceptions(bool)
-    {}
-
-    void DebugBreak()
-    {
-        raise(SIGINT);
-    }
 #endif // AZ_ENABLE_DEBUG_TOOLS
 
-    void Terminate(int exitCode)
+        void Terminate(int exitCode)
+        {
+            _exit(exitCode);
+        }
+    } // namespace Platform
+
+#if defined(AZ_ENABLE_DEBUG_TOOLS)
+    void ExceptionHandler(int signal)
     {
-        _exit(exitCode);
+        char message[MaxMessageLength];
+        Debug::Trace::Instance().Output(nullptr, "==================================================================\n");
+        azsnprintf(message, MaxMessageLength, "Error: signal %s: \n", strsignal(signal));
+        Debug::Trace::Instance().Output(nullptr, message);
+
+        void* buffers[MaxStackLines];
+        int numberBacktraceStrings = backtrace(buffers, MaxStackLines);
+        char** backtraceResults = backtrace_symbols(buffers, numberBacktraceStrings);
+        if (backtraceResults == nullptr)
+        {
+            Debug::Trace::Instance().Output(nullptr, "==================================================================\n");
+            return;
+        }
+        for (int j = 0; j < numberBacktraceStrings; j++)
+        {
+            Debug::Trace::Instance().Output(nullptr, backtraceResults[j]);
+        }
+
+        Debug::Trace::Instance().Output(nullptr, "==================================================================\n");
     }
-} // namespace AZ::Debug::Platform
+#endif
+
+} // namespace AZ::Debug
