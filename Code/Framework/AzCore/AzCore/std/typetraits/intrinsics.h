87365099682013994009116f511609e34c0dd387
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2021-12-16 19:20:16 -0800

    Fix macro AZSTD_IS_CONVERTIBLE definition for CLANG/GCC
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- Code/Framework/AzCore/AzCore/std/typetraits/intrinsics.h
+++ Code/Framework/AzCore/AzCore/std/typetraits/intrinsics.h
@@ -86,7 +86,7 @@
 #   define AZSTD_IS_BASE_OF(T, U) (__is_base_of(T, U) && !is_same<T, U>::value)
 #   define AZSTD_IS_CLASS(T) __is_class(T)
 #   define AZSTD_IS_ENUM(T) __is_enum(T)
-#   define AZSTD_IS_CONVERTIBLE(_From, _To) AZStd::is_convertible_v(_From, _To)
+#   define AZSTD_IS_CONVERTIBLE(_From, _To) AZStd::is_convertible_v<_From, _To>
 #   define AZSTD_IS_POLYMORPHIC(T) __is_polymorphic(T)
 #endif
 
