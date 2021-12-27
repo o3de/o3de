121cdda799b2f975359a4153f21601924a74628b
jckand-amzn <82226555+jckand-amzn@users.noreply.github.com>
2021-12-27 17:25:34 -0600

    Converting Gradient Signal tests to utilize the prefab system
    Signed-off-by: jckand-amzn <82226555+jckand-amzn@users.noreply.github.com>

--- AutomatedTesting/Gem/PythonTests/largeworlds/gradient_signal/TestSuite_Periodic_Optimized.py
+++ AutomatedTesting/Gem/PythonTests/largeworlds/gradient_signal/TestSuite_Periodic_Optimized.py
@@ -15,19 +15,17 @@ from ly_test_tools.o3de.editor_test import EditorSingleTest, EditorSharedTest, E
 @pytest.mark.parametrize("project", ["AutomatedTesting"])
 class TestAutomation(EditorTestSuite):
 
-    enable_prefab_system = False
-
     class test_GradientGenerators_Incompatibilities(EditorSharedTest):
         from .EditorScripts import GradientGenerators_Incompatibilities as test_module
 
     class test_GradientModifiers_Incompatibilities(EditorSharedTest):
         from .EditorScripts import GradientModifiers_Incompatibilities as test_module
 
-    class test_GradientPreviewSettings_DefaultPinnedEntityIsSelf(EditorSharedTest):
-        from .EditorScripts import GradientPreviewSettings_DefaultPinnedEntityIsSelf as test_module
-
     class test_GradientPreviewSettings_ClearingPinnedEntitySetsPreviewToOrigin(EditorSharedTest):
         from .EditorScripts import GradientPreviewSettings_ClearingPinnedEntitySetsPreviewToOrigin as test_module
+
+    class test_GradientPreviewSettings_DefaultPinnedEntityIsSelf(EditorSharedTest):
+        from .EditorScripts import GradientPreviewSettings_DefaultPinnedEntityIsSelf as test_module
 
     class test_GradientSampling_GradientReferencesAddRemoveSuccessfully(EditorSharedTest):
         from .EditorScripts import GradientSampling_GradientReferencesAddRemoveSuccessfully as test_module
@@ -38,20 +36,20 @@ class TestAutomation(EditorTestSuite):
     class test_GradientSurfaceTagEmitter_SurfaceTagsAddRemoveSuccessfully(EditorSharedTest):
         from .EditorScripts import GradientSurfaceTagEmitter_SurfaceTagsAddRemoveSuccessfully as test_module
 
-    class test_GradientTransform_RequiresShape(EditorSharedTest):
-        from .EditorScripts import GradientTransform_RequiresShape as test_module
-
-    class test_GradientTransform_FrequencyZoomCanBeSetBeyondSliderRange(EditorSharedTest):
-        from .EditorScripts import GradientTransform_FrequencyZoomCanBeSetBeyondSliderRange as test_module
+    class test_GradientTransform_ComponentIncompatibleWithExpectedGradients(EditorSharedTest):
+        from .EditorScripts import GradientTransform_ComponentIncompatibleWithExpectedGradients as test_module
 
     class test_GradientTransform_ComponentIncompatibleWithSpawners(EditorSharedTest):
         from .EditorScripts import GradientTransform_ComponentIncompatibleWithSpawners as test_module
 
-    class test_GradientTransform_ComponentIncompatibleWithExpectedGradients(EditorSharedTest):
-        from .EditorScripts import GradientTransform_ComponentIncompatibleWithExpectedGradients as test_module
+    class test_GradientTransform_FrequencyZoomCanBeSetBeyondSliderRange(EditorSharedTest):
+        from .EditorScripts import GradientTransform_FrequencyZoomCanBeSetBeyondSliderRange as test_module
 
-    class test_ImageGradient_RequiresShape(EditorSharedTest):
-        from .EditorScripts import ImageGradient_RequiresShape as test_module
+    class test_GradientTransform_RequiresShape(EditorSharedTest):
+        from .EditorScripts import GradientTransform_RequiresShape as test_module
 
     class test_ImageGradient_ProcessedImageAssignedSuccessfully(EditorSharedTest):
         from .EditorScripts import ImageGradient_ProcessedImageAssignedSuccessfully as test_module
+
+    class test_ImageGradient_RequiresShape(EditorSharedTest):
+        from .EditorScripts import ImageGradient_RequiresShape as test_module
