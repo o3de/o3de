c1d2da990a6d355df30471a957623e600d5b1e67
Esteban Papp <81431996+amznestebanpapp@users.noreply.github.com>
2022-01-06 16:11:39 -0800

    Removes WhiteBoxAllocator and EditorWhiteBoxBus from Gems/WhiteBox
    Signed-off-by: Esteban Papp <81431996+amznestebanpapp@users.noreply.github.com>

--- Gems/WhiteBox/Code/whitebox_editor_supported_files.cmake
+++ Gems/WhiteBox/Code/whitebox_editor_supported_files.cmake
@@ -7,7 +7,6 @@
 #
 
 set(FILES
-    Include/WhiteBox/EditorWhiteBoxBus.h
     Include/WhiteBox/EditorWhiteBoxComponentBus.h
     Include/WhiteBox/WhiteBoxToolApi.h
     Include/WhiteBox/EditorWhiteBoxColliderBus.h
