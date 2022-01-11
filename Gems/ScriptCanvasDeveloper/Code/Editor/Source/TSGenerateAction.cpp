54880583d15d698f70396ab659088a7c4627edbb
Alex Peterson <26804013+AMZN-alexpete@users.noreply.github.com>
2022-01-11 11:28:33 -0800

    Let the user specify folder to write to
    Signed-off-by: Alex Peterson <26804013+AMZN-alexpete@users.noreply.github.com>

--- Gems/ScriptCanvasDeveloper/Code/Editor/Source/TSGenerateAction.cpp
+++ Gems/ScriptCanvasDeveloper/Code/Editor/Source/TSGenerateAction.cpp
@@ -11,6 +11,9 @@
 #include <QAction>
 #include <QObject>
 #include <QMenu>
+#include <QFileDialog>
+#include <QStandardPaths>
+#include <QMessageBox>
 #endif
 
 #include <Source/Translation/TranslationBus.h>
@@ -34,14 +37,24 @@ namespace ScriptCanvasDeveloperEditor
             qAction->setShortcut(QAction::tr("Ctrl+Alt+R", "Developer|Reload Text"));
             QObject::connect(qAction, &QAction::triggered, [mainWindow]() { ReloadText(mainWindow); });
 
-            qAction = mainMenu->addAction(QAction::tr("Dump Database"));
+            qAction = mainMenu->addAction(QAction::tr("Dump Translation Database"));
             qAction->setAutoRepeat(false);
-            qAction->setShortcut(QAction::tr("Ctrl+Alt+L", "Developer|Dump Database"));
+            qAction->setShortcut(QAction::tr("Ctrl+Alt+L", "Developer|Dump Translation Database"));
             QObject::connect(
                 qAction, &QAction::triggered,
-                []()
+                [mainWindow]()
                 {
-                    GraphCanvas::TranslationRequestBus::Broadcast(&GraphCanvas::TranslationRequests::DumpDatabase, "sc_translation.log");
+                    QString defaultPath = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
+                    QString directory = QFileDialog::getExistingDirectory(mainWindow,
+                        QObject::tr("Select output folder for sc_translation.log file"), defaultPath);
+                    if (!directory.isEmpty())
+                    {
+                        const QString path = QDir::toNativeSeparators(directory + "/sc_translation.log");
+                        GraphCanvas::TranslationRequestBus::Broadcast(&GraphCanvas::TranslationRequests::DumpDatabase, path.toUtf8().constData());
+                        QMessageBox::information(
+                            mainWindow, QObject::tr("Finished writing translation database"),
+                            QObject::tr("Translation database written to:<br/><a href=\"file:///%1\">%1</a>").arg(path));
+                    }
                 });
 
         }
