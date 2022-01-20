b5c466e72d6be5a948fab024d93f540ffe5cc6b9
carlitosan <82187351+carlitosan@users.noreply.github.com>
2022-01-20 12:59:42 -0800

    add comment to use modifiabledatumview
    Signed-off-by: carlitosan <82187351+carlitosan@users.noreply.github.com>

--- Gems/ScriptCanvas/Code/Editor/View/Widgets/VariablePanel/VariableDockWidget.cpp
+++ Gems/ScriptCanvas/Code/Editor/View/Widgets/VariablePanel/VariableDockWidget.cpp
@@ -891,8 +891,12 @@ namespace ScriptCanvasEditor
                  if (output.m_typeChanged && output.m_type.IsValid())
                  {
                      // check the UX display
+                     //
+                     // use this: ModifiableDatumView
                      graphVariable->ModDatum().SetType(output.m_type, ScriptCanvas::Datum::TypeChange::Forced);
                      ScriptCanvas::GraphRequestBus::Event(m_scriptCanvasId, &ScriptCanvas::GraphRequests::RefreshDatumReferences, graphVariable->ModDatum());
+                     // VariableNotificationBus::Event(m_scopedVariableId, &VariableNotifications::OnVariableValueChanged);
+
                      // get the variable type to update first, then handle the slots, and the get/set variables
 
                     // then here, this needs to be called on every node in the graph as the notification id
