5b00b762f264dd56afc1eacc70f656d938ec3f89
mrieggeramzn <mriegger@amazon.com>
2022-01-20 15:42:35 -0800

    Trying to get it working
    Signed-off-by: mrieggeramzn <mriegger@amazon.com>

--- Gems/Atom/Feature/Common/Assets/atom_feature_common_asset_files.cmake
+++ Gems/Atom/Feature/Common/Assets/atom_feature_common_asset_files.cmake
@@ -143,6 +143,7 @@ set(FILES
     Passes/ForwardMSAA.pass
     Passes/ForwardSubsurfaceMSAA.pass
     Passes/FullscreenCopy.pass
+    Passes/FullscreenShadow.pass
     Passes/FullscreenOutputOnly.pass
     Passes/HDRColorGrading.pass
     Passes/ImGui.pass
@@ -512,6 +513,8 @@ set(FILES
     Shaders/ScreenSpace/DeferredFog.shader
     Shaders/Shadow/DepthExponentiation.azsl
     Shaders/Shadow/DepthExponentiation.shader
+    Shaders/Shadow/FullscreenShadow.azsl
+    Shaders/Shadow/FullscreenShadow.shader
     Shaders/Shadow/KawaseShadowBlur.azsl
     Shaders/Shadow/KawaseShadowBlur.shader
     Shaders/Shadow/Shadowmap.azsl
