56f778483b979808c916c988718bc995af1c9fc8
Tom Hulton-Harrop <82228511+hultonha@users.noreply.github.com>
2022-01-26 11:16:18 +0000

    Fix for editor intersection with helpers disabled for EditorSplineComponent (#7143)
    * update file name for editor intersection tests
    
    Signed-off-by: Tom Hulton-Harrop <82228511+hultonha@users.noreply.github.com>
    
    * add additional tests for editor spline component for viewport intersection
    
    Signed-off-by: Tom Hulton-Harrop <82228511+hultonha@users.noreply.github.com>

--- Gems/LmbrCentral/Code/Tests/lmbrcentral_editor_tests_files.cmake
+++ Gems/LmbrCentral/Code/Tests/lmbrcentral_editor_tests_files.cmake
@@ -10,7 +10,7 @@ set(FILES
     LmbrCentralEditorTest.cpp
     LmbrCentralReflectionTest.h
     LmbrCentralReflectionTest.cpp
-    EditorShapeComponentIntersectionTests.cpp
+    EditorComponentIntersectionTests.cpp
     EditorBoxShapeComponentTests.cpp
     EditorSphereShapeComponentTests.cpp
     EditorCapsuleShapeComponentTests.cpp
