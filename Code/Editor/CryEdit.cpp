c789814d75ca75002733d8ddcf83a7193d379458
Esteban Papp <81431996+amznestebanpapp@users.noreply.github.com>
2022-01-26 07:36:33 -0800

    adds another maybe_unused to a new variable using assert
    Signed-off-by: Esteban Papp <81431996+amznestebanpapp@users.noreply.github.com>

--- Code/Editor/CryEdit.cpp
+++ Code/Editor/CryEdit.cpp
@@ -1820,7 +1820,7 @@ bool CCryEditApp::InitInstance()
     if (GetIEditor()->GetCommandManager()->IsRegistered("editor.open_lnm_editor"))
     {
         CCommand0::SUIInfo uiInfo;
-        bool ok = GetIEditor()->GetCommandManager()->GetUIInfo("editor.open_lnm_editor", uiInfo);
+        [[maybe_unused]] bool ok = GetIEditor()->GetCommandManager()->GetUIInfo("editor.open_lnm_editor", uiInfo);
         assert(ok);
     }
 
