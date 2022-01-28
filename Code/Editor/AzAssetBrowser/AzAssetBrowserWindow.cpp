506c2d2b83a35784d034e9e5ba165ba30695bc16
AMZN-Igarri <82394219+AMZN-Igarri@users.noreply.github.com>
2022-01-28 12:26:25 +0000

    Changed tooltip duration
    Signed-off-by: AMZN-Igarri <82394219+AMZN-Igarri@users.noreply.github.com>

--- Code/Editor/AzAssetBrowser/AzAssetBrowserWindow.cpp
+++ Code/Editor/AzAssetBrowser/AzAssetBrowserWindow.cpp
@@ -99,7 +99,7 @@ AzAssetBrowserWindow::AzAssetBrowserWindow(QWidget* parent)
     m_ui->m_collapseAllButton->setAutoRaise(true); // hover highlight
     m_ui->m_collapseAllButton->setIcon(QIcon(AzAssetBrowser::CollapseAllIcon));
     m_ui->m_collapseAllButton->setToolTip(tr("Collapse All"));
-    m_ui->m_collapseAllButton->setToolTipDuration(1000);
+    m_ui->m_collapseAllButton->setToolTipDuration(10000);
 
     connect(
         m_ui->m_collapseAllButton, &QToolButton::clicked, this,
