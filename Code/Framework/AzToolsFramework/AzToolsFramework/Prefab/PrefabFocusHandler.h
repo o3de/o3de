1018ab0c324e5000383f4b93ccc0ef2ac9668539
Danilo Aimini <82231674+AMZN-daimini@users.noreply.github.com>
2022-01-28 13:03:11 -0800

    Revert Overrides UI changes - will split them into different prototypes in separate branches.
    Signed-off-by: Danilo Aimini <82231674+AMZN-daimini@users.noreply.github.com>

--- Code/Framework/AzToolsFramework/AzToolsFramework/Prefab/PrefabFocusHandler.h
+++ Code/Framework/AzToolsFramework/AzToolsFramework/Prefab/PrefabFocusHandler.h
@@ -49,7 +49,6 @@ namespace AzToolsFramework::Prefab
         // PrefabFocusInterface overrides ...
         void InitializeEditorInterfaces() override;
         PrefabFocusOperationResult FocusOnPrefabInstanceOwningEntityId(AZ::EntityId entityId) override;
-        PrefabFocusOperationResult OpenPrefabInstanceOwningEntityIdAndDescendants(AZ::EntityId entityId) override;
         TemplateId GetFocusedPrefabTemplateId(AzFramework::EntityContextId entityContextId) const override;
         InstanceOptionalReference GetFocusedPrefabInstance(AzFramework::EntityContextId entityContextId) const override;
 
