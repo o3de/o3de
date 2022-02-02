5509764fc2b793db31110cf610ff76d0cff3263a
Roman <69218254+amzn-rhhong@users.noreply.github.com>
2022-02-02 11:46:00 -0800

    Make atom render viewport the default for animation editor. (#7282)
    * Make atom render viewport the default for animation editor.
    
    Signed-off-by: rhhong <rhhong@amazon.com>
    
    * Fixed the automated test problem caused by making atom vp the default
    
    Signed-off-by: rhhong <rhhong@amazon.com>
    
    * Fix another test failure
    
    Signed-off-by: rhhong <rhhong@amazon.com>
    
    * fix failed test
    
    Signed-off-by: rhhong <rhhong@amazon.com>
    
    * more fix to the automation test
    
    Signed-off-by: rhhong <rhhong@amazon.com>
    
    * we don't need to manuelly call init on the ragdoll plugin anymore
    
    Signed-off-by: rhhong <rhhong@amazon.com>

--- Gems/EMotionFX/Code/Tests/ProvidesUI/Ragdoll/CanCopyPasteJointLimits.cpp
+++ Gems/EMotionFX/Code/Tests/ProvidesUI/Ragdoll/CanCopyPasteJointLimits.cpp
@@ -31,6 +31,8 @@ namespace EMotionFX
 {
     class CopyPasteRagdollJointLimitsFixture : public UIFixture
     {
+    protected:
+        virtual bool ShouldReflectPhysicSystem() override { return true; }
     };
 
 #if AZ_TRAIT_DISABLE_FAILED_EMOTION_FX_EDITOR_TESTS
@@ -41,16 +43,10 @@ namespace EMotionFX
     {
         using testing::_;
 
-        AZ::SerializeContext* serializeContext = GetSerializeContext();
-
-        Physics::MockPhysicsSystem::Reflect(serializeContext); // Required by Ragdoll plugin to fake PhysX Gem is available
-        D6JointLimitConfiguration::Reflect(serializeContext);
-
         EMStudio::GetMainWindow()->ApplicationModeChanged("Physics");
 
         auto ragdollPlugin = static_cast<EMotionFX::RagdollNodeInspectorPlugin*>(EMStudio::GetPluginManager()->FindActivePlugin(EMotionFX::RagdollNodeInspectorPlugin::CLASS_ID));
         ASSERT_TRUE(ragdollPlugin) << "Ragdoll plugin not found.";
-        ragdollPlugin->Init();
 
         Physics::MockPhysicsSystem physicsSystem;
         Physics::MockPhysicsInterface physicsInterface;
