7c6c01d5a1bb83924c27d4782f36cf0c25c7282f
chiyenteng <82238204+chiyenteng@users.noreply.github.com>
2022-02-07 12:11:01 -0800

    Convert automated tests related to content and unused levels (#7416)
    * Convert automated tests related to content/WaterSample level/Physics's Base level
    
    Signed-off-by: chiyenteng <82238204+chiyenteng@users.noreply.github.com>
    
    * Fix nits
    
    Signed-off-by: chiyenteng <82238204+chiyenteng@users.noreply.github.com>
    
    * Add missing .prefab level file
    
    Signed-off-by: chiyenteng <82238204+chiyenteng@users.noreply.github.com>
    
    * Fix nits
    
    Signed-off-by: chiyenteng <82238204+chiyenteng@users.noreply.github.com>
    
    * Convert Blast tests
    
    Signed-off-by: chiyenteng <82238204+chiyenteng@users.noreply.github.com>
    
    * Convert scripting tests
    
    Signed-off-by: chiyenteng <82238204+chiyenteng@users.noreply.github.com>
    
    * Remove useless comments
    
    Signed-off-by: chiyenteng <82238204+chiyenteng@users.noreply.github.com>
    
    * Revert Collider_AddColliderComponent change until prefab error message bug fixed
    
    Signed-off-by: chiyenteng <82238204+chiyenteng@users.noreply.github.com>

--- AutomatedTesting/Gem/PythonTests/EditorPythonBindings/ObjectManagerCommands_test_case.py
+++ AutomatedTesting/Gem/PythonTests/EditorPythonBindings/ObjectManagerCommands_test_case.py
@@ -20,12 +20,10 @@ def fetch_vector3_parts(vec3):
 
 general.idle_enable(True)
 
-# Try to open the WaterSample level. If not, fail the test.
-# We need to rely on an existing level since the API does not provide
-# a way to create entities, but only lets us manipulate them.
-editor.EditorToolsApplicationRequestBus(bus.Broadcast, 'OpenLevelNoPrompt', 'WaterSample')
+# It needs a new test level in prefab format to make it testable again.
+editor.EditorToolsApplicationRequestBus(bus.Broadcast, 'OpenLevelNoPrompt', 'Base')
 
-if (editor.EditorToolsApplicationRequestBus(bus.Broadcast, 'GetCurrentLevelName') == 'WaterSample'):
+if (editor.EditorToolsApplicationRequestBus(bus.Broadcast, 'GetCurrentLevelName') == 'Base'):
     
     objs_list = general.get_all_objects()
     
