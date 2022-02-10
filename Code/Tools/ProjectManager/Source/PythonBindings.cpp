a370a9076baea84fde75fd8299aefebfd8eb11cf
nggieber <52797929+AMZN-nggieber@users.noreply.github.com>
2022-02-10 08:52:42 -0800

    Fix crash if there is python error but no redirected output from python
    Signed-off-by: nggieber <52797929+AMZN-nggieber@users.noreply.github.com>

--- Code/Tools/ProjectManager/Source/PythonBindings.cpp
+++ Code/Tools/ProjectManager/Source/PythonBindings.cpp
@@ -1356,11 +1356,19 @@ namespace O3DE::ProjectManager
 
     IPythonBindings::ErrorPair PythonBindings::GetErrorPair()
     {
-        AZStd::string detailedString = m_pythonErrorStrings.size() == 1
-            ? ""
-            : AZStd::accumulate(m_pythonErrorStrings.begin(), m_pythonErrorStrings.end(), AZStd::string(""));
+        size_t errorSize = m_pythonErrorStrings.size();
+        if (errorSize > 0)
+        {
+            AZStd::string detailedString =
+                errorSize == 1 ? "" : AZStd::accumulate(m_pythonErrorStrings.begin(), m_pythonErrorStrings.end(), AZStd::string(""));
 
-        return IPythonBindings::ErrorPair(m_pythonErrorStrings.front(), detailedString);
+            return IPythonBindings::ErrorPair(m_pythonErrorStrings.front(), detailedString);
+        }
+        // If no error was found
+        else
+        {
+            return IPythonBindings::ErrorPair(AZStd::string("Unknown Error"), AZStd::string(""));
+        }
     }
 
     void PythonBindings::AddErrorString(AZStd::string errorString)
