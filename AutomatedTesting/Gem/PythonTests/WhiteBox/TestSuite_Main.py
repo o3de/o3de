6219527cf7e075846a5c2e3ceaebbd599882b130
Broganab <broganab@amazon.com>
2022-02-11 17:24:09 -0600

    updated with changes to add EditorSharedTest
    Signed-off-by: Broganab <broganab@amazon.com>

--- AutomatedTesting/Gem/PythonTests/WhiteBox/TestSuite_Main.py
+++ AutomatedTesting/Gem/PythonTests/WhiteBox/TestSuite_Main.py
@@ -9,17 +9,17 @@ SPDX-License-Identifier: Apache-2.0 OR MIT
 # This suite consists of all test cases that are passing and have been verified.
 
 import pytest
-from ly_test_tools.o3de.editor_test import EditorTestSuite, EditorSingleTest
+from ly_test_tools.o3de.editor_test import EditorTestSuite, EditorSharedTest
 
 @pytest.mark.SUITE_main
 @pytest.mark.parametrize("launcher_platform", ['windows_editor'])
 @pytest.mark.parametrize("project", ["AutomatedTesting"])
 class TestAutomation(EditorTestSuite):
-    class test_WhiteBox_AddComponentToEntity(EditorSingleTest):
+    class test_WhiteBox_AddComponentToEntity(EditorSharedTest):
         from .tests import WhiteBox_AddComponentToEntity as test_module
 
-    class test_WhiteBox_SetDefaultShape(EditorSingleTest):
+    class test_WhiteBox_SetDefaultShape(EditorSharedTest):
         from .tests import WhiteBox_SetDefaultShape as test_module
 
-    class test_WhiteBox_SetInvisible(EditorSingleTest):
+    class test_WhiteBox_SetInvisible(EditorSharedTest):
         from .tests import WhiteBox_SetInvisible as test_module
