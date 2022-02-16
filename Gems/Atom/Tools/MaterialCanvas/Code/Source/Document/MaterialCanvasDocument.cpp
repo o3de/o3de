ec5c68d524de36bd305d977590e53bc8e0d91fcd
Guthrie Adams <guthadam@amazon.com>
2022-02-16 08:55:23 -0600

    Cleaning up inspector and moving document reflection into document class
    Signed-off-by: Guthrie Adams <guthadam@amazon.com>

--- Gems/Atom/Tools/MaterialCanvas/Code/Source/Document/MaterialCanvasDocument.cpp
+++ Gems/Atom/Tools/MaterialCanvas/Code/Source/Document/MaterialCanvasDocument.cpp
@@ -10,10 +10,31 @@
 #include <Atom/RPI.Edit/Common/JsonUtils.h>
 #include <AtomToolsFramework/Document/AtomToolsDocumentNotificationBus.h>
 #include <AtomToolsFramework/Util/MaterialPropertyUtil.h>
+#include <AzCore/RTTI/BehaviorContext.h>
+#include <AzCore/Serialization/EditContext.h>
+#include <AzCore/Serialization/SerializeContext.h>
 #include <Document/MaterialCanvasDocument.h>
 
 namespace MaterialCanvas
 {
+    void MaterialCanvasDocument::Reflect(AZ::ReflectContext* context)
+    {
+        if (auto serialize = azrtti_cast<AZ::SerializeContext*>(context))
+        {
+            serialize->Class<MaterialCanvasDocument, AtomToolsFramework::AtomToolsDocument>()
+                ->Version(0);
+        }
+
+        if (auto behaviorContext = azrtti_cast<AZ::BehaviorContext*>(context))
+        {
+            behaviorContext->EBus<MaterialCanvasDocumentRequestBus>("MaterialCanvasDocumentRequestBus")
+                ->Attribute(AZ::Script::Attributes::Scope, AZ::Script::Attributes::ScopeFlags::Common)
+                ->Attribute(AZ::Script::Attributes::Category, "Editor")
+                ->Attribute(AZ::Script::Attributes::Module, "materialcanvas")
+                ;
+        }
+    }
+
     MaterialCanvasDocument::MaterialCanvasDocument(const AZ::Crc32& toolId)
         : AtomToolsFramework::AtomToolsDocument(toolId)
     {
