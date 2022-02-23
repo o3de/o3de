dffe20d50e2787c99bbbbe60fa2bcc926f6f98bd
Fuzzy Carter <jscar@amazon.com>
2022-02-23 14:56:33 -0800

    prefab conversion for test_ForceRegion_LocalSpaceForceOnRigidBodies
    Signed-off-by: Fuzzy Carter <jscar@amazon.com>

--- AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Main.py
+++ AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Main.py
@@ -35,7 +35,7 @@ class TestAutomation(TestAutomationBase):
     @revert_physics_config
     def test_ForceRegion_LocalSpaceForceOnRigidBodies(self, request, workspace, editor, launcher_platform):
         from .tests.force_region import ForceRegion_LocalSpaceForceOnRigidBodies as test_module
-        self._run_test(request, workspace, editor, test_module, enable_prefab_system=False)
+        self._run_test(request, workspace, editor, test_module)
 
     @revert_physics_config
     @fm.file_override('physxsystemconfiguration.setreg','Material_DynamicFriction.setreg_override',
