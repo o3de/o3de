2f8f55007ff303a1f3e805f6f5c15b936b17a967
Fuzzy Carter <jscar@amazon.com>
2022-02-24 13:13:28 -0800

    prefab conversion for shared level tests test_RigidBody_StartGravityEnabledWorks and C4976206_RigidBodies_GravityEnabledActive
    Signed-off-by: Fuzzy Carter <jscar@amazon.com>

--- AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Periodic.py
+++ AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Periodic.py
@@ -41,7 +41,7 @@ class TestAutomation(TestAutomationBase):
     @revert_physics_config
     def test_RigidBody_StartGravityEnabledWorks(self, request, workspace, editor, launcher_platform):
         from .tests.rigid_body import RigidBody_StartGravityEnabledWorks as test_module
-        self._run_test(request, workspace, editor, test_module, enable_prefab_system=False)
+        self._run_test(request, workspace, editor, test_module)
 
     @revert_physics_config
     def test_RigidBody_KinematicModeWorks(self, request, workspace, editor, launcher_platform):
