beb62bea40163453b006512348b32385e9da7e7b
Fuzzy Carter <jscar@amazon.com>
2022-02-28 10:30:35 -0800

    prefab conversion for shared level tests C5932045_ForceRegion_Spline and test_ForceRegion_SplineForceOnRigidBodies
    Signed-off-by: Fuzzy Carter <jscar@amazon.com>

--- AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Periodic.py
+++ AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Periodic.py
@@ -86,7 +86,7 @@ class TestAutomation(TestAutomationBase):
     @revert_physics_config
     def test_ForceRegion_SplineForceOnRigidBodies(self, request, workspace, editor, launcher_platform):
         from .tests.force_region import ForceRegion_SplineForceOnRigidBodies as test_module
-        self._run_test(request, workspace, editor, test_module, enable_prefab_system=False)
+        self._run_test(request, workspace, editor, test_module)
 
     @revert_physics_config
     @fm.file_override('physxsystemconfiguration.setreg','Material_RestitutionCombine.setreg_override',
