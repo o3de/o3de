a76efc6af2cebd8914f93907306e73e1cb8a92be
Fuzzy Carter <jscar@amazon.com>
2022-02-28 12:24:23 -0800

    prefab conversion for shared level tests C4976209_RigidBody_ComputesCOM and test_RigidBody_COM_ComputingWorks
    Signed-off-by: Fuzzy Carter <jscar@amazon.com>

--- AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Main_Optimized.py
+++ AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Main_Optimized.py
@@ -242,14 +242,17 @@ class TestAutomationWithPrefabSystemEnabled(EditorTestSuite):
     class C6090553_ForceRegion_SimpleDragForceOnRigidBodies(EditorSharedTest):
         from .tests.force_region import ForceRegion_ZeroSimpleDragForceDoesNothing as test_module
 
+    class C4976209_RigidBody_ComputesCOM(EditorSharedTest):
+        from .tests.rigid_body import RigidBody_COM_ComputingWorks as test_module
 
-#@pytest.mark.xfail(reason="Optimized tests are experimental, we will enable xfail and monitor them temporarily.")
+
+@pytest.mark.xfail(reason="Optimized tests are experimental, we will enable xfail and monitor them temporarily.")
 @pytest.mark.SUITE_main
 @pytest.mark.parametrize("launcher_platform", ['windows_editor'])
 @pytest.mark.parametrize("project", ["AutomatedTesting"])
 class TestAutomation(EditorTestSuite):
 
-    #enable_prefab_system = False
+    enable_prefab_system = False
 
     @staticmethod
     def get_number_parallel_editors():
@@ -260,9 +263,6 @@ class TestAutomation(EditorTestSuite):
 
 
 
-
-    class C4976209_RigidBody_ComputesCOM(EditorSharedTest):
-        from .tests.rigid_body import RigidBody_COM_ComputingWorks as test_module
 
     class C4976201_RigidBody_MassIsAssigned(EditorSharedTest):
         from .tests.rigid_body import RigidBody_MassDifferentValuesWorks as test_module
--- AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Periodic.py
+++ AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Periodic.py
@@ -186,7 +186,7 @@ class TestAutomation(TestAutomationBase):
     @revert_physics_config
     def test_RigidBody_COM_ComputingWorks(self, request, workspace, editor, launcher_platform):
         from .tests.rigid_body import RigidBody_COM_ComputingWorks as test_module
-        self._run_test(request, workspace, editor, test_module, enable_prefab_system=False)
+        self._run_test(request, workspace, editor, test_module)
 
     @revert_physics_config
     def test_RigidBody_MassDifferentValuesWorks(self, request, workspace, editor, launcher_platform):
