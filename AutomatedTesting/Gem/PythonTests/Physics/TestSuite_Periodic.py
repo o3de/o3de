5acb085c8f49054888f21bf372094e74f8773661
Fuzzy Carter <jscar@amazon.com>
2022-02-28 11:39:48 -0800

    prefab conversion for shared level tests C12712452_ScriptCanvas_CollisionEvents and test_ScriptCanvas_CollisionEvents
    Signed-off-by: Fuzzy Carter <jscar@amazon.com>

--- AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Periodic.py
+++ AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Periodic.py
@@ -156,7 +156,7 @@ class TestAutomation(TestAutomationBase):
     @revert_physics_config
     def test_ScriptCanvas_CollisionEvents(self, request, workspace, editor, launcher_platform):
         from .tests.script_canvas import ScriptCanvas_CollisionEvents as test_module
-        self._run_test(request, workspace, editor, test_module, enable_prefab_system=False)
+        self._run_test(request, workspace, editor, test_module)
 
     @revert_physics_config
     def test_ForceRegion_DirectionHasNoAffectOnTotalForce(self, request, workspace, editor, launcher_platform):
