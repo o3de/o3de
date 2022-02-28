c218d213bdc2d2745f2f20b16a5d0512f936ae0f
Fuzzy Carter <jscar@amazon.com>
2022-02-28 12:19:07 -0800

    prefab conversion for shared level tests C6090551_ForceRegion_LocalSpaceForceNegative and test_ForceRegion_ZeroLocalSpaceForceDoesNothing
    Signed-off-by: Fuzzy Carter <jscar@amazon.com>

--- AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Main_Optimized.py
+++ AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Main_Optimized.py
@@ -239,6 +239,9 @@ class TestAutomationWithPrefabSystemEnabled(EditorTestSuite):
     class C6090551_ForceRegion_LocalSpaceForceNegative(EditorSharedTest):
         from .tests.force_region import ForceRegion_ZeroLocalSpaceForceDoesNothing as test_module
 
+    class C6090553_ForceRegion_SimpleDragForceOnRigidBodies(EditorSharedTest):
+        from .tests.force_region import ForceRegion_ZeroSimpleDragForceDoesNothing as test_module
+
 
 @pytest.mark.xfail(reason="Optimized tests are experimental, we will enable xfail and monitor them temporarily.")
 @pytest.mark.SUITE_main
@@ -257,9 +260,6 @@ class TestAutomation(EditorTestSuite):
 
 
 
-
-    class C6090553_ForceRegion_SimpleDragForceOnRigidBodies(EditorSharedTest):
-        from .tests.force_region import ForceRegion_ZeroSimpleDragForceDoesNothing as test_module
 
     class C4976209_RigidBody_ComputesCOM(EditorSharedTest):
         from .tests.rigid_body import RigidBody_COM_ComputingWorks as test_module
--- AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Periodic.py
+++ AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Periodic.py
@@ -181,7 +181,7 @@ class TestAutomation(TestAutomationBase):
     @revert_physics_config
     def test_ForceRegion_ZeroSimpleDragForceDoesNothing(self, request, workspace, editor, launcher_platform):
         from .tests.force_region import ForceRegion_ZeroSimpleDragForceDoesNothing as test_module
-        self._run_test(request, workspace, editor, test_module, enable_prefab_system=False)
+        self._run_test(request, workspace, editor, test_module)
 
     @revert_physics_config
     def test_RigidBody_COM_ComputingWorks(self, request, workspace, editor, launcher_platform):
