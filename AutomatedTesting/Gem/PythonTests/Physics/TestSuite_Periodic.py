6b122a3f40d2a3aa04e82ea0ff7c3642c1c1edd7
Fuzzy Carter <jscar@amazon.com>
2022-02-28 10:17:47 -0800

    prefab conversion for shared level tests C4976210_COM_ManualSetting and test_RigidBody_COM_ManualSettingWorks
    Signed-off-by: Fuzzy Carter <jscar@amazon.com>

--- AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Periodic.py
+++ AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Periodic.py
@@ -76,7 +76,7 @@ class TestAutomation(TestAutomationBase):
     @revert_physics_config
     def test_RigidBody_COM_ManualSettingWorks(self, request, workspace, editor, launcher_platform):
         from .tests.rigid_body import RigidBody_COM_ManualSettingWorks as test_module
-        self._run_test(request, workspace, editor, test_module, enable_prefab_system=False)
+        self._run_test(request, workspace, editor, test_module)
 
     @revert_physics_config
     def test_RigidBody_AddRigidBodyComponent(self, request, workspace, editor, launcher_platform):
