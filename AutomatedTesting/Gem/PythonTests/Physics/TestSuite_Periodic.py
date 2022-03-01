96fa1aa2eec310c347b44e8b327f137f2353d7d5
Fuzzy Carter <jscar@amazon.com>
2022-02-28 16:36:06 -0800

    prefab conversion for shared level tests C5932044_ForceRegion_PointForceOnRigidBody and test_ForceRegion_PointForceOnRigidBodies
    Signed-off-by: Fuzzy Carter <jscar@amazon.com>

--- AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Periodic.py
+++ AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Periodic.py
@@ -421,7 +421,7 @@ class TestAutomation(TestAutomationBase):
 
     def test_ForceRegion_PointForceOnRigidBodies(self, request, workspace, editor, launcher_platform):
         from .tests.force_region import ForceRegion_PointForceOnRigidBodies as test_module
-        self._run_test(request, workspace, editor, test_module, enable_prefab_system=False)
+        self._run_test(request, workspace, editor, test_module)
 
     def test_ForceRegion_SphereShapedForce(self, request, workspace, editor, launcher_platform):
         from .tests.force_region import ForceRegion_SphereShapedForce as test_module
