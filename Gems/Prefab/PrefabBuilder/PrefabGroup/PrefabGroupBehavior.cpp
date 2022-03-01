cbb1ee8f6d8599ccd1c0b559a2f31d2351db3809
Allen Jackson <23512001+jackalbe@users.noreply.github.com>
2022-03-01 15:18:04 -0600

    better error message for value-type string mismatch
    Signed-off-by: Allen Jackson <23512001+jackalbe@users.noreply.github.com>

--- Gems/Prefab/PrefabBuilder/PrefabGroup/PrefabGroupBehavior.cpp
+++ Gems/Prefab/PrefabBuilder/PrefabGroup/PrefabGroupBehavior.cpp
@@ -202,7 +202,8 @@ namespace AZ::SceneAPI::Behaviors
             const AZStd::any& propertyMaterialPath = propertyMaterialPathIterator->second;
             if (propertyMaterialPath.empty() || propertyMaterialPath.is<AZStd::string>() == false)
             {
-                AZ_Error("prefab", false, "Must be a string");
+                AZ_Error("prefab", false, "The 'o3de.default.material' custom property value type must be a string."
+                                          "This will need to be fixed in the DCC tool and re-export the file asset.");
                 return false;
             }
 
