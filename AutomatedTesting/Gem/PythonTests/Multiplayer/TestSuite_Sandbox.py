eddc2ca839ed9f219638a2557eeadd2af3953adb
Gene Walters <genewalt@amazon.com>
2022-03-04 05:34:44 -0800

    Removed unused lib import
    Signed-off-by: Gene Walters <genewalt@amazon.com>

--- AutomatedTesting/Gem/PythonTests/Multiplayer/TestSuite_Sandbox.py
+++ AutomatedTesting/Gem/PythonTests/Multiplayer/TestSuite_Sandbox.py
@@ -13,7 +13,6 @@ import pytest
 import os
 import sys
 from ly_test_tools.o3de.editor_test import EditorTestSuite, EditorSingleTest
-import ly_test_tools.environment.process_utils as process_utils
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../automatedtesting_shared')
 
