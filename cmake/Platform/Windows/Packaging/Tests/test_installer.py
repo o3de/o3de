7704d9a6f75794bbfd3efbb45a286f361e2d40e5
Alex Peterson <26804013+AMZN-alexpete@users.noreply.github.com>
2022-03-04 18:00:20 -0800

    fix uninstall folder check and refactor
    Signed-off-by: Alex Peterson <26804013+AMZN-alexpete@users.noreply.github.com>

--- cmake/Platform/Windows/Packaging/Tests/test_installer.py
+++ cmake/Platform/Windows/Packaging/Tests/test_installer.py
@@ -66,8 +66,10 @@ def context(request):
     yield session_context
     session_context.cleanup()
 
+
 @pytest.fixture(scope="session")
-def test_installer(context):
+def test_installer_fixture(context):
+    """Installer executable succeeds"""
     assert context.installer_path.is_file(), f"Invalid installer path {context.installer_path}"
 
     # when the installer is run with timeout of 30 minutes
@@ -87,11 +89,13 @@ def test_installer(context):
         pytest.param('o3de.exe'),
     ]
 )
-def test_binaries_exist(test_installer, context, filename):
+def test_binaries_exist(test_installer_fixture, context, filename):
+    """Installer key binaries exist"""
     assert (context.engine_bin_path / filename).is_file(), f"{filename} not found in {context.engine_bin_path}"
 
 @pytest.fixture(scope="session")
-def test_o3de_registers_engine_fixture(test_installer, context):
+def test_o3de_registers_engine_fixture(test_installer_fixture, context):
+    """Engine is registered when o3de.exe is run"""
     try:
         result = context.run([str(context.engine_bin_path / 'o3de.exe')], timeout=7)
 
@@ -117,22 +121,15 @@ def test_o3de_registers_engine_fixture(test_installer, context):
     assert engine_name in manifest_json_data['engines_path'], f"{engine_path} not found in manifest engines_path"
     assert manifest_json_data['engines_path'][engine_name] == engine_path, f"Engines path has invalid entry for {engine_name} expected {engine_path}" 
 
-def test_o3de_registers_engine(test_o3de_registers_engine_fixture):
-    """Utility test to run the fixture test from Visual Code Test Explorer"""
-    assert True
-
 @pytest.fixture(scope="session")
 def test_create_project_fixture(test_o3de_registers_engine_fixture, context):
+    """o3de CLI creates a project"""
     o3de_path = context.install_root / 'scripts' / 'o3de.bat'
     result = context.run([str(o3de_path),'create-project','--project-path', str(context.project_path)])
     assert result.returncode == 0, f"o3de.bat failed to create a project with exit code {result.returncode}"
 
     project_json_path = Path(context.project_path) / 'project.json'
     assert project_json_path.is_file(), f"No project.json found at {project_json_path}"
-
-def test_create_project(test_create_project_fixture):
-    """Utility test to run the fixture test from Visual Code Test Explorer"""
-    assert True
 
 @pytest.fixture(scope="session")
 def test_compile_project_fixture(test_create_project_fixture, context):
@@ -150,10 +147,6 @@ def test_compile_project_fixture(test_create_project_fixture, context):
     assert result.returncode == 0
     assert (context.project_build_path / 'bin' / 'profile' / f'{launcher_target}.exe').is_file()
 
-def test_compile_project(test_compile_project_fixture):
-    """Utility test to run the fixture test from Visual Code Test Explorer"""
-    assert True
-
 @pytest.fixture(scope="session")
 def test_run_asset_processor_batch_fixture(test_compile_project_fixture, context):
     """Test asset batch processing before running Editor and Launcher tests which need assets"""
@@ -162,10 +155,6 @@ def test_run_asset_processor_batch_fixture(test_compile_project_fixture, context
         assert result.returncode == 0, f"AssetProcessorBatch.exe failed with exit code {result.returncode}"
     except TimeoutExpired as e:
         pass
-
-def test_run_asset_processor_batch(test_run_asset_processor_batch_fixture):
-    """Utility test to run the fixture test from Visual Code Test Explorer"""
-    assert True
 
 @pytest.fixture(scope="session")
 def test_run_editor_fixture(test_run_asset_processor_batch_fixture, context):
@@ -202,10 +191,6 @@ def test_run_editor_fixture(test_run_asset_processor_batch_fixture, context):
 
     assert engine_initialized_message_found, "Engine initialized message not found in editor.log"
 
-def test_run_editor(test_run_editor_fixture):
-    """Utility test to run the fixture test from Visual Code Test Explorer"""
-    assert True
-
 @pytest.fixture(scope="session")
 def test_run_launcher_fixture(test_run_asset_processor_batch_fixture, context):
     """Game launcher can be run without crashing"""
@@ -229,12 +214,8 @@ def test_run_launcher_fixture(test_run_asset_processor_batch_fixture, context):
     
     assert not exception_found, "Exception found in Game.log"
 
-def test_run_launcher(test_run_launcher_fixture):
-    """Utility test to run the fixture test from Visual Code Test Explorer"""
-    assert True
-
 def test_uninstall(test_run_launcher_fixture, test_run_editor_fixture, context):
-
+    """Uninstall succeeds and unregisters the engine"""
     assert context.installer_path.is_file(), f"Invalid installer path {context.installer_path}"
 
     engine_json_path = context.install_root / 'engine.json'
@@ -259,3 +240,26 @@ def test_uninstall(test_run_launcher_fixture, test_run_editor_fixture, context):
     engine_path = Path(context.install_root).as_posix()
     assert engine_path not in manifest_json_data['engines'] , f"Engine path {engine_path} still exists in o3de_manifest.json"
     assert engine_name not in manifest_json_data['engines_path'], f"{engine_path} still exists in manifest engines_path"
+
+
+# Convenience functions for running test fixtures from VS Code Test Explorer
+def test_installer(test_installer_fixture):
+    assert True
+
+def test_o3de_registers_engine(test_o3de_registers_engine_fixture):
+    assert True
+
+def test_create_project(test_create_project_fixture):
+    assert True
+
+def test_compile_project(test_compile_project_fixture):
+    assert True
+
+def test_run_asset_processor_batch(test_run_asset_processor_batch_fixture):
+    assert True
+
+def test_run_editor(test_run_editor_fixture):
+    assert True
+
+def test_run_launcher(test_run_launcher_fixture):
+    assert True
