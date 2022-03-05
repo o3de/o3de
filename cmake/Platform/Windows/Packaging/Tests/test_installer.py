1944025f542bd67b223294e86e7b46e43a3815ca
Alex Peterson <26804013+AMZN-alexpete@users.noreply.github.com>
2022-03-04 17:10:32 -0800

    condense fixtures and add logging
    Signed-off-by: Alex Peterson <26804013+AMZN-alexpete@users.noreply.github.com>

--- cmake/Platform/Windows/Packaging/Tests/test_installer.py
+++ cmake/Platform/Windows/Packaging/Tests/test_installer.py
@@ -6,6 +6,14 @@ SPDX-License-Identifier: Apache-2.0 OR MIT
 
 Pytest test configuration file.
 
+Example method to run these tests:
+
+pytest cmake/Platform/Windows/Packaging/Tests -s \
+    --log-file=C:/workspace/test_installer.log \
+    --installer-path=C:/path/to/o3de_installer.exe \
+    --install-root=C:/O3DE/0.0.0.0 \
+    --project-path=C:/workspace/TestProject
+
 """
 import pytest
 from pathlib import Path
@@ -14,61 +22,62 @@ from time import sleep
 import json
 import os
 import shutil
+from tempfile import NamedTemporaryFile
+
+class SessionContext:
+    def __init__(self, request):
+        self.installer_path = Path(request.config.getoption("--installer-path")).resolve()
+        self.install_root = Path(request.config.getoption("--install-root")).resolve()
+        self.project_path = Path(request.config.getoption("--project-path")).resolve()
+        self.engine_bin_path = self.install_root / 'bin' / 'Windows' / 'profile' / 'Default'
+        self.project_build_path = self.project_path / 'build' / 'Windows'
+        self.project_bin_path = self.project_build_path / 'bin' / 'profile'
+
+        cmake_runtime_path = self.install_root / 'cmake' / 'runtime'
+        self.cmake_path = next(cmake_runtime_path.glob('**/cmake.exe'))
+
+        self.log_file = request.config.getoption("--log-file")
+        self.temp_file = NamedTemporaryFile(mode='w+t', delete=False)
+
+    def run(self, command, timeout=None, cwd=None):
+        self.temp_file.write(' '.join(command) + '\n')
+        self.temp_file.flush()
+        return run(command, timeout=timeout, cwd=cwd, stdout=self.temp_file, stderr=self.temp_file, text=True)
+
+    def cleanup(self):
+        if self.project_path.is_dir():
+            # wait a few seconds for processes to stop using the project folder 
+            sleep(5)
+
+            o3de_path = self.install_root / 'scripts' / 'o3de.bat'
+            if o3de_path.is_file():
+                self.run([str(o3de_path),'register','--project-path', str(self.project_path), '--remove'])
+
+            shutil.rmtree(self.project_path)
+
+        self.temp_file.close()
+        if self.log_file:
+            shutil.copy(self.temp_file.name, Path(self.log_file).resolve())
+        os.remove(self.temp_file.name)
 
 @pytest.fixture(scope="session")
-def installer_path(request):
-    return Path(request.config.getoption("--installer-path")).resolve()
+def context(request):
+    session_context = SessionContext(request)
+    yield session_context
+    session_context.cleanup()
 
 @pytest.fixture(scope="session")
-def install_root(request):
-    return Path(request.config.getoption("--install-root")).resolve()
-
-@pytest.fixture(scope="session")
-def engine_bin_path(request, install_root):
-    return install_root / 'bin' / 'Windows' / 'profile' / 'Default'
-
-@pytest.fixture(scope="session")
-def log_path(request):
-    return Path(request.config.getoption("--log-path")).resolve()
-
-@pytest.fixture(scope="session")
-def project_path(request):
-    # MSBuild doesn't want us to output in the temp directory MSB8029
-    # so we use a pre-defined folder outside of %TMP%
-    #return tmpdir_factory.mktemp("TestProject")
-    path = Path(request.config.getoption("--project-path")).resolve()
-    yield path
-
-    # wait a few seconds for processes to quit
-    sleep(5)
-
-    shutil.rmtree(path)
-
-@pytest.fixture(scope="session")
-def project_build_path(request, project_path):
-    return project_path / 'build' / 'Windows'
-
-@pytest.fixture(scope="session")
-def project_bin_path(request, project_build_path):
-    return project_build_path / 'bin' / 'profile'
-
-@pytest.fixture(scope="session")
-def cmake_path(install_root):
-    cmake_runtime_path = install_root / 'cmake' / 'runtime'
-    return cmake_runtime_path.glob('**/cmake.exe')
-
-@pytest.fixture(scope="session")
-def test_installer(installer_path, install_root):
-    assert installer_path.is_file(), f"Invalid installer path {installer_path}"
+def test_installer(context):
+    assert context.installer_path.is_file(), f"Invalid installer path {context.installer_path}"
 
     # when the installer is run with timeout of 30 minutes
-    result = run([installer_path , f"InstallFolder={install_root}", "/quiet"], timeout=30*60)
+    result = context.run([str(context.installer_path) , f"InstallFolder={context.install_root}", "/quiet"], timeout=30*60)
 
     # the installer succeeds
     assert result.returncode == 0, f"Installer failed with exit code {result.returncode}"
 
     # the install root is created
-    assert install_root.is_dir(), f"Invalid install root {install_root}"
+    assert context.install_root.is_dir(), f"Invalid install root {context.install_root}"
 
 @pytest.mark.parametrize(
     "filename", [
@@ -78,14 +87,13 @@ def test_installer(installer_path, install_root):
         pytest.param('o3de.exe'),
     ]
 )
-def test_binaries_exist(test_installer, install_root, filename, engine_bin_path):
-    assert (engine_bin_path / filename).is_file(), f"{filename} not found in {engine_bin_path}"
-
+def test_binaries_exist(test_installer, context, filename):
+    assert (context.engine_bin_path / filename).is_file(), f"{filename} not found in {context.engine_bin_path}"
 
 @pytest.fixture(scope="session")
-def test_o3de_registers_engine_fixture(install_root, test_installer, engine_bin_path):
+def test_o3de_registers_engine_fixture(test_installer, context):
     try:
-        result = run([str(engine_bin_path / 'o3de.exe')], timeout=7)
+        result = context.run([str(context.engine_bin_path / 'o3de.exe')], timeout=7)
 
         # o3de.exe should not close with an error code 
         assert result.returncode == 0, f"o3de.exe failed with exit code {result.returncode}"
@@ -94,7 +102,7 @@ def test_o3de_registers_engine_fixture(install_root, test_installer, engine_bin_
         pass
 
     # the engine is registered
-    engine_json_path = Path(install_root).resolve() / 'engine.json'
+    engine_json_path = context.install_root / 'engine.json'
     with engine_json_path.open('r') as f:
         engine_json_data = json.load(f)
 
@@ -104,7 +112,7 @@ def test_o3de_registers_engine_fixture(install_root, test_installer, engine_bin_
     with manifest_path.open('r') as f:
         manifest_json_data = json.load(f)
     
-    engine_path = Path(install_root).as_posix()
+    engine_path = Path(context.install_root).as_posix()
     assert engine_path in manifest_json_data['engines'] , f"Engine path {engine_path} not found in o3de_manifest.json"
     assert engine_name in manifest_json_data['engines_path'], f"{engine_path} not found in manifest engines_path"
     assert manifest_json_data['engines_path'][engine_name] == engine_path, f"Engines path has invalid entry for {engine_name} expected {engine_path}" 
@@ -114,12 +122,12 @@ def test_o3de_registers_engine(test_o3de_registers_engine_fixture):
     assert True
 
 @pytest.fixture(scope="session")
-def test_create_project_fixture(install_root, project_path, test_o3de_registers_engine_fixture):
-    o3de_path = install_root / 'scripts' / 'o3de.bat'
-    result = run([str(o3de_path),'create-project','--project-path', str(project_path)])
+def test_create_project_fixture(test_o3de_registers_engine_fixture, context):
+    o3de_path = context.install_root / 'scripts' / 'o3de.bat'
+    result = context.run([str(o3de_path),'create-project','--project-path', str(context.project_path)])
     assert result.returncode == 0, f"o3de.bat failed to create a project with exit code {result.returncode}"
 
-    project_json_path = Path(project_path) / 'project.json'
+    project_json_path = Path(context.project_path) / 'project.json'
     assert project_json_path.is_file(), f"No project.json found at {project_json_path}"
 
 def test_create_project(test_create_project_fixture):
@@ -127,30 +135,30 @@ def test_create_project(test_create_project_fixture):
     assert True
 
 @pytest.fixture(scope="session")
-def test_compile_project_fixture(install_root, project_path, test_create_project_fixture, cmake_path, project_build_path):
+def test_compile_project_fixture(test_create_project_fixture, context):
     """Project can be configured and compiled"""
-    project_name = Path(project_path).name
+    project_name = Path(context.project_path).name
     launcher_target = f"{project_name}.GameLauncher"
 
     # configure
-    result = run([str(cmake_path),'-B', str(project_build_path), '-S', '.', '-G', 'Visual Studio 16 2019'], cwd=project_path, capture_output=True)
+    result = context.run([str(context.cmake_path),'-B', str(context.project_build_path), '-S', '.', '-G', 'Visual Studio 16 2019'], cwd=context.project_path)
     assert result.returncode == 0
-    assert (project_build_path / f'{project_name}.sln').is_file()
+    assert (context.project_build_path / f'{project_name}.sln').is_file()
 
     # build profile
-    result = run([str(cmake_path),'--build', str(project_build_path), '--target', launcher_target, 'Editor', '--config', 'profile','--','-m'], cwd=project_path)
+    result = context.run([str(context.cmake_path),'--build', str(context.project_build_path), '--target', launcher_target, 'Editor', '--config', 'profile','--','-m'], cwd=context.project_path)
     assert result.returncode == 0
-    assert (project_build_path / 'bin' / 'profile' / f'{launcher_target}.exe').is_file()
+    assert (context.project_build_path / 'bin' / 'profile' / f'{launcher_target}.exe').is_file()
 
 def test_compile_project(test_compile_project_fixture):
     """Utility test to run the fixture test from Visual Code Test Explorer"""
     assert True
 
 @pytest.fixture(scope="session")
-def test_run_asset_processor_batch_fixture(install_root, project_path, test_compile_project_fixture, engine_bin_path):
+def test_run_asset_processor_batch_fixture(test_compile_project_fixture, context):
     """Test asset batch processing before running Editor and Launcher tests which need assets"""
     try:
-        result = run([str(engine_bin_path / 'AssetProcessorBatch.exe'),f'--project-path="{project_path}"','/platform=pc'], timeout=30*60)
+        result = context.run([str(context.engine_bin_path / 'AssetProcessorBatch.exe'),f'--project-path="{context.project_path}"','/platform=pc'], timeout=30*60)
         assert result.returncode == 0, f"AssetProcessorBatch.exe failed with exit code {result.returncode}"
     except TimeoutExpired as e:
         pass
@@ -160,7 +168,7 @@ def test_run_asset_processor_batch(test_run_asset_processor_batch_fixture):
     assert True
 
 @pytest.fixture(scope="session")
-def test_run_editor_fixture(install_root, project_path, test_run_asset_processor_batch_fixture, engine_bin_path):
+def test_run_editor_fixture(test_run_asset_processor_batch_fixture, context):
     """Editor can be run without crashing"""
     # write out the attribution shown to avoid the popup stalling editor load
     aws_attribution_regset = {
@@ -173,18 +181,18 @@ def test_run_editor_fixture(install_root, project_path, test_run_asset_processor
                 }
             }
         }
-    aws_attribution_path = Path(project_path).resolve() / 'user' / 'Registry' / 'editor_aws_preferences.setreg'
+    aws_attribution_path = context.project_path / 'user' / 'Registry' / 'editor_aws_preferences.setreg'
     with aws_attribution_path.open('w') as f:
         json.dump(aws_attribution_regset, f) 
     
     try:
         # run Editor.exe for 2 mins 
-        result = run([str(engine_bin_path / 'Editor.exe'),f'--project-path="{project_path}"','--rhi=null','--skipWelcomeScreenDialog=True','+wait_seconds','10','+quit'], timeout=2*60)
+        result = context.run([str(context.engine_bin_path / 'Editor.exe'),f'--project-path="{context.project_path}"','--rhi=null','--skipWelcomeScreenDialog=True','+wait_seconds','10','+quit'], timeout=2*60)
         assert result.returncode == 0, f"Editor.exe failed with exit code {result.returncode}"
     except TimeoutExpired as e:
         pass
 
-    editor_log = Path(project_path).resolve() / 'user'/ 'log'/ 'Editor.log'
+    editor_log = context.project_path / 'user'/ 'log'/ 'Editor.log'
     assert editor_log.is_file(), "Editor.log file not found"
 
     # expect to find "Engine initialized" in Editor log
@@ -199,20 +207,20 @@ def test_run_editor(test_run_editor_fixture):
     assert True
 
 @pytest.fixture(scope="session")
-def test_run_launcher_fixture(install_root, project_path, test_run_asset_processor_batch_fixture, project_bin_path):
+def test_run_launcher_fixture(test_run_asset_processor_batch_fixture, context):
     """Game launcher can be run without crashing"""
-    project_name = Path(project_path).name
+    project_name = Path(context.project_path).name
     launcher_filename = f"{project_name}.GameLauncher.exe"
     try:
         # run launcher for 2 mins 
-        result = run([str(launcher_filename),'--rhi=null'], cwd=project_bin_path, timeout=2*60)
+        result = context.run([str(context.project_bin_path / launcher_filename),'--rhi=null'], cwd=context.project_bin_path, timeout=2*60)
         assert result.returncode == 0, f"{launcher_filename} failed with exit code {result.returncode}"
     except TimeoutExpired as e:
         # we expect to close the app on timeout ourselves
         # deferred console commands are disabled in launcher 
         pass
 
-    game_log = Path(project_path).resolve() / 'user'/ 'log'/ 'Game.log'
+    game_log = context.project_path / 'user'/ 'log'/ 'Game.log'
     assert game_log.is_file(), "Game.log file not found"
 
     exception_found = False
@@ -225,29 +233,29 @@ def test_run_launcher(test_run_launcher_fixture):
     """Utility test to run the fixture test from Visual Code Test Explorer"""
     assert True
 
-def test_uninstall(installer_path, install_root, test_run_launcher_fixture, test_run_editor_fixture):
+def test_uninstall(test_run_launcher_fixture, test_run_editor_fixture, context):
 
-    assert installer_path.is_file(), f"Invalid installer path {installer_path}"
+    assert context.installer_path.is_file(), f"Invalid installer path {context.installer_path}"
 
-    engine_json_path = Path(install_root).resolve() / 'engine.json'
+    engine_json_path = context.install_root / 'engine.json'
     with engine_json_path.open('r') as f:
         engine_json_data = json.load(f)
 
     engine_name = engine_json_data['engine_name']
 
     # when the installer is run with timeout of 30 minutes
-    result = run([installer_path , f"InstallFolder={install_root}", "/quiet","/uninstall"], timeout=30*60)
+    result = context.run([str(context.installer_path) , f"InstallFolder={context.install_root}", "/quiet","/uninstall"], timeout=30*60)
 
     # the installer succeeds
     assert result.returncode == 0, f"Installer failed with exit code {result.returncode}"
 
     # the install root is created
-    assert install_root.is_dir(), f"Invalid install root {install_root}"
+    assert context.install_root.is_dir(), f"Invalid install root {context.install_root}"
 
     manifest_path = Path(os.path.expanduser("~")).resolve() / '.o3de' / 'o3de_manifest.json'
     with manifest_path.open('r') as f:
         manifest_json_data = json.load(f)
     
-    engine_path = Path(install_root).as_posix()
+    engine_path = Path(context.install_root).as_posix()
     assert engine_path not in manifest_json_data['engines'] , f"Engine path {engine_path} still exists in o3de_manifest.json"
     assert engine_name not in manifest_json_data['engines_path'], f"{engine_path} still exists in manifest engines_path"
