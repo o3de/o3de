8e8275fa1a89b46d1bc70bbd8c39e787a6f4486a
chiyenteng <82238204+chiyenteng@users.noreply.github.com>
2022-03-10 15:31:04 -0800

    Convert ScriptCanvas_ShapeCast test to use prefab system
    Signed-off-by: chiyenteng <82238204+chiyenteng@users.noreply.github.com>

--- AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Periodic.py
+++ AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Periodic.py
@@ -208,7 +208,7 @@ class TestAutomation(TestAutomationBase):
     @revert_physics_config
     def test_ScriptCanvas_ShapeCast(self, request, workspace, editor, launcher_platform):
         from .tests.script_canvas import ScriptCanvas_ShapeCast as test_module
-        self._run_test(request, workspace, editor, test_module, enable_prefab_system=False)
+        self._run_test(request, workspace, editor, test_module)
 
     @revert_physics_config
     def test_RigidBody_InitialAngularVelocity(self, request, workspace, editor, launcher_platform):
