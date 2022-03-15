16ac71a488d373cf981d6ee1ebd66c6951853897
T.J. McGrath-Daly <tj.mcgrath.daly@huawei.com>
2022-03-15 09:09:31 +0000

    tab removal
    Signed-off-by: T.J. McGrath-Daly <tj.mcgrath.daly@huawei.com>

--- Gems/Atom/Feature/Common/Code/atom_feature_common_files.cmake
+++ Gems/Atom/Feature/Common/Code/atom_feature_common_files.cmake
@@ -204,8 +204,8 @@ set(FILES
     Source/PostProcess/ColorGrading/HDRColorGradingSettings.cpp
     Source/PostProcess/Bloom/BloomSettings.cpp
     Source/PostProcess/Bloom/BloomSettings.h
-	Source/PostProcess/ChromaticAberration/ChromaticAberrationSettings.cpp
-	Source/PostProcess/ChromaticAberration/ChromaticAberrationSettings.h
+    Source/PostProcess/ChromaticAberration/ChromaticAberrationSettings.cpp
+    Source/PostProcess/ChromaticAberration/ChromaticAberrationSettings.h
     Source/PostProcess/DepthOfField/DepthOfFieldSettings.cpp
     Source/PostProcess/DepthOfField/DepthOfFieldSettings.h
     Source/PostProcess/ExposureControl/ExposureControlSettings.cpp
@@ -226,8 +226,8 @@ set(FILES
     Source/PostProcessing/BloomCompositePass.cpp
     Source/PostProcessing/BloomParentPass.h
     Source/PostProcessing/BloomParentPass.cpp
-	Source/PostProcessing/ChromaticAberrationPass.h
-	Source/PostProcessing/ChromaticAberrationPass.cpp
+    Source/PostProcessing/ChromaticAberrationPass.h
+    Source/PostProcessing/ChromaticAberrationPass.cpp
     Source/PostProcessing/DepthOfFieldCompositePass.h
     Source/PostProcessing/DepthOfFieldCompositePass.cpp
     Source/PostProcessing/DepthOfFieldBokehBlurPass.h
