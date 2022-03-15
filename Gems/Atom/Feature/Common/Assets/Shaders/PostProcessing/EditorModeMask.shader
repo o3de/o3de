41746f5838453004f12cfe357e7fc181e294ed2a
John <jonawals@amazon.co.uk>
2022-03-15 17:24:56 +0000

    Add depth fix and new outline mode.
    Signed-off-by: John <jonawals@amazon.co.uk>

--- Gems/Atom/Feature/Common/Assets/Shaders/PostProcessing/EditorModeMask.shader
+++ Gems/Atom/Feature/Common/Assets/Shaders/PostProcessing/EditorModeMask.shader
@@ -2,16 +2,15 @@
     "Source" : "EditorModeMask.azsl",
  
     "DepthStencilState" : { 
-        "Depth" : { "Enable" : true, "CompareFunc" : "GreaterEqual" }
+        "Depth" : { "Enable" : true, "CompareFunc" : "Always" }
     },
 
-    "DrawList" : "editormodemask",
-
-    //"RasterState" :
-    //{
-    //    "depthBias" : "10",
-    //    "depthBiasSlopeScale" : "4"
-    //},
+    "BlendState" : {
+        "Enable" : true,
+        "BlendSource" : "AlphaSource",
+        "BlendDest" : "AlphaSourceInverse",
+        "BlendOp" : "Maximum"
+    },
 
     "ProgramSettings":
     {
--- Gems/Atom/Feature/Common/Code/Source/PostProcessing/EditorModeOutlinePass.h
+++ Gems/Atom/Feature/Common/Code/Source/PostProcessing/EditorModeOutlinePass.h
@@ -18,6 +18,12 @@ namespace AZ
             : public EditorModeFeedbackPassBase
         {
         public:
+            enum class LineMode : AZ::u32
+            {
+                OutlineAlways,
+                OutlineVisible
+            };
+
             AZ_RTTI(EditorModeOutlinePass, "{5DEBA4FC-6BB3-417B-B052-7CB87EF15F84}", EditorModeFeedbackPassBase);
             AZ_CLASS_ALLOCATOR(EditorModeOutlinePass, SystemAllocator, 0);
 
@@ -32,6 +38,9 @@ namespace AZ
             //! Sets the outline line color.
             void SetLineColor(AZ::Color color);
 
+            //! Sets the outline mode.
+            void SetLineMode(LineMode mode);
+
         protected:
             EditorModeOutlinePass(const RPI::PassDescriptor& descriptor);
             
@@ -45,8 +54,10 @@ namespace AZ
 
             RHI::ShaderInputNameIndex m_lineThicknessIndex = "m_lineThickness";
             RHI::ShaderInputNameIndex m_lineColorIndex = "m_lineColor";
+            RHI::ShaderInputNameIndex m_lineModeIndex = "m_lineMode";
             float m_lineThickness = 3.0f; //!< Default line thickness for the outline effect.
             AZ::Color m_lineColor = AZ::Color(0.96f, 0.65f, 0.13f, 1.0f); //!< Default line color for the outline effect.
+            LineMode m_lineMode = LineMode::OutlineAlways;
         };
     }   // namespace Render
 }   // namespace AZ
