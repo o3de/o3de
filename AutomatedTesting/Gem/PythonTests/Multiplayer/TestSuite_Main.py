c0e61242c2a0a27f17ca1903bf0fd012cb23c3bc
Gene Walters <genewalt@amazon.com>
2022-03-16 15:55:38 -0700

    Fail pytest if the multiplayer level cache isn't found; we store level asset cache to debug any multiplayer test failures
    Signed-off-by: Gene Walters <genewalt@amazon.com>

--- AutomatedTesting/Gem/PythonTests/Multiplayer/TestSuite_Main.py
+++ AutomatedTesting/Gem/PythonTests/Multiplayer/TestSuite_Main.py
@@ -13,6 +13,14 @@ from ly_test_tools.o3de.editor_test import EditorTestSuite, EditorSingleTest
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../automatedtesting_shared')
 
+def save_multiplayer_level_cache_folder_artifact(workspace, multiplayer_level):
+    level_cache_folder_path = os.path.join(workspace.paths.platform_cache(), "levels", "multiplayer", multiplayer_level)
+
+    if os.path.exists(level_cache_folder_path):
+        workspace.artifact_manager.save_artifact(level_cache_folder_path)
+    else:
+        pytest.fail(f"Failed to find level asset cache for '{multiplayer_level}', located here: '{level_cache_folder_path}'! Make sure AssetProcessor successfully built the level.")
+
 @pytest.mark.SUITE_main
 @pytest.mark.parametrize("project", ["AutomatedTesting"])
 @pytest.mark.parametrize("launcher_platform", ['windows_editor'])
@@ -22,34 +30,26 @@ class TestAutomation(EditorTestSuite):
 
         @classmethod
         def setup(cls, instance, request, workspace, editor, editor_test_results, launcher_platform):
-            level_cache_folder_path = os.path.join(workspace.paths.platform_cache(), "levels", "multiplayer", "autocomponent_networkinput")
-            if os.path.exists(level_cache_folder_path):
-                workspace.artifact_manager.save_artifact(level_cache_folder_path)
+            save_multiplayer_level_cache_folder_artifact(workspace, "autocomponent_networkinput")
 
     class test_Multiplayer_AutoComponent_RPC(EditorSingleTest):
         from .tests import Multiplayer_AutoComponent_RPC as test_module
 
         @classmethod
         def setup(cls, instance, request, workspace, editor, editor_test_results, launcher_platform):
-            level_cache_folder_path = os.path.join(workspace.paths.platform_cache(), "levels", "multiplayer", "autocomponent_rpc")
-            if os.path.exists(level_cache_folder_path):
-                workspace.artifact_manager.save_artifact(level_cache_folder_path)
+            save_multiplayer_level_cache_folder_artifact(workspace, "autocomponent_rpc")
 
     class test_Multiplayer_BasicConnectivity_Connects(EditorSingleTest):
         from .tests import Multiplayer_BasicConnectivity_Connects as test_module
 
         @classmethod
         def setup(cls, instance, request, workspace, editor, editor_test_results, launcher_platform):
-            level_cache_folder_path = os.path.join(workspace.paths.platform_cache(), "levels", "multiplayer", "basicconnectivity_connects")
-            if os.path.exists(level_cache_folder_path):
-                workspace.artifact_manager.save_artifact(level_cache_folder_path)
+            save_multiplayer_level_cache_folder_artifact(workspace, "basicconnectivity_connects")
 
     class test_Multiplayer_SimpleNetworkLevelEntity(EditorSingleTest):
         from .tests import Multiplayer_SimpleNetworkLevelEntity as test_module
 
         @classmethod
         def setup(cls, instance, request, workspace, editor, editor_test_results, launcher_platform):
-            level_cache_folder_path = os.path.join(workspace.paths.platform_cache(), "levels", "multiplayer", "simplenetworklevelentity")
-            if os.path.exists(level_cache_folder_path):
-                workspace.artifact_manager.save_artifact(level_cache_folder_path)
+            save_multiplayer_level_cache_folder_artifact(workspace, "simplenetworklevelentity")
 
