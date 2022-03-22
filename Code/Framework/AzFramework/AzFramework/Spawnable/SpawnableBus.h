6419e1d566b39bc95645f75dffb43bcb96025189
Mikhail Naumov <mnaumov@amazon.com>
2022-03-22 14:58:14 -0500

    Adding comments
    Signed-off-by: Mikhail Naumov <mnaumov@amazon.com>

--- Code/Framework/AzFramework/AzFramework/Spawnable/SpawnableBus.h
+++ Code/Framework/AzFramework/AzFramework/Spawnable/SpawnableBus.h
@@ -15,6 +15,7 @@
 
 namespace AzFramework
 {
+    // Allows EBus calls to Spawnable API
     class SpawnableRequests
         : public AZ::EBusTraits
     {
@@ -22,8 +23,11 @@ namespace AzFramework
         // Only a single handler is allowed
         static const AZ::EBusHandlerPolicy HandlerPolicy = AZ::EBusHandlerPolicy::Single;
 
+        //! Creates EntitySpawnTicket using provided prefab asset
         virtual EntitySpawnTicket CreateSpawnTicket(const SpawnableAssetRef& spawnableAsset) = 0;
 
+        //! Spawns a prefab and places it relative to provided parent,
+        //! if no parentId is specified then places it in world coordinates
         virtual bool Spawn(
             EntitySpawnTicket spawnTicket,
             AZ::EntityId parentId,
@@ -31,11 +35,13 @@ namespace AzFramework
             AZ::Vector3 rotation,
             float scale) = 0;
 
+        //! Despawns a prefab
         virtual bool Despawn(EntitySpawnTicket spawnTicket) = 0;
     };
 
     using SpawnableRequestsBus = AZ::EBus<SpawnableRequests>;
 
+    // Provides spawn notifications from Spawnable API
     class SpawnableNotifications
         : public AZ::EBusTraits
     {
@@ -44,10 +50,12 @@ namespace AzFramework
         using BusIdType = EntitySpawnTicket::Id;
         static const AZ::EBusAddressPolicy AddressPolicy = AZ::EBusAddressPolicy::ById;
 
+        //! Notifies that spawn operation was completed
         virtual void OnSpawn(
             [[maybe_unused]] EntitySpawnTicket spawnTicket,
             [[maybe_unused]] AZStd::vector<AZ::EntityId> entityList) {}
 
+        //! Notifies that despawn operation was completed
         virtual void OnDespawn([[maybe_unused]] EntitySpawnTicket spawnTicket) {}
     };
     
