7eb3f8dcc1241d446cd8c0aaf6bb9751fa372fc1
Ken Pruiksma <pruiksma@amazon.com>
2022-03-22 11:39:10 -0500

    [Terrain] Height blending / bug fixes (#8370)
    This adds optional height blending to terrain detail materials and includes a variety of bug fixes.
    
    Height blending can be turned on in the terrain rendering component. While pom heights on the StandardPbr material can be used for height information, it's recommended to use the new TerrainDetailMaterial material type for terrain detail materials. This material type exposes additional parameters to have fine grain control over height based blending without affecting POM settings.
    
    Additional options are now available in terrain components.
    - The surface data resolution can now be set independently of the height query resolution
    - The terrain renderer component has options for tweaking the render distance, fade out, and scale of the detail material
    
    Material changes:
    Lua functors can now use material properties without having a hard dependency on them. They can query for the existence of a property with HasMaterialProperty. This will allow lua functors to be more flexible and be used with multiple similar material types.
    Fixed an issue with importing material json snippets across gem boundaries
    
    Bug fixes:
    Sometimes on initial load, terrain detail materials wouldn't be visible until some change was made. This should no longer happen.
    Query resolutions that didn't evenly divide the world bounds of terrain would often result in a crash, this is now supported
    Reloading the terrain material or shader would usually result in turning all the terrain metallic, this should no longer happen.

--- Gems/Terrain/Code/Source/Components/TerrainWorldRendererComponent.cpp
+++ Gems/Terrain/Code/Source/Components/TerrainWorldRendererComponent.cpp
@@ -26,14 +26,38 @@ namespace Terrain
         AZ::SerializeContext* serialize = azrtti_cast<AZ::SerializeContext*>(context);
         if (serialize)
         {
-            serialize->Class<TerrainWorldRendererConfig, AZ::ComponentConfig>()
+            serialize->Class<DetailMaterialConfiguration>()
                 ->Version(1)
+                ->Field("UseHeightBasedBlending", &DetailMaterialConfiguration::m_useHeightBasedBlending)
+                ->Field("RenderDistance", &DetailMaterialConfiguration::m_renderDistance)
+                ->Field("FadeDistance", &DetailMaterialConfiguration::m_fadeDistance)
+                ->Field("Scale", &DetailMaterialConfiguration::m_scale)
+                ;
+
+            serialize->Class<TerrainWorldRendererConfig, AZ::ComponentConfig>()
+                ->Version(2)
                 ->Field("WorldSize", &TerrainWorldRendererConfig::m_worldSize)
+                ->Field("DetailMaterialConfiguration", &TerrainWorldRendererConfig::m_detailMaterialConfig)
                 ;
 
             AZ::EditContext* editContext = serialize->GetEditContext();
             if (editContext)
             {
+                editContext->Class<DetailMaterialConfiguration>("Detail material", "Settings related to rendering detail surface materials.")
+                    ->DataElement(AZ::Edit::UIHandlers::CheckBox, &DetailMaterialConfiguration::m_useHeightBasedBlending, "Height based texture blending", "When turned on, detail materials will use the height texture to aid with blending.")
+                    ->DataElement(AZ::Edit::UIHandlers::Slider, &DetailMaterialConfiguration::m_renderDistance, "Detail material render distance", "The distance from the camera that the detail material will render.")
+                        ->Attribute(AZ::Edit::Attributes::Min, 1.0f)
+                        ->Attribute(AZ::Edit::Attributes::Max, 2048.0f)
+                    ->DataElement(AZ::Edit::UIHandlers::Slider, &DetailMaterialConfiguration::m_fadeDistance, "Detail material fade distance", "The distance over which the detail material will fade out into the macro material.")
+                        ->Attribute(AZ::Edit::Attributes::Min, 0.0f)
+                        ->Attribute(AZ::Edit::Attributes::Max, 2048.0f)
+                    ->DataElement(AZ::Edit::UIHandlers::Slider, &DetailMaterialConfiguration::m_scale, "Detail material scale", "The scale at which all detail materials are rendered at.")
+                        ->Attribute(AZ::Edit::Attributes::SoftMin, 0.1f)
+                        ->Attribute(AZ::Edit::Attributes::Min, 0.0001f)
+                        ->Attribute(AZ::Edit::Attributes::SoftMax, 10.0f)
+                        ->Attribute(AZ::Edit::Attributes::Max, 10000.0f)
+                    ;
+
                 editContext->Class<TerrainWorldRendererConfig>("Terrain World Renderer Component", "Enables terrain rendering")
                     ->ClassElement(AZ::Edit::ClassElements::EditorData, "")
                         ->Attribute(AZ::Edit::Attributes::AppearsInAddComponentMenu, AZStd::vector<AZ::Crc32>({ AZ_CRC_CE("Level") }))
@@ -47,6 +71,8 @@ namespace Terrain
                         ->EnumAttribute(TerrainWorldRendererConfig::WorldSize::_8192Meters, "8 Kilometers")
                         ->EnumAttribute(TerrainWorldRendererConfig::WorldSize::_16384Meters, "16 Kilometers")
                         ->Attribute(AZ::Edit::Attributes::Visibility, false) // Keeping invisible until it's hooked up under the hood
+                    ->DataElement(AZ::Edit::UIHandlers::Default, &TerrainWorldRendererConfig::m_detailMaterialConfig, "Detail material configuration", "")
+                        ->Attribute(AZ::Edit::Attributes::Visibility, AZ::Edit::PropertyVisibility::ShowChildrenOnly)
                         ;
             }
         }
@@ -82,28 +108,6 @@ namespace Terrain
     TerrainWorldRendererComponent::TerrainWorldRendererComponent(const TerrainWorldRendererConfig& configuration)
         : m_configuration(configuration)
     {
-        switch (configuration.m_worldSize)
-        {
-        case TerrainWorldRendererConfig::WorldSize::_512Meters:
-            m_terrainFeatureProcessor->SetWorldSize(AZ::Vector2(512.0f, 512.0f));
-            break;
-        case TerrainWorldRendererConfig::WorldSize::_1024Meters:
-            m_terrainFeatureProcessor->SetWorldSize(AZ::Vector2(1024.0f, 1024.0f));
-            break;
-        case TerrainWorldRendererConfig::WorldSize::_2048Meters:
-            m_terrainFeatureProcessor->SetWorldSize(AZ::Vector2(2048.0f, 2048.0f));
-            break;
-        case TerrainWorldRendererConfig::WorldSize::_4096Meters:
-            m_terrainFeatureProcessor->SetWorldSize(AZ::Vector2(4096.0f, 4096.0f));
-            break;
-        case TerrainWorldRendererConfig::WorldSize::_8192Meters:
-            m_terrainFeatureProcessor->SetWorldSize(AZ::Vector2(8192.0f, 8192.0f));
-            break;
-        case TerrainWorldRendererConfig::WorldSize::_16384Meters:
-            m_terrainFeatureProcessor->SetWorldSize(AZ::Vector2(16384.0f, 16384.0f));
-            break;
-
-        }
     }
 
     TerrainWorldRendererComponent::~TerrainWorldRendererComponent()
@@ -133,6 +137,29 @@ namespace Terrain
         if (AZ::RPI::Scene* scene = GetScene(); scene)
         {
             m_terrainFeatureProcessor = scene->EnableFeatureProcessor<Terrain::TerrainFeatureProcessor>();
+            
+            m_terrainFeatureProcessor->SetDetailMaterialConfiguration(m_configuration.m_detailMaterialConfig);
+            switch (m_configuration.m_worldSize)
+            {
+            case TerrainWorldRendererConfig::WorldSize::_512Meters:
+                m_terrainFeatureProcessor->SetWorldSize(AZ::Vector2(512.0f, 512.0f));
+                break;
+            case TerrainWorldRendererConfig::WorldSize::_1024Meters:
+                m_terrainFeatureProcessor->SetWorldSize(AZ::Vector2(1024.0f, 1024.0f));
+                break;
+            case TerrainWorldRendererConfig::WorldSize::_2048Meters:
+                m_terrainFeatureProcessor->SetWorldSize(AZ::Vector2(2048.0f, 2048.0f));
+                break;
+            case TerrainWorldRendererConfig::WorldSize::_4096Meters:
+                m_terrainFeatureProcessor->SetWorldSize(AZ::Vector2(4096.0f, 4096.0f));
+                break;
+            case TerrainWorldRendererConfig::WorldSize::_8192Meters:
+                m_terrainFeatureProcessor->SetWorldSize(AZ::Vector2(8192.0f, 8192.0f));
+                break;
+            case TerrainWorldRendererConfig::WorldSize::_16384Meters:
+                m_terrainFeatureProcessor->SetWorldSize(AZ::Vector2(16384.0f, 16384.0f));
+                break;
+            }
         }
         m_terrainRendererActive = true;
     }
