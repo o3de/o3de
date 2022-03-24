2bffef6e7a054b9f735422e5cb98c79a6f2f863d
William Hayward <wilhayw@amazon.com>
2022-03-24 17:09:21 -0400

    Remove unused file ComponentPlaceholder.svg and update default icon for edit context
    Signed-off-by: William Hayward <wilhayw@amazon.com>

--- Templates/DefaultComponent/Template/Source/${Name}Component.cpp
+++ Templates/DefaultComponent/Template/Source/${Name}Component.cpp
@@ -39,7 +39,7 @@ namespace ${GemName}
                 editContext->Class<${SanitizedCppName}Component>("${SanitizedCppName}Component", "[Description of functionality provided by this component]")
                     ->ClassElement(AZ::Edit::ClassElements::EditorData, "")
                     ->Attribute(AZ::Edit::Attributes::Category, "ComponentCategory")
-                    ->Attribute(AZ::Edit::Attributes::Icon, "Icons/Components/ComponentPlaceholder.svg")
+                    ->Attribute(AZ::Edit::Attributes::Icon, "Icons/Components/Component_Placeholder.svg")
                     ->Attribute(AZ::Edit::Attributes::AppearsInAddComponentMenu, AZ_CRC("Game"))
                     ;
             }
