008fd5a9c085389bb434395bf1dbe7da854a0591
Benjamin Jillich <jillich@amazon.com>
2022-03-25 13:07:54 +0100

    EMotion FX: Added inspector window
    Added inspector plugin containing the content header widget, the content widget, the inspector plugin, no selection widget and the inspector bus.
    
    Signed-off-by: Benjamin Jillich <jillich@amazon.com>

---
+++ Gems/EMotionFX/Code/EMotionFX/Tools/EMotionStudio/Plugins/StandardPlugins/Source/Inspector/NoSelectionWidget.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+
+#pragma once
+
+#include <QWidget>
+QT_FORWARD_DECLARE_CLASS(QLabel)
+
+namespace EMStudio
+{
+    class NoSelectionWidget
+        : public QWidget
+    {
+        Q_OBJECT //AUTOMOC
+
+    public:
+        explicit NoSelectionWidget(QWidget* parent = nullptr);
+        ~NoSelectionWidget() override = default;
+
+    private:
+        QLabel* m_label = nullptr;
+    };
+} // namespace EMStudio
