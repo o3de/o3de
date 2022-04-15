ebbb394ac265348144446b0a916475e75203b838
Gene Walters <genewalt@amazon.com>
2022-04-14 19:58:44 -0700

    Changing cvar to Null console flag; this wont ever appear on the server
    Signed-off-by: Gene Walters <genewalt@amazon.com>

--- Gems/Multiplayer/Code/Source/Editor/MultiplayerEditorConnectionViewportMessageSystemComponent.cpp
+++ Gems/Multiplayer/Code/Source/Editor/MultiplayerEditorConnectionViewportMessageSystemComponent.cpp
@@ -14,7 +14,7 @@
 
 namespace Multiplayer
 {
-    AZ_CVAR(float, editorsv_connection_debug_fontsize, 0.7f, nullptr, AZ::ConsoleFunctorFlags::DontReplicate, "The font size used for displaying updates on screen while the multiplayer editor is connecting to the server.");
+    AZ_CVAR(float, editorsv_connection_debug_fontsize, 0.7f, nullptr, AZ::ConsoleFunctorFlags::Null, "The font size used for displaying updates on screen while the multiplayer editor is connecting to the server.");
 
     void MultiplayerEditorConnectionViewportMessageSystemComponent::Reflect(AZ::ReflectContext* context)
     {
