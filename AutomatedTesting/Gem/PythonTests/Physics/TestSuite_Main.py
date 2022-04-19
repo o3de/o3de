bc032c49dc9cf47c141a1eb2c673af89029d34b9
puvvadar <puvvadar@amazon.com>
2022-04-19 10:31:53 -0700

    Revert speculative change
    Signed-off-by: puvvadar <puvvadar@amazon.com>

--- AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Main.py
+++ AutomatedTesting/Gem/PythonTests/Physics/TestSuite_Main.py
@@ -58,7 +58,7 @@ class EditorSingleTest_WithFileOverrides(EditorSingleTest):
 @pytest.mark.parametrize("project", ["AutomatedTesting"])
 class TestAutomation(EditorTestSuite):
 
-    global_extra_cmdline_args = ['-BatchMode', '-autotest_mode', '-target_autoconnect=false']
+    global_extra_cmdline_args = ['-BatchMode', '-autotest_mode']
 
     @staticmethod
     def get_number_parallel_editors():
