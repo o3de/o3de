35caad89359f08aeb26956bf3ea036212f43d7a2
Gene Walters <genewalt@amazon.com>
2022-04-19 18:11:15 -0700

    Adding [[maybe_used]] to stop compilation error
    Signed-off-by: Gene Walters <genewalt@amazon.com>

--- Code/Framework/AzFramework/AzFramework/Spawnable/SpawnableAssetBus.h
+++ Code/Framework/AzFramework/AzFramework/Spawnable/SpawnableAssetBus.h
@@ -35,7 +35,7 @@ namespace AzFramework
 
         //! Notification to allow systems to access a spawnable before it's been modified by aliasing.
         //! This ebus function will be called for all spawnables, even if there are no aliases on the spawnable.
-        virtual void OnPreparingSpawnable(const Spawnable& spawnable, const AZ::Data::AssetId& id) {}
+        virtual void OnPreparingSpawnable([[maybe_unused]]const Spawnable& spawnable, [[maybe_unused]]const AZ::Data::AssetId& id) {}
     };
 
     using SpawnableAssetEventsBus = AZ::EBus<SpawnableAssetEvents>;
