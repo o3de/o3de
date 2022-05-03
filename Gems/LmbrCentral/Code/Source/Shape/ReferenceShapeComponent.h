746e3e96148a618fb2f23c8ac9aaf6158ed7f555
Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
2022-05-03 11:44:47 -0500

    Shape References weren't notifying on changes. The notification enable boolean was getting initialized the wrong way. It should only be disabled while changing the shape ID.
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>

--- Gems/LmbrCentral/Code/Source/Shape/ReferenceShapeComponent.h
+++ Gems/LmbrCentral/Code/Source/Shape/ReferenceShapeComponent.h
@@ -96,7 +96,7 @@ namespace LmbrCentral
     private:
         ReferenceShapeConfig m_configuration;
         mutable AZStd::shared_mutex m_mutex; ///< Mutex to allow multiple readers but single writer for efficient thread safety
-        bool m_allowNotifications = false; ///< temporarily disable sending notifications to avoid redundancies
+        bool m_allowNotifications = true; ///< temporarily disable sending notifications to avoid redundancies
 
         bool AllowRequest() const;
         bool AllowNotification() const;
