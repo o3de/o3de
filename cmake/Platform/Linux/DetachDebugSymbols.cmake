4ef752cf1a26dd6d19bb4a0445d57ce5c97fb9e1
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2022-05-05 20:03:42 -0700

    - Update process to use the debug filename only instead of an absolute file during setting the gnu-debuglink - Updated comments
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- cmake/Platform/Linux/DetachDebugSymbols.cmake
+++ cmake/Platform/Linux/DetachDebugSymbols.cmake
@@ -14,28 +14,37 @@ endif()
 string(TOLOWER ${CMAKE_ARGV4} STATIC_LIBRARY_SUFFIX)
 string(TOLOWER ${CMAKE_ARGV5} DEBUG_SYMBOL_EXT)
 
-
+# Make sure that both gnu tools 'strip' and 'objcopy' are available
 find_program(GNU_STRIP_TOOL strip)
 find_program(GNU_OBJCOPY objcopy)
 
-get_filename_component(file_extension ${CMAKE_ARGV3} LAST_EXT)
-
 if (GNU_STRIP_TOOL AND GNU_OBJCOPY)
 
+    # Only perform the debug symbol detachment on non-static libraries. (You cannot add the gnu debug link on an static library)
+    get_filename_component(file_extension ${CMAKE_ARGV3} LAST_EXT)
     if (NOT ${file_extension} STREQUAL ${STATIC_LIBRARY_SUFFIX})
 
-        message(STATUS "Detaching debug symbols from ${CMAKE_ARGV3} into ${CMAKE_ARGV3}.${DEBUG_SYMBOL_EXT}")
+        # When specifying the location of the debug info file, just use the filename so that the debugger will search
+        # for the name through the set of known locations. Using the absolute filename will cause the debugger to look
+        # only for that absolute path, which is not portable
+        get_filename_component(file_path ${CMAKE_ARGV3} DIRECTORY)
+        get_filename_component(filename_only ${CMAKE_ARGV3} NAME)
 
-        execute_process(COMMAND
-            ${GNU_OBJCOPY} --only-keep-debug ${CMAKE_ARGV3} ${CMAKE_ARGV3}.${DEBUG_SYMBOL_EXT}
+        message(STATUS "Detaching debug symbols from ${CMAKE_ARGV3} into ${filename_only}.${DEBUG_SYMBOL_EXT}")
+
+        # First extract out the debug symbols into a separate file 
+        execute_process(COMMAND 
+                            ${GNU_OBJCOPY} --only-keep-debug ${CMAKE_ARGV3} ${CMAKE_ARGV3}.${DEBUG_SYMBOL_EXT}
         )
-
-        execute_process(COMMAND
-            ${GNU_STRIP_TOOL} --strip-debug ${CMAKE_ARGV3}
+        # Then strip out the debug symbols completely from the shared library or executable
+        execute_process(COMMAND 
+                            ${GNU_STRIP_TOOL} --strip-debug ${CMAKE_ARGV3}
         )
-
-        execute_process(COMMAND
-            ${GNU_OBJCOPY} --add-gnu-debuglink=${CMAKE_ARGV3}.dbg ${CMAKE_ARGV3}
+        # Then re-link the debug information by creating a .gnu_debuglink section to the filename
+        execute_process(COMMAND 
+                            ${GNU_OBJCOPY} --add-gnu-debuglink=${filename_only}.${DEBUG_SYMBOL_EXT} ${CMAKE_ARGV3}
+                        WORKING_DIRECTORY 
+                            file_path
         )
     endif()
 
