c5cf693e4bba5b9685d48b4584f86592eae12e30
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2022-05-06 13:00:54 -0700

    Remove commented out lines
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- cmake/LYWrappers.cmake
+++ cmake/LYWrappers.cmake
@@ -238,9 +238,6 @@ function(ly_add_target)
         else()
             ly_handle_custom_output_directory(${ly_add_target_NAME} "")
         endif()
-        #ly_apply_debug_strip_options(${ly_add_target_NAME})
-    elseif(ly_add_target_STATIC)
-        #ly_apply_debug_strip_options(${ly_add_target_NAME})
     endif()
 
 
