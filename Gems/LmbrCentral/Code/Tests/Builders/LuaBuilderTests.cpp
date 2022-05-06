76afcb6642aeb3450dfdc92d0b6dc67809e0b41b
carlitosan <82187351+carlitosan@users.noreply.github.com>
2022-05-06 13:21:24 -0700

    make Lua dependency regex more robust
    Signed-off-by: carlitosan <82187351+carlitosan@users.noreply.github.com>

--- Gems/LmbrCentral/Code/Tests/Builders/LuaBuilderTests.cpp
+++ Gems/LmbrCentral/Code/Tests/Builders/LuaBuilderTests.cpp
@@ -62,6 +62,59 @@ namespace UnitTest
         AZ::ComponentApplication::Descriptor m_descriptor;
     };
 
+    TEST_F(LuaBuilderTests, ParseLuaScript_DependencyRegexStress_Success)
+    {
+        LuaBuilderWorker worker;
+
+        AssetBuilderSDK::ProductPathDependencySet pathDependencies;
+
+        char resolvedPath[AZ_MAX_PATH_LEN];
+        AZ::IO::FileIOBase::GetInstance()->ResolvePath("@gemroot:LmbrCentral@/Code/Tests/Lua/dependencyRegexStress.lua", resolvedPath, AZ_MAX_PATH_LEN);
+
+        worker.ParseDependencies(AZStd::string(resolvedPath), pathDependencies);
+
+        ASSERT_THAT(pathDependencies,
+            testing::UnorderedElementsAre(
+                ProductPathDependency("test0.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/test1.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/separated/test2.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/separated/test3.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/test4.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("test5.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/test6.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("test7.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/test8.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/test9.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/test10.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/test11.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/test12.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/test13.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/test14.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/test15.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/test16.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("test17.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("local_test0.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/local_test1.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/separated/local_test2.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/separated/local_test3.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/local_test4.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("local_test5.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/local_test6.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("local_test7.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/local_test8.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/local_test9.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/local_test10.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/local_test11.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/local_test12.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/local_test13.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/local_test14.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/local_test15.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("folder/local_test16.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile),
+                ProductPathDependency("local_test17.luac", AssetBuilderSDK::ProductPathDependencyType::ProductFile)
+            )
+        );
+    }
+
     TEST_F(LuaBuilderTests, ParseLuaScript_UsingRequire_ShouldFindDependencies)
     {
         LuaBuilderWorker worker;
