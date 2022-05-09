2d4a4e8bb1610a276a5a8cc920e40f2b0bca75f7
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2022-05-09 15:55:58 -0700

    - Refactored debug detaching/stripping logic to combine operations using one file - Fixed LY_STRIP_DEBUG_SYMBOLS and 'debug' to perform the debug symbol detachment for debug builds when LY_STRIP_DEBUG_SYMBOLS is TRUE - Deleted StripDebugSymbols.cmake
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- cmake/Platform/Linux/DetachDebugSymbols.cmake
+++ cmake/Platform/Linux/DetachDebugSymbols.cmake
@@ -7,14 +7,30 @@
 #
 
 
-if(NOT ${CMAKE_ARGC} EQUAL 7)
+if(${CMAKE_ARGC} LESS 9)
     message(FATAL_ERROR "DetachDebugSymbols script called with the wrong number of arguments: ${CMAKE_ARGC}")
 endif()
 
-set(GNU_STRIP_TOOL ${CMAKE_ARGV3})
-set(GNU_OBJCOPY ${CMAKE_ARGV4})
-set(STRIP_TARGET ${CMAKE_ARGV5})
-set(DEBUG_SYMBOL_EXT ${CMAKE_ARGV6})
+# cmake command arguments (not including the first 2 arguments of '-P', 'DetachDebugSymbols.cmake')
+
+set(GNU_STRIP_TOOL ${CMAKE_ARGV3})          # GNU_STRIP_TOOL      : The location of the gnu strip tool (e.g. /usr/bin/strip)
+set(GNU_OBJCOPY ${CMAKE_ARGV4})             # GNU_OBJCOPY         : The location of the gnu objcopy tool (e.g. /usr/bin/objcopy)
+set(STRIP_TARGET ${CMAKE_ARGV5})            # STRIP_TARGET        : The built binary to process
+set(DEBUG_SYMBOL_EXT ${CMAKE_ARGV6})        # DEBUG_SYMBOL_EXT    : When extracting debug information, the extension to use for the debug database file
+set(TARGET_TYPE ${CMAKE_ARGV7})             # TARGET_TYPE         : The target type (STATIC_LIBRARY, MODULE_LIBRARY, SHARED_LIBRARY, EXECUTABLE, or APPLICATION)
+string(TOLOWER ${CMAKE_ARGV8} CONFIG_LOWER) # CONFIG_LOWER        : The build configuration (debug, profile, or release)
+set(DEBUG_SYMBOL_OPTION ${CMAKE_ARGV9})     # DEBUG_SYMBOL_OPTION : Either 
+                                            #                          - 'DISCARD' : strip debug information except from debug builds
+                                            #                          - 'DETACH'  : detach debug information
+
+# This script only supports executables, applications, modules, static libraries, and shared libraries
+if (NOT ${TARGET_TYPE} STREQUAL "STATIC_LIBRARY" AND 
+    NOT ${TARGET_TYPE} STREQUAL "MODULE_LIBRARY" AND 
+    NOT ${TARGET_TYPE} STREQUAL "SHARED_LIBRARY" AND 
+    NOT ${TARGET_TYPE} STREQUAL "EXECUTABLE" AND 
+    NOT ${TARGET_TYPE} STREQUAL "APPLICATION")
+    return()
+endif()
 
 # When specifying the location of the debug info file, just use the filename so that the debugger will search
 # for the name through the set of known locations. Using the absolute filename will cause the debugger to look
@@ -22,19 +38,49 @@ set(DEBUG_SYMBOL_EXT ${CMAKE_ARGV6})
 get_filename_component(file_path ${STRIP_TARGET} DIRECTORY)
 get_filename_component(filename_only ${STRIP_TARGET} NAME)
 
-message(STATUS "Detaching debug symbols from ${STRIP_TARGET} into ${filename_only}.${DEBUG_SYMBOL_EXT}")
 
-# First extract out the debug symbols into a separate file 
-execute_process(COMMAND 
-                    ${GNU_OBJCOPY} --only-keep-debug ${STRIP_TARGET} ${STRIP_TARGET}.${DEBUG_SYMBOL_EXT}
-)
-# Then strip out the debug symbols completely from the shared library or executable
+if (${DEBUG_SYMBOL_OPTION} STREQUAL "DISCARD")
+
+    if (${CONFIG_LOWER} STREQUAL "debug")
+        # We will not discard debug information for debug builds, instead we follow the detach flow instead
+        set(DEBUG_SYMBOL_OPTION "DETACH")
+    else()
+        message(STATUS "Stripping debug symbols from ${STRIP_TARGET}")
+    endif()
+endif()
+
+if (${DEBUG_SYMBOL_OPTION} STREQUAL "DETACH")
+
+    if (${TARGET_TYPE} STREQUAL "STATIC_LIBRARY")
+        # We will not detach the debug information from static libraries since we are unable to attach them back
+        # to that static library. Instead, static libraries will retain their debug information.
+        return ()
+    endif()
+
+    message(STATUS "Detaching debug symbols from ${STRIP_TARGET} into ${filename_only}.${DEBUG_SYMBOL_EXT}")
+    
+endif()
+
+
+# Step 1: Extract the debug information into the debug datafile (For 'DETACH' actions)
+if (${DEBUG_SYMBOL_OPTION} STREQUAL "DETACH")
+
+    execute_process(COMMAND 
+                        ${GNU_OBJCOPY} --only-keep-debug ${STRIP_TARGET} ${STRIP_TARGET}.${DEBUG_SYMBOL_EXT}
+    )
+endif()
+
+# Step 2: Stripping out the debug symbols (For both 'DISCARD' and 'DETACH')
 execute_process(COMMAND 
                     ${GNU_STRIP_TOOL} --strip-debug ${STRIP_TARGET}
 )
-# Then re-link the debug information by creating a .gnu_debuglink section to the filename
-execute_process(COMMAND 
-                    ${GNU_OBJCOPY} --add-gnu-debuglink=${filename_only}.${DEBUG_SYMBOL_EXT} ${STRIP_TARGET}
-                WORKING_DIRECTORY 
-                    ${file_path}
-)
+
+# Step 3: Re-link the debug information file from stemp 1 (For 'DETACH' actions)
+if (${DEBUG_SYMBOL_OPTION} STREQUAL "DETACH")
+
+    execute_process(COMMAND 
+                        ${GNU_OBJCOPY} --add-gnu-debuglink=${filename_only}.${DEBUG_SYMBOL_EXT} ${STRIP_TARGET}
+                    WORKING_DIRECTORY 
+                        ${file_path}
+    )
+endif()
