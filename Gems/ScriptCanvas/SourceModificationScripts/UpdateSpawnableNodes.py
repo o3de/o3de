6c595d599869d45536b7a730b3c4dc9b29619841
Mikhail Naumov <mnaumov@amazon.com>
2022-05-11 17:45:03 -0500

    cleanup
    Signed-off-by: Mikhail Naumov <mnaumov@amazon.com>

--- Gems/ScriptCanvas/SourceModificationScripts/UpdateSpawnableNodes.py
+++ Gems/ScriptCanvas/SourceModificationScripts/UpdateSpawnableNodes.py
@@ -16,17 +16,14 @@ shutil.copy(fullPath, fullBackupPath)
 
 print("Backup saved to \'%s\'" % fullBackupPath)
 
-# Read in the file
 with open(fullPath, 'r') as file :
   filedata = file.read()
 
-# Replace the target string
 filedata = filedata.replace('"$type": "SpawnTicketInstance"', '"$type": "AzFramework::EntitySpawnTicket"')
 filedata = filedata.replace('"$type": "SpawnableAsset"', '"$type": "AzFramework::Scripts::SpawnableScriptAssetRef"')
 filedata = filedata.replace('\"Asset\"', '\"asset\"')
 filedata = filedata.replace('{2B5EB938-8962-4A43-A97B-112F398C604B}', '{BA62FF9A-A01E-4FEB-84C6-200881DF2B2B}')
 
-# Write the file out again
 with open(fullPath, 'w') as file:
   file.write(filedata)
 
