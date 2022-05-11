828557feb4f52b1845183311467d951aa8cfa908
sphrose <82213493+sphrose@users.noreply.github.com>
2022-05-11 17:05:25 +0100

    shiboken build files.
    Signed-off-by: sphrose <82213493+sphrose@users.noreply.github.com>

---
+++ Gems/AzQtComponentsForPython/bindings_azqtcomponents.h
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+ 
+#pragma once
+/*
+#include <QPointer>
+
+//#include <AzCore/RTTI/TypeInfo.h>
+//#include <AzCore/Math/MathUtils.h>
+#include <AzCore/Math/Color.h>
+#include <AzCore/std/function/function_template.h>
+#include <AzCore/std/chrono/chrono.h>
+*/
+
+//#include <AzQtComponents/Components/AutoCustomWindowDecorations.h>
+#include <AzQtComponents/Components/ButtonDivider.h>
+#include <AzQtComponents/Components/ButtonStripe.h>
+#include <AzQtComponents/Components/ConfigHelpers.h>
+#include <AzQtComponents/Components/DockBar.h>
+#include <AzQtComponents/Components/DockBarButton.h>
+#include <AzQtComponents/Components/DockMainWindow.h>
+#include <AzQtComponents/Components/DockTabBar.h>
+#include <AzQtComponents/Components/DockTabWidget.h>
+
+#include <AzQtComponents/Components/ExtendedLabel.h>    
+#include <AzQtComponents/Components/FancyDocking.h>
+#include <AzQtComponents/Components/FancyDockingDropZoneWidget.h>
+#include <AzQtComponents/Components/FancyDockingGhostWidget.h>
+#include <AzQtComponents/Components/FlowLayout.h>
+//#include <AzQtComponents/Components/ui_FilteredSearchWidget.h>
+#include <AzQtComponents/Components/FilteredSearchWidget.h>
+#include <AzQtComponents/Components/GlobalEventFilter.h>
+#include <AzQtComponents/Components/HelpButton.h>
+#include <AzQtComponents/Components/InteractiveWindowGeometryChanger.h>
+#include <AzQtComponents/Components/SearchLineEdit.h>
+#include <AzQtComponents/Components/Style.h>
+#include <AzQtComponents/Components/StyledBusyLabel.h>
+#include <AzQtComponents/Components/StyledDetailsTableModel.h>
+#include <AzQtComponents/Components/StyledDetailsTableView.h>
+#include <AzQtComponents/Components/StyledDialog.h>
+#include <AzQtComponents/Components/StyledDockWidget.h>
+#include <AzQtComponents/Components/StyledLineEdit.h>
+#include <AzQtComponents/Components/StyledSpinBox.h>
+#include <AzQtComponents/Components/StyleManager.h>
+#include <AzQtComponents/Components/StyleSheetCache.h>
+#include <AzQtComponents/Components/StylesheetPreprocessor.h>
+#include <AzQtComponents/Components/TagSelector.h>
+#include <AzQtComponents/Components/Titlebar.h>
+#include <AzQtComponents/Components/TitleBarOverdrawHandler.h>
+#include <AzQtComponents/Components/ToastNotification.h>
+#include <AzQtComponents/Components/ToastNotificationConfiguration.h>
+
+#include <AzQtComponents/Components/ToolBarArea.h>
+#include <AzQtComponents/Components/ToolButtonComboBox.h>
+#include <AzQtComponents/Components/ToolButtonLineEdit.h>
+#include <AzQtComponents/Components/ToolButtonWithWidget.h>
+
+#include <AzQtComponents/Components/VectorEdit.h>
+#include <AzQtComponents/Components/WindowDecorationWrapper.h>
+
+
+#include <AzQtComponents/Components/Widgets/AssetFolderListView.h>
+#include <AzQtComponents/Components/Widgets/AssetFolderThumbnailView.h>
+#include <AzQtComponents/Components/Widgets/BreadCrumbs.h>
+#include <AzQtComponents/Components/Widgets/BrowseEdit.h>
+#include <AzQtComponents/Components/Widgets/Card.h>
+#include <AzQtComponents/Components/Widgets/CardHeader.h>
+#include <AzQtComponents/Components/Widgets/CardNotification.h>
+#include <AzQtComponents/Components/Widgets/CheckBox.h>
+#include <AzQtComponents/Components/Widgets/ColorLabel.h>
+//#include <AzQtComponents/Components/Widgets/ColorPicker.h>
+#include <AzQtComponents/Components/Widgets/ComboBox.h>
+#include <AzQtComponents/Components/Widgets/DialogButtonBox.h>
+#include <AzQtComponents/Components/Widgets/DragAndDrop.h>
+#include <AzQtComponents/Components/Widgets/ElidingLabel.h>
+#include <AzQtComponents/Components/Widgets/Eyedropper.h>
+#include <AzQtComponents/Components/Widgets/FileDialog.h>
+#include <AzQtComponents/Components/Widgets/GradientSlider.h>
+#include <AzQtComponents/Components/Widgets/LineEdit.h>
+// #include <AzQtComponents/Components/Widgets/LogicalTabOrderingWidget.h>
+#include <AzQtComponents/Components/Widgets/Menu.h>
+#include <AzQtComponents/Components/Widgets/MessageBox.h>
+#include <AzQtComponents/Components/Widgets/OverlayWidget.h>
+
+#include <AzQtComponents/Components/Widgets/ProgressBar.h>
+#include <AzQtComponents/Components/Widgets/PushButton.h>
+#include <AzQtComponents/Components/Widgets/RadioButton.h>
+#include <AzQtComponents/Components/Widgets/ScrollBar.h>
+#include <AzQtComponents/Components/Widgets/SegmentBar.h>
+#include <AzQtComponents/Components/Widgets/SegmentControl.h>
+#include <AzQtComponents/Components/Widgets/Slider.h>
+#include <AzQtComponents/Components/Widgets/SliderCombo.h>
+#include <AzQtComponents/Components/Widgets/SpinBox.h>
+#include <AzQtComponents/Components/Widgets/StatusBar.h>
+#include <AzQtComponents/Components/Widgets/TableView.h>
+#include <AzQtComponents/Components/Widgets/TabWidget.h>
+
+#include <AzQtComponents/Components/Widgets/TabWidgetActionToolBar.h>
+#include <AzQtComponents/Components/Widgets/Text.h>
+#include <AzQtComponents/Components/Widgets/ToolBar.h>
+#include <AzQtComponents/Components/Widgets/ToolButton.h>
+#include <AzQtComponents/Components/Widgets/TreeView.h>
+#include <AzQtComponents/Components/Widgets/VectorInput.h>
+
+#include <AzQtComponents/Components/Widgets/ColorPicker/ColorComponentSliders.h>
+#include <AzQtComponents/Components/Widgets/ColorPicker/ColorController.h>
+//#include <AzQtComponents/Components/Widgets/ColorPicker/ColorGrid.h>
+#include <AzQtComponents/Components/Widgets/ColorPicker/ColorHexEdit.h>
+#include <AzQtComponents/Components/Widgets/ColorPicker/ColorPreview.h>
+#include <AzQtComponents/Components/Widgets/ColorPicker/ColorRGBAEdit.h>
+#include <AzQtComponents/Components/Widgets/ColorPicker/ColorWarning.h>
+#include <AzQtComponents/Components/Widgets/ColorPicker/GammaEdit.h>
+// #include <AzQtComponents/Components/Widgets/ColorPicker/Palette.h>
+// #include <AzQtComponents/Components/Widgets/ColorPicker/PaletteCard.h>
+// #include <AzQtComponents/Components/Widgets/ColorPicker/PaletteCardCollection.h>
+//#include <AzQtComponents/Components/Widgets/ColorPicker/PaletteView.h>
+#include <AzQtComponents/Components/Widgets/ColorPicker/Swatch.h>
---
+++ Gems/AzQtComponentsForPython/test.py
@@ -0,0 +1,62 @@
+# Temporary only. Modify these two values to match your locations.
+
+# Location of the unpacked new version of pyside.
+package_path='C:\\ly\\3rd_party\\packages\\pyside2-qt-5.15.2-rev1-windows\\pyside2'
+
+# Location of the top directory of the o3de project.
+project_path='C:\\ly\\git\\o3de'
+
+import sys
+
+# Remove the previous version of pyside from the path - it should be the last entry.
+sys.path.pop()
+
+# Add the path to the new version of pyside. Modify this to match your location.
+sys.path.append(package_path)
+
+sys.path.append('.')
+
+# Now the path is correct, import Qt libs.
+from PySide2 import QtWidgets, QtCore
+
+# Set up a QApplication.
+app = QtWidgets.QApplication()
+
+# Import AzQtComponents module. Doing this before creating the app will break it.
+import azqtpyside
+from azqtpyside import AzQtComponents as az
+
+# Create a window and add some content.
+win = az.DockMainWindow()
+
+centralWidget = QtWidgets.QWidget(win);
+layout = QtWidgets.QVBoxLayout(centralWidget);
+layout.setMargin(0);
+layout.setContentsMargins(0, 0, 0, 0);
+centralWidget.setLayout(layout);
+win.setCentralWidget(centralWidget);
+        
+one = QtWidgets.QPushButton("One")
+two = QtWidgets.QPushButton("two")
+three = QtWidgets.QPushButton("three")
+
+layout.addWidget(one)
+layout.addWidget(two)
+layout.addWidget(three)
+
+# Set up an event loop to wait for the window to close.
+win.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+eventLoop = QtCore.QEventLoop()
+win.destroyed.connect(eventLoop.quit)
+
+# Set up the StyleManager and load the stylesheet so that window looks correct.
+style = az.StyleManager(win)
+
+style.initializePath(project_path)
+style.setStyleSheet(win, 'Code\\Editor\\Style\\Editor.qss')
+
+win.resize(600, 400)
+win.show()
+
+# Wait for the window to close.
+eventLoop.exec_()
