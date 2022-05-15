462b903143f9196f4e6524661bf8bde6f3ee248e
Olex Lozitskiy <5432499+AMZN-Olex@users.noreply.github.com>
2022-05-15 00:50:32 -0500

    Sample ready
    Signed-off-by: Olex Lozitskiy <5432499+AMZN-Olex@users.noreply.github.com>

--- Gems/RecastNavigation/Code/Source/Components/RecastNavigationTiledSurveyorComponent.cpp
+++ Gems/RecastNavigation/Code/Source/Components/RecastNavigationTiledSurveyorComponent.cpp
@@ -10,16 +10,19 @@
 
 #include <AzCore/Component/TransformBus.h>
 #include <AzCore/Interface/Interface.h>
-#include <AzCore/Math/MathStringConversions.h>
 #include <AzCore/RTTI/BehaviorContext.h>
-#include <AzCore/Serialization/EditContext.h>
 #include <AzCore/Serialization/SerializeContext.h>
 #include <AzFramework/Physics/PhysicsScene.h>
 #include <AzFramework/Physics/Shape.h>
 #include <AzFramework/Physics/Common/PhysicsSceneQueries.h>
+#include <DebugDraw/DebugDrawBus.h>
 #include <LmbrCentral/Shape/ShapeComponentBus.h>
 
 #pragma optimize("", off)
+
+AZ_CVAR(
+    bool, cl_navmesh_showInputData, false, nullptr, AZ::ConsoleFunctorFlags::Null,
+    "If enabled, draws triangle mesh input data that was used for the navigation mesh calculation");
 
 namespace RecastNavigation
 {
@@ -86,27 +89,81 @@ namespace RecastNavigation
             {
                 AZ::EntityId hitEntityId = overlapHit.m_entityId;
 
-                // most physics bodies just have world transforms, but some also have local transforms including terrain.
-                // we are not applying the local orientation because it causes terrain geometry to be oriented incorrectly
-
                 AZ::Transform t = AZ::Transform::CreateIdentity();
                 AZ::TransformBus::EventResult(t, hitEntityId, &AZ::TransformBus::Events::GetWorldTM);
+                // Physical geometry already takes scale into account. Discard the scale.
                 t.SetUniformScale(1.0f);
 
                 overlapHit.m_shape->GetGeometry(vertices, indices, nullptr);
-                if (!vertices.empty() && !indices.empty())
-                {
-                    for (const AZ::Vector3& vertex : vertices)
-                    {
-                        const AZ::Vector3 translated = t.TransformPoint(vertex);
-                        geometry.m_vertices.push_back(RecastVector3(translated));
-                    }
 
-                    for (size_t i = 2; i < indices.size(); i += 3)
+                if (!vertices.empty())
+                {
+                    if (indices.empty())
                     {
-                        geometry.m_indices.push_back(aznumeric_cast<AZ::u32>(indicesCount + indices[i]));
-                        geometry.m_indices.push_back(aznumeric_cast<AZ::u32>(indicesCount + indices[i - 1]));
-                        geometry.m_indices.push_back(aznumeric_cast<AZ::u32>(indicesCount + indices[i - 2]));
+                        AZStd::vector<AZ::Vector3> transformed;
+
+                        int currentLocalIndex = 0;
+                        for (const AZ::Vector3& vertex : vertices)
+                        {
+                            const AZ::Vector3 translated = t.TransformPoint(vertex);
+                            geometry.m_vertices.push_back(RecastVector3(translated));
+
+                            if (cl_navmesh_showInputData)
+                            {
+                                transformed.push_back(translated);
+                            }
+
+                            geometry.m_indices.push_back(aznumeric_cast<AZ::u32>(indicesCount + currentLocalIndex));
+                            currentLocalIndex++;
+                        }
+
+                        if (cl_navmesh_showInputData)
+                        {
+                            for (size_t i = 2; i < vertices.size(); i += 3)
+                            {
+                                DebugDraw::DebugDrawRequestBus::Broadcast(&DebugDraw::DebugDrawRequests::DrawLineLocationToLocation,
+                                    transformed[i - 2], transformed[i - 1], AZ::Colors::Red, 30.f);
+                                DebugDraw::DebugDrawRequestBus::Broadcast(&DebugDraw::DebugDrawRequests::DrawLineLocationToLocation,
+                                    transformed[i - 1], transformed[i - 0], AZ::Colors::Red, 30.f);
+                                DebugDraw::DebugDrawRequestBus::Broadcast(&DebugDraw::DebugDrawRequests::DrawLineLocationToLocation,
+                                    transformed[i - 0], transformed[i - 2], AZ::Colors::Red, 30.f);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        AZStd::vector<AZ::Vector3> transformed;
+
+                        for (const AZ::Vector3& vertex : vertices)
+                        {
+                            const AZ::Vector3 translated = t.TransformPoint(vertex);
+                            geometry.m_vertices.push_back(RecastVector3(translated));
+
+                            if (cl_navmesh_showInputData)
+                            {
+                                transformed.push_back(translated);
+                            }
+                        }
+
+                        for (size_t i = 2; i < indices.size(); i += 3)
+                        {
+                            geometry.m_indices.push_back(aznumeric_cast<AZ::u32>(indicesCount + indices[i]));
+                            geometry.m_indices.push_back(aznumeric_cast<AZ::u32>(indicesCount + indices[i - 1]));
+                            geometry.m_indices.push_back(aznumeric_cast<AZ::u32>(indicesCount + indices[i - 2]));
+                        }
+
+                        if (cl_navmesh_showInputData)
+                        {
+                            for (size_t i = 2; i < indices.size(); i += 3)
+                            {
+                                DebugDraw::DebugDrawRequestBus::Broadcast(&DebugDraw::DebugDrawRequests::DrawLineLocationToLocation,
+                                    transformed[indices[i - 2]], transformed[indices[i - 1]], AZ::Colors::Red, 30.f);
+                                DebugDraw::DebugDrawRequestBus::Broadcast(&DebugDraw::DebugDrawRequests::DrawLineLocationToLocation,
+                                    transformed[indices[i - 1]], transformed[indices[i - 0]], AZ::Colors::Red, 30.f);
+                                DebugDraw::DebugDrawRequestBus::Broadcast(&DebugDraw::DebugDrawRequests::DrawLineLocationToLocation,
+                                    transformed[indices[i - 0]], transformed[indices[i - 2]], AZ::Colors::Red, 30.f);
+                            }
+                        }
                     }
 
                     indicesCount += vertices.size();
