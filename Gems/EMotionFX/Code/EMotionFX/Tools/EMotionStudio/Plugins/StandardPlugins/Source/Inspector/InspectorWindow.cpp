o3de/o3de

commit 20873d8427f3f13ff53c6eab7140d4c1a7293726

[EMotionFX] Fix leaks of QObjects that do not have parents set
These objects are not destoyed unless the owning class explicitly deletes
them, or they are parented to some other QObject. Neither was happening
before, causing these objects to leak.

Signed-off-by: Chris Burel <burelc@amazon.com>

Gems/EMotionFX/Code/EMotionFX/Tools/EMotionStudio/Plugins/StandardPlugins/Source/Inspector/InspectorWindow.cpp
@@ -96,11 +96,11 @@ namespace EMStudio
         if (m_scrollArea->widget() != widget)
         {
             // Get back ownership of the cached widgets to avoid recreating it each time.
-            if (m_scrollArea->widget())
+            if (QWidget* oldWidget = m_scrollArea->takeWidget())
             {
-                m_scrollArea->widget()->hide();
+                oldWidget->hide();
+                oldWidget->deleteLater();
             }
-            m_scrollArea->takeWidget();
 
             // Set the no selection widget and destroy the previous one.
             m_scrollArea->setWidget(widget);

Gems/EMotionFX/Code/EMotionFX/Tools/EMotionStudio/Plugins/StandardPlugins/Source/AnimGraph/BlendGraphViewWidget.cpp
@@ -294,7 +294,8 @@ namespace EMStudio
 
             QAction* action = new QAction(
                 QIcon(":/EMotionFX/Open.svg"),
-                tr("Open"));
+                tr("Open"),
+                this);
             action->setMenu(m_openMenu);
 
             QToolButton* button = new QToolButton();
@@ -313,7 +314,8 @@ namespace EMStudio
 
             QAction* saveMenuAction = new QAction(
                 QIcon(":/EMotionFX/Save.svg"),
-                tr("Save anim graph"));
+                tr("Save anim graph"),
+                this);
             saveMenuAction->setMenu(contextMenu);
 
             QToolButton* button = new QToolButton();

Gems/EMotionFX/Code/EMotionFX/Tools/EMotionStudio/Plugins/StandardPlugins/Source/AnimGraph/NodeGraph.cpp
@@ -90,7 +90,7 @@ namespace EMStudio
 
         // Group nodes
         m_groupFont.setPixelSize(18);
-        m_groupFontMetrics = new QFontMetrics(m_font);
+        m_groupFontMetrics.reset(new QFontMetrics(m_font));
     }
 
 

Gems/EMotionFX/Code/EMotionFX/Tools/EMotionStudio/Plugins/StandardPlugins/Source/AnimGraph/NodeGraph.h
@@ -231,7 +231,7 @@ namespace EMStudio
 
         // Group drawing
         QFont                       m_groupFont;
-        QFontMetrics*               m_groupFontMetrics;
+        QScopedPointer<QFontMetrics> m_groupFontMetrics;
 
     protected slots:
         void UpdateAnimatedScrollOffset();

commit d1fe53a9155f9fa2ee773c9f00ba0a64a87476e0

[EMotionFX] Fix crash from Inspector window when closing the EMotionFX editor
This fixes a crash introduced by
20873d8427f3f13ff53c6eab7140d4c1a7293726. The Inspector window reuses
the widgets it creates. It does this by calling `takeWidget()` paired
with `setWidget()` on its `QScrollArea`. Calling `takeWidget()` removes
the current widget's parent, and returns ownership back to the caller.
To ensure that both the content widget and the no selection widget are
both destroyed, the Inspector window now takes back ownership of
whatever widget is in the scroll area (which can only be one of the
content widget or the no selection widget), and then deletes both of
them.

Signed-off-by: Chris Burel <burelc@amazon.com>

Gems/EMotionFX/Code/EMotionFX/Tools/EMotionStudio/Plugins/StandardPlugins/Source/Inspector/InspectorWindow.cpp
@@ -18,6 +18,18 @@ namespace EMStudio
     InspectorWindow::~InspectorWindow()
     {
         InspectorRequestBus::Handler::BusDisconnect();
+
+        if (m_scrollArea)
+        {
+            m_scrollArea->takeWidget();
+
+            delete m_contentWidget;
+            m_contentWidget = nullptr;
+
+            delete m_noSelectionWidget;
+            m_noSelectionWidget = nullptr;
+        }
+
     }
 
     bool InspectorWindow::Init()
@@ -96,11 +108,11 @@ namespace EMStudio
         if (m_scrollArea->widget() != widget)
         {
             // Get back ownership of the cached widgets to avoid recreating it each time.
-            if (QWidget* oldWidget = m_scrollArea->takeWidget())
+            if (m_scrollArea->widget())
             {
-                oldWidget->hide();
-                oldWidget->deleteLater();
+                m_scrollArea->widget()->hide();
             }
+            m_scrollArea->takeWidget();
 
             // Set the no selection widget and destroy the previous one.
             m_scrollArea->setWidget(widget);