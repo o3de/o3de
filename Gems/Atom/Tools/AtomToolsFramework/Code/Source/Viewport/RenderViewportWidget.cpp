882bfe282190f33622c59e6dcef4df6d088c4784
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2022-05-25 16:59:33 -0700

    Add additional event to trigger a resize for a RenderViewportWidget on Linux
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- Gems/Atom/Tools/AtomToolsFramework/Code/Source/Viewport/RenderViewportWidget.cpp
+++ Gems/Atom/Tools/AtomToolsFramework/Code/Source/Viewport/RenderViewportWidget.cpp
@@ -223,6 +223,11 @@ namespace AtomToolsFramework
         {
             case QEvent::Resize:
             case QEvent::ShowToParent: //This event exists to capture the case where a resize event is missed "after" the underlying surface is modified by Qt (Seen during level load in Editor)
+#if defined(AZ_PLATFORM_LINUX)
+            // On Linux, Qt is sending this event as the final events after a viewport change. This even needs to trigger a resize at this point or else the 
+            // swapchain won't be resized to the correct viewport size(s)
+            case QEvent::UpdateLater:   
+#endif
             {
                 SendWindowResizeEvent();
                 break;
