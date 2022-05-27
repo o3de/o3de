db052082e3db0fbd3da71bc5688d428ac70c5439
Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
2022-05-27 17:27:28 -0500

    First pass of async terrain physics. These changes make all terrain physics heightfield updates asynchronous, which makes the overall update time faster and keeps the editor more responsive. The following changes were made to support this: * Changed the HeightfieldProviderBus to support concurrent requests * Removed the Create/Destroy flags from the Heightfield data updates. They didn't behave any different than Settings, and just added to the number of conditions and flags that needed to be checked, so they were consolidated down to just a "Settings" notification. * Renamed "NumJobsMax" constant to "UseMaxJobs" for increased clarity. * Added mutex to PhysXSystemBus so that it can be used from multiple threads, though not concurrently. * Refactored all the duplicated HeightfieldCollider logic out of EditorHeightfieldColliderComponent / HeightfieldColliderComponent and moved into a new HeightfieldCollider class. * Removed serialization of the shape config, since it only contained runtime data. * Made the Heightfield Collider use an async job to update the heightfield shape configuration and spawn it. The job gets divided into subregions so that it can be canceled when changes occur. The subregion size is exposed as a CVar for tunability. * Made the Terrain Physics Collider Component use the async queries to update the heightfield data. It still blocks on completion, but this lets it multithread the query while it processes. The number of threads is exposed as a CVar for tunability.
    This code is likely to get iterated further, but as a first pass it's made the Editor responsive at interactive framerates when modifying a 4 km x 4 km terrain region.
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>

---
+++ Gems/PhysX/Code/Source/HeightfieldCollider.h
@@ -0,0 +1,151 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+
+#pragma once
+
+#include <AzCore/Jobs/Job.h>
+#include <AzCore/std/parallel/condition_variable.h>
+
+#include <AzFramework/Physics/Components/SimulatedBodyComponentBus.h>
+#include <AzFramework/Physics/HeightfieldProviderBus.h>
+#include <AzFramework/Physics/PhysicsScene.h>
+#include <AzFramework/Physics/Shape.h>
+
+#include <PhysX/ColliderShapeBus.h>
+
+namespace PhysX
+{
+    //! PhysX Heightfield Collider base class.
+    //! This contains all the logic shared between the Editor Heightfield Collider Component and the Heightfield Collider Component
+    //! to create, update, and destroy the heightfield collider at runtime.
+    class HeightfieldCollider
+        : protected AzPhysics::SimulatedBodyComponentRequestsBus::Handler
+        , protected Physics::HeightfieldProviderNotificationBus::Handler
+        , protected PhysX::ColliderShapeRequestBus::Handler
+    {
+    public:
+        HeightfieldCollider() = default;
+        ~HeightfieldCollider();
+
+        //! Create a HeightfieldCollider that operates on the given set of data.
+        //! @param entityId The entity Id for the entity that contains this heightfield collider
+        //! @param entityName The enntity name for the entity that contains this heightfield collider (for debug purposes)
+        //! @param sceneHandler The physics scene to create the collider in (Editor or runtime)
+        //! @param colliderConfig The collider configuration to use. Some of its data will get modified based on the heightfield data.
+        //! @param shapeConfig The shape configuration to use. All of its data will get modified based on the heightfield data.
+        HeightfieldCollider(
+            AZ::EntityId entityId,
+            const AZStd::string& entityName,
+            AzPhysics::SceneHandle sceneHandle, 
+            AZStd::shared_ptr<Physics::ColliderConfiguration> colliderConfig,
+            AZStd::shared_ptr<Physics::HeightfieldShapeConfiguration> shapeConfig);
+
+        //! Get the currently-spawned heightfield shape.
+        //! @return Pointer to the heightfield shape.
+        AZStd::shared_ptr<Physics::Shape> GetHeightfieldShape();
+
+        //! Notify the heightfield that it may need to refresh some or all of its data.
+        //! @param changeMask The types of data changes causing the notification.
+        //! @param dirtyRegion The area affected by the notification, or a Null Aabb if everything is affected.
+        void RefreshHeightfield(
+            Physics::HeightfieldProviderNotifications::HeightfieldChangeMask changeMask,
+            const AZ::Aabb& dirtyRegion = AZ::Aabb::CreateNull());
+
+        //! Get a pointer to the currently-spawned simulated body.
+        //! @return Pointer to the simulated body.
+        const AzPhysics::SimulatedBody* GetSimulatedBody() const;
+
+        // AzPhysics::SimulatedBodyComponentRequestsBus::Handler overrides ...
+        void EnablePhysics() override;
+        void DisablePhysics() override;
+        bool IsPhysicsEnabled() const override;
+        AZ::Aabb GetAabb() const override;
+        AzPhysics::SimulatedBody* GetSimulatedBody() override;
+        AzPhysics::SimulatedBodyHandle GetSimulatedBodyHandle() const override;
+        AzPhysics::SceneQueryHit RayCast(const AzPhysics::RayCastRequest& request) override;
+
+    protected:
+        // Physics::HeightfieldProviderNotificationBus::Handler overrides ...
+        void OnHeightfieldDataChanged(
+            const AZ::Aabb& dirtyRegion, Physics::HeightfieldProviderNotifications::HeightfieldChangeMask changeMask) override;
+
+        // ColliderShapeRequestBus
+        AZ::Aabb GetColliderShapeAabb() override;
+        bool IsTrigger() override
+        {
+            // PhysX Heightfields don't support triggers.
+            return false;
+        }
+
+        void ClearHeightfield();
+        void InitStaticRigidBody(const AZ::Transform& baseTransform);
+
+        void UpdateHeightfieldMaterialSelection(const Physics::MaterialSelection& updatedMaterialSelection);
+
+    private:
+        //! Helper class to manage the spawned physics update jobs.
+        class HeightfieldUpdateJobContext : public AZ::JobContext
+        {
+        public:
+            HeightfieldUpdateJobContext(AZ::JobManager& jobManager)
+                : JobContext(jobManager)
+            {
+            }
+
+            //! Cancel any running jobs
+            void Cancel();
+
+            //! Check to see if the jobs should be canceled.
+            bool IsCanceled() const;
+
+            //! Track all jobs that are being started.
+            void OnJobStart();
+
+            //! Track all jobs that have been completed.
+            void OnJobComplete();
+
+            //! Block until all jobs have been completed.
+            void BlockUntilComplete();
+
+        private:
+            //! Track the number of currently-running jobs. (This will currently either be 0 or 1, but may get more complicated someday)
+            int m_numRunningJobs = 0;
+            //! Mutex to protect the job-running state
+            AZStd::mutex m_jobsRunningNotificationMutex;
+            //! Notification mechanism for knowing when the jobs have stopped running.
+            //! This uses a condition_variable instead of a semaphore so that there doesn't need to be an equal number of job starts
+            //! vs "block on complete" calls.
+            AZStd::condition_variable m_jobsRunning;
+
+            //! Track whether or not the currently-running jobs should be canceled.
+            AZStd::atomic_bool m_isCanceled = false;
+        };
+
+        //! Stores collision layers, whether the collider is a trigger, etc.
+        AZStd::shared_ptr<Physics::ColliderConfiguration> m_colliderConfig;
+
+        //! Stores all of the cached information for the heightfield shape.
+        AZStd::shared_ptr<Physics::HeightfieldShapeConfiguration> m_shapeConfig;
+
+        //! Handle to the body in the provided physics scene.
+        AzPhysics::SimulatedBodyHandle m_staticRigidBodyHandle = AzPhysics::InvalidSimulatedBodyHandle; 
+
+        //! Handle to the provided physics scene.
+        AzPhysics::SceneHandle m_attachedSceneHandle = AzPhysics::InvalidSceneHandle;
+
+        //! Job context for managing the collider update jobs that get spawned.
+        AZStd::unique_ptr<HeightfieldUpdateJobContext> m_jobContext;
+
+        //! Cached entity ID for the entity this collider is attached to.
+        AZ::EntityId m_entityId;
+
+        //! Cached entity name for the entity this collider is attached to.
+        AZStd::string m_entityName;
+    };
+
+} // namespace PhysX
