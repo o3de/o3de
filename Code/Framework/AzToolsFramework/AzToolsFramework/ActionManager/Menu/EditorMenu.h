c9fde531f263a42bc80474d4f471d019a6ac38d5
Danilo Aimini <82231674+AMZN-daimini@users.noreply.github.com>
2022-06-01 09:53:05 -0700

    Refactor EditorMenu to store identifiers instead of pointers.
    Signed-off-by: Danilo Aimini <82231674+AMZN-daimini@users.noreply.github.com>

--- Code/Framework/AzToolsFramework/AzToolsFramework/ActionManager/Menu/EditorMenu.h
+++ Code/Framework/AzToolsFramework/AzToolsFramework/ActionManager/Menu/EditorMenu.h
@@ -9,7 +9,6 @@
 #pragma once
 
 #include <AzCore/std/containers/map.h>
-#include <AzCore/std/containers/variant.h>
 #include <AzCore/std/string/string.h>
 
 class QAction;
@@ -17,15 +16,20 @@ class QMenu;
 
 namespace AzToolsFramework
 {
+    class ActionManagerInterface;
+    class MenuManagerInterface;
+
     class EditorMenu
     {
     public:
         EditorMenu();
         explicit EditorMenu(const AZStd::string& name);
 
-        void AddAction(int sortKey, QAction* action);
+        static void Initialize();
+
         void AddSeparator(int sortKey);
-        void AddSubMenu(int sortKey, QMenu* submenu);
+        void AddAction(int sortKey, AZStd::string actionIdentifier);
+        void AddSubMenu(int sortKey, AZStd::string menuIdentifier);
         
         // Returns the pointer to the menu.
         QMenu* GetMenu();
@@ -42,17 +46,18 @@ namespace AzToolsFramework
 
         struct MenuItem
         {
-            MenuItem();
-            MenuItem(QAction* action);
-            MenuItem(QMenu* menu);
+            explicit MenuItem(MenuItemType type = MenuItemType::Separator, AZStd::string identifier = "");
 
             MenuItemType m_type;
 
-            AZStd::variant<QAction*, QMenu*> m_value;
+            AZStd::string m_identifier;
         };
 
         QMenu* m_menu = nullptr;
         AZStd::multimap<int, MenuItem> m_menuItems;
+
+        inline static ActionManagerInterface* m_actionManagerInterface = nullptr;
+        inline static MenuManagerInterface* m_menuManagerInterface = nullptr;
     };
 
 } // namespace AzToolsFramework
