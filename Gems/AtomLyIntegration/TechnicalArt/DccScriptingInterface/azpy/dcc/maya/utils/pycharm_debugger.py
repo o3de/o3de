2f3f9f5bc18dfef821c609eb26b4002fec5b6cb0
HogJonny-AMZN <gallowj@amazon.com>
2022-06-07 18:47:24 -0500

    license headers and file stub sweep for AR
    Signed-off-by: HogJonny-AMZN <gallowj@amazon.com>

--- Gems/AtomLyIntegration/TechnicalArt/DccScriptingInterface/azpy/dcc/maya/utils/pycharm_debugger.py
+++ Gems/AtomLyIntegration/TechnicalArt/DccScriptingInterface/azpy/dcc/maya/utils/pycharm_debugger.py
@@ -1,3 +1,14 @@
+# coding:utf-8
+#!/usr/bin/python
+#
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+#
+# -------------------------------------------------------------------------
+
 import sys
 # This should be the path your PyCharm installation
 pydevd_egg = r"C:\Program Files\JetBrains\PyCharm 2020.3.2\debug-eggs\pydevd-pycharm.egg"
