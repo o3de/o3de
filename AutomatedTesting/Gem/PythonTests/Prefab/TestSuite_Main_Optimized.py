d908989e3c4a307a88c107d47bea58bc75176a56
jckand-amzn <82226555+jckand-amzn@users.noreply.github.com>
2022-06-07 15:13:28 -0500

    Merging latest Dev
    Signed-off-by: jckand-amzn <82226555+jckand-amzn@users.noreply.github.com>

--- AutomatedTesting/Gem/PythonTests/Prefab/TestSuite_Main_Optimized.py
+++ AutomatedTesting/Gem/PythonTests/Prefab/TestSuite_Main_Optimized.py
@@ -66,6 +66,9 @@ class TestAutomationNoAutoTestMode(EditorTestSuite):
     class test_DuplicatePrefab_ContainingASingleEntity(EditorSharedTest):
         from .tests.duplicate_prefab import DuplicatePrefab_ContainingASingleEntity as test_module
 
+    class test_DuplicatePrefab_ContainingNestedEntitiesAndNestedPrefabs(EditorSharedTest):
+        from .tests.duplicate_prefab import DuplicatePrefab_ContainingNestedEntitiesAndNestedPrefabs as test_module
+
     class test_SC_Spawnables_SimpleSpawnAndDespawn(EditorSharedTest):
         from .tests.spawnables import SC_Spawnables_SimpleSpawnAndDespawn as test_module
 
