199c4855c5d29b619f5c72acfbd85f570b6bb812
HogJonny-AMZN <gallowj@amazon.com>
2022-06-07 19:22:15 -0500

    trying to fix unicode validation error
    Signed-off-by: HogJonny-AMZN <gallowj@amazon.com>

--- Gems/AtomLyIntegration/TechnicalArt/DccScriptingInterface/Tools/DCC/Blender/AddOns/SceneExporter/__init__.py
+++ Gems/AtomLyIntegration/TechnicalArt/DccScriptingInterface/Tools/DCC/Blender/AddOns/SceneExporter/__init__.py
@@ -43,7 +43,7 @@ bl_info = {
     "warning": "",
     "doc_url": "",
     "category": "Import-Export",
-} # This is needed for Blender Plugin
+}  # This is needed for Blender Plugin
 
 from typing import Any
 import bpy
@@ -58,6 +58,7 @@ from . import o3de_utils
 from . import ui
 from . import constants
 
+
 def register():
     """! This is the function that will register Classes and Global Vars for this plugin
     """
@@ -68,13 +69,12 @@ def register():
     bpy.utils.register_class(ui.SceneExporterFileMenu)
     bpy.utils.register_class(ui.ExportOptionsListDropDown)
     bpy.utils.register_class(ui.AnimationOptionsListDropDown)
-    bpy.types.Scene.export_file_name_o3de = bpy.props.StringProperty \
-        (
-        name = "",
-        description = "Export File Name",
-        default = "o3de_export"
+    bpy.types.Scene.export_file_name_o3de = bpy.props.StringProperty(
+        name="",
+        description="Export File Name",
+        default="o3de_export"
     )
-    bpy.types.TOPBAR_MT_file_export.append(ui.file_export_menu_add) # Blender Specific Class and Naming Convention. 
+    bpy.types.TOPBAR_MT_file_export.append(ui.file_export_menu_add)  # Blender Specific Class and Naming Convention.
     bpy.types.Scene.selected_o3de_project_path = ''
     bpy.types.Scene.export_textures_folder = True
     bpy.types.Scene.animation_export = constants.NO_ANIMATION
@@ -87,6 +87,7 @@ def register():
     bpy.types.Scene.animation_options_list = EnumProperty(items=constants.ANIMATION_LIST_OPTIONS, name='', default='0')
     bpy.types.WindowManager.multi_file_export_toggle = bpy.props.BoolProperty()
 
+
 def unregister():
     """! This is the function that will unregister Classes and Global Vars for this plugin
     """
@@ -97,7 +98,7 @@ def unregister():
     bpy.utils.unregister_class(ui.SceneExporterFileMenu)
     bpy.utils.unregister_class(ui.ExportOptionsListDropDown)
     bpy.utils.unregister_class(ui.AnimationOptionsListDropDown)
-    bpy.types.TOPBAR_MT_file_export.remove(ui.file_export_menu_add) # Blender Specific Class and Naming Convention. 
+    bpy.types.TOPBAR_MT_file_export.remove(ui.file_export_menu_add)  # Blender Specific Class and Naming Convention.
     del bpy.types.Scene.export_file_name_o3de
     del bpy.types.Scene.selected_o3de_project_path
     del bpy.types.Scene.o3de_projects_list
@@ -110,6 +111,7 @@ def unregister():
     del bpy.types.Scene.file_menu_animation_export
     del bpy.types.Scene.stored_image_source_paths
     del bpy.types.WindowManager.multi_file_export_toggle
+
 
 if __name__ == "__main__":
     register()
