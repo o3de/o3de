eb001421d75436bea55c57e346093b18eddffe62
Danilo Aimini <82231674+AMZN-daimini@users.noreply.github.com>
2022-06-08 16:05:39 -0700

    Introduce new API function to install an enabled state callback, and add comments to the interface.
    Signed-off-by: Danilo Aimini <82231674+AMZN-daimini@users.noreply.github.com>

--- Code/Framework/AzToolsFramework/AzToolsFramework/ActionManager/Action/ActionManagerInterface.h
+++ Code/Framework/AzToolsFramework/AzToolsFramework/ActionManager/Action/ActionManagerInterface.h
@@ -51,6 +51,11 @@ namespace AzToolsFramework
         AZ_RTTI(ActionManagerInterface, "{2E2A421E-0842-4F90-9F5C-DDE0C4F820DE}");
 
         //! Register a new Action Context to the Action Manager.
+        //! @param parentContextIdentifier The identifier for the action context the newly registered context should be parented to.
+        //! @param contextIdentifier The identifier for the newly registered action context.
+        //! @param properties The properties object for the newly registered action context.
+        //! @param widget The owning widget for the newly registered action context.
+        //! @return A successful outcome object, or a string with a message detailing the error in case of failure.
         virtual ActionManagerOperationResult RegisterActionContext(
             const AZStd::string& parentContextIdentifier,
             const AZStd::string& contextIdentifier,
@@ -59,6 +64,11 @@ namespace AzToolsFramework
         ) = 0;
 
         //! Register a new Action to the Action Manager.
+        //! @param contextIdentifier The identifier for the action context the newly registered action should be added to.
+        //! @param actionIdentifier The identifier for the newly registered action.
+        //! @param properties The properties object for the newly registered action.
+        //! @param handler The handler function that should be called when the action is triggered.
+        //! @return A successful outcome object, or a string with a message detailing the error in case of failure.
         virtual ActionManagerOperationResult RegisterAction(
             const AZStd::string& contextIdentifier,
             const AZStd::string& actionIdentifier,
@@ -67,6 +77,13 @@ namespace AzToolsFramework
         ) = 0;
 
         //! Register a new Checkable Action to the Action Manager.
+        //! @param contextIdentifier The identifier for the action context the newly registered action should be added to.
+        //! @param actionIdentifier The identifier for the newly registered action.
+        //! @param properties The properties object for the newly registered action.
+        //! @param handler The handler function that should be called when the action is triggered.
+        //! @param checkStateCallback The handler function that should be called when the checked state needs to be updated.
+        //!                           It should return a boolean 
+        //! @return A successful outcome object, or a string with a message detailing the error in case of failure.
         virtual ActionManagerOperationResult RegisterCheckableAction(
             const AZStd::string& contextIdentifier,
             const AZStd::string& actionIdentifier,
@@ -76,13 +93,32 @@ namespace AzToolsFramework
         ) = 0;
 
         //! Trigger an Action via its identifier.
+        //! @param actionIdentifier The identifier for the action to trigger.
+        //! @return A successful outcome object, or a string with a message detailing the error in case of failure.
         virtual ActionManagerOperationResult TriggerAction(const AZStd::string& actionIdentifier) = 0;
 
         //! Retrieve a QAction via its identifier.
+        //! @param actionIdentifier The identifier for the action to retrieve.
+        //! @return A raw pointer to the QAction, or nullptr if the action could not be found.
         virtual QAction* GetAction(const AZStd::string& actionIdentifier) = 0;
+        
+        //! Retrieve a QAction via its identifier (const version).
+        //! @param actionIdentifier The identifier for the action to retrieve.
+        //! @return A raw const pointer to the QAction, or nullptr if the action could not be found.
         virtual const QAction* GetActionConst(const AZStd::string& actionIdentifier) = 0;
 
+        //! Installs an enabled state callback to an action that will set its enabled state when the action is updated.
+        //! An action can only have a single enabled state callback. The function will fail if called multiple times.
+        //! 
+        virtual ActionManagerOperationResult InstallEnabledStateCallback(
+            const AZStd::string& actionIdentifier,
+            AZStd::function<bool()> enabledStateCallback
+        ) = 0;
+
         //! Update the state of a Checkable Action via its identifier.
+        //! This will update both the enabled and checked state.
+        //! @param actionIdentifier The identifier for the action to trigger.
+        //! @return A successful outcome object, or a string with a message detailing the error in case of failure.
         virtual ActionManagerOperationResult UpdateAction(const AZStd::string& actionIdentifier) = 0;
     };
 
