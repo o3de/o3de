8a7309a73a537aec8fb1c722c971f8d04a6006b6
Jérôme Leclercq <jleclercq@cyanide-studio.com>
2022-06-10 16:16:00 +0200

    AssetProcessor: allow to disable initial scan
    Signed-off-by: Jérôme Leclercq <jleclercq@cyanide-studio.com>

--- Code/Tools/AssetProcessor/native/utilities/ApplicationManagerBase.cpp
+++ Code/Tools/AssetProcessor/native/utilities/ApplicationManagerBase.cpp
@@ -322,6 +322,12 @@ void ApplicationManagerBase::Rescan()
     GetAssetScanner()->StartScan();
 }
 
+void ApplicationManagerBase::FastScan()
+{
+    m_assetProcessorManager->SetEnableModtimeSkippingFeature(true);
+    GetAssetScanner()->StartScan();
+}
+
 void ApplicationManagerBase::InitAssetCatalog()
 {
     using namespace AssetProcessor;
