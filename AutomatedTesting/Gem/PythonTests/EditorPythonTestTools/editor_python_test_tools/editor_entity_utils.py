7cef855541dfa8d78f88e7120052afab4ca3fc2d
Scott Murray <scottmur@amazon.com>
2022-06-13 19:39:17 -0700

    restoring the circular import bomb placed in editor_entity_utils.py by prefab_utils.py
    Signed-off-by: Scott Murray <scottmur@amazon.com>

--- AutomatedTesting/Gem/PythonTests/EditorPythonTestTools/editor_python_test_tools/editor_entity_utils.py
+++ AutomatedTesting/Gem/PythonTests/EditorPythonTestTools/editor_python_test_tools/editor_entity_utils.py
@@ -19,8 +19,6 @@ import azlmbr.editor as editor
 import azlmbr.math as math
 import azlmbr.legacy.general as general
 
-import editor_python_test_tools.prefab_utils as prefab_utils
-
 # Helper file Imports
 from editor_python_test_tools.utils import Report
 
@@ -266,7 +264,7 @@ class EditorComponent:
         assert (
             outcome.IsSuccess()
         ), f"Failure: Could not set value to '{self.get_component_name()}' : '{component_property_path}'"
-        prefab_utils.wait_for_propagation()
+        wait_for_propagation()
         self.get_property_tree(True)
 
     def is_enabled(self):
@@ -469,7 +467,7 @@ class EditorEntity:
         :return: Component object of newly added component.
         """
         component = self.add_components([component_name])[0]
-        prefab_utils.wait_for_propagation()
+        wait_for_propagation()
         return component
 
     def add_components(self, component_names: list) -> List[EditorComponent]:
@@ -601,7 +599,7 @@ class EditorEntity:
         :return: None
         """
         editor.ToolsApplicationRequestBus(bus.Broadcast, "DeleteEntityById", self.id)
-        prefab_utils.wait_for_propagation()
+        wait_for_propagation()
 
     def set_visibility_state(self, is_visible: bool) -> None:
         """
@@ -815,3 +813,6 @@ class EditorLevelEntity:
         """
         type_ids = EditorComponent.get_type_ids([component_name], EditorEntityType.LEVEL)
         return editor.EditorLevelComponentAPIBus(bus.Broadcast, "CountComponentsOfType", type_ids[0])
+
+# prefab_utils imports editor_entity_utils setting up a circular import causing this import to need to reside here
+from editor_python_test_tools.prefab_utils import wait_for_propagation
