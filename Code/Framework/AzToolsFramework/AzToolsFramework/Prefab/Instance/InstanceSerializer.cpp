4f4789aa81dfa6a661edde79fd8473ca9aa239c2
srikappa-amzn <82230713+srikappa-amzn@users.noreply.github.com>
2022-06-16 09:52:07 -0700

    Fix const conversion error in GetCachedInstanceDom
    Signed-off-by: srikappa-amzn <82230713+srikappa-amzn@users.noreply.github.com>

--- Code/Framework/AzToolsFramework/AzToolsFramework/Prefab/Instance/InstanceSerializer.cpp
+++ Code/Framework/AzToolsFramework/AzToolsFramework/Prefab/Instance/InstanceSerializer.cpp
@@ -159,7 +159,7 @@ namespace AzToolsFramework
             }
 
             PrefabDomUtils::InstanceDomMetadata* instanceDomMetadata = context.GetMetadata().Find<PrefabDomUtils::InstanceDomMetadata>();
-            PrefabDomValueConstReference cachedInstanceDom = instance->GetCachedInstanceDom();
+            PrefabDomConstReference cachedInstanceDom = instance->GetCachedInstanceDom();
 
             if (instanceDomMetadata == nullptr || cachedInstanceDom == AZStd::nullopt)
             {
