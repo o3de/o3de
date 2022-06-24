ca25ce7c5e924513ed20ab456c423abbd4711ec8
Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
2022-06-24 12:28:57 -0500

    Move modifySamples outside of the scene lock since it's only the shape modification that requires the lock.
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>

--- Gems/PhysX/Code/Source/Utils.cpp
+++ Gems/PhysX/Code/Source/Utils.cpp
@@ -295,13 +295,15 @@ namespace PhysX
             desc.samples.data = physxSamples.data();
             desc.samples.stride = sizeof(physx::PxHeightFieldSample);
 
-            // Lock the scene and modify the heightfield itself, as well as the shape that's using it.
+            // Modify the heightfield samples
+            constexpr bool shrinkBounds = false;
+            pxHeightfield->modifySamples(static_cast<physx::PxI32>(startCol), static_cast<physx::PxI32>(startRow), desc, shrinkBounds);
+
+            // Lock the scene and modify the heightfield shape in the scene.
             // (If only the heightfield is modified, the shape won't get refreshed with the new data)
             {
                 PHYSX_SCENE_WRITE_LOCK(pxScene);
 
-                constexpr bool shrinkBounds = false;
-                pxHeightfield->modifySamples(static_cast<physx::PxI32>(startCol), static_cast<physx::PxI32>(startRow), desc, shrinkBounds);
                 physx::PxHeightFieldGeometry hfGeom;
                 pxShape->getHeightFieldGeometry(hfGeom);
                 hfGeom.heightField = pxHeightfield;
