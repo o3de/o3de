0095937a724e34e7c10b9d5085bb0ffafce455c7
rgba16f <82187279+rgba16f@users.noreply.github.com>
2022-06-28 16:22:10 -0500

    TaskGraphEvent asserts if signal is called when no one is waiting on the event, add a 'wait' if it's going to be signalled immediately because of an empty TaskGraph
    Signed-off-by: rgba16f <82187279+rgba16f@users.noreply.github.com>

--- Code/Framework/AzCore/AzCore/Task/TaskGraph.cpp
+++ Code/Framework/AzCore/AzCore/Task/TaskGraph.cpp
@@ -105,6 +105,7 @@ namespace AZ
         {
             if (waitEvent)
             {
+                waitEvent->IncWaitCount();
                 waitEvent->Signal();
             }
             return;
