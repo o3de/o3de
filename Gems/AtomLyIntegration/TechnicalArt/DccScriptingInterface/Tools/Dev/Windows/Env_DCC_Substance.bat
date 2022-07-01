fff55df92feec5fcc12cbdf5fc36231a74c01143
HogJonny-AMZN <gallowj@amazon.com>
2022-07-01 13:58:55 -0500

    removed unnecissary attribution
    Signed-off-by: HogJonny-AMZN <gallowj@amazon.com>

--- Gems/AtomLyIntegration/TechnicalArt/DccScriptingInterface/Tools/Dev/Windows/Env_DCC_Substance.bat
+++ Gems/AtomLyIntegration/TechnicalArt/DccScriptingInterface/Tools/Dev/Windows/Env_DCC_Substance.bat
@@ -1,5 +1,5 @@
 @echo off
-REM 
+REM
 REM Copyright (c) Contributors to the Open 3D Engine Project.
 REM For complete copyright and license terms please see the LICENSE at the root of this distribution.
 REM
@@ -22,7 +22,7 @@ IF "%DCCSI_ENV_SUBSTANCE_INIT%"=="1" GOTO :END_OF_FILE
 cd %~dp0
 PUSHD %~dp0
 
-:: maintenance: latest version of Adobe CC Substance Designer, June-23-2022 @HogJonny-AMZN
+:: maintenance: latest version of Adobe CC Substance Designer, June-23-2022
 :: print(sys.version): 3.9.9 (tags/v3.9.9:ccb0e6a, Nov 15 2021, 18:08:50) [MSC v.1929 64 bit (AMD64)
 IF "%DCCSI_PY_VERSION_MAJOR%"=="" (set DCCSI_PY_VERSION_MAJOR=3)
 IF "%DCCSI_PY_VERSION_MINOR%"=="" (set DCCSI_PY_VERSION_MINOR=9)
@@ -65,7 +65,7 @@ IF "%DCCSI_SUBSTANCE_RESOURCES%"=="" (set "DCCSI_SUBSTANCE_RESOURCES=%DCCSI_TOOL
 echo     DCCSI_SUBSTANCE_RESOURCES = %DCCSI_SUBSTANCE_RESOURCES%
 
 :: Path to .exe, "C:\Program Files\Adobe\Adobe Substance 3D Designer\Adobe Substance 3D Designer.exe"
-IF "%DCCSI_SUBSTANCE_LOCATION%"=="" (set "DCCSI_SUBSTANCE_LOCATION=%ProgramFiles%\Adobe\Adobe Substance 3D Designer") 
+IF "%DCCSI_SUBSTANCE_LOCATION%"=="" (set "DCCSI_SUBSTANCE_LOCATION=%ProgramFiles%\Adobe\Adobe Substance 3D Designer")
 echo     DCCSI_SUBSTANCE_LOCATION = %DCCSI_SUBSTANCE_LOCATION%
 
 IF "%DCCSI_SUBSTANCE_EXE%"=="" (set "DCCSI_SUBSTANCE_EXE=%DCCSI_SUBSTANCE_LOCATION%\Adobe Substance 3D Designer.exe")
