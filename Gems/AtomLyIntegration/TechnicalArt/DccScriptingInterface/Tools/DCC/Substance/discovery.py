86ef9f529c19cbf6c383a5ea3e3b6941d7d1aed3
HogJonny-AMZN <gallowj@amazon.com>
2022-07-01 14:28:58 -0500

    changes to start, can't name a pkg o3de or will get a conflict
    Signed-off-by: HogJonny-AMZN <gallowj@amazon.com>

---
+++ Gems/AtomLyIntegration/TechnicalArt/DccScriptingInterface/Tools/DCC/Substance/discovery.py
@@ -0,0 +1,22 @@
+
+# coding:utf-8
+#!/usr/bin/python
+#
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+#
+# -------------------------------------------------------------------------
+"""! @brief
+Module Documentation:
+    < DCCsi >:: Tools//DCC//Substance//discovery.py
+
+This module handles discovery of the Substance3D installation.
+
+(Not Implemented): This module is stubbed but not implemented yet.
+
+# To Do: the approach Substance suggests is discovery via the registry
+# Substance docs: https://substance3d.adobe.com/documentation/sddoc/retrieving-the-installation-path-172823228.html
+"""
