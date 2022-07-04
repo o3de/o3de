69ae3854248caf2b310ae50e37f85ac6fc77d06a
Jack Curtis <jcurtisk@amazon.com>
2022-07-04 15:03:45 +0100

    Implement local storage querying
    Signed-off-by: Jack Curtis <jcurtisk@amazon.com>

--- scripts/build/TestImpactAnalysis/local_storage_query_tool.py
+++ scripts/build/TestImpactAnalysis/local_storage_query_tool.py
@@ -5,54 +5,132 @@
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 #
 #
-
+import os
+import json
+from io import BytesIO
+from pathlib import Path
 from tiaf_logger import get_logger
 from storage_query_tool import StorageQueryTool
-import boto3
+import datetime
+
 logger = get_logger(__file__)
 
 
 class LocalStorageQueryTool(StorageQueryTool):
+    """
+    Local Storage oriented subclass of the Storage Query Tool, with Create, Read, Update and Delete functionality for dealing with historic.json.zip files in buckets
+
+    """
 
     def __init__(self, **kwargs):
         """
         Initialise storage query tool with search parameters and access/delete parameters.
         """
         super().__init__(**kwargs)
-        self._search_in = kwargs.get('search_in')
-        self._search()
+        if self.has_full_address:
+            match self._action:
+                case self.Action.CREATE:
+                    logger.info('Creating file')
+                    self._put(file=self._file, storage_location=self._full_address)
+                case self.Action.READ:
+                    logger.info('Opening file')
+                    self._access(file=self._full_address)
+                case self.Action.UPDATE:
+                    logger.info('Updating File')
+                    self._update(file=self._file, storage_location=self._full_address)
+                case self.Action.DELETE:
+                    logger.info("Deleting File")
+                    self._delete(file=self._full_address)
+                case self.Action.QUERY:
+                    self._display()
+        else:
+            self._display()
 
-        if self._access_flag:
-            self._access()
-
-        if self._delete_flag:
-            self._delete()
-
-    def _search(self):
+    def _display(self):
         """
         Executes the search based on the search parameters initialised beforehand, in either the file directory or in the s3 bucket.
         """
 
-        pass
+        if self.has_full_address:
+            self._check_object_exists(self._bucket_name, self._full_address)
+        else:
+            if self._root_directory:
+                l = Path.glob(Path(self._root_directory),"**/*.json")
+                for obj in l:
+                    logger.info(obj.__str__() + ", last modified at: " + str(datetime.datetime.utcfromtimestamp(obj.stat().st_mtime)))
+            else:
+                raise SystemError("You must provide a root directory to search in or a full address when using local storage.")                    
 
-    def _write_tree(self):
+    def _check_object_exists(self, key: str):
         """
-        Displays file tree to user in console output.
-        """
-        pass
+        Method that returns whether an object exists in the specified bucket. It also writes into the console whether the file exists.
 
-    def _delete(self, file: str):
+        @param key: address of the file to look for.
+        """
+        if Path(key).is_file():
+            logger.info(f"File found at {key}")
+            return True
+        else:
+            logger.info(f"File not found at {key}")
+            return False
+
+    def _delete(self,file: str):
         """
         Deletes the specified file.
 
+        @param bucket_name: Bucket to delete file from.
         @param file: The file to be deleted.
         """
-        pass
+        if self._check_object_exists(file):
+            logger.info(
+                f"Deleting file at address: {file}")
+            Path.unlink(Path(file))
 
     def _access(self, file: str):
         """
         Accesses the specified file.
 
+        @param bucket_name: Bucket to access file in.
         @param file: The file to be accessed.
+        @param destination: Path to where file should be saved on local machine.
         """
-        pass
+        if self._check_object_exists(file):
+            os.startfile(os.path.split(file)[0])
+
+    def _put(self, file: str, storage_location: str):
+        """
+        Put the specified file in the specified location.
+
+        @param bucket-name: Bucket to store data in.
+        @param file: File to store in bucket.
+        @param storage_location: Location to store the file.
+        """
+        try:
+            json_obj = json.loads(file)
+            if not self._check_object_exists(storage_location):
+                with open(f"{storage_location}historic_data.json", "w", encoding="UTF-8") as raw_output_file:
+                        json.dump(json_obj, raw_output_file,
+                                ensure_ascii=False, indent=4)
+            else:
+                logger.info("Cancelling create, as file exists already")
+        except json.JSONDecodeError:
+            logger.error("The historic data does not contain valid json")
+
+    def _update(self, file: str, storage_location: str):
+        """
+        Replace the file in the specified location with the provided file if it exists.
+
+        @param bucket-name: Bucket to store data in.
+        @param file: File to store in bucket.
+        @param storage_location: Location to store the file.
+        """
+        try:
+            json_obj = json.loads(file)
+            if self._check_object_exists(storage_location):
+                with open(f"{storage_location}historic_data.json", "w", encoding="UTF-8") as raw_output_file:
+                        json.dump(json_obj, raw_output_file,
+                                ensure_ascii=False, indent=4)
+            else:
+                logger.info("Cancelling update, as file does not exist")
+        except json.JSONDecodeError:
+            logger.error("The historic data does not contain valid json")
