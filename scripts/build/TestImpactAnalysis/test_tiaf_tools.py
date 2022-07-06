093890e890ee0949081dbcec18e74d5187b58229
Jack Curtis <jcurtisk@amazon.com>
2022-07-06 18:10:20 +0100

    add header to test file and remove unneeded imports
    Signed-off-by: Jack Curtis <jcurtisk@amazon.com>

--- scripts/build/TestImpactAnalysis/test_tiaf_tools.py
+++ scripts/build/TestImpactAnalysis/test_tiaf_tools.py
@@ -1,10 +1,15 @@
+#
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+#
+
 
 from logging import getLogger
 from tiaf_tools import run
-import pytest
-import pytest_mock
 import os
-from pathlib import Path
 import json
 
 logging = getLogger("tiaf_tools")
