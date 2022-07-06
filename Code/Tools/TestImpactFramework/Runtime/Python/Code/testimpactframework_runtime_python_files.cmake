2c72b21511907bbbc69107f9dd9e28194a0e0093
John <jonawals@amazon.co.uk>
2022-07-06 16:42:14 +0100

    Add Python.Runtime static library.
    Signed-off-by: John <jonawals@amazon.co.uk>

---
+++ Code/Tools/TestImpactFramework/Runtime/Python/Code/testimpactframework_runtime_python_files.cmake
@@ -0,0 +1,26 @@
+#
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+#
+
+set(FILES
+    Source/Artifact/Factory/TestImpactPythonTestTargetDescriptorFactory.cpp
+    Source/Artifact/Factory/TestImpactPythonTestTargetDescriptorFactory.h
+    Source/Artifact/Static/TestImpactPythonTestTargetDescriptor.cpp
+    Source/Artifact/Static/TestImpactPythonTestTargetDescriptor.h
+    Source/Target/Python/TestImpactPythonTestTarget.cpp
+    Source/Target/Python/TestImpactPythonTestTarget.h
+    Source/TestRunner/Python/TestImpactPythonTestRunner.cpp
+    Source/TestRunner/Python/TestImpactPythonTestRunner.h
+    Source/TestEngine/Python/Job/TestImpactPythonTestJobInfoUtils.cpp
+    Source/TestEngine/Python/Job/TestImpactPythonTestJobInfoUtils.h
+    Source/TestEngine/Python/Job/TestImpactPythonTestJobInfoGenerator.cpp
+    Source/TestEngine/Python/Job/TestImpactPythonTestJobInfoGenerator.h
+    Source/TestEngine/Python/TestImpactPythonTestEngine.cpp
+    Source/TestEngine/Python/TestImpactPythonTestEngine.h
+    Source/TestEngine/Python/TestImpactPythonErrorCodeChecker.cpp
+    Source/TestEngine/Python/TestImpactPythonErrorCodeChecker.h
+)
