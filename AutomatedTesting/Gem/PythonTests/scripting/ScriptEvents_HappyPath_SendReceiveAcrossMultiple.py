507bde8d985f8fa17381f3c1e46ae58df1d7f42e
Michael <michleza@amazon.com>
2022-07-12 16:56:57 -0700

    fixed test script's deprecated code. test now waits for level entities to be instantiated on run. test uses updated component get/set property code test script canvas files recreated to send and receive the correct test signal
    Signed-off-by: Michael <michleza@amazon.com>

--- AutomatedTesting/Gem/PythonTests/scripting/ScriptEvents_HappyPath_SendReceiveAcrossMultiple.py
+++ AutomatedTesting/Gem/PythonTests/scripting/ScriptEvents_HappyPath_SendReceiveAcrossMultiple.py
@@ -5,32 +5,36 @@ For complete copyright and license terms please see the LICENSE at the root of t
 SPDX-License-Identifier: Apache-2.0 OR MIT
 """
 import os
-from utils import Report
-from utils import TestHelper as helper
-from utils import Tracer
+from editor_python_test_tools.utils import TestHelper as helper
+from editor_python_test_tools.utils import Report, Tracer
+import azlmbr.math as math
 import editor_python_test_tools.pyside_utils as pyside_utils
 import azlmbr.legacy.general as general
+import editor_python_test_tools.hydra_editor_utils as hydra
+import azlmbr.scriptcanvas as scriptcanvas
 import azlmbr.paths as paths
 
+
 # fmt: off
-class Tests():
-    entitya_created = ("Successfully created EntityA",         "Failed to create EntityA")
-    entityb_created = ("Successfully created EntityB",         "Failed to create EntityB")
+class Tests:
+    entity_created = ("Successfully created Entity",         "Failed to create Entity")
     enter_game_mode = ("Successfully entered game mode",       "Failed to enter game mode")
     lines_found     = ("Successfully found expected message",  "Failed to find expected message")
     exit_game_mode  = ("Successfully exited game mode",        "Failed to exit game mode")
 # fmt: on
 
+
 LEVEL_NAME = "Base"
 WAIT_TIME = 3.0
 ASSET_PREFIX = "T92567321"
 asset_paths = {
-    "event": os.path.join("TestAssets", f"{ASSET_PREFIX}.scriptevents"),
-    "assetA": os.path.join("ScriptCanvas", f"{ASSET_PREFIX}A.scriptcanvas"),
-    "assetB": os.path.join("ScriptCanvas", f"{ASSET_PREFIX}B.scriptcanvas"),
+    "event": os.path.join(paths.projectroot, "TestAssets", f"{ASSET_PREFIX}.scriptevents"),
+    "assetA": os.path.join(paths.projectroot, "ScriptCanvas", f"{ASSET_PREFIX}A.scriptcanvas"),
+    "assetB": os.path.join(paths.projectroot, "ScriptCanvas", f"{ASSET_PREFIX}B.scriptcanvas"),
 }
 sc_for_entities = {"EntityA": asset_paths["assetA"], "EntityB": asset_paths["assetB"]}
 EXPECTED_LINES = ["Incoming Message Received"]
+
 
 class ScriptEvents_HappyPath_SendReceiveAcrossMultiple:
     """
@@ -62,10 +66,17 @@ class ScriptEvents_HappyPath_SendReceiveAcrossMultiple:
         editor_window = None
 
     def create_editor_entity(self, name, sc_asset):
-        entity = Entity.create_editor_entity(name)
-        sc_comp = entity.add_component("Script Canvas")
-        sc_comp.set_component_property_value("Script Canvas Asset|Script Canvas Asset", get_asset(sc_asset))
-        Report.critical_result(Tests.__dict__[name.lower() + "_created"], entity.id.isValid())
+
+        sourcehandle = scriptcanvas.SourceHandleFromPath(sc_asset)
+
+        entity = hydra.Entity(name)
+        entity.create_entity(math.Vector3(512.0, 512.0, 32.0), ["Script Canvas"])
+
+        script_canvas_component = entity.components[0]
+        hydra.set_component_property_value(script_canvas_component, "Configuration|Source", sourcehandle)
+
+        Report.critical_result(Tests.entity_created, entity.id.isValid())
+        return entity
 
     def locate_expected_lines(self, line_list: list, section_tracer ):
         found_lines = [printInfo.message.strip() for printInfo in section_tracer.prints]
@@ -79,15 +90,14 @@ class ScriptEvents_HappyPath_SendReceiveAcrossMultiple:
         general.idle_enable(True)
 
         # 1) Create temp level
-
-        result = general.create_level_no_prompt(LEVEL_NAME, 128, 1, 512, True)
-        Report.critical_result(Tests.level_created, result == 0)
+        hydra.open_base_level()
         helper.wait_for_condition(lambda: general.get_current_level_name() == LEVEL_NAME, WAIT_TIME)
         general.close_pane("Error Report")
 
         # 2) Create EntityA/EntityB
         for key in sc_for_entities.keys():
-            create_editor_entity(key, sc_for_entities[key])
+            entity = self.create_editor_entity(key, sc_for_entities[key])
+            helper.wait_for_condition(lambda: entity is not None, WAIT_TIME)
 
         # 3) Start Tracer
         with Tracer() as section_tracer:
@@ -96,7 +106,7 @@ class ScriptEvents_HappyPath_SendReceiveAcrossMultiple:
             helper.enter_game_mode(Tests.enter_game_mode)
 
             # 5) Read for line
-            lines_located = helper.wait_for_condition(lambda: locate_expected_lines(EXPECTED_LINES, section_tracer), WAIT_TIME)
+            lines_located = helper.wait_for_condition(lambda: self.locate_expected_lines(EXPECTED_LINES, section_tracer), WAIT_TIME)
             Report.result(Tests.lines_found, lines_located)
 
         # 6) Exit Game Mode
@@ -105,4 +115,3 @@ class ScriptEvents_HappyPath_SendReceiveAcrossMultiple:
 
 test = ScriptEvents_HappyPath_SendReceiveAcrossMultiple()
 test.run_test()
-
