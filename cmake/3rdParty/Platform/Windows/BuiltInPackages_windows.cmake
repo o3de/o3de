f378f0432bfe694b0be7022b36e53ccd52de3a42
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2022-07-11 22:52:28 -0700

    Update pytest and pybind versions
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- cmake/3rdParty/Platform/Windows/BuiltInPackages_windows.cmake
+++ cmake/3rdParty/Platform/Windows/BuiltInPackages_windows.cmake
@@ -12,7 +12,7 @@
 ly_associate_package(PACKAGE_NAME md5-2.0-multiplatform                                 TARGETS md5                         PACKAGE_HASH 29e52ad22c78051551f78a40c2709594f0378762ae03b417adca3f4b700affdf)
 ly_associate_package(PACKAGE_NAME RapidJSON-1.1.0-rev1-multiplatform                    TARGETS RapidJSON                   PACKAGE_HASH 2f5e26ecf86c3b7a262753e7da69ac59928e78e9534361f3d00c1ad5879e4023)
 ly_associate_package(PACKAGE_NAME RapidXML-1.13-rev1-multiplatform                      TARGETS RapidXML                    PACKAGE_HASH 4b7b5651e47cfd019b6b295cc17bb147b65e53073eaab4a0c0d20a37ab74a246)
-ly_associate_package(PACKAGE_NAME pybind11-2.4.3-rev3-multiplatform                     TARGETS pybind11                    PACKAGE_HASH dccb5546607b8b31cd207033aaf24ab044ce6e188a9f12411236a010f9e0c4ff)
+ly_associate_package(PACKAGE_NAME pybind11-2.7.1-rev1-multiplatform                     TARGETS pybind11                    PACKAGE_HASH 6d55442490b4d168c6aa88b413608ebfab2e0c9c005aa15b36a95358e9a6784d)
 ly_associate_package(PACKAGE_NAME cityhash-1.1-multiplatform                            TARGETS cityhash                    PACKAGE_HASH 0ace9e6f0b2438c5837510032d2d4109125845c0efd7d807f4561ec905512dd2)
 ly_associate_package(PACKAGE_NAME zstd-1.35-multiplatform                               TARGETS zstd                        PACKAGE_HASH 45d466c435f1095898578eedde85acf1fd27190e7ea99aeaa9acfd2f09e12665)
 ly_associate_package(PACKAGE_NAME glad-2.0.0-beta-rev2-multiplatform                    TARGETS glad                        PACKAGE_HASH ff97ee9664e97d0854b52a3734c2289329d9f2b4cd69478df6d0ca1f1c9392ee)
