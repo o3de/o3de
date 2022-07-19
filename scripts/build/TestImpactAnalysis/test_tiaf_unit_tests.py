bcd09474ccd458359405905aaabb129257c43c6f
Jack Curtis <jcurtisk@amazon.com>
2022-07-19 17:01:40 +0100

    Add extensive unit testing
    Signed-off-by: Jack Curtis <jcurtisk@amazon.com>

--- scripts/build/TestImpactAnalysis/test_tiaf_unit_tests.py
+++ scripts/build/TestImpactAnalysis/test_tiaf_unit_tests.py
@@ -7,6 +7,8 @@
 #
 
 
+from cmath import exp
+from email.policy import default
 from logging import getLogger
 import os
 from tiaf import TestImpact
@@ -33,7 +35,7 @@ class TestTIAFUnitTests():
 
     @pytest.fixture
     def mock_s3_resource(self, mocker, s3_stub):
-        return mocker.patch("boto3.client", return_value = s3_stub)
+        return mocker.patch("boto3.client", return_value=s3_stub)
 
     @pytest.fixture
     def test_data_file(self):
@@ -87,13 +89,21 @@ class TestTIAFUnitTests():
     @pytest.fixture
     def default_runtime_args(self, mock_uuid, binary_path, report_path):
         runtime_args = {}
-        runtime_args['bin'] = str(binary_path).replace("/","\\")
-        runtime_args['sequence'] = "--sequence=seed" 
+        runtime_args['bin'] = str(binary_path).replace("/", "\\")
+        runtime_args['sequence'] = "--sequence=seed"
         runtime_args['safemode'] = "--safemode=off"
         runtime_args['test_failure_policy'] = "--fpolicy=continue"
-        runtime_args['report'] = "--report="+str(report_path).replace("/","\\")
+        runtime_args['report'] = "--report=" + \
+            str(report_path).replace("/", "\\")
         runtime_args['suite'] = "--suite=main"
         return runtime_args
+
+    def call_args_equal_expected_args(self, call_args, expected_args):
+        set_expected_args = set(expected_args)
+        for args_list in call_args:
+            if set(args_list) == set_expected_args:
+                return True
+        return False
 
     def test_config_files_exist(self, config_path):
         # given:
@@ -107,9 +117,10 @@ class TestTIAFUnitTests():
         # given:
         # default arguments
         # when:
-        main(tiaf_args)
+        with pytest.raises(SystemExit):
+            main(tiaf_args)
         # then:
-        subprocess.run.assert_called_with(list(default_runtime_args.values()))
+        assert self.call_args_equal_expected_args(mock_runtime.call_args.args, default_runtime_args.values())
 
     def test_invalid_config(self, caplog, tiaf_args, mock_runtime, tmp_path_factory, default_runtime_args):
         # given:
@@ -123,25 +134,27 @@ class TestTIAFUnitTests():
         main(tiaf_args)
 
         # then:
-        assert test_string in caplog.messages 
+        assert test_string in caplog.messages
 
     def test_valid_src_branch(self, caplog, tiaf_args, mock_runtime, default_runtime_args):
         # given:
         tiaf_args['src_branch'] = "development"
         test_string = "Src branch: 'development'."
         # when:
-        main(tiaf_args)
+        with pytest.raises(SystemExit):
+            main(tiaf_args)
 
         # then:
         assert test_string in caplog.messages
-    
+
     def test_invalid_src_branch(self, caplog, tiaf_args, mock_runtime, default_runtime_args):
         # given:
         tiaf_args['src_branch'] = "not_a_real_branch"
         test_string = "Src branch: 'not_a_real_branch'."
 
         # when:
-        main(tiaf_args)
+        with pytest.raises(SystemExit):
+            main(tiaf_args)
 
         # then:
         assert test_string in caplog.messages
@@ -151,18 +164,20 @@ class TestTIAFUnitTests():
         tiaf_args['dst_branch'] = "development"
         test_string = "Dst branch: 'development'."
         # when:
-        main(tiaf_args)
+        with pytest.raises(SystemExit):
+            main(tiaf_args)
 
         # then:
         assert test_string in caplog.messages
-    
+
     def test_invalid_dst_branch(self, caplog, tiaf_args, mock_runtime, default_runtime_args):
         # given:
         tiaf_args['dst_branch'] = "not_a_real_branch"
         test_string = "Dst branch: 'not_a_real_branch'."
 
         # when:
-        main(tiaf_args)
+        with pytest.raises(SystemExit):
+            main(tiaf_args)
 
         # then:
         assert test_string in caplog.messages
@@ -173,18 +188,20 @@ class TestTIAFUnitTests():
         test_string = "Commit: '9a15f038807ba8b987c9e689952d9271ef7fd086'."
 
         # when:
-        main(tiaf_args)
+        with pytest.raises(SystemExit):
+            main(tiaf_args)
 
         # then:
         assert test_string in caplog.messages
-    
+
     def test_invalid_commit(self, caplog, tiaf_args, mock_runtime, default_runtime_args):
         # given:
         tiaf_args['commit'] = "foobar"
         test_string = "Commit: 'foobar'."
 
         # when:
-        main(tiaf_args)
+        with pytest.raises(SystemExit):
+            main(tiaf_args)
 
         # then:
         assert test_string in caplog.messages
@@ -193,9 +210,10 @@ class TestTIAFUnitTests():
         # given:
         # default args
         test_string = "Attempting to access persistent storage for the commit 'foobar' for suite 'main'"
-        
+
         # when:
-        main(tiaf_args)
+        with pytest.raises(SystemExit):
+            main(tiaf_args)
 
         # then:
         assert test_string in caplog.messages
@@ -205,12 +223,99 @@ class TestTIAFUnitTests():
 
         test_response = {}
 
-        s3_stub.add_response('get_object', expected_params = {"Bucket" : "example-bucket"}, service_response = {})
-        
-        mocker.patch("boto3.client", return_value = s3_stub)
+        s3_stub.add_response('get_object', expected_params={
+                             "Bucket": "example-bucket"}, service_response={})
+
+        mocker.patch("boto3.client", return_value=s3_stub)
         main(tiaf_args)
-        pass
 
-    
+    def test_mars_index_prefix_is_supplied(self, caplog, tiaf_args, mock_runtime, mocker):
+        # given:
+        tiaf_args['mars_index_prefix'] = "test_prefix"
+        mock_mars = mocker.patch("mars_utils.transmit_report_to_mars")
+        # when:
+        with pytest.raises(SystemExit):
+            main(tiaf_args)
 
-    
+        # then:
+        mock_mars.assert_called()
+
+    def test_mars_index_prefix_is_not_supplied(self, caplog, tiaf_args, mock_runtime, mocker):
+        # given:
+        # default_args
+        mock_mars = mocker.patch("mars_utils.transmit_report_to_mars")
+        # when:
+        with pytest.raises(SystemExit):
+            main(tiaf_args)
+
+        # then:
+        mock_mars.assert_not_called()
+
+    def test_valid_test_suite_name(self, caplog, tiaf_args, mock_runtime, default_runtime_args):
+        # given:
+        # default args
+
+        # when:
+        with pytest.raises(SystemExit):
+            main(tiaf_args)
+
+        assert self.call_args_equal_expected_args(mock_runtime.call_args.args, default_runtime_args.values())
+
+    def test_invalid_test_suite_name(self, caplog, tiaf_args, mock_runtime, default_runtime_args):
+        # given:
+        tiaf_args['suite'] = "foobar"
+        default_runtime_args['suite'] = "--suite=foobar"
+        # when:
+        with pytest.raises(SystemExit):
+            main(tiaf_args)
+
+        # then:
+        assert self.call_args_equal_expected_args(mock_runtime.call_args.args, default_runtime_args.values())
+
+    @pytest.mark.parametrize("policy",["continue","abort","ignore"])
+    def test_valid_failure_policy(self, caplog, tiaf_args, mock_runtime, default_runtime_args, policy):
+        # given:
+        tiaf_args['test_failure_policy'] = policy
+        default_runtime_args['test_failure_policy'] = "--fpolicy="+policy
+
+        # set src branch to be different than dst branch so that we can get a regular sequence, otherwise TIAF will default to a see sequence and overwrite our failure policy
+        tiaf_args['src_branch'] = 122
+        default_runtime_args['sequence'] = "--sequence=regular"
+        default_runtime_args['ipolicy'] = "--ipolicy=continue"
+        # when:
+        with pytest.raises(SystemExit):
+            main(tiaf_args)
+        # then:
+        assert self.call_args_equal_expected_args(mock_runtime.call_args.args, default_runtime_args.values())
+
+    @pytest.mark.parametrize("safemode, arg_val",[(True, "on"),(None, "off")])
+    def test_safe_mode_arguments(self, caplog, tiaf_args, mock_runtime, default_runtime_args, safemode, arg_val):
+        tiaf_args['safe_mode'] = safemode
+        default_runtime_args['safemode'] = "--safemode="+arg_val
+
+        with pytest.raises(SystemExit):
+            main(tiaf_args)
+        # then:
+        assert self.call_args_equal_expected_args(mock_runtime.call_args.args, default_runtime_args.values())
+
+    def test_exclude_file_not_supplied(self, caplog, tiaf_args, mock_runtime, default_runtime_args):
+        # given:
+        # default args
+
+        # when:
+        with pytest.raises(SystemExit):
+            main(tiaf_args)
+
+        # then:
+        assert self.call_args_equal_expected_args(mock_runtime.call_args.args, default_runtime_args.values())
+
+    def test_exclude_file_supplied(self, caplog, tiaf_args, mock_runtime, default_runtime_args):
+        # given:
+        tiaf_args['exclude_file'] = "testpath"
+        default_runtime_args['exclude'] = "--exclude=testpath"
+        # when:
+        with pytest.raises(SystemExit):
+            main(tiaf_args)
+        
+        # then:
+        assert self.call_args_equal_expected_args(mock_runtime.call_args.args, default_runtime_args.values())
