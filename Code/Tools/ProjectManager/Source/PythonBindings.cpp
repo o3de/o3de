3576cf75653df8fe0b55f623890b97e9136e20f4
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2022-07-18 20:22:21 -0700

    Fix crash caused by calling trying to access 'PyErr_Occurred' after pybind11::finalize_interpreter.
    According to  https://pybind11.readthedocs.io/en/stable/reference.html :
    
    "Shut down the Python interpreter. No pybind11 or CPython API functions can be called after this. In addition, pybind11 objects must not outlive the interpreter:"
    
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- Code/Tools/ProjectManager/Source/PythonBindings.cpp
+++ Code/Tools/ProjectManager/Source/PythonBindings.cpp
@@ -232,7 +232,10 @@ namespace O3DE::ProjectManager
 
     PythonBindings::~PythonBindings()
     {
-        StopPython();
+        if (m_pythonStarted)
+        {
+            StopPython();
+        }
     }
 
     void PythonBindings::OnStdOut(const char* msg)
@@ -342,7 +345,7 @@ namespace O3DE::ProjectManager
             pybind11::finalize_interpreter();
         }
 
-        return !PyErr_Occurred();
+        return true;
     }
 
     AZ::Outcome<void, AZStd::string> PythonBindings::ExecuteWithLockErrorHandling(AZStd::function<void()> executionCallback)
