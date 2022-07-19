126003c884f94dd7896924609f80a27fa6cbeef2
John <jonawals@amazon.com>
2022-07-19 12:47:26 +0100

    Fix broken conditional for mask generation.
    Signed-off-by: John <jonawals@amazon.com>

--- Gems/AtomLyIntegration/EditorModeFeedback/Code/Source/EditorModeFeedbackFeatureProcessor.cpp
+++ Gems/AtomLyIntegration/EditorModeFeedback/Code/Source/EditorModeFeedbackFeatureProcessor.cpp
@@ -82,7 +82,7 @@ namespace AZ
         {
             m_editorStatePassSystem->AddPassesToRenderPipeline(renderPipeline);
 
-            if(m_maskRenderers.empty())
+            if(!m_maskRenderers.empty())
             {
                 return;
             }
