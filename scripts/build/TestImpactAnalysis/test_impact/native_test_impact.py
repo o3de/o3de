6f27f402025343281f9908bf73cd6b08873d1264
Jack Curtis <jcurtisk@amazon.com>
2022-07-27 16:23:52 +0100

    Remove safemode argument from NativeTestImpact
    Signed-off-by: Jack Curtis <jcurtisk@amazon.com>

--- scripts/build/TestImpactAnalysis/test_impact/native_test_impact.py
+++ scripts/build/TestImpactAnalysis/test_impact/native_test_impact.py
@@ -19,26 +19,6 @@ class NativeTestImpact(BaseTestImpact):
     _runtime_type = "native"
     _default_sequence_type = "regular"
 
-    def _parse_arguments_to_runtime(self, args, sequence_type, runtime_args):
-        """
-        Fetches the relevant keys from the provided dictionary, and applies the values of the arguments(or applies them as a flag) to our runtime_args list.
-
-        @param args: Dictionary containing the arguments passed to this TestImpact object. Will contain all the runtime arguments we need to apply.
-        @sequence_type: The sequence type as determined when initialising this TestImpact object.
-        @runtime_args: A list of strings that will become the arguments for our runtime.
-        """
-        super()._parse_arguments_to_runtime(
-            args, sequence_type, runtime_args)
-
-        if args.get(ARG_SAFE_MODE):
-            runtime_args.append("--safemode=on")
-            logger.info("Safe mode set to 'on'.")
-        else:
-            runtime_args.append("--safemode=off")
-            logger.info("Safe mode set to 'off'.")
-
-        return runtime_args
-
     @property
     def default_sequence_type(self):
         """
