b0f8849d378ba8f7cb6b3aaf427a80364d28091b
Benjamin Black <benblac@amazon.com>
2022-07-27 20:24:37 -0700

    Files for Maya materials/objects scene conversion
    Signed-off-by: Benjamin Black <benblac@amazon.com>

--- Gems/AtomLyIntegration/TechnicalArt/DccScriptingInterface/azpy/dcc/blender/helpers/blender_materials_conversion.py
+++ Gems/AtomLyIntegration/TechnicalArt/DccScriptingInterface/azpy/dcc/blender/helpers/blender_materials_conversion.py
@@ -1,15 +1,4 @@
-# coding:utf-8
-#!/usr/bin/python
-#
-# Copyright (c) Contributors to the Open 3D Engine Project.
-# For complete copyright and license terms please see the LICENSE at the root of this distribution.
-#
-# SPDX-License-Identifier: Apache-2.0 OR MIT
-#
-#
-# -------------------------------------------------------------------------
-
-#from pathlib import Path
+from pathlib import Path
 import logging as _logging
 import bpy
 from azpy.dcc.blender.helpers import convert_bsdf_material as bsdf
@@ -20,6 +9,12 @@ _LOGGER = _logging.getLogger('azpy.dcc.blender.helpers.blender_materials')
 supported_material_types = [
     'Principled BSDF'
 ]
+
+
+def prepare_material_export(target_mesh, export_location):
+    mesh_materials = get_mesh_materials(target_mesh)
+    export_path = get_mesh_export_path(target_mesh, export_location) if export_location else ''
+    return export_path, mesh_materials
 
 
 def get_current_scene():
@@ -45,6 +40,10 @@ def get_material_type(target_material):
     for node in target_material.node_tree.nodes:
         if node.name in shader_types:
             return node.name
+
+
+def get_mesh_export_path(target_mesh, export_location):
+    return Path(export_location) / f'{target_mesh}.fbx'
 
 
 def get_shader_types():
