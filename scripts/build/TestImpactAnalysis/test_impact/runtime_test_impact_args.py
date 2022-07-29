81ee8d9d2fe3080e7ea4124ec3c3e37c872b3a63
Jack Curtis <jcurtisk@amazon.com>
2022-07-29 10:44:39 +0100

    Rename runtime_args file. Rename py_arg to driver_arg. Add prefix to all enums to denote runtime support.
    Signed-off-by: Jack Curtis <jcurtisk@amazon.com>

--- scripts/build/TestImpactAnalysis/test_impact/runtime_test_impact_args.py
+++ scripts/build/TestImpactAnalysis/test_impact/runtime_test_impact_args.py
@@ -15,32 +15,32 @@ class RuntimeArgs(Enum):
     """
     Enum for all of the possible arguments that we could pass through to the TIAF runtime.
     Defined by three properties:
-    py_arg: The argument that will be passed to the tiaf_driver.
+    driver_argument: The argument that will be passed to the tiaf_driver. This can be used in TestImpact objects to access any of the values that might be passed by TIAF driver arguments.
     runtime_arg: The argument that we will set the value to.
     message: The message to log when we apply this argument to the runtime.
     """
     # Base arguments
-    SEQUENCE = ("sequence", "--sequence=", "Sequence type is set to: ")
-    FPOLICY = ("test_failure_policy", "--fpolicy=",
+    COMMON_SEQUENCE = ("sequence", "--sequence=", "Sequence type is set to: ")
+    COMMON_FPOLICY = ("test_failure_policy", "--fpolicy=",
                "Test failure policy is set to: ")
-    SUITE = ("suite", "--suite=", "Test suite is set to: ")
-    EXCLUDE = ("exclude_file", "--exclude=",
+    COMMON_SUITE = ("suite", "--suite=", "Test suite is set to: ")
+    COMMON_EXCLUDE = ("exclude_file", "--exclude=",
                "Exclude file found, excluding tests stored at: ")
-    TEST_TIMEOUT = ("test_timeout", "--ttimeout=",
+    COMMON_TEST_TIMEOUT = ("test_timeout", "--ttimeout=",
                     "Test target timeout in seconds is set to: ")
-    GLOBAL_TIMEOUT = ("global_timeout", "--gtimeout=",
+    COMMON_GLOBAL_TIMEOUT = ("global_timeout", "--gtimeout=",
                       "Global sequence timeout in seconds is set to: ")
-    IPOLICY = ("integration_policy", "--ipolicy=",
+    COMMON_IPOLICY = ("integration_policy", "--ipolicy=",
                "Integration failure policy is set to: ")
-    CHANGELIST = ("change_list", "--changelist=", "Change list is set to: ")
-    REPORT = ("report", "--report=", "Sequencer report file is set to: ")
+    COMMON_CHANGELIST = ("change_list", "--changelist=", "Change list is set to: ")
+    COMMON_REPORT = ("report", "--report=", "Sequencer report file is set to: ")
 
     # Native arguments
-    SAFEMODE = ("safe_mode", "--safemode=", "Safe mode set to: ")
+    NATIVE_SAFEMODE = ("safe_mode", "--safemode=", "Safe mode set to: ")
 
     # Python arguments
 
-    def __init__(self, py_arg, runtime_arg, message):
-        self.py_arg = py_arg
+    def __init__(self, driver_argument, runtime_arg, message):
+        self.driver_argument = driver_argument
         self.runtime_arg = runtime_arg
         self.message = message
