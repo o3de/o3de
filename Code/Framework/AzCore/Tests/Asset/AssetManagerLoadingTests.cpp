5660f5896f42298a7cd93478d78536240edb4743
amzn-mike <80125227+amzn-mike@users.noreply.github.com>
2022-08-05 11:12:34 -0500

    Switch out debug messages
    Signed-off-by: amzn-mike <80125227+amzn-mike@users.noreply.github.com>

--- Code/Framework/AzCore/Tests/Asset/AssetManagerLoadingTests.cpp
+++ Code/Framework/AzCore/Tests/Asset/AssetManagerLoadingTests.cpp
@@ -3494,14 +3494,15 @@ namespace UnitTest
 
             void OnAssetReady(Asset<AssetData> asset) override
             {
-                AZ_TracePrintf("ThreadA", "OnAssetReady called\n");
+                ColoredPrintf(COLOR_YELLOW, "ThreadA: OnAssetReady called \n");
                 m_signal.release();
                 m_signal.acquire();
-                AZ_TracePrintf("ThreadA", "OnAssetReady resumed\n");
+
+                ColoredPrintf(COLOR_YELLOW, "ThreadA: OnAssetReady resumed\n");
                 m_otherAsset = AssetManager::Instance().GetAsset<AssetWithSerializedData>(AssetC, Default);
-                AZ_TracePrintf("ThreadA", "OnAssetReady GetAsset done\n");
+                ColoredPrintf(COLOR_YELLOW, "ThreadA: OnAssetReady GetAsset done\n");
                 m_otherAsset.BlockUntilLoadComplete();
-                AZ_TracePrintf("ThreadA", "OnAssetReady asset loaded\n");
+                ColoredPrintf(COLOR_YELLOW, "ThreadA: OnAssetReady asset loaded\n");
             }
 
             AZ::Data::Asset<AZ::Data::AssetData> m_otherAsset;
@@ -3520,11 +3521,11 @@ namespace UnitTest
 
             ContainerListener listener(signal);
 
-            AZ_TracePrintf("ThreadB", "Starting\n");
+            ColoredPrintf(COLOR_YELLOW, "ThreadB: Starting\n");
             auto asset = AssetManager::Instance().GetAsset<AssetWithSerializedData>(AssetB, Default);
-            AZ_TracePrintf("ThreadB", "Got asset\n");
+            ColoredPrintf(COLOR_YELLOW, "ThreadB: Got asset\n");
             asset.BlockUntilLoadComplete();
-            AZ_TracePrintf("ThreadB", "Asset loaded\n");
+            ColoredPrintf(COLOR_YELLOW, "ThreadB: Asset loaded\n");
             finishedSignal.release();
         });
 
