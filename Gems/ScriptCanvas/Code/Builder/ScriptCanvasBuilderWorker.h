098ee3098c17295973131b3a5b8b85dc5e08a2e1
carlitosan <82187351+carlitosan@users.noreply.github.com>
2022-08-08 15:08:58 -0700

    bump builder version
    Signed-off-by: carlitosan <82187351+carlitosan@users.noreply.github.com>

--- Gems/ScriptCanvas/Code/Builder/ScriptCanvasBuilderWorker.h
+++ Gems/ScriptCanvas/Code/Builder/ScriptCanvasBuilderWorker.h
@@ -69,6 +69,7 @@ namespace ScriptCanvasBuilder
         UpdateDependencyHandling,
         AddExplicitDestructCallForMemberVariables,
         FixEntityIdReturnValuesInEvents,
+        DoNotLoadScriptEventsDuringCreateJobs,
         // add new entries above
         Current,
     };
