a3a1e31898ebed7ab5d9dba15a4c3356600667cf
Benjamin Black <benblac@amazon.com>
2022-08-09 12:25:31 -0700

    Update to MockTool main.py
    Signed-off-by: Benjamin Black <benblac@amazon.com>

--- Gems/AtomLyIntegration/TechnicalArt/DccScriptingInterface/Tools/Python/MockTool/main.py
+++ Gems/AtomLyIntegration/TechnicalArt/DccScriptingInterface/Tools/Python/MockTool/main.py
@@ -1,20 +1,56 @@
-# coding:utf-8
-#!/usr/bin/python
+# # coding:utf-8
+# #!/usr/bin/python
+# #
+# # All or portions of this file Copyright (c) Amazon.com, Inc. or its affiliates or
+# # its licensors.
+# #
+# # For complete copyright and license terms please see the LICENSE at the root of this
+# # distribution (the "License"). All use of this software is governed by the License,
+# # or, if provided, by the license below or the license accompanying this file. Do not
+# # remove or modify any license notices. This file is distributed on an "AS IS" BASIS,
+# # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# #
+
+"""! @brief MockTool is a collection of template files that can be used to start a new tool within the DCCsi. """
+
+##
+# @file main.py
 #
-# Copyright (c) Contributors to the Open 3D Engine Project.
-# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+# @brief It is difficult to predict in what form tools developed in the DCCsi will take, but a few elements will
+# remain constant when creating a new tool. Environment will usually play a significant role in tools creation. The
+# DCCsi is supported with Dynaconf for setting environments- main settings are created and maintained within both the
+# config.py and settings.json files in the base directory of the DCCsi, but per tool settings can be created by way of
+# and ".env" file. A sample of this file has been included in the sample MockTool directory.
 #
-# SPDX-License-Identifier: Apache-2.0 OR MIT
+# Logging is also an important requirement for developing tools and workflows to assist in debugging. The formatting
+# is very easy to follow- see below for an example of the setup. By changing the "_MODULENAME" path to correspond with
+# a tool's module path is all that is needed- by adding logging statements (also included below), formatted logging
+# output will be enabled.
 #
+# The last important element to point out is that each tool should include the three imports listed below:
+#  - The "config" module ensures that Python has access to O3DE-utilizied third party libraries, and establishes the
+#    Dynaconf settings
+#  - Logging must be imported in order to access the main logging system that supports the DCCsi. No new configurations
+#    are necessary. Users can leverage the default settings, or adjust the existing configuration with their own
+#    desired settings when developing tools.
+#  - The last import of "settings" connects the established Dynaconf environment. The best access of environment
+#    settings can be achieved by using a combination of settings established in the .env file, as well as leveraging
+#    DCCsi default settings, as well as targeted DCCsi environment settings that exist specific to development needs.
 #
-# -------------------------------------------------------------------------
+#  Examples of each of the elements above can be found below, and tailored to the needs of a project.
+#
+# @section Launcher Notes
+# - Comments are Doxygen compatible
+
+# Required Imports
 import config
 import logging
 from dynaconf import settings
 
-# Logging
+# Logging Formatting
 _MODULENAME = 'Tools.Python.MockTool.main'
 _LOGGER = logging.getLogger(_MODULENAME)
+_LOGGER.info('This is an example log statement')
 
 
 # To get values from single environment (and default). This is to loop through all values, but to
@@ -22,6 +58,11 @@ _LOGGER = logging.getLogger(_MODULENAME)
 for key, value in settings.items():
     if key in settings.from_env('maya'):
         _LOGGER.info(f"Key: {key}  Value: {value}")
+
+
+# Access variables from the .env file
+_LOGGER.info(f"Access the DYNACONF_ICON_PATH value from the .env file: {settings.get('ICON_PATH')}")
+_LOGGER.info(f"Access the DYNACONF_DB_PATH value from the .env file: {settings.get('DB_PATH')}")
 
 
 # Check if a value is present- if it isn't will return the second value (in this case "404")
