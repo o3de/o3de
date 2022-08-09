e79f25b3d4b044677a5c640f9c30ebb2ac735991
Chris Galvan <chgalvan@amazon.com>
2022-08-09 16:12:02 -0500

    Removed explicit dependency on Vegetation gem from Landscape Canvas
    Signed-off-by: Chris Galvan <chgalvan@amazon.com>

--- Gems/LandscapeCanvas/Code/Source/Editor/Nodes/Gradients/FastNoiseGradientNode.h
+++ Gems/LandscapeCanvas/Code/Source/Editor/Nodes/Gradients/FastNoiseGradientNode.h
@@ -22,10 +22,6 @@ namespace AZ
 
 namespace LandscapeCanvas
 {
-    // The FastNoise gem is optional, so we need to keep track of its component type ID
-    // ourselves since we can't rely on the headers being there.
-    static const AZ::TypeId EditorFastNoiseGradientComponentTypeId = "{FD018DE5-5EB4-4219-9D0C-CB3C55DE656B}";
-
     class FastNoiseGradientNode : public BaseGradientNode
     {
     public:
