3887e758e86a88e0fb6272cafee845e5840923f0
FuzzyCarterAWS <jscar@amazon.com>
2022-08-10 13:54:21 -0700

    Relocate physics_constants to new physics_utils package and required package inits.
    Signed-off-by: FuzzyCarterAWS <jscar@amazon.com>

---
+++ AutomatedTesting/Gem/PythonTests/Physics/tests/physics_utils/physics_constants.py
@@ -0,0 +1,40 @@
+"""
+Copyright (c) Contributors to the Open 3D Engine Project.
+For complete copyright and license terms please see the LICENSE at the root of this distribution.
+
+SPDX-License-Identifier: Apache-2.0 OR MIT
+
+Holds constants used across both hydra and non-hydra scripts.
+"""
+
+"""
+General Constants
+"""
+IDLE_FRAMES_1 = 1
+WAIT_TIME_1 = 1
+WAIT_TIME_3 = 3
+
+"""
+PhysX Components
+"""
+PHYSX_COLLIDER = "PhysX Collider"
+PHYSX_SHAPE_COLLIDER = "PhysX Shape Collider"
+
+"""
+Shape Components
+"""
+BOX_SHAPE_COMPONENT = "Box Shape"
+CAPSULE_SHAPE_COMPONENT = "Capsule Shape"
+CYLINDER_SHAPE_COMPONENT = "Cylinder Shape"
+POLYGON_PRISM_SHAPE_COMPONENT = "Polygon Prism Shape"
+QUAD_SHAPE_COMPONENT = "Quad Shape"
+SPHERE_SHAPE_COMPONENT = "Sphere Shape"
+
+"""
+Component Properties
+"""
+PHSX_SHAPE_COLLIDER_SHAPE = 'Shape Configuration|Shape'
+
+
+
+
