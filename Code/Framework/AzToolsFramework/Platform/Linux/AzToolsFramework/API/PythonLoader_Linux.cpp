86e67125fe770e21216ca55bf478390ea110b20d
spham-amzn <82231385+spham-amzn@users.noreply.github.com>
2022-08-12 13:56:35 -0700

    Replace hard coded python shared library name with one that is extracted from the python library target
    Signed-off-by: spham-amzn <82231385+spham-amzn@users.noreply.github.com>

--- Code/Framework/AzToolsFramework/Platform/Linux/AzToolsFramework/API/PythonLoader_Linux.cpp
+++ Code/Framework/AzToolsFramework/Platform/Linux/AzToolsFramework/API/PythonLoader_Linux.cpp
@@ -8,18 +8,28 @@
 
 #include <AzToolsFramework/API/PythonLoader.h>
 #include <AzCore/Debug/Trace.h>
+#include <AzFramework/StringFunc/StringFunc.h>
 #include <dlfcn.h>
 
 namespace AzToolsFramework::EmbeddedPython
 {
+
+
     PythonLoader::PythonLoader()
     {
-        constexpr char libPythonName[] = "libpython3.10.so.1.0";
-        m_embeddedLibPythonHandle = dlopen(libPythonName, RTLD_NOW | RTLD_GLOBAL);
+        // PYTHON_SHARED_LIBRARY_PATH must be defined in the build scripts and referencing the path to the python shared library
+        #if !defined(PYTHON_SHARED_LIBRARY_PATH)
+        #error "PYTHON_SHARED_LIBRARY_PATH is not defined"
+        #endif
+
+        AZStd::string libPythonName;
+        // Extract only the full filename and base the library load on the python shared library that is in the library load path
+        AzFramework::StringFunc::Path::GetFullFileName(PYTHON_SHARED_LIBRARY_PATH, libPythonName);
+        m_embeddedLibPythonHandle = dlopen(libPythonName.c_str(), RTLD_NOW | RTLD_GLOBAL);
         if (m_embeddedLibPythonHandle == nullptr)
         {
             [[maybe_unused]] const char* err = dlerror();
-            AZ_Error("PythonLoader", false, "Failed to load %s with error: %s\n", libPythonName, err ? err : "Unknown Error");
+            AZ_Error("PythonLoader", false, "Failed to load %s with error: %s\n", libPythonName.c_str(), err ? err : "Unknown Error");
         }
     }
 
