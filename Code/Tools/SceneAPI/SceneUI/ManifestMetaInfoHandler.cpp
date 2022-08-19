6dffa453ab884924bde1f6560f31664773990eaa
AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
2022-08-19 06:58:26 -0700

    Top level dividers in scene settings
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>

--- Code/Tools/SceneAPI/SceneUI/ManifestMetaInfoHandler.cpp
+++ Code/Tools/SceneAPI/SceneUI/ManifestMetaInfoHandler.cpp
@@ -8,17 +8,12 @@
 
 #include <AzCore/Component/ComponentApplicationBus.h>
 #include <AzCore/Memory/SystemAllocator.h>
-#include <AzCore/Serialization/EditContext.h>
-#include <AzCore/Serialization/SerializeContext.h>
 #include <SceneAPI/SceneUI/ManifestMetaInfoHandler.h>
 #include <SceneAPI/SceneCore/DataTypes/Groups/IMeshGroup.h>
 #include <SceneAPI/SceneCore/DataTypes/Groups/ISkeletonGroup.h>
 #include <SceneAPI/SceneCore/DataTypes/Groups/ISkinGroup.h>
 #include <SceneAPI/SceneCore/DataTypes/Groups/IAnimationGroup.h>
 
-#include <AzToolsFramework/API/EditorAssetSystemAPI.h>
-
-#include <QFile>
 
 namespace AZ
 {
@@ -56,56 +51,6 @@ namespace AZ
                 else if (target.RTTI_IsTypeOf(DataTypes::IAnimationGroup::TYPEINFO_Uuid()))
                 {
                     iconPath = ":/SceneUI/Manifest/AnimationGroupIcon.png";
-                }
-                // Icons for scene node group nodes that have edit serialize contexts
-                else if (target.RTTI_IsTypeOf(DataTypes::ISceneNodeGroup::TYPEINFO_Uuid()))
-                {
-                    const DataTypes::ISceneNodeGroup* sceneNodeGroup = azrtti_cast<const DataTypes::ISceneNodeGroup*>(&target);
-                    
-                    AZ::SerializeContext* serializeContext = nullptr;
-                    AZ::ComponentApplicationBus::BroadcastResult(serializeContext, &AZ::ComponentApplicationBus::Events::GetSerializeContext);
-                    AZ_Assert(serializeContext, "No serialize context");
-
-                    // Search the Edit Context for an icon.
-                    // It will have been reflected like:
-                    //  ->Attribute(AZ::Edit::Attributes::Icon, "Editor/Icons/MeshCollider.svg")
-                    auto classData = serializeContext->FindClassData(target.RTTI_GetType());
-                    if (classData && classData->m_editData)
-                    {
-                        auto editorElementData = classData->m_editData->FindElementData(AZ::Edit::ClassElements::EditorData);
-                        if (editorElementData)
-                        {
-                            if (auto iconAttribute = editorElementData->FindAttribute(AZ::Edit::Attributes::Icon))
-                            {
-                                if (auto iconAttributeData = azdynamic_cast<const AZ::Edit::AttributeData<const char*>*>(iconAttribute))
-                                {
-                                    AZStd::string iconAttributeValue = iconAttributeData->Get(&sceneNodeGroup);
-                                    if (!iconAttributeValue.empty())
-                                    {
-                                        iconPath = AZStd::move(iconAttributeValue);
-                                        
-                                        // The path is probably going to be relative to a scan directory,
-                                        // especially if this node was defined in a Gem.
-                                        // If a first check doesn't find the file, then see if the path can
-                                        // be resolved via the asset system, and pull an absolute path from there.
-                                        if (!QFile::exists(QString(iconPath.c_str())))
-                                        {
-                                            AZStd::string iconFullPath;
-                                            bool pathFound = false;
-                                            using AssetSysReqBus = AzToolsFramework::AssetSystemRequestBus;
-                                            AssetSysReqBus::BroadcastResult(
-                                                pathFound, &AssetSysReqBus::Events::GetFullSourcePathFromRelativeProductPath,
-                                                iconPath, iconFullPath);
-                                            if (pathFound)
-                                            {
-                                                iconPath = AZStd::move(iconFullPath);
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
                 }
             }
         } // SceneData
--- Gems/PhysX/Code/Source/Pipeline/MeshGroup.cpp
+++ Gems/PhysX/Code/Source/Pipeline/MeshGroup.cpp
@@ -619,6 +619,7 @@ namespace PhysX
                             ->Attribute(AZ::Edit::Attributes::AutoExpand, true)
                             ->Attribute(AZ::Edit::Attributes::NameLabelOverride, "")
                             ->Attribute(AZ::Edit::Attributes::Icon, "Editor/Icons/MeshCollider.svg")
+                            ->Attribute(AZ::Edit::Attributes::CategoryStyle, "display divider")
 
                         ->DataElement(AZ_CRC_CE("ManifestName"), &MeshGroup::m_name, "Name PhysX Mesh",
                             "<span>Name for the group. This name will also be used as a part of the name for the "
