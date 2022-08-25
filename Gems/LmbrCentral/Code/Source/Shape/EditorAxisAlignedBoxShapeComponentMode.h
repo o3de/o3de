8e5ba0a34aefb1ec0256e942e49bdbb9d9580b2c
guillaume-haerinck <guillaume.haerinck@outlook.com>
2022-08-25 19:34:08 +0200

    Use new class AxisAlignedBoxShapeComponentMode
    Signed-off-by: guillaume-haerinck <guillaume.haerinck@outlook.com>

---
+++ Gems/LmbrCentral/Code/Source/Shape/EditorAxisAlignedBoxShapeComponentMode.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+
+#pragma once
+
+#include <AzToolsFramework/ComponentModes/BoxComponentMode.h>
+
+namespace LmbrCentral
+{
+    class EditorAxisAlignedBoxShapeComponentMode : public AzToolsFramework::BoxComponentMode
+    {
+    public:
+        AZ_CLASS_ALLOCATOR_DECL
+
+        EditorAxisAlignedBoxShapeComponentMode(const AZ::EntityComponentIdPair& entityComponentIdPair, AZ::Uuid componentType);
+
+        AZStd::string GetComponentModeName() const override;
+    };
+}
