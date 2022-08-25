2c8b5a5fa6fec584909c06b55b9129bf577b6b04
amzn-ahmadkrm <105638312+amzn-ahmadkrm@users.noreply.github.com>
2022-08-25 18:21:38 +0100

    tidy up
    Signed-off-by: amzn-ahmadkrm <105638312+amzn-ahmadkrm@users.noreply.github.com>

--- Code/Framework/AzToolsFramework/AzToolsFramework/ViewportUi/ViewportUiManager.cpp
+++ Code/Framework/AzToolsFramework/AzToolsFramework/ViewportUi/ViewportUiManager.cpp
@@ -266,7 +266,7 @@ namespace AzToolsFramework::ViewportUi
         m_viewportUi->CreateViewportBorder(borderTitle, backButtonCallback);
     }
 
-    const bool ViewportUiManager::GetViewportBorderVisible()
+    bool ViewportUiManager::GetViewportBorderVisible() const
     {
         return m_viewportUi->GetViewportBorderVisible();
     }
