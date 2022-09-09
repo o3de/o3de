f1b113aff5284f3679503f2b8e13ae1e9c4a49fa
Daniel Tamkin <jotamkin@amazon.com>
2022-09-09 15:52:14 -0500

    Fix for alignment in columns without an Eliding Label
    Signed-off-by: Daniel Tamkin <jotamkin@amazon.com>

--- Code/Framework/AzFramework/AzFramework/DocumentPropertyEditor/ReflectionAdapter.cpp
+++ Code/Framework/AzFramework/AzFramework/DocumentPropertyEditor/ReflectionAdapter.cpp
@@ -335,6 +335,7 @@ namespace AZ::DocumentPropertyEditor
                 {
                     m_builder.BeginPropertyEditor<Nodes::ContainerActionButton>();
                     m_builder.Attribute(Nodes::PropertyEditor::SharePriorColumn, true);
+                    m_builder.Attribute(Nodes::PropertyEditor::Alignment, Nodes::PropertyEditor::Align::AlignRight);
                     m_builder.Attribute(Nodes::ContainerActionButton::Action, Nodes::ContainerAction::RemoveElement);
                     m_builder.AddMessageHandler(m_adapter, Nodes::ContainerActionButton::OnActivate.GetName());
                     m_builder.EndPropertyEditor();
