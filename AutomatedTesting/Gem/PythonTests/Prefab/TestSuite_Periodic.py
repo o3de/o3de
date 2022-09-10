4c2be2d732c0d1574f8576675297500c99fe6680
dm-amzn <7991771+dm-amzn@users.noreply.github.com>
2022-09-09 17:47:25 -0700

    Adding new add_entity tests
    Signed-off-by: dm-amzn <7991771+dm-amzn@users.noreply.github.com>

--- AutomatedTesting/Gem/PythonTests/Prefab/TestSuite_Periodic.py
+++ AutomatedTesting/Gem/PythonTests/Prefab/TestSuite_Periodic.py
@@ -18,5 +18,15 @@ class TestAutomationNoAutoTestMode(EditorTestSuite):
     # Enable only -BatchMode for these tests. Some tests cannot run in -autotest_mode due to UI interactions
     global_extra_cmdline_args = ["-BatchMode"]
 
-    class test_DeletePrefab_DuplicatedPrefabInstance(EditorSharedTest):
-        from .tests.delete_prefab import DeletePrefab_DuplicatedPrefabInstance as test_module
+    class test_AddEntity_UnderLevelPrefab(EditorSharedTest):
+        from .tests.add_entity import AddEntity_UnderLevelPrefab as test_module
+
+    class test_AddEntity_UnderAnotherEntity(EditorSharedTest):
+       from .tests.add_entity import AddEntity_UnderAnotherEntity as test_module
+
+    class test_AddEntity_UnderAPrefab(EditorSharedTest):
+        from .tests.add_entity import AddEntity_UnderAPrefab as test_module
+
+    class test_AddEntity_UnderChildEntityOfAnotherPrefab(EditorSharedTest):
+        from .tests.add_entity import AddEntity_UnderChildEntityOfAnotherPrefab as test_module
+
