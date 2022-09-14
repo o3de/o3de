80c2fd9705aba1d69e696ca99c64c9507cc70f9d
Sergey Pereslavtsev <pereslav@amazon.com>
2022-09-14 18:55:50 +0100

    Removed joint components restriction on the lead entity to be a collider
    Signed-off-by: Sergey Pereslavtsev <pereslav@amazon.com>

--- Gems/PhysX/Code/Editor/EditorJointConfiguration.cpp
+++ Gems/PhysX/Code/Editor/EditorJointConfiguration.cpp
@@ -8,10 +8,11 @@
 
 #include <AzCore/Serialization/EditContext.h>
 #include <AzToolsFramework/API/ToolsApplicationAPI.h>
+#include <AzToolsFramework/Entity/EditorEntityInfoBus.h>
 #include <Editor/EditorJointConfiguration.h>
 #include <Source/EditorColliderComponent.h>
 #include <Source/EditorRigidBodyComponent.h>
-#include <AzToolsFramework/Entity/EditorEntityInfoBus.h>
+#include <Source/EditorShapeColliderComponent.h>
 
 namespace
 {
@@ -483,13 +484,14 @@ namespace PhysX
             m_leadEntity);
         if (entity)
         {
+            const bool leadEntityHasRigidActor =
+                (entity->FindComponent<PhysX::EditorRigidBodyComponent>() ||
+                 entity->FindComponent<PhysX::EditorColliderComponent>() ||
+                 entity->FindComponent<PhysX::EditorShapeColliderComponent>());
+
             AZ_Warning("EditorJointComponent",
-                entity->FindComponent<PhysX::EditorRigidBodyComponent>() != nullptr,
-                "Please add a rigid body component to Entity %s. Joints do not work with a lead entity without a rigid body component.",
-                entity->GetName().c_str());
-            AZ_Warning("EditorJointComponent",
-                entity->FindComponent<PhysX::EditorColliderComponent>() != nullptr,
-                "Please add a collider component to Entity %s. Joints do not work with a lead entity without a collider component.",
+                leadEntityHasRigidActor,
+                "Please add a rigid body or collider component to Entity %s. Joints do not work with a lead entity without a rigid actor.",
                 entity->GetName().c_str());
         }
         else
