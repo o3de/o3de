83b9a808bba14e05c5f76c26472005876427a281
Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
2022-09-21 15:38:20 -0500

    [Terrain] Implemented undo/redo, and added global Paint Brush Settings (#12107)
    * First attempt at adding undo/redo to the paintbrush.
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
    
    * Switched image modification to optionally use pixel indices.
    This reduces our undo/redo buffer size by 50% and makes it easier to optimize it further.
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
    
    * Add checks for invalid AABBs to avoid debug asserts.
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
    
    * Moved Paint Brush Settings to a separate window.
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
    
    * Removed unnecessary forward declaration.
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
    
    * Moved PaintBrushConfig inside of PaintBrushSettingsWindow since that's the owner of it now.
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
    
    * Moved ownership of the global settings to PaintBrushSettingsSystemComponent.
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
    
    * Added Component Mode button for showing Paint Brush Settings
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
    
    * Added auto close to the Paint Brush Settings.
    Also removed it from the menu and added a new Paintbrush icon in-viewport.
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
    
    * More cleanup
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
    
    * Fixed some names to make the API a little more clear.
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
    
    * Missed submitting this file.
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
    
    * Moved PaintBrushSettings into separate file.
    This should fix the Linux compile error.
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
    
    * Addressed PR feedback.
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>

--- Code/Framework/AzToolsFramework/AzToolsFramework/PaintBrushSettings/PaintBrushSettingsRequestBus.h
+++ Code/Framework/AzToolsFramework/AzToolsFramework/PaintBrushSettings/PaintBrushSettingsRequestBus.h
@@ -10,48 +10,51 @@
 
 #include <AzCore/Component/ComponentBus.h>
 #include <AzCore/EBus/EBus.h>
-#include <AzCore/std/containers/span.h>
-
-#include <AzToolsFramework/Viewport/ViewportTypes.h>
+#include <AzToolsFramework/PaintBrushSettings/PaintBrushSettings.h>
 
 namespace AzToolsFramework
 {
-    //! PaintBrushRequestBus is used to query the paintbrush for its settings and the current painted values while painting.
-    class PaintBrushRequests : public AZ::EBusTraits
+    //! This is used to get/set the global paintbrush settings.
+    class PaintBrushSettingsRequests : public AZ::EBusTraits
     {
     public:
         // EBusTraits
         static const AZ::EBusHandlerPolicy HandlerPolicy = AZ::EBusHandlerPolicy::Single;
-        static const AZ::EBusAddressPolicy AddressPolicy = AZ::EBusAddressPolicy::ById;
-        using BusIdType = AZ::EntityComponentIdPair;
         using MutexType = AZStd::recursive_mutex;
 
-        //! GetRadius returns the current paintbrush radius.
+        //! Returns all of the current paintbrush settings.
+        //! This shouldn't normally be used, but it's necessary for exposing the global paintbrush settings
+        //! outwards to the Property Editor window.
+        //! @return The paintbrush settings
+        virtual PaintBrushSettings* GetSettings() = 0;
+
+        //! Returns the current paintbrush radius.
         //! @return The radius of the paintbrush
         virtual float GetRadius() const = 0;
 
-        //! GetIntensity returns the current paintbrush intensity (0=black, 1=white).
+        //! Returns the current paintbrush intensity (0=black, 1=white).
         //! @return The intensity of the paintbrush
         virtual float GetIntensity() const = 0;
 
-        //! GetOpacity returns the current paintbrush opacity (0=transparent, 1=opaque).
+        //! Returns the current paintbrush opacity (0=transparent, 1=opaque).
         virtual float GetOpacity() const = 0;
 
-        //! SetRadius sets the paintbrush radius.
+        //! Sets the paintbrush radius.
         //! @param radius The new radius, in meters.
         virtual void SetRadius(float radius) = 0;
 
-        //! SetIntensity sets the paintbrush intensity.
+        //! Sets the paintbrush intensity.
         //! @param intensity The new intensity, in 0-1 range (0=black, 1=white).
         virtual void SetIntensity(float intensity) = 0;
 
-        //! SetOpacity sets the paintbrush opacity.
+        //! Sets the paintbrush opacity.
         //! @param opacity The new opacity, in 0-1 range (0=transparent, 1=opaque).
         virtual void SetOpacity(float opacity) = 0;
 
     protected:
-        ~PaintBrushRequests() = default;
+        ~PaintBrushSettingsRequests() = default;
     };
 
-    using PaintBrushRequestBus = AZ::EBus<PaintBrushRequests>;
+    using PaintBrushSettingsRequestBus = AZ::EBus<PaintBrushSettingsRequests>;
+
 } // namespace AzToolsFramework
