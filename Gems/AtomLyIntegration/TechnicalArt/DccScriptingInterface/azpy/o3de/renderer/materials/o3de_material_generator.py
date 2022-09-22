1206c068582e80b36127b771c531f9259a7aac59
Jonny Galloway <gallowj@amazon.com>
2022-09-22 10:59:18 -0500

    license header validation correction
    Signed-off-by: Jonny Galloway <gallowj@amazon.com>

--- Gems/AtomLyIntegration/TechnicalArt/DccScriptingInterface/azpy/o3de/renderer/materials/o3de_material_generator.py
+++ Gems/AtomLyIntegration/TechnicalArt/DccScriptingInterface/azpy/o3de/renderer/materials/o3de_material_generator.py
@@ -1,15 +1,11 @@
-# -*- coding: utf-8 -*-
-# !/usr/bin/python
 #
-# All or portions of this file Copyright (c) Amazon.com, Inc. or its affiliates or
-# its licensors.
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
 #
-# For complete copyright and license terms please see the LICENSE at the root of this
-# distribution (the "License"). All use of this software is governed by the License,
-# or, if provided, by the license below or the license accompanying this file. Do not
-# remove or modify any license notices. This file is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# SPDX-License-Identifier: Apache-2.0 OR MIT
 #
+#
+# -------------------------------------------------------------------------
 
 """! @brief Description here..."""
 from DccScriptingInterface.azpy.o3de.renderer.materials import material_utilities
@@ -22,7 +18,7 @@ import json
 import os
 
 
-_LOGGER = _logging.getLogger('azpy.o3de.renderer.materials.o3de_material_generator')
+_LOGGER = _logging.getLogger('DCCsi.azpy.o3de.renderer.materials.o3de_material_generator')
 
 
 class MaterialGenerator:
