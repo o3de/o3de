25b30f689820eb65eb2a1fc638cb1884cb601ae6
FuzzyCarterAWS <jscar@amazon.com>
2022-09-27 16:52:50 -0700

    Adjusting file after getting bugfix into the branch
    Signed-off-by: FuzzyCarterAWS <jscar@amazon.com>

--- AutomatedTesting/Gem/PythonTests/Physics/tests/EntityComponentTests/PhsyX_ColliderFamily_Component_CRUD.py
+++ AutomatedTesting/Gem/PythonTests/Physics/tests/EntityComponentTests/PhsyX_ColliderFamily_Component_CRUD.py
@@ -150,26 +150,26 @@ def PhysX_ColliderFamily_Component_CRUD():
 
         # 6) PhysX Collider PhysicsAsset Mesh Support
         # --Crash--
-        # proptree = physx_collider_physicsasset_component.get_property_tree(True)
-        # proptree_count = proptree.get_container_count()
-        # proptree_list = physx_collider_physicsasset_component.get_property_type_visibility()
+        proptree = physx_collider_physicsasset_component.get_property_tree(True)
+        proptree_count = proptree.get_container_count()
+        proptree_list = physx_collider_physicsasset_component.get_property_type_visibility()
+
+        #--Crash--
+        test_entity = EditorEntity.create_editor_entity("Test")
+        test_component = test_entity.add_component("PhysX Collider")
+        has_component = test_entity.has_component("PhysX Collider")
+        print(test_component.get_property_type_visibility())
 
         # --Crash--
-        # test_entity = EditorEntity.create_editor_entity("Test")
-        # test_component = test_entity.add_component("PhysX Collider")
-        # has_component = test_entity.has_component("PhysX Collider")
-        # print(test_component.get_property_type_visibility())
+        test_entity = EditorEntity.create_editor_entity("Test")
+        test_component = test_entity.add_component("PhysX Heightfield Collider")
+        print(test_component.get_property_type_visibility())
 
         # --Crash--
-        # test_entity = EditorEntity.create_editor_entity("Test")
-        # test_component = test_entity.add_component("PhysX Hinge Joint")
-        # print(test_component.get_property_type_visibility())
-
-        # --Crash--
-        # test_entity = EditorEntity.create_editor_entity("Test")
-        # test_component = add_validated_component(test_entity, "PhysX Shape Collider", ["Test Componenet Added", "Test Component Failed."])
-        # print(test_component.get_property_type_visibility())
-        # output = ["", "Proptree: " + str(test_component.get_property_type_visibility())]
+        test_entity = EditorEntity.create_editor_entity("Test")
+        test_component = add_validated_component(test_entity, "PhysX Shape Collider", ["Test Componenet Added", "Test Component Failed."])
+        print(test_component.get_property_type_visibility())
+        output = ["", "Proptree: " + str(test_component.get_property_type_visibility())]
 
         # --Works--
         # test_entity = EditorEntity.create_editor_entity("Test")
@@ -178,15 +178,15 @@ def PhysX_ColliderFamily_Component_CRUD():
         # test_tree = test_component.get_property_tree()
         # output = ["", "Proptree: " + str(test_tree.build_paths_list())]
         # print(test_tree.build_paths_list())
-
-        # --Works--
+        #
+        # # --Works--
         # position = math.Vector3(0.0, 0.0, 0.0)
         # test_entity = hydra.Entity("test")
         # test_entity.create_entity(position, ["PhysX Collider"])
         # component = test_entity.components[0]
         # print(hydra.get_property_tree(component))
-
-        # --Works--
+        #
+        # # --Works--
         # test_entity = EditorEntity.create_editor_entity("Test")
         # test_component = test_entity.add_component("PhysX Collider")
         # has_component = test_entity.has_component("PhysX Collider")
@@ -194,7 +194,7 @@ def PhysX_ColliderFamily_Component_CRUD():
         # print(test_tree.build_paths_list())
         # print(has_component)
 
-        #Report.critical_result(output, False)
+        Report.critical_result(output, False)
 
 if __name__ == "__main__":
     from editor_python_test_tools.utils import Report
