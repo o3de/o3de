1386973429e4de98843a00f60ab1896a78019662
Michael Pollind <mpollind@gmail.com>
2022-09-27 22:13:42 -0700

    feature: work on adding support for color picker
    Signed-off-by: Michael Pollind <mpollind@gmail.com>

---
+++ Gems/GraphCanvas/Code/StaticLib/GraphCanvas/Components/NodePropertyDisplay/ColorDataInterface.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+#pragma once
+
+#include "DataInterface.h"
+#include <AzCore/Math/Color.h>
+
+namespace GraphCanvas
+{
+    class ColorDataInterface
+        : public DataInterface
+    {
+    public:
+        virtual AZ::Color GetColor() const = 0;
+        virtual void SetColor(const AZ::Color& value) = 0;
+    };
+}
