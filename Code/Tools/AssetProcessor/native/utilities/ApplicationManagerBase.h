311183563f70d2b401e1cf15eebf3ffdce02990d
AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
2022-09-28 14:43:34 -0700

    Added new flag to clear the asset database if the version of the on-disk database is a future version. This is to be used with Jenkins builds, to make it easier to share the cache across builds.
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>

--- Code/Tools/AssetProcessor/native/utilities/ApplicationManagerBase.h
+++ Code/Tools/AssetProcessor/native/utilities/ApplicationManagerBase.h
@@ -119,6 +119,20 @@ public:
 
     bool IsAssetProcessorManagerIdle() const override;
     bool CheckFullIdle();
+
+    // Used to track AP command line checks, so the help can be easily printed.
+    struct APCommandLineSwitch
+    {
+        APCommandLineSwitch(AZStd::vector<APCommandLineSwitch>& commandLineInfo, const char* switchTitle, const char* helpText)
+            : m_switch(switchTitle)
+            , m_helpText(helpText)
+        {
+            commandLineInfo.push_back(*this);
+        }
+        const char* m_switch;
+        const char* m_helpText;
+    };
+
 Q_SIGNALS:
     void CheckAssetProcessorManagerIdleState();
     void ConnectionStatusMsg(QString message);
@@ -130,7 +144,7 @@ public Q_SLOTS:
     void OnAssetProcessorManagerIdleState(bool isIdle);
 
 protected:
-    virtual void InitAssetProcessorManager();//Deletion of assetProcessor Manager will be handled by the ThreadController
+    virtual void InitAssetProcessorManager(AZStd::vector<APCommandLineSwitch>& commandLineInfo);//Deletion of assetProcessor Manager will be handled by the ThreadController
     virtual void InitAssetCatalog();//Deletion of AssetCatalog will be handled when the ThreadController is deleted by the base ApplicationManager
     virtual void InitRCController();
     virtual void DestroyRCController();
@@ -154,7 +168,7 @@ protected:
     bool InitializeInternalBuilders();
     void InitBuilderManager();
     void ShutdownBuilderManager();
-    bool InitAssetDatabase();
+    bool InitAssetDatabase(bool ignoreFutureAssetDBVersionError);
     void ShutDownAssetDatabase();
     void InitAssetServerHandler();
     void DestroyAssetServerHandler();
@@ -163,6 +177,8 @@ protected:
     virtual void InitSourceControl() = 0;
     void InitInputThread();
     void InputThread();
+
+    void HandleCommandLineHelp(AZStd::vector<APCommandLineSwitch>& commandLineInfo);
 
     // Give an opportunity to derived classes to make connections before the application server starts listening
     virtual void MakeActivationConnections() {}
