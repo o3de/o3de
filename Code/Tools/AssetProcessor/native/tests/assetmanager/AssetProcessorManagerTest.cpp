e6ffd39d9def6a898f6f8637c2fc58f0b4df0613
Junbo Liang <68558268+junbo75@users.noreply.github.com>
2022-10-10 15:18:20 -0700

    Update the variable name to make it more meaningful
    Signed-off-by: Junbo Liang <68558268+junbo75@users.noreply.github.com>

--- Code/Tools/AssetProcessor/native/tests/assetmanager/AssetProcessorManagerTest.cpp
+++ Code/Tools/AssetProcessor/native/tests/assetmanager/AssetProcessorManagerTest.cpp
@@ -3628,15 +3628,15 @@ TEST_F(AssetProcessorManagerTest, SourceFileProcessFailure_AutoFailedLfsPointerF
     settingsRegistry->Set(AZ::SettingsRegistryMergeUtils::FilePathKey_EngineRootFolder, m_assetRootDir.path().toUtf8().data());
     settingsRegistry->Set(AZ::SettingsRegistryMergeUtils::FilePathKey_ProjectPath, m_assetRootDir.path().toUtf8().data());
 
-    QDir tempPath(m_assetRootDir.path());
-    QString gitAttributesPath = tempPath.absoluteFilePath(".gitattributes");
+    QDir assetRootDir(m_assetRootDir.path());
+    QString gitAttributesPath = assetRootDir.absoluteFilePath(".gitattributes");
     ASSERT_TRUE(UnitTestUtils::CreateDummyFile(gitAttributesPath, QString(
         "#\n"
         "# Git LFS(see https ://git-lfs.github.com/)\n"
         "#\n"
         "*.txt filter=lfs diff=lfs merge=lfs -text\n")));
 
-    QString sourcePath = tempPath.absoluteFilePath("subfolder1/test.txt");
+    QString sourcePath = assetRootDir.absoluteFilePath("subfolder1/test.txt");
     ASSERT_TRUE(UnitTestUtils::CreateDummyFile(sourcePath, QString(
         "version https://git-lfs.github.com/spec/v1\n"
         "oid sha256:ee4799379bfcfa99e95afd6494da51fbeda95f21ea71d267ae7102f048edec85\n"
