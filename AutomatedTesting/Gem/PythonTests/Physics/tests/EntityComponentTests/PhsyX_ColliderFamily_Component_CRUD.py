bace59530897c2de292bb7c8c259e256496a823a
FuzzyCarterAWS <jscar@amazon.com>
2022-10-13 11:57:41 -0700

    Started rewrite of Physx Collider Component tests to use PhysxCollider.
    Signed-off-by: FuzzyCarterAWS <jscar@amazon.com>

--- AutomatedTesting/Gem/PythonTests/Physics/tests/EntityComponentTests/PhsyX_ColliderFamily_Component_CRUD.py
+++ AutomatedTesting/Gem/PythonTests/Physics/tests/EntityComponentTests/PhsyX_ColliderFamily_Component_CRUD.py
@@ -5,56 +5,6 @@ For complete copyright and license terms please see the LICENSE at the root of t
 SPDX-License-Identifier: Apache-2.0 OR MIT
 """
 
-class Tests():
-    create_collider_entity = (
-        "PhysX Collider Entity Created",
-        "PhysX Collider Entity failed to respond after creation.")
-    create_shape_collider_entity = (
-        "PhysX Shape Collider Entity Created.",
-        "PhysX Shape Collider Entity failed to respond after creation.")
-    create_physx_collider_component = (
-        "PhysX Collider Component Added.",
-        "PhysX Collider Component failed to respond after creation.")
-    create_shape_collider_component = (
-        "PhysX Shape Collider Component Added.",
-        "PhysX Shape Collider Component failed to respond after creation.")
-    set_physx_box_shape = (
-        "PhysX Collider Box Shape set.",
-        "PhysX Collider Box Shape failed to set."
-    )
-    set_physx_sphere_shape = (
-        "PhysX Collider Sphere Shape set.",
-        "PhysX Collider Sphere Shape failed to set."
-    )
-    set_physx_capsule_shape = (
-        "PhysX Collider Capsule Shape set.",
-        "PhysX Collider Capsule Shape failed to set."
-    )
-    add_box_shape_component = (
-        "Box Shape component added to PhysX Shape Collider entity successfully.",
-        "Box Shape component failed to add to PhysX Shape Collider entity")
-    add_capsule_shape_component = (
-        "Capsule Shape component added to PhysX Shape Collider entity successfully.",
-        "Capsule Shape component failed to add to PhysX Shape Collider entity"
-    )
-    add_cylinder_shape_component = (
-        "Cylinder Shape component added to PhysX Shape Collider entity successfully.",
-        "Cylinder Shape component failed to add to PhysX Shape Collider entity"
-    )
-    add_polygon_prism_shape_component = (
-        "Polygon Prism Shape component added to PhysX Shape Collider entity successfully.",
-        "Polygon Prism Shape component failed to add to PhysX Shape Collider entity"
-    )
-    add_quad_shape_component = (
-        "Quad Shape component added to PhysX Shape Collider entity successfully.",
-        "Quad Shape component failed to add to PhysX Shape Collider entity"
-    )
-    add_sphere_shape_component = (
-        "Sphere Shape component added to PhysX Shape Collider entity successfully.",
-        "Sphere Shape component failed to add to PhysX Shape Collider entity"
-    )
-
-
 def PhysX_ColliderFamily_Component_CRUD():
     """
     Summary:
@@ -63,82 +13,60 @@ def PhysX_ColliderFamily_Component_CRUD():
     Expected Behavior:
     Some Behavior
 
-    PhysX Collider Family Components:
+    PhysX Collider Components:
      - PhysX Collider
-     - PhysX Shape Collider
 
     Test Steps:
-     1) Add an Entity for each Collider Family Component
-     2) Add each Collider Family Component to its own entity (PhysX Collider, PhysX Shape Collider)
-     3) Manipulate each modifiable field and validate they are in expected state
-     4) Validate Fields are in expected state
-     5) Delete components and validate its removal
+     1) Add an Entity to manipulate
+     2) Add a PhysX Collider Component to Entity
+     3) Set Box Shape and Child Properties
+     4) Set Capsule Shape and Child Properties
+     5) Set Cylinder Shape and Child Properties
+     6) Set Sphere Shape and Child Properties
+     7) Set General Properties
+     8) Set PhyicsAsset Shape and Child Properties
+     9) Delete Component
 
     :return: None
     """
 
     # Helper file Imports
     import os
-
-    import editor_python_test_tools.hydra_editor_utils as hydra
+    import azlmbr.legacy.general as general
 
     from editor_python_test_tools.utils import Report
     from editor_python_test_tools.utils import TestHelper as helper
 
     from editor_python_test_tools.editor_component.physx_collider import PhysxCollider
-    from editor_python_test_tools.editor_entity_utils import EditorEntity as EE
-    from editor_python_test_tools.utils import Tracer
+    from editor_python_test_tools.editor_entity_utils import EditorEntity
+    from editor_python_test_tools.utils import Tracer, TestHelper
 
 
     # 0) Pre-conditions
     physx_mesh = os.path.join("objects", "_primitives", "_box_1x1.pxmesh")
     physx_material = os.path.join("physx", "glass.physxmaterial")
 
-    hydra.open_base_level()
-
-    # 1) Create entities to hold PhysX Collider Components
-    physx_collider_box_entity = EE.create_editor_entity("physx_collider_box_entity")
-    physx_collider_capsule_entity = EE.create_editor_entity("physx_collider_capsule_entity")
-    physx_collider_sphere_entity = EE.create_editor_entity("physx_collider_sphere_entity")
-    physx_collider_cylinder_entity = EE.create_editor_entity("physx_collider_cylinder_entity")
-    physx_collider_physicsasset_entity = EE.create_editor_entity("physx_collider_physicsasset_entity")
+    TestHelper.init_idle()
+    TestHelper.open_level("", "Base")
 
 
-    physx_shape_collider_box_entity = EE.create_editor_entity("physx_shape_collider_box_entity")
-    physx_shape_collider_capsule_entity = EE.create_editor_entity("physx_shape_collider_capsule_entity")
-    physx_shape_collider_cylinder_entity = EE.create_editor_entity("physx_shape_collider_cylinder_entity")
-    physx_shape_collider_polygon_prism_entity = EE.create_editor_entity("physx_shape_collider_polygon_prism_entity")
-    physx_shape_collider_quad_entity = EE.create_editor_entity("physx_shape_collider_quad_entity")
-    physx_shape_collider_sphere_entity = EE.create_editor_entity("physx_shape_collider_sphere_entity")
 
     with Tracer() as section_tracer:
+        # 1 ) Add an Entity to manipulate
+        phsyx_collider_entity = EditorEntity.create_editor_entity("TestEntity")
 
-        # 2) Adding Collider Components to their entities
-        physx_collider_box_shape_component = PhysxCollider(physx_collider_box_entity)
-        physx_collider_capsule_shape_component = PhysxCollider(physx_collider_capsule_entity)
-        physx_collider_sphere_shape_component = PhysxCollider(physx_collider_sphere_entity)
-        physx_collider_cylinder_shape_component = PhysxCollider(physx_collider_cylinder_entity)
-        physx_collider_physicsasset_component = PhysxCollider(physx_collider_physicsasset_entity)
+        # 2) Add a PhysX Collider Component to Entity
+        physx_collider = PhysxCollider(phsyx_collider_entity)
 
+        # 3) Set Box Shape and Child Properties
+        physx_collider.set_box_shape()
+        physx_collider.set_box_dimensions(0.0, 0.0, 0.0)
+        physx_collider.set_box_dimensions(256.0, 256.0, 256.0)
+        physx_collider.set_box_dimensions(2, 2.5, 2.5)
 
-        # 3) Adding Physx Shape Collider Components to corresponding Shape Collider Entities
 
         # 4) Setting Physx Collider Shapes
-        physx_collider_box_shape_component.set_box_shape()
-        physx_collider_capsule_shape_component.set_capsule_shape()
-        physx_collider_sphere_shape_component.set_sphere_shape()
-        physx_collider_cylinder_shape_component.set_cylinder_shape()
-        physx_collider_physicsasset_component.set_physx_mesh_from_path(physx_mesh)
 
-        physx_collider_box_shape_component.set_box_dimensions(1.0, 2.0, 3.0)
-
-
-        test_entity = EE.create_editor_entity("TestEntity")
-        test_collider = PhysxCollider(test_entity)
-        test_collider.set_box_shape()
-        test_collider.set_box_dimensions(5.0, 1.0, 2.7)
-
-        test_collider.set_collision_layer()
 
 
         # test_entity = EE.create_editor_entity("Test")
