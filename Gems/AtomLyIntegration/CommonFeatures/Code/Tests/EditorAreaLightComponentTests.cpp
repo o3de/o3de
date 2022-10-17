d6dac3f8f40ba894b80f1aaeb81768863cacbc2d
Tom Hulton-Harrop <82228511+hultonha@users.noreply.github.com>
2022-10-17 16:56:54 +0100

    updates to bounds test for Area Light Capsule
    Signed-off-by: Tom Hulton-Harrop <82228511+hultonha@users.noreply.github.com>

--- Gems/AtomLyIntegration/CommonFeatures/Code/Tests/EditorAreaLightComponentTests.cpp
+++ Gems/AtomLyIntegration/CommonFeatures/Code/Tests/EditorAreaLightComponentTests.cpp
@@ -12,6 +12,7 @@
 #include <Atom/RPI.Public/RPISystem.h>
 #include <Atom/RPI.Public/Scene.h>
 #include <Atom/RPI.Reflect/System/SceneDescriptor.h>
+#include <AzFramework/Visibility/BoundsBus.h>
 #include <AzTest/AzTest.h>
 #include <AzToolsFramework/ToolsComponents/TransformComponent.h>
 #include <AzToolsFramework/UnitTest/AzToolsFrameworkTestHelpers.h>
@@ -26,59 +27,81 @@ namespace UnitTest
     {
     };
 
-    TEST_F(EditorAreaLightComponentFixture, CheckCapsuleAreaLightBounds)
+    static AZStd::unique_ptr<AZ::Entity> CreateEditorAreaLightEntity(
+        const AZ::Render::AreaLightComponentConfig::LightType lightType, const AZ::TypeId& shapeTypeId, const float attenuationRadius)
+    {
+        auto entity = AZStd::make_unique<AZ::Entity>();
+        entity->Init();
+
+        entity->CreateComponent<AzToolsFramework::Components::TransformComponent>();
+        entity->CreateComponent(shapeTypeId);
+
+        AZ::Render::AreaLightComponentConfig areaLightComponentConfig;
+        areaLightComponentConfig.m_lightType = lightType;
+        areaLightComponentConfig.m_attenuationRadiusMode = AZ::Render::LightAttenuationRadiusMode::Explicit;
+        areaLightComponentConfig.m_attenuationRadius = attenuationRadius;
+
+        entity->AddComponent(aznew AZ::Render::EditorAreaLightComponent(areaLightComponentConfig));
+        entity->Activate();
+
+        return entity;
+    }
+
+    static void SetCapsuleShapeHeightAndRadius(const AZ::EntityId entityId, const float height, const float radius)
+    {
+        LmbrCentral::CapsuleShapeComponentRequestsBus::Event(
+            entityId, &LmbrCentral::CapsuleShapeComponentRequestsBus::Events::SetHeight, height);
+        LmbrCentral::CapsuleShapeComponentRequestsBus::Event(
+            entityId, &LmbrCentral::CapsuleShapeComponentRequestsBus::Events::SetRadius, radius);
+    }
+
+    TEST_F(EditorAreaLightComponentFixture, CheckEditorAreaCapsuleLightBounds)
     {
         // suppress warning when feature process is not created in test environment
         UnitTest::ErrorHandler physxCookFailed("Unable to find a AZ::Render::CapsuleLightFeatureProcessorInterface on the scene.");
 
+        // capsule shape contained within attenuation radius (effectively a sphere)
         {
-            auto entity = AZStd::make_unique<AZ::Entity>();
-            entity->Init();
+            auto entity = CreateEditorAreaLightEntity(
+                AZ::Render::AreaLightComponentConfig::LightType::Capsule, LmbrCentral::EditorCapsuleShapeComponentTypeId, 10.0f);
 
-            AZ::Render::AreaLightComponentConfig areaLightComponentConfig;
-            areaLightComponentConfig.m_attenuationRadiusMode = AZ::Render::LightAttenuationRadiusMode::Explicit;
-            areaLightComponentConfig.m_attenuationRadius = 10.0f;
-            areaLightComponentConfig.m_lightType = AZ::Render::AreaLightComponentConfig::LightType::Capsule;
+            SetCapsuleShapeHeightAndRadius(entity->GetId(), 10.0f, 2.0f);
 
-            entity->CreateComponent<AzToolsFramework::Components::TransformComponent>();
-            entity->CreateComponent(LmbrCentral::CapsuleShapeComponentTypeId);
-            entity->AddComponent(aznew AZ::Render::EditorAreaLightComponent(areaLightComponentConfig));
-            entity->Activate();
-
-            LmbrCentral::CapsuleShapeComponentRequestsBus::Event(
-                entity->GetId(), &LmbrCentral::CapsuleShapeComponentRequestsBus::Events::SetHeight, 10.0f);
-            LmbrCentral::CapsuleShapeComponentRequestsBus::Event(
-                entity->GetId(), &LmbrCentral::CapsuleShapeComponentRequestsBus::Events::SetRadius, 2.0f);
-
-            AZ::Aabb aabb = AZ::Aabb::CreateNull();
-            AzFramework::BoundsRequestBus::EventResult(aabb, entity->GetId(), &AzFramework::BoundsRequestBus::Events::GetLocalBounds);
-
+            const AZ::Aabb aabb = AzFramework::CalculateEntityLocalBoundsUnion(entity.get());
             EXPECT_THAT(aabb, IsClose(AZ::Aabb::CreateFromMinMax(AZ::Vector3(-10.0f), AZ::Vector3(10.0f))));
         }
 
+        // capsule shape contained within attenuation radius with capsule height contributing to overall height
         {
-            auto entity = AZStd::make_unique<AZ::Entity>();
-            entity->Init();
+            auto entity = CreateEditorAreaLightEntity(
+                AZ::Render::AreaLightComponentConfig::LightType::Capsule, LmbrCentral::EditorCapsuleShapeComponentTypeId, 10.0f);
 
-            AZ::Render::AreaLightComponentConfig areaLightComponentConfig;
-            areaLightComponentConfig.m_attenuationRadiusMode = AZ::Render::LightAttenuationRadiusMode::Explicit;
-            areaLightComponentConfig.m_attenuationRadius = 5.0f;
-            areaLightComponentConfig.m_lightType = AZ::Render::AreaLightComponentConfig::LightType::Capsule;
+            SetCapsuleShapeHeightAndRadius(entity->GetId(), 40.0f, 2.0f);
 
-            entity->CreateComponent<AzToolsFramework::Components::TransformComponent>();
-            entity->CreateComponent(LmbrCentral::CapsuleShapeComponentTypeId);
-            entity->AddComponent(aznew AZ::Render::EditorAreaLightComponent(areaLightComponentConfig));
-            entity->Activate();
+            const AZ::Aabb aabb = AzFramework::CalculateEntityLocalBoundsUnion(entity.get());
+            EXPECT_THAT(aabb, IsClose(AZ::Aabb::CreateFromMinMax(AZ::Vector3(-10.0f, -10.0f, -20.0f), AZ::Vector3(10.0f, 10.0f, 20.0f))));
+        }
 
-            LmbrCentral::CapsuleShapeComponentRequestsBus::Event(
-                entity->GetId(), &LmbrCentral::CapsuleShapeComponentRequestsBus::Events::SetHeight, 30.0f);
-            LmbrCentral::CapsuleShapeComponentRequestsBus::Event(
-                entity->GetId(), &LmbrCentral::CapsuleShapeComponentRequestsBus::Events::SetRadius, 2.0f);
+        // attenuation radius contained within capsule shape
+        {
+            auto entity = CreateEditorAreaLightEntity(
+                AZ::Render::AreaLightComponentConfig::LightType::Capsule, LmbrCentral::EditorCapsuleShapeComponentTypeId, 10.0f);
 
-            AZ::Aabb aabb = AZ::Aabb::CreateNull();
-            AzFramework::BoundsRequestBus::EventResult(aabb, entity->GetId(), &AzFramework::BoundsRequestBus::Events::GetLocalBounds);
+            SetCapsuleShapeHeightAndRadius(entity->GetId(), 40.0f, 15.0f);
 
-            EXPECT_THAT(aabb, IsClose(AZ::Aabb::CreateFromMinMax(AZ::Vector3(-5.0f, -5.0f, -15.0f), AZ::Vector3(5.0f, 5.0f, 15.0f))));
+            const AZ::Aabb aabb = AzFramework::CalculateEntityLocalBoundsUnion(entity.get());
+            EXPECT_THAT(aabb, IsClose(AZ::Aabb::CreateFromMinMax(AZ::Vector3(-15.0f, -15.0f, -20.0f), AZ::Vector3(15.0f, 15.0f, 20.0f))));
+        }
+
+        // attenuation radius contained within capsule shape (now effectively a sphere)
+        {
+            auto entity = CreateEditorAreaLightEntity(
+                AZ::Render::AreaLightComponentConfig::LightType::Capsule, LmbrCentral::EditorCapsuleShapeComponentTypeId, 10.0f);
+
+            SetCapsuleShapeHeightAndRadius(entity->GetId(), 50.0f, 25.0f);
+
+            const AZ::Aabb aabb = AzFramework::CalculateEntityLocalBoundsUnion(entity.get());
+            EXPECT_THAT(aabb, IsClose(AZ::Aabb::CreateFromMinMax(AZ::Vector3(-25.0f), AZ::Vector3(25.0f))));
         }
     }
 } // namespace UnitTest
