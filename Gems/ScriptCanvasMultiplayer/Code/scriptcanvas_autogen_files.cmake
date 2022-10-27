ed7f84b5cec365ba251cb452ccd1f8d3ae458cb9
Olex Lozitskiy <5432499+AMZN-Olex@users.noreply.github.com>
2022-10-27 10:40:32 -0500

    Updated ScriptCanvasMultiplayer
    Signed-off-by: Olex Lozitskiy <5432499+AMZN-Olex@users.noreply.github.com>

--- Gems/ScriptCanvasMultiplayer/Code/scriptcanvas_autogen_files.cmake
+++ Gems/ScriptCanvasMultiplayer/Code/scriptcanvas_autogen_files.cmake
@@ -13,6 +13,4 @@ set(FILES
     ${scriptcanvas_gem_root}/Code/Include/ScriptCanvas/AutoGen/ScriptCanvasNodeable_Source.jinja
     ${scriptcanvas_gem_root}/Code/Include/ScriptCanvas/AutoGen/ScriptCanvasNodeableRegistry_Header.jinja
     ${scriptcanvas_gem_root}/Code/Include/ScriptCanvas/AutoGen/ScriptCanvasNodeableRegistry_Source.jinja
-    #${scriptcanvas_gem_root}/Code/Include/ScriptCanvas/AutoGen/ScriptCanvas_Macros.jinja
-    #${scriptcanvas_gem_root}/Code/Include/ScriptCanvas/AutoGen/ScriptCanvas_Nodeable_Macros.jinja
 )
