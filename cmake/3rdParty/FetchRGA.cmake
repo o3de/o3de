e652b5d45e70cd378a52685fe231450d06f48cb5
lawsonamzn <70027408+lawsonamzn@users.noreply.github.com>
2022-11-02 14:41:53 -0700

    Makes FetchRGA older-cmake-version compatible and adds hashes
    Hashes were added (SHA256) to ensure that the files downloaded
    were exactly the expected ones at the time of snapshot (security).
    
    Timestamp option was removed - its not necessary for this package and
    is a brand new feature only in the very newest version of cmake.
    
    Signed-off-by: lawsonamzn <70027408+lawsonamzn@users.noreply.github.com>

--- cmake/3rdParty/FetchRGA.cmake
+++ cmake/3rdParty/FetchRGA.cmake
@@ -1,26 +1,27 @@
 # This script encapsulates fetching RGA as a dependency and providing the
 # requested targets. To use it, simply include this script from the CMakeLists.txt
 # file of the target platform.
+include(FetchContent)
 
-message(STATUS "started FetchRGA")
-
-if (NOT FETCHED_RGA)
-    include(FetchContent)
+# note that this won't stop it from trying each time, but will stop it from doing anything multiple times in the same
+# cmake invocation.
+FetchContent_GetProperties(rga)
+if (NOT rga_POPULATED) 
+    message(STATUS "Checking to see if Radeon(™) GPU Analyzer needs to be downloaded... ")
     if (WIN32)
         FetchContent_Declare(
             RGA
             URL https://github.com/GPUOpen-Tools/radeon_gpu_analyzer/releases/download/2.6.2/rga-windows-x64-2.6.2.zip
-            DOWNLOAD_EXTRACT_TIMESTAMP true
+            URL_HASH SHA256=35247f29bc81cd86e935b29af26a72cb5f762d4faba2b6aad404f661e639faee
         )
     else()
         FetchContent_Declare(
             RGA
             URL https://github.com/GPUOpen-Tools/radeon_gpu_analyzer/releases/download/2.6.2/rga-linux-2.6.2.tgz
-            DOWNLOAD_EXTRACT_TIMESTAMP true
+            URL_HASH SHA256=e42e51a12de5ff908785603d9fc5bf88fb8f59dd98f7ed728e8346cae251f712
         )
     endif()
-    FetchContent_MakeAvailable(RGA)
-    message("rga will be stored in ${rga_SOURCE_DIR}")
 
-    set(FETCHED_RGA ON)
+    FetchContent_MakeAvailable(RGA)
+    message(STATUS "Radeon(™) GPU Analyzer is located in ${rga_SOURCE_DIR}")
 endif()
