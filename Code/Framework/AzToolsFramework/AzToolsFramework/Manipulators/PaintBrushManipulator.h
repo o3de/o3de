2962f5491110542805b1083bf1a60234f66a4ec7
Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
2022-11-11 13:02:22 -0600

    Small fixups to the split comments.
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>

--- Code/Framework/AzToolsFramework/AzToolsFramework/Manipulators/PaintBrushManipulator.h
+++ Code/Framework/AzToolsFramework/AzToolsFramework/Manipulators/PaintBrushManipulator.h
@@ -22,23 +22,13 @@ namespace AzToolsFramework
 {
     class ManipulatorViewProjectedCircle;
 
-    //! PaintBrushManipulator contains the core logic for painting functionality.
-    //! It handles the paintbrush settings, the logic for converting mouse events into paintbrush actions,
-    //! the rendering of the paintbrush into the viewport,  and the specific value calculations for what
-    //! values are painted at each world space location.
-    //!
-    //! The component mode logic is handled separately by the component that is using the paintbrush.
-    //!
-    //! The general painting flow consists of the following:
-    //! 1. For each mouse movement while painting, the paintbrush sends OnPaint messages with the AABB of the region that has changed
-    //! and a callback for getting paint values for specific world positions.
-    //! 2. The listener calls the callback for each position in the region that it cares about.
-    //! 3. The paintbrush responds with the specific painted values for each of those positions based on the brush shape and settings.
-    //! This back-and-forth is needed so that we can keep a clean separation between the paintbrush and the listener. The paintbrush
-    //! doesn't have knowledge of which points in the world (or at which resolution) the listener needs, and the listener doesn't have
-    //! knowledge of the exact shape, size, and hardness of the paintbrush. The separation allows the paintbrush manipulator to add
-    //! features over time without having to change the listeners, and it enables more systems to use the paintbrush without requiring
-    //! specialized logic inside the paintbrush itself.
+    //! PaintBrushManipulator contains the core logic for managing the paintbrush in the Editor.
+    //! It uses the global paintbrush settings for painting and provides logic for converting mouse events into paintbrush actions.
+    //! It also draws the paintbrush circles in the viewport and provides the UX for switching paint modes.
+    //! The PaintBrushManipulator relies a PaintBrush instance to do the actual painting logic and on a component-specific
+    //! Component Mode to manage the usage of the PaintBrushManipulator (and PaintBrush) to apply the painted values to a specific
+    //! data source and to save the data appropriately.
+    //! 
     class PaintBrushManipulator
         : public BaseManipulator
         , public ManipulatorSpace
--- Code/Framework/AzToolsFramework/AzToolsFramework/PaintBrush/PaintBrush.h
+++ Code/Framework/AzToolsFramework/AzToolsFramework/PaintBrush/PaintBrush.h
@@ -14,21 +14,19 @@
 
 namespace AzToolsFramework
 {
-    //! PaintBrushManipulator contains the core logic for painting functionality.
-    //! It handles the paintbrush settings, the logic for converting mouse events into paintbrush actions,
-    //! the rendering of the paintbrush into the viewport,  and the specific value calculations for what
-    //! values are painted at each world space location.
-    //!
-    //! The component mode logic is handled separately by the component that is using the paintbrush.
+    //! PaintBrush contains the core logic for painting functionality.
+    //! It handles all of the specific calculations for determining the correct values to paint or smooth as a brush moves
+    //! across the world in a generic way. It relies on a listener to the PaintBrushNotificationBus to provide the other half
+    //! of the logic for fetching and applying the values themselves. 
     //!
     //! The general painting flow consists of the following:
-    //! 1. For each mouse movement while painting, the paintbrush sends OnPaint messages with the AABB of the region that has changed
+    //! 1. For each brush movement, the paintbrush sends OnPaint messages with the AABB of the region that has changed
     //! and a callback for getting paint values for specific world positions.
     //! 2. The listener calls the callback for each position in the region that it cares about.
     //! 3. The paintbrush responds with the specific painted values for each of those positions based on the brush shape and settings.
     //! This back-and-forth is needed so that we can keep a clean separation between the paintbrush and the listener. The paintbrush
     //! doesn't have knowledge of which points in the world (or at which resolution) the listener needs, and the listener doesn't have
-    //! knowledge of the exact shape, size, and hardness of the paintbrush. The separation allows the paintbrush manipulator to add
+    //! knowledge of the exact shape, size, and hardness of the paintbrush. The separation allows the paintbrush to add
     //! features over time without having to change the listeners, and it enables more systems to use the paintbrush without requiring
     //! specialized logic inside the paintbrush itself.
     class PaintBrush
