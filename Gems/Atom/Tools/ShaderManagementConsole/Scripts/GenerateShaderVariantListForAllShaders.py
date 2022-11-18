776a08f2fc557a237a35b4205f308c1a2e6abc2a
Ethan Chen <ethanchen1227@gmail.com>
2022-11-18 17:02:29 +0900

    Scan for project Shaders folder
    Signed-off-by: Ethan Chen <ethanchen1227@gmail.com>

--- Gems/Atom/Tools/ShaderManagementConsole/Scripts/GenerateShaderVariantListForAllShaders.py
+++ Gems/Atom/Tools/ShaderManagementConsole/Scripts/GenerateShaderVariantListForAllShaders.py
@@ -8,17 +8,23 @@ SPDX-License-Identifier: Apache-2.0 OR MIT
 import re
 import GenerateShaderVariantListUtil
 
-def main():
-    print("==== Begin processing shader ==========================================================")
-    # Search all .shader in Gems folder
-    gemsdir = azlmbr.paths.engroot + '/Gems'
-    for subdir, dirs, files in os.walk(gemsdir):
+def search_and_save_shadervariant(searchPath):
+    for subdir, dirs, files in os.walk(searchPath):
         for file in files:
             filename = os.path.join(subdir, file)
             if re.search('^.*\.shader$', filename):
                 shaderVariantList, defaultShaderVariantListFilePath = GenerateShaderVariantListUtil.create_shadervariantlist_for_shader(filename)
                 if shaderVariantList.shaderVariants:
                     azlmbr.shader.SaveShaderVariantListSourceData(defaultShaderVariantListFilePath, shaderVariantList)
+
+
+def main():
+    print("==== Begin processing shader ==========================================================")
+    # Search all .shader in Engine Gems folder
+    search_and_save_shadervariant(azlmbr.paths.engroot + '/Gems')
+
+    # Search in project folder
+    search_and_save_shadervariant(azlmbr.paths.projectroot + '/Shaders')
 
     print("==== Finish processing shader ==========================================================")
 if __name__ == "__main__":
