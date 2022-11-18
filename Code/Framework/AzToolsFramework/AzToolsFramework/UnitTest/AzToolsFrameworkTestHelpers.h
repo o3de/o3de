b14f7a80ca3caf01e5c85cb5a685542693b8d522
Junhao Wang <wjunhao@amazon.com>
2022-11-18 15:23:31 -0800

    Connect PrefabTestFixture to EditorRequestBus (#13211)
    * Connect PrefabTestFixture to EditorRequestBus
    
    Signed-off-by: Junhao Wang <wjunhao@amazon.com>
    
    * Add templated helper CreateComponentIfMissing
    
    Signed-off-by: Junhao Wang <wjunhao@amazon.com>
    
    * AddRequiredEditorComponents takes entity id list
    
    Signed-off-by: Junhao Wang <wjunhao@amazon.com>
    
    * Address some PR comments
    
    Signed-off-by: Junhao Wang <wjunhao@amazon.com>
    
    * Remove unnecessary tick call after root prefab creation
    
    Signed-off-by: Junhao Wang <wjunhao@amazon.com>
    
    Signed-off-by: Junhao Wang <wjunhao@amazon.com>

--- Code/Framework/AzToolsFramework/AzToolsFramework/UnitTest/AzToolsFrameworkTestHelpers.h
+++ Code/Framework/AzToolsFramework/AzToolsFramework/UnitTest/AzToolsFrameworkTestHelpers.h
@@ -421,6 +421,18 @@ namespace UnitTest
     /// Optional second parameter of Entity pointer if required.
     AZ::EntityId CreateEditorLayerEntity(const char* name, AZ::Entity** outEntity = nullptr);
 
+    /// Create a component for a given entity if the component is missing.
+    template<class ComponentType>
+    void CreateComponentIfMissing(AZ::Entity* entity)
+    {
+        ASSERT_TRUE(entity != nullptr) << "Cannot create a component for a null entity.";
+
+        if (!(entity->FindComponent<ComponentType>()))
+        {
+            entity->CreateComponent<ComponentType>();
+        }
+    }
+    
     using SliceAssets = AZStd::unordered_map<AZ::Data::AssetId, AZ::Data::Asset<AZ::SliceAsset>>;
 
     /// This function transfers the ownership of all the entity pointers - do not delete or use them afterwards.
