23ded6fa4340019a71d7d611b0041bc49d78dee7
Ethan Chen <ethanchen1227@gmail.com>
2022-11-29 00:11:16 +0900

    Fix jenkins test failed
    Signed-off-by: Ethan Chen <ethanchen1227@gmail.com>

--- Code/Framework/AzCore/AzCore/Name/Name.cpp
+++ Code/Framework/AzCore/AzCore/Name/Name.cpp
@@ -341,10 +341,10 @@ namespace AZ
                 ->Constructor()
                 ->Constructor<AZStd::string_view>()
                 ->Method("ToString", &Name::GetCStr)
+                ->Method("__repr__", &Name::GetCStr)
                 ->Method("Set", [](Name* thisPtr, AZStd::string_view name) { thisPtr->SetName(name); })
                 ->Method("IsEmpty", &Name::IsEmpty)
                 ->Method("Equal", static_cast<bool(Name::*)(const Name&)const>(&Name::operator==))
-                ->Method("__repr__", &Name::GetCStr)
                 ->Attribute(AZ::Script::Attributes::Operator, AZ::Script::Attributes::OperatorType::Equal)
                 ->Attribute(AZ::Script::Attributes::ExcludeFrom, AZ::Script::Attributes::ExcludeFlags::All)
                 ;
