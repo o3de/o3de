6f566e590ee38c2b359ec551587373ac99e9271f
sphrose <82213493+sphrose@users.noreply.github.com>
2022-11-29 17:04:02 +0000

    Ensure autosave timers are stopped and started correctly.
    Signed-off-by: sphrose <82213493+sphrose@users.noreply.github.com>

--- Code/Editor/MainWindow.cpp
+++ Code/Editor/MainWindow.cpp
@@ -1487,11 +1487,11 @@ void MainWindow::OnEditorNotifyEvent(EEditorNotifyEvent ev)
     case eNotify_OnBeginSceneOpen:
     case eNotify_OnBeginNewScene:
     case eNotify_OnCloseScene:
-        ResetAutoSaveTimers();
+        StopAutoSaveTimers();
         break;
     case eNotify_OnEndSceneOpen:
     case eNotify_OnEndNewScene:
-        ResetAutoSaveTimers(true);
+        StartAutoSaveTimers();
         break;
     }
 }
@@ -1533,7 +1533,7 @@ void MainWindow::RefreshStyle()
     GetIEditor()->Notify(eNotify_OnStyleChanged);
 }
 
-void MainWindow::ResetAutoSaveTimers(bool bForceInit)
+void MainWindow::StopAutoSaveTimers()
 {
     if (m_autoSaveTimer)
     {
@@ -1545,35 +1545,50 @@ void MainWindow::ResetAutoSaveTimers(bool bForceInit)
     }
     m_autoSaveTimer = nullptr;
     m_autoRemindTimer = nullptr;
+}
 
-    if (bForceInit)
+void MainWindow::StartAutoSaveTimers()
+{
+    if (gSettings.autoBackupTime > 0 && gSettings.autoBackupEnabled)
     {
-        if (gSettings.autoBackupTime > 0 && gSettings.autoBackupEnabled)
-        {
-            m_autoSaveTimer = new QTimer(this);
-            m_autoSaveTimer->start(gSettings.autoBackupTime * 1000 * 60);
-            connect(m_autoSaveTimer, &QTimer::timeout, this, [&]() {
+        m_autoSaveTimer = new QTimer(this);
+        m_autoSaveTimer->start(gSettings.autoBackupTime * 1000 * 60);
+        connect(
+            m_autoSaveTimer,
+            &QTimer::timeout,
+            this,
+            [&]()
+            {
                 if (gSettings.autoBackupEnabled)
                 {
                     // Call autosave function of CryEditApp
                     GetIEditor()->GetDocument()->SaveAutoBackup();
                 }
             });
-        }
-        if (gSettings.autoRemindTime > 0)
-        {
-            m_autoRemindTimer = new QTimer(this);
-            m_autoRemindTimer->start(gSettings.autoRemindTime * 1000 * 60);
-            connect(m_autoRemindTimer, &QTimer::timeout, this, [&]() {
+    }
+    if (gSettings.autoRemindTime > 0)
+    {
+        m_autoRemindTimer = new QTimer(this);
+        m_autoRemindTimer->start(gSettings.autoRemindTime * 1000 * 60);
+        connect(
+            m_autoRemindTimer,
+            &QTimer::timeout,
+            this,
+            [&]()
+            {
                 if (gSettings.autoRemindTime > 0)
                 {
                     // Remind to save.
                     CCryEditApp::instance()->SaveAutoRemind();
                 }
             });
-        }
     }
+}
 
+void MainWindow::ResetAutoSaveTimers()
+{
+    StopAutoSaveTimers();
+    StartAutoSaveTimers();
 }
 
 void MainWindow::ResetBackgroundUpdateTimer()
