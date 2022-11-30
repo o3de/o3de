acffbfc69d4f58cadaf01cea77c73822da7f8cfc
Chris Burel <burelc@amazon.com>
2022-11-30 13:25:14 -0500

    Remove redundant azmalloc, azcreate, and azcalloc overloads
    The allocation records no longer fill in allocation names that are supplied
    by the user. In many cases these names would come from AZStd containers,
    and were not that useful. It is more informative to track the location that
    the allocation originated from, which is captured in the record's
    callstack.
    
    Signed-off-by: Chris Burel <burelc@amazon.com>

--- Code/Framework/AzCore/AzCore/Serialization/SerializeContext.h
+++ Code/Framework/AzCore/AzCore/Serialization/SerializeContext.h
@@ -1473,7 +1473,7 @@ namespace AZ
             else
             {
                 // Otherwise use the AZ::SystemAllocator
-                return azmalloc(sizeof(T), alignof(T), AZ::SystemAllocator, "");
+                return azmalloc(sizeof(T), alignof(T), AZ::SystemAllocator);
             }
         }
         template<typename T>
@@ -1731,7 +1731,7 @@ namespace AZ
         {
             void* Create([[maybe_unused]] const char* name) override
             {
-                return new(azmalloc(sizeof(T), AZStd::alignment_of<T>::value, AZ::SystemAllocator, name))T;
+                return new(azmalloc(sizeof(T), AZStd::alignment_of<T>::value, AZ::SystemAllocator))T;
             }
             void Destroy(void* ptr) override
             {
