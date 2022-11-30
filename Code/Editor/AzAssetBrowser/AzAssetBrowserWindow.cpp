c9412ec4d5a4f01887a2c846050d2310e8eda9b2
Daniel Tamkin <jotamkin@amazon.com>
2022-11-30 15:23:49 -0600

    Add a folder filter for two column mode
    Signed-off-by: Daniel Tamkin <jotamkin@amazon.com>

--- Code/Editor/AzAssetBrowser/AzAssetBrowserWindow.cpp
+++ Code/Editor/AzAssetBrowser/AzAssetBrowserWindow.cpp
@@ -257,8 +257,9 @@ void AzAssetBrowserWindow::CreateToolsMenu()
         auto* projectSourceAssets = new QAction(tr("Filter Project and Source Assets"), this);
         projectSourceAssets->setCheckable(true);
         projectSourceAssets->setChecked(true);
-        connect(projectSourceAssets, &QAction::triggered, this, [this] { m_ui->m_searchWidget->FilterProjectSourceAssets(); });
+        connect(projectSourceAssets, &QAction::triggered, this, [this] { m_ui->m_searchWidget->ToggleProjectSourceAssetFilter(); });
         m_ui->m_searchWidget->GetFilter()->AddFilter(m_ui->m_searchWidget->GetProjectSourceFilter());
+        m_ui->m_searchWidget->AddFolderFilter();
         m_toolsMenu->addAction(projectSourceAssets);
     }
 
@@ -344,11 +345,13 @@ void AzAssetBrowserWindow::SetTwoColumnMode(QWidget* viewToShow)
 {
     m_ui->m_middleStackWidget->show();
     m_ui->m_middleStackWidget->setCurrentWidget(viewToShow);
+    m_ui->m_searchWidget->AddFolderFilter();
 }
 
 void AzAssetBrowserWindow::SetOneColumnMode()
 {
     m_ui->m_middleStackWidget->hide();
+    m_ui->m_searchWidget->RemoveFolderFilter();
 }
 
 static void ExpandTreeToIndex(QTreeView* treeView, const QModelIndex& index)
