3371b50290454224ad641e148ecd3fde81603aa9
Rosario Cox <lesaelr@amazon.com>
2022-12-09 12:50:24 -0600

    Renaming files, removing commented coded used for debugging while building the tests.
    Signed-off-by: Rosario Cox <lesaelr@amazon.com>

--- AutomatedTesting/Gem/PythonTests/assetpipeline/ap_fixtures/debug_parse_and_compare.py
+++ AutomatedTesting/Gem/PythonTests/assetpipeline/ap_fixtures/debug_parse_and_compare.py
@@ -5,39 +5,14 @@ For complete copyright and license terms please see the LICENSE at the root of t
 SPDX-License-Identifier: Apache-2.0 OR MIT
 """
 
-import json
 import os
 import re
-import xml.etree.ElementTree as xml
-import itertools
 from typing import List
-from dataclasses import dataclass
-import xml.parsers.expat as expat
-from automatedtesting_shared import asset_database_utils as asset_db_utils
-from ly_test_tools.o3de.asset_processor import ASSET_PROCESSOR_PLATFORM_MAP
 import ly_test_tools.o3de.pipeline_utils as utils
 import warnings
 import logging
 
 logger = logging.getLogger(__name__)
-
-
-@dataclass
-class BlackboxDebugTest:
-    values_to_find = {}
-
-
-def populate_value_list_from_json(data_products):
-    """
-    Helper function used by the test_builder.py module to populate an expected product assets list from a json file.
-    :param data_products: The "product" key from the target json dict.
-    :return: list_products: A list of products.
-    """
-
-    list_products = []
-    for product in data_products:
-        pass
-    return list_products
 
 
 def populate_expected_product_assets(assets_to_validate: List[str]):
@@ -230,55 +205,3 @@ def compare_scene_debug_file(asset_processor, expected_file_path: str, actual_fi
                                                                         diff_expected,
                                                                         actual_file_path,
                                                                         expected_file_path)
-
-
-"""
-def build_dict(lists):
-
-    built_dict = {}
-    i = 0
-    try:
-        for sub_list in lists:
-            sub_dict = {}
-            for line in sub_list:
-                if ":" in line:
-                    key, value = line.strip().split(":", 1)
-                    sub_dict[key] = value
-                elif line == '\n':
-                    pass
-                else:
-                    key = line
-                    value = None
-                    sub_dict[key] = value
-            built_dict[sub_dict['Node Name']] = sub_dict
-            i += 1
-
-    except: ValueError
-
-    return built_dict
-
-#def build_dict_from_xml(xml_file):
-def test_build_dict_from_xml():
-    
-    #A function that reads the contents of an xml file to create a dictionary.
-    #Returns the dict data as an instance of the BlackboxDebugTest dataclass.
-    #:param xml_file: An xml file that provides the data required to compare test results vs expected values.
-    #:return: Dictionary.
-    
-    debug_file = "C:/new/onemeshonematerial.dbgsg"
-    with open(debug_file, "r") as file:
-        data = file.read()
-
-    split_data = data.strip().splitlines()
-
-    split_list = []
-    for key, value in itertools.groupby(split_data, key=lambda line: line == ""):
-        if not key:
-            split_list.append(list(value))
-
-    lines = build_dict(split_list)
-
-    test_file = "C:/new/onemeshonematerial.json"
-    with open(test_file, "w") as test:
-        json.dump(lines, test, indent=4)
-"""
