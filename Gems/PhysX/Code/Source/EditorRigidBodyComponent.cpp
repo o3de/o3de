304d6e494c1c04cf9fa2784448e7d14d1ccfd895
amzn-ahmadkrm <ahmadkrm@amazon.com>
2022-12-12 13:28:49 +0000

    explicitly qualified member function in attributes
    Signed-off-by: amzn-ahmadkrm <ahmadkrm@amazon.com>

--- Gems/PhysX/Code/Source/EditorRigidBodyComponent.cpp
+++ Gems/PhysX/Code/Source/EditorRigidBodyComponent.cpp
@@ -197,7 +197,7 @@ namespace PhysX
                         "CCD enabled", "When active, the rigid body has continuous collision detection (CCD). Use this to ensure accurate "
                         "collision detection, particularly for fast moving rigid bodies. CCD must be activated in the global PhysX configuration.")
                         ->Attribute(AZ::Edit::Attributes::Visibility, &AzPhysics::RigidBodyConfiguration::GetCcdVisibility)
-                    ->Attribute(AZ::Edit::Attributes::ReadOnly, &IsSceneCcdDisabled)
+                    ->Attribute(AZ::Edit::Attributes::ReadOnly, &EditorRigidBodyConfiguration::IsSceneCcdDisabled)
                         ->Attribute(AZ::Edit::Attributes::ChangeNotify, AZ::Edit::PropertyRefreshLevels::EntireTree)
                     ->DataElement(AZ::Edit::UIHandlers::Default, &AzPhysics::RigidBodyConfiguration::m_ccdMinAdvanceCoefficient,
                         "Min advance coefficient", "Lower values reduce clipping but can affect simulation smoothness.")
@@ -205,18 +205,18 @@ namespace PhysX
                         ->Attribute(AZ::Edit::Attributes::Step, 0.01f)
                         ->Attribute(AZ::Edit::Attributes::Max, 0.99f)
                         ->Attribute(AZ::Edit::Attributes::Visibility, &AzPhysics::RigidBodyConfiguration::IsCcdEnabled)
-                        ->Attribute(AZ::Edit::Attributes::ReadOnly, &IsSceneCcdDisabled)
+                    ->Attribute(AZ::Edit::Attributes::ReadOnly, &EditorRigidBodyConfiguration::IsSceneCcdDisabled)
                     ->DataElement(AZ::Edit::UIHandlers::Default, &AzPhysics::RigidBodyConfiguration::m_ccdFrictionEnabled,
                         "CCD friction", "When active, friction is applied when continuous collision detection (CCD) collisions are resolved.")
                         ->Attribute(AZ::Edit::Attributes::Visibility, &AzPhysics::RigidBodyConfiguration::IsCcdEnabled)
-                        ->Attribute(AZ::Edit::Attributes::ReadOnly, &IsSceneCcdDisabled)
+                    ->Attribute(AZ::Edit::Attributes::ReadOnly, &EditorRigidBodyConfiguration::IsSceneCcdDisabled)
                     ->DataElement(
                         AZ::Edit::UIHandlers::Button,
                         &AzPhysics::RigidBodyConfiguration::m_configButton,
                         "",
                         "Click here to open the PhysX Configuration window. Enable global CCD to enable component CCD editing.")
                         ->Attribute(AZ::Edit::Attributes::ButtonText, "Open PhysX Configuration to Enable CCD")
-                        ->Attribute(AZ::Edit::Attributes::Visibility, &IsSceneCcdDisabled)
+                    ->Attribute(AZ::Edit::Attributes::Visibility, &EditorRigidBodyConfiguration::IsSceneCcdDisabled)
                         ->Attribute(AZ::Edit::Attributes::ChangeNotify, &EditorRigidBodyComponent::OpenPhysXConfigurationPane)
                     ->EndGroup()
 
