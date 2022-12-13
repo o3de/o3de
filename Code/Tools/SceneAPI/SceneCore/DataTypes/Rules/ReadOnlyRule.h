84d6b8f3d0696099d3d16505c1b146b6f8fd8f88
AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
2022-12-13 12:05:21 -0800

    Adding missing header file
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>

---
+++ Code/Tools/SceneAPI/SceneCore/DataTypes/Rules/ReadOnlyRule.h
@@ -0,0 +1,30 @@
+#pragma once
+
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+
+#include <AzCore/RTTI/RTTI.h>
+#include <SceneAPI/SceneCore/DataTypes/Rules/IRule.h>
+
+namespace AZ
+{
+    namespace SceneAPI
+    {
+        namespace DataTypes
+        {
+            // Disables UI interaction for the node and children of the node with this rule.
+            class ReadOnlyRule : public IRule
+            {
+            public:
+                AZ_RTTI(ReadOnlyRule, "{071A7F4A-70A5-4D32-AC9C-1D49AFCB1480}", IRule);
+
+                virtual ~ReadOnlyRule() override = default;
+            };
+        } // namespace DataTypes
+    } // namespace SceneAPI
+} // namespace AZ
