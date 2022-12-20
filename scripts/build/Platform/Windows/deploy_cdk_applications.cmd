95df8ce26abaa6a09e31fc4bce9462a0f900ff94
allisaurus <34254888+allisaurus@users.noreply.github.com>
2022-12-20 15:34:44 -0800

    Add comment about pipeline char cap to CDK deploy scripts
    Signed-off-by: allisaurus <34254888+allisaurus@users.noreply.github.com>

--- scripts/build/Platform/Windows/deploy_cdk_applications.cmd
+++ scripts/build/Platform/Windows/deploy_cdk_applications.cmd
@@ -38,6 +38,8 @@ FOR /f "tokens=1,2,3" %%a IN ('CALL aws sts assume-role --query Credentials.[Sec
 FOR /F "tokens=4 delims=:" %%a IN ("%ASSUME_ROLE_ARN%") DO SET O3DE_AWS_DEPLOY_ACCOUNT=%%a
 
 IF "%O3DE_AWS_PROJECT_NAME%"=="" (
+    REM To avoid resource name length issues, potentially verbose pipeline names are capped at 25 chars.
+    REM TODO: consolidate project name formulation for tests and deploy scripts to same place.
     ECHO Truncated pipeline name is: %PIPELINE_NAME:~0,25%
     SET O3DE_AWS_PROJECT_NAME=%BRANCH_NAME%-%PIPELINE_NAME:~0,25%-Windows
     SET slashreplace=
