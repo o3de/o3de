1c18d707dbb638899fdc9a1764f06e1471fcd7eb
Chiang <evanchia@amazon.com>
2023-01-13 11:29:49 -0800

    Fixed copyright headers
    Signed-off-by: Chiang <evanchia@amazon.com>

---
+++ scripts/metrics/common/__init__.py
@@ -0,0 +1,8 @@
+"""
+Copyright (c) Contributors to the Open 3D Engine Project.
+For complete copyright and license terms please see the LICENSE at the root of this distribution.
+
+SPDX-License-Identifier: Apache-2.0 OR MIT
+
+Helper file for assisting in building workspaces and setting up LTT with the current Lumberyard environment.
+"""
