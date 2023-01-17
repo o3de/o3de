42ae8055b23dc5e6a25caf7bcd565c50640e9cfe
Alex Peterson <26804013+AMZN-alexpete@users.noreply.github.com>
2023-01-17 15:24:59 -0800

    Fix invalid version error with empty list
    Signed-off-by: Alex Peterson <26804013+AMZN-alexpete@users.noreply.github.com>

--- scripts/o3de/o3de/utils.py
+++ scripts/o3de/o3de/utils.py
@@ -122,8 +122,8 @@ def validate_version_specifier(version_specifier:str) -> bool:
 
 def validate_version_specifier_list(version_specifiers:str or list) -> bool:
     version_specifier_list = version_specifiers.split() if isinstance(version_specifiers, str) else version_specifiers
-    if not version_specifier_list:
-        logger.error(f'Version specifiers must be in the format <name><version specifiers>. e.g. name==1.2.3')
+    if not isinstance(version_specifier_list, list):
+        logger.error(f'Version specifiers must be in the format <name><version specifiers>. e.g. name==1.2.3 \n {version_specifiers}')
         return False
 
     for version_specifier in version_specifier_list:
