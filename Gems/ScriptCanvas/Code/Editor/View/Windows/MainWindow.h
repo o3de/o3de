3f56eb5da787175cd1e27228c8137f10fdb38627
sphrose <82213493+sphrose@users.noreply.github.com>
2023-01-23 10:26:52 +0000

    Add allocator macro to ScriptCanvasAssetBrowserModel
    Signed-off-by: sphrose <82213493+sphrose@users.noreply.github.com>

--- Gems/ScriptCanvas/Code/Editor/View/Windows/MainWindow.h
+++ Gems/ScriptCanvas/Code/Editor/View/Windows/MainWindow.h
@@ -96,6 +96,7 @@ namespace ScriptCanvasEditor
         , private UpgradeNotificationsBus::Handler
     {
     public:
+        AZ_CLASS_ALLOCATOR(ScriptCanvasAssetBrowserModel, AZ::SystemAllocator, 0);
 
         explicit ScriptCanvasAssetBrowserModel(QObject* parent = nullptr)
             : AzToolsFramework::AssetBrowser::AssetBrowserFilterModel(parent)
