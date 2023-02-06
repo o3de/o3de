1f176cd13887d70b9f1318fff4876b9f13af129b
moraaar <moraaar@amazon.com>
2023-02-06 10:25:19 +0000

    Revert "Merging changes to port remaining material types to the new material pipeline"
    Signed-off-by: moraaar <moraaar@amazon.com>

--- Gems/Atom/Feature/Common/Code/Source/Material/MaterialConverterSystemComponent.cpp
+++ Gems/Atom/Feature/Common/Code/Source/Material/MaterialConverterSystemComponent.cpp
@@ -16,7 +16,6 @@
 #include <AzToolsFramework/API/EditorAssetSystemAPI.h>
 
 #include <Atom/RPI.Reflect/Material/MaterialAsset.h>
-#include <Atom/RPI.Edit/Material/MaterialUtils.h>
 
 namespace AZ
 {
@@ -75,7 +74,7 @@ namespace AZ
         
         AZStd::string MaterialConverterSystemComponent::GetFingerprintInfo() const
         {
-            static constexpr int Version = 2; // Bump this version whenever changes are made to the material conversion code to force the AP to reprocess scene files
+            static constexpr int Version = 1; // Bump this version whenever changes are made to the material conversion code to force the AP to reprocess scene files
 
             AZStd::string fingerprintInfo = AZStd::string::format("[MaterialConverter version=%d enabled=%d", Version, IsEnabled());
              
@@ -203,8 +202,7 @@ namespace AZ
 
         AZStd::string MaterialConverterSystemComponent::GetMaterialTypePath() const
         {
-            AZStd::string intermediateMaterialTypePath = AZ::RPI::MaterialUtils::PredictIntermediateMaterialTypeSourcePath("Materials/Types/StandardPBR.materialtype");
-            return intermediateMaterialTypePath;
+            return "Materials/Types/StandardPBR.materialtype";
         }
 
         AZStd::string MaterialConverterSystemComponent::GetDefaultMaterialPath() const
