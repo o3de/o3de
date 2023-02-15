e0beeb53b3eefc2402b87725399a6ee565056585
Chiang <evanchia@amazon.com>
2023-02-15 14:39:57 -0800

    improved performance on codeowners retrieval data
    Signed-off-by: Chiang <evanchia@amazon.com>

--- scripts/metrics/pytest_metrics_xml_to_csv.py
+++ scripts/metrics/pytest_metrics_xml_to_csv.py
@@ -37,6 +37,7 @@ PYTEST_FIELDS_HEADER = [
     'duration_seconds',
     'sig_owner'
 ]
+SIG_OWNER_CACHE = {}
 
 
 def main():
@@ -146,8 +147,14 @@ def parse_pytest_xmls_to_csv(full_xml_path, writer):
             test_data_dict['duration_seconds'] = float(test.attrib['time'])
             # using 'status' to keep it consistent with CTest xml schema
             test_data_dict['status'] = _determine_test_result(test)
-            # Index 1 is the sig owner, replace slashes to match codeowners file
-            sig_owner = codeowners_hint.get_codeowners(test.attrib['file'].replace("\\", "/"))[1]
+            # replace slashes to match codeowners file
+            test_file_path = test.attrib['file'].replace("\\", "/")
+            try:
+                sig_owner = SIG_OWNER_CACHE[test_file_path]
+            except KeyError:
+                # Index 1 is the sig owner
+                sig_owner = codeowners_hint.get_codeowners(test_file_path)[1]
+                SIG_OWNER_CACHE[test_file_path] = sig_owner
             test_data_dict['sig_owner'] = sig_owner if sig_owner else "N/A"
 
             writer.writerow(test_data_dict)
