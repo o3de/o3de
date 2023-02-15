a930f275e657b2677ec43dda4a51a9e7156d8682
Alex Peterson <26804013+AMZN-alexpete@users.noreply.github.com>
2023-02-15 09:46:17 -0800

    Findo3de.cmake checks PACKAGE_VERSION_COMPATIBLE
    New projects should check version compatibility, old projects just use engine name.  When a simple project upgrade path is available, a message will be added telling users how to upgrade their project.
    
    Signed-off-by: Alex Peterson <26804013+AMZN-alexpete@users.noreply.github.com>

--- cmake/Findo3de.cmake
+++ cmake/Findo3de.cmake
@@ -11,6 +11,8 @@
 
 include(FindPackageHandleStandardArgs)
 
+
+# Use CMAKE_CURRENT_FUNCTION_LIST_DIR in case an older projects used add_directory()
 function(o3de_current_file_path path)
     set(${path} ${CMAKE_CURRENT_FUNCTION_LIST_DIR} PARENT_SCOPE)
 endfunction()
@@ -21,53 +23,35 @@ o3de_current_file_path(current_path)
 # because later it's read again using a path like ${LY_ROOT_FOLDER}/engine.json, which
 # is also normalized.  They should match to avoid errors on some build systems.
 cmake_path(SET engine_json_path NORMALIZE ${current_path}/../engine.json)
-file(READ ${engine_json_path} engine_json)
 set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${engine_json_path})
 
-string(JSON this_engine_name ERROR_VARIABLE json_error GET ${engine_json} engine_name)
-if(json_error)
-    message(FATAL_ERROR "Unable to read key 'engine_name' from '${engine_json_path}', error: ${json_error}")
-endif()
-
-
-# Make sure we are matching O3DE_ENGINE_NAME_TO_USE with the current engine
-set(found_matching_engine FALSE)
-
-if(NOT O3DE_ENGINE_NAME_TO_USE)
-    # Look for a local engine override in <project>/user/project.json
-    cmake_path(SET O3DE_USER_PROJECT_JSON_PATH ${CMAKE_CURRENT_SOURCE_DIR}/user/project.json)
-    if(EXISTS "${O3DE_USER_PROJECT_JSON_PATH}")
-        file(READ "${O3DE_USER_PROJECT_JSON_PATH}" user_project_json)
-        string(JSON O3DE_ENGINE_NAME_TO_USE ERROR_VARIABLE json_error GET ${user_project_json} engine)
-    endif()
-
-    # Look in project.json if no local override was found
-    if(NOT O3DE_ENGINE_NAME_TO_USE)
-        cmake_path(SET O3DE_PROJECT_JSON_PATH ${CMAKE_CURRENT_SOURCE_DIR}/project.json)
-        file(READ "${O3DE_PROJECT_JSON_PATH}" project_json)
-        string(JSON O3DE_ENGINE_NAME_TO_USE ERROR_VARIABLE json_error GET ${project_json} engine)
-        if(json_error)
-            message(FATAL_ERROR "Unable to read key 'engine' from '${O3DE_PROJECT_JSON_PATH}'\nPlease register the project with an engine.\nError: ${json_error}")
-        endif()
-    endif()
-endif()
-
-# Support older CMake prefixes
-if(LY_ENGINE_NAME_TO_USE AND NOT O3DE_ENGINE_NAME_TO_USE)
-    set(O3DE_ENGINE_NAME_TO_USE ${LY_ENGINE_NAME_TO_USE})
-endif()
-
-if(this_engine_name STREQUAL O3DE_ENGINE_NAME_TO_USE)
-    set(found_matching_engine TRUE)
+if(NOT LY_ENGINE_NAME_TO_USE)
+    # PACKAGE_VERSION_COMPATIBLE should be set TRUE by o3deConfigVersion.cmake
+    find_package_handle_standard_args(o3de
+        "This engine was not found to be compatible with your project, or no compatibility checks were done. For more information, run the command again with '--log-level VERBOSE'."
+        PACKAGE_VERSION_COMPATIBLE
+    )
 else()
-    message(VERBOSE "Project engine name '${O3DE_ENGINE_NAME_TO_USE}' does not match this engine name '${this_engine_name}' in '${engine_json_path}'")
+    # LY_ENGINE_NAME_TO_USE compatibility check for older projects 
+    set(found_matching_engine FALSE)
+
+    file(READ ${engine_json_path} engine_json)
+    string(JSON this_engine_name ERROR_VARIABLE json_error GET ${engine_json} engine_name)
+    if(json_error)
+        message(FATAL_ERROR "Unable to read key 'engine_name' from '${engine_json_path}', error: ${json_error}")
+    endif()
+
+    if(this_engine_name STREQUAL LY_ENGINE_NAME_TO_USE)
+        set(found_matching_engine TRUE)
+    else()
+        message(VERBOSE "Project engine name '${LY_ENGINE_NAME_TO_USE}' does not match this engine name '${this_engine_name}' in '${engine_json_path}'")
+    endif()
+
+    find_package_handle_standard_args(o3de
+        "The engine name for this engine '${this_engine_name}' does not match the projects engine name '${O3DE_ENGINE_NAME_TO_USE}'."
+        found_matching_engine
+    )
 endif()
-
-
-find_package_handle_standard_args(o3de
-    "The engine name for this engine '${this_engine_name}' does not match the projects engine name '${O3DE_ENGINE_NAME_TO_USE}'."
-    found_matching_engine
-)
 
 cmake_path(SET engine_root_folder NORMALIZE ${current_path}/..)
 set_property(GLOBAL PROPERTY O3DE_ENGINE_ROOT_FOLDER "${engine_root_folder}")
