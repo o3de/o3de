d5266a61bc170ef9da95fe4ff8b554df89551d3c
lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
2023-02-15 01:46:39 -0600

    Updated NodeFunctionGenericMultiReturn to remove the explicit function non-type template paramters.
    Added a custom `GetAzTypeInfo` free function for the
    NodeFunctionGenericMultiReturn template to properly aggregate the Type
    Uuid and Type Name of the template parameters for the class tempalte
    
    With the explicit non-type tempalte parameter removed,
    This allows the `AZ_RTTI_NO_TYPE_INFO_IMPL` to be used with the `auto`
    template placeholder, so that it can bind to an existing
    `GetTemplateIdentity` function in TemplateInfo.h
    
    Signed-off-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>

--- Gems/ScriptCanvas/Code/Include/ScriptCanvas/Core/NodeFunctionGeneric.h
+++ Gems/ScriptCanvas/Code/Include/ScriptCanvas/Core/NodeFunctionGeneric.h
@@ -54,15 +54,54 @@
 
 namespace ScriptCanvas
 {
+    template<auto Function, typename t_Traits>
+    class NodeFunctionGenericMultiReturnImpl;
+
     template<typename t_Func, typename t_Traits, t_Func function>
-    class NodeFunctionGenericMultiReturn
+    using NodeFunctionGenericMultiReturn = NodeFunctionGenericMultiReturnImpl<function, t_Traits>;
+
+    template<auto Function, typename t_Traits>
+    inline constexpr AZ::TypeInfoObject GetAzTypeInfo(AZ::Adl, AZStd::type_identity<NodeFunctionGenericMultiReturnImpl<Function, t_Traits>>)
+    {
+        using t_Func = decltype(Function);
+        constexpr AZ::TypeId templateUuid{ "{DC5B1799-6C5B-4190-8D90-EF0C2D1BCE4E}" };
+        constexpr const char* displayName = "NodeFunctionGenericMultiReturn";
+        AZStd::fixed_string<512> typeName{ displayName };
+        typeName += '<';
+
+        bool prependSeparator = false;
+        for (AZStd::string_view templateParamName : { AZ::Internal::AggregateTypes<t_Func, t_Traits>::TypeName() })
+        {
+            typeName += prependSeparator ? AZ::Internal::TypeNameSeparator : "";
+            typeName += templateParamName;
+            prependSeparator = true;
+        }
+        typeName += '>';
+
+        AZ::TypeTraits typeTraits{};
+        typeTraits |= AZ::TypeTraits::is_template;
+        AZ::TypeInfoObject typeInfoObject;
+        typeInfoObject.m_name = typeName;
+        typeInfoObject.m_templateId = templateUuid;
+        typeInfoObject.m_canonicalTypeId = templateUuid + AZ::Internal::AggregateTypes<t_Func, t_Traits>::GetCanonicalTypeId();
+
+        typeInfoObject.m_pointerTypeId = typeInfoObject.m_canonicalTypeId + AZ::Internal::PointerId_v;
+        typeInfoObject.m_typeTraits = typeTraits;
+        return typeInfoObject;
+    }
+
+
+    template<auto Function, typename t_Traits>
+    class NodeFunctionGenericMultiReturnImpl
         : public Node
     {
     public:
+        using t_Func = decltype(Function);
+
         AZ_PUSH_DISABLE_WARNING(5046, "-Wunknown-warning-option"); // 'function' : Symbol involving type with internal linkage not defined
-        AZ_RTTI((NodeFunctionGenericMultiReturn, "{DC5B1799-6C5B-4190-8D90-EF0C2D1BCE4E}", t_Func, t_Traits), Node);
-        AZ_COMPONENT_INTRUSIVE_DESCRIPTOR_TYPE(NodeFunctionGenericMultiReturn);
-        AZ_COMPONENT_BASE(NodeFunctionGenericMultiReturn, Node);
+        AZ_RTTI_NO_TYPE_INFO_DECL();
+        AZ_COMPONENT_INTRUSIVE_DESCRIPTOR_TYPE(NodeFunctionGenericMultiReturnImpl);
+        AZ_COMPONENT_BASE(NodeFunctionGenericMultiReturnImpl, Node);
         AZ_POP_DISABLE_WARNING;
 
         static constexpr void DeprecatedTypeNameVisitor(
@@ -110,22 +149,22 @@ namespace ScriptCanvas
 
         static t_Func GetFunction()
         {
-            return function;
+            return Function;
         }
 
         static void Reflect(AZ::ReflectContext* reflectContext)
         {
             if (AZ::SerializeContext* serializeContext = azrtti_cast<AZ::SerializeContext*>(reflectContext))
             {
-                serializeContext->Class<NodeFunctionGenericMultiReturn, Node>()
+                serializeContext->Class<NodeFunctionGenericMultiReturnImpl, Node>()
                     ->Version(1)
                     ->Attribute(AZ::Script::Attributes::Deprecated, true)
-                    ->Field("Initialized", &NodeFunctionGenericMultiReturn::m_initialized)
+                    ->Field("Initialized", &NodeFunctionGenericMultiReturnImpl::m_initialized)
                     ;
 
                 if (AZ::EditContext* editContext = serializeContext->GetEditContext())
                 {
-                    editContext->Class<NodeFunctionGenericMultiReturn>(t_Traits::GetNodeName(), "Deprecated Generic Function Node")
+                    editContext->Class<NodeFunctionGenericMultiReturnImpl>(t_Traits::GetNodeName(), "Deprecated Generic Function Node")
                         ->ClassElement(AZ::Edit::ClassElements::EditorData, "Deprecated Generic Function Node")
                         ->Attribute(AZ::Script::Attributes::Deprecated, true)
                         ->Attribute(ScriptCanvas::Attributes::Node::TitlePaletteOverride, "DeprecatedNodeTitlePalette")
@@ -172,6 +211,8 @@ namespace ScriptCanvas
     private:
         bool m_initialized = false;
     };
+
+    AZ_RTTI_NO_TYPE_INFO_IMPL_INLINE((NodeFunctionGenericMultiReturnImpl, AZ_TYPE_INFO_AUTO, AZ_TYPE_INFO_CLASS), Node);
 
 #define SCRIPT_CANVAS_GENERICS_TO_VM(GenericClass, ReflectClass, BehaviorContext, CategoryName)\
     GenericClass::Reflect<ReflectClass>(BehaviorContext, ScriptCanvas::Translation::Context::GetCategoryLibraryName(CategoryName).c_str());
