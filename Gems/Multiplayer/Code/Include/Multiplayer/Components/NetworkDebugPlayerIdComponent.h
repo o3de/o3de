b5fc483abb93d57c9661bd43b70ad3ae22040f98
Gene Walters <genewalt@amazon.com>
2023-02-21 22:55:01 -0800

    Minor: code comments based on PR feedback
    Signed-off-by: Gene Walters <genewalt@amazon.com>

--- Gems/Multiplayer/Code/Include/Multiplayer/Components/NetworkDebugPlayerIdComponent.h
+++ Gems/Multiplayer/Code/Include/Multiplayer/Components/NetworkDebugPlayerIdComponent.h
@@ -19,6 +19,11 @@ namespace AZ::RPI
 
 namespace Multiplayer
 {
+    /*!
+     * \class NetworkDebugPlayerIdComponent
+     * \brief A component for network players to render their player id in world-space.
+     * Both autonomous players and client proxies will have their player ids rendered on screen.
+     */
     class NetworkDebugPlayerIdComponent
     : public NetworkDebugPlayerIdComponentBase
     , private AZ::TickBus::Handler
@@ -42,12 +47,18 @@ namespace Multiplayer
         void OnTick(float deltaTime, AZ::ScriptTimePoint time) override;
 
     private:
-        // Properties exposed to editor
+        // Properties exposed to editor...
+        //! \brief The player id will render debug text at the player's world-space location.
+        //   This offset is exposed to the editor. It's useful if the player's origin is at the feet, but you wish to render id on the head.
         AZ::Vector3 m_translationOffset = AZ::Vector3(0, 0, 0);
+
+        //! \brief Number exposed to Editor to scale the player id debug text.*/
         float m_fontScale = 0.7f;
+
+        //! \brief Color exposed to Editor to change the player id debug text color.*/
         AZ::Color m_fontColor = AZ::Colors::Wheat;
 
-        // Cache properties required for font rendering
+        // Cache properties required for font rendering...
         AZ::RPI::ViewportContextPtr m_viewport;
         AzFramework::FontDrawInterface* m_fontDrawInterface = nullptr;
         AzFramework::TextDrawParameters m_drawParams;
