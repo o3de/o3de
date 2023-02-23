9d994f09875dddc658ca9c673742f4ac6c4c4b4d
Gene Walters <genewalt@amazon.com>
2023-02-23 15:46:00 -0800

    Minor: more simple spawner api code comment updates
    Signed-off-by: Gene Walters <genewalt@amazon.com>

--- Gems/Multiplayer/Code/Include/Multiplayer/Components/SimplePlayerSpawnerComponentBus.h
+++ Gems/Multiplayer/Code/Include/Multiplayer/Components/SimplePlayerSpawnerComponentBus.h
@@ -23,19 +23,20 @@ namespace Multiplayer
         static const AZ::EBusHandlerPolicy HandlerPolicy = AZ::EBusHandlerPolicy::Single;
         static const AZ::EBusAddressPolicy AddressPolicy = AZ::EBusAddressPolicy::Single;
         
-        //! Returns and increments the next player spawn point.
+        //! Returns the location where the next joining player will be spawned, and then advances the spawn point index.
+        //! The index will wrap back to 0 in a round-robin fashion so that a valid spawn point is always available.
         //! Method is only valid if called from the multiplayer host/authority; clients are not given information regarding the spawn point index.
         //! @return AZ::Transform The location of the next spawn point
         virtual AZ::Transform RoundRobinNextSpawnPoint() = 0;
 
-        //! Returns the next player spawn point. Unlike RoundRobinNextSpawnPoint(), this will not cause the current spawn point index to increment.
+        //! Returns the location where the next joining player will be spawned. Unlike RoundRobinNextSpawnPoint(), this will not cause the current spawn point index to increment.
         //! Method is only valid if called from the multiplayer host/authority; clients are not given information regarding the spawn point index.
         //! @return AZ::Transform The location of the next spawn point
         virtual AZ::Transform GetNextSpawnPoint() const = 0;
 
 
         //! Returns a mutable list of all the spawn points.
-        //! Only use this list on multiplayer hosts; clients aren't in charge of spawn players so this list might not actually represent where the players spawn.
+        //! Only access/edit this list on the multiplayer host; spawn points are not synced across the network and the host is responsible for spawning players.
         //! @return AZStd::vector<AZ::EntityId> List of spawn points.
         virtual AZStd::vector<AZ::EntityId>& GetSpawnPoints() = 0;
 
@@ -43,12 +44,12 @@ namespace Multiplayer
         //! @return Number of spawn points.
         virtual uint32_t GetSpawnPointCount() const = 0;
 
-        //! Returns the index of the player spawn point.
+        //! Returns the spawn point index where the next joining player will be spawned.
         //! @return AZ::Outcome<uint32_t, AZStd::string> If success, the value present a valid index that can be used to lookup into the spawn points array.
         virtual AZ::Outcome<uint32_t, AZStd::string> GetNextSpawnPointIndex() const = 0;
 
-        //! Overwrites the next player's spawn index. The spawn index provided must be a valid in-bounds index into the array of available spawn points.
-        //! @return AZ::Outcome<void, AZStd::string> Success means a valid index was provided; otherwise the index is out-of-bounds.
+        //! Overwrites the next joining player's spawn index. The spawn index provided must be a valid (in-bounds) index into the array of available spawn points.
+        //! @return AZ::Outcome<void, AZStd::string> Success means a valid index was provided and the next spawn point was updated; failure signifies a bad (out-of-bounds) index.
         virtual AZ::Outcome<void, AZStd::string> SetNextSpawnPointIndex(uint32_t index) = 0;
     };
     using SimplePlayerSpawnerRequestBus = AZ::EBus<SimplePlayerSpawnerRequests>;
