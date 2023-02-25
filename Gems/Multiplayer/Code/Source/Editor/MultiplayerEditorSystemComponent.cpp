77ab3a697383d92eade561ce4a9bf0d012f22427
Gene Walters <genewalt@amazon.com>
2023-02-24 17:45:25 -0800

    Removing helper code comment
    Signed-off-by: Gene Walters <genewalt@amazon.com>

--- Gems/Multiplayer/Code/Source/Editor/MultiplayerEditorSystemComponent.cpp
+++ Gems/Multiplayer/Code/Source/Editor/MultiplayerEditorSystemComponent.cpp
@@ -288,29 +288,6 @@ namespace Multiplayer
         return false;
     }
 
-    /*!
-    \brief Check if a process is running
-    \param [in] processName Name of process to check if is running
-    \returns \c True if the process is running, or \c False if the process is not running
-    
-    bool IsProcessRunning(const wchar_t* processName)
-    {
-        bool exists = false;
-        PROCESSENTRY32 entry;
-        entry.dwSize = sizeof(PROCESSENTRY32);
-
-        HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
-
-        if (Process32First(snapshot, &entry))
-            while (Process32Next(snapshot, &entry))
-                if (!wcsicmp(entry.szExeFile, processName))
-                    exists = true;
-
-        CloseHandle(snapshot);
-        return exists;
-    }
-    */
-
     bool MultiplayerEditorSystemComponent::LaunchEditorServer()
     {
         // Assemble the server's path
