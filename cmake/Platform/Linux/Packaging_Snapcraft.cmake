6bf870d5af585501055a9451dc44cbc9b315918f
AMZN-Phil <pconroy@amazon.com>
2023-02-28 02:04:31 -0800

    Fix running script during packaging
    Signed-off-by: AMZN-Phil <pconroy@amazon.com>

--- cmake/Platform/Linux/Packaging_Snapcraft.cmake
+++ cmake/Platform/Linux/Packaging_Snapcraft.cmake
@@ -11,9 +11,7 @@ configure_file("${LY_ROOT_FOLDER}/cmake/Platform/Linux/Packaging/snapcraft.yaml.
     "${CPACK_TEMPORARY_DIRECTORY}/snapcraft.yaml"
 )
 
-execute_process (COMMAND get_python.sh
-                 WORKING_DIRECTORY ${CPACK_TEMPORARY_DIRECTORY}/O3DE/${CPACK_PACKAGE_VERSION}/python
-)
+execute_process (COMMAND /bin/sh ${CPACK_TEMPORARY_DIRECTORY}/O3DE/${CPACK_PACKAGE_VERSION}/python/get_python.sh)
 
 # make sure that all executables have the correct permissions
 execute_process (COMMAND find ./O3DE/${CPACK_PACKAGE_VERSION}/bin/Linux -type f -executable -exec chmod +x {} \;
