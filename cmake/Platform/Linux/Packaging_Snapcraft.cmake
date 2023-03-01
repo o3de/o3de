e7e0771ba3e23803cef06901a35741d1700f283c
AMZN-Phil <pconroy@amazon.com>
2023-02-28 20:24:37 -0800

    Make sure downloaded python has correct rpaths
    Signed-off-by: AMZN-Phil <pconroy@amazon.com>

--- cmake/Platform/Linux/Packaging_Snapcraft.cmake
+++ cmake/Platform/Linux/Packaging_Snapcraft.cmake
@@ -11,13 +11,15 @@ configure_file("${LY_ROOT_FOLDER}/cmake/Platform/Linux/Packaging/snapcraft.yaml.
     "${CPACK_TEMPORARY_DIRECTORY}/snapcraft.yaml"
 )
 
+execute_process (COMMAND bash ${CPACK_TEMPORARY_DIRECTORY}/O3DE/${CPACK_PACKAGE_VERSION}/python/get_python.sh)
+
 # make sure that all executables have the correct permissions
 execute_process (COMMAND find ./O3DE/${CPACK_PACKAGE_VERSION}/bin/Linux -type f -executable -exec chmod +x {} \;
                  WORKING_DIRECTORY ${CPACK_TEMPORARY_DIRECTORY}
 )
 
 # Patch binaries to setup rpath and interpreter. Once snapcraft 7.3 is in stable, it should be possible to remove 
-# these 2 processes since automatic elf patching will be available for core22
+# these 4 processes since automatic elf patching will be available for core22
 
 # setup the rpath
 execute_process (COMMAND find ./O3DE/${CPACK_PACKAGE_VERSION}/bin/Linux -type f -executable -exec patchelf --force-rpath --set-rpath \$ORIGIN:/snap/core22/current/lib/x86_64-linux-gnu:/snap/core22/current/usr/lib/x86_64-linux-gnu {} \;
@@ -26,6 +28,15 @@ execute_process (COMMAND find ./O3DE/${CPACK_PACKAGE_VERSION}/bin/Linux -type f 
 
 # setup the correct elf interpreter
 execute_process (COMMAND find ./O3DE/${CPACK_PACKAGE_VERSION}/bin/Linux -type f -executable -exec patchelf --set-interpreter /snap/core22/current/lib64/ld-linux-x86-64.so.2 {} \;
+                 WORKING_DIRECTORY ${CPACK_TEMPORARY_DIRECTORY}
+)
+
+execute_process (COMMAND find ./O3DE/${CPACK_PACKAGE_VERSION}/python/runtime -type f -executable -exec patchelf --force-rpath --set-rpath \$ORIGIN:/snap/core22/current/lib/x86_64-linux-gnu:/snap/core22/current/usr/lib/x86_64-linux-gnu {} \;
+                 WORKING_DIRECTORY ${CPACK_TEMPORARY_DIRECTORY}
+)
+
+# setup the correct elf interpreter
+execute_process (COMMAND find ./O3DE/${CPACK_PACKAGE_VERSION}/python/runtime -type f -executable -exec patchelf --set-interpreter /snap/core22/current/lib64/ld-linux-x86-64.so.2 {} \;
                  WORKING_DIRECTORY ${CPACK_TEMPORARY_DIRECTORY}
 )
 
