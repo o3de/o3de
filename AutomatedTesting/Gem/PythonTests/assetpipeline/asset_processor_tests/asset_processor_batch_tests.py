d87460b61998cb1422a5e8986ec5e0bdde5596f2
Rosario Cox <lesaelr@amazon.com>
2023-02-28 20:36:43 -0600

    Updated tests based on feedback.  - Removed unused import text  - Removed unused debug output text from fbx assets in test  - Updated test pass conditions to limit flakiness
    Signed-off-by: Rosario Cox <lesaelr@amazon.com>

--- AutomatedTesting/Gem/PythonTests/assetpipeline/asset_processor_tests/asset_processor_batch_tests.py
+++ AutomatedTesting/Gem/PythonTests/assetpipeline/asset_processor_tests/asset_processor_batch_tests.py
@@ -17,14 +17,12 @@ from pprint import pformat
 import stat
 import shutil
 
-# Import LyTestTools
 from ly_test_tools.o3de import asset_processor as asset_processor_utils
+import ly_test_tools.environment.waiter as waiter
 
 # Import fixtures
 from ..ap_fixtures.asset_processor_fixture import asset_processor as asset_processor
 from ..ap_fixtures.ap_setup_fixture import ap_setup_fixture as ap_setup_fixture
-
-# Import LyShared
 
 from ly_test_tools.o3de.ap_log_parser import APLogParser, APOutputParser
 import ly_test_tools.o3de.pipeline_utils as utils
@@ -328,42 +326,41 @@ class TestsAssetProcessorBatch_AllPlatforms(object):
         """
 
         def run_ap_debug_skip_atom_output(asset_processor):
-            result, output = asset_processor.batch_process(capture_output=True, extra_params=["--debugOutput",
-                                                                                              "--regset=\"/O3DE/SceneAPI/AssetImporter/SkipAtomOutput=true\""])
+            result, output = asset_processor.batch_process(capture_output=True, extra_params=["--regset=\"/O3DE/SceneAPI/AssetImporter/SkipAtomOutput=true\""])
             # If the test fails, it's helpful to have the output from asset processor in the logs, to track the failure down.
             logger.info(f"Asset Processor Output: {pformat(output)}")
             return result, output
 
         # Copying test assets to project folder
         asset_processor.prepare_test_environment(ap_setup_fixture["tests_dir"], "test_TwoAssetsWithSameProductName_ShouldProcessAfterRename")
+
         # Launching AP, verify it is failing
         result, output = run_ap_debug_skip_atom_output(asset_processor)
         assert result == False, \
             'AssetProcessorBatch should have failed because the generated output products should share the same name.'
 
         # Renaming output files so they won't collide in cache after second processing
-        file_to_rename = os.path.join(asset_processor.temp_asset_root(), "AutomatedTesting", "test_TwoAssetsWithSameProductName_ShouldProcessAfterRename", "a.fbx.assetinfo")
+        asset_info_file = os.path.join(asset_processor.temp_asset_root(), "AutomatedTesting", "test_TwoAssetsWithSameProductName_ShouldProcessAfterRename", "a.fbx.assetinfo")
         data_for_rename = os.path.join(asset_processor.temp_asset_root(), "AutomatedTesting", "test_TwoAssetsWithSameProductName_ShouldProcessAfterRename", "rename.txt")
 
-        assert os.path.exists(file_to_rename), "An assetinfo file is missing."
-        assert os.path.exists(data_for_rename), "Text file to copy into the assetinfo file is missing."
+        os.remove(asset_info_file)
+        waiter.wait_for(lambda: not os.path.exists(asset_info_file), timeout=5)
+        assert not os.path.exists(asset_info_file), "The assetinfo file was not successfully removed missing."
 
-        shutil.copyfile(data_for_rename, file_to_rename)
-
-        # Clear the cache folder
-        asset_processor.delete_temp_cache()
+        os.rename(data_for_rename, asset_info_file)
+        waiter.wait_for(lambda : os.path.exists(asset_info_file), timeout=5)
+        assert os.path.exists(asset_info_file), "The assetinfo file is missing."
 
         # Reprocessing files and making sure there are no failed jobs
         result, output = run_ap_debug_skip_atom_output(asset_processor)
         assert result, "AssetProcessorBatch failed when it should have succeeded after renaming output."
 
         num_failed_assets = asset_processor_utils.get_num_failed_processed_assets(output)
-        assert num_failed_assets is 0, 'Wrong number of failed assets'
+        assert num_failed_assets is 0, 'Expected no failed assets.'
 
-        num_processed_assets = asset_processor_utils.get_num_processed_assets(output)
-        expected_asset_count = 2
-        assert num_processed_assets >= expected_asset_count, f'Wrong number of successfully processed assets found in output: '\
-                                          f'expected at least {expected_asset_count}, but only {num_processed_assets} were processed'
+        expected_assets = ['a.actor', 'b.actor']
+        missing_assets, _ = utils.compare_assets_with_cache(expected_assets, asset_processor.project_test_cache_folder())
+        assert not missing_assets, f"The following assets were expected, but not found in cache {missing_assets}."
 
     @pytest.mark.BAT
     @pytest.mark.assetpipeline
