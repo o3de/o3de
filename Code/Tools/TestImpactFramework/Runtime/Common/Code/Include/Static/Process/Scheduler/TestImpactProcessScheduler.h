edb22019ed09ae16bf71f7505fd945d69b71bda9
John <jonawals@amazon.com>
2023-03-06 11:50:45 +0000

    Remove duplicate bus.
    Signed-off-by: John <jonawals@amazon.com>

--- Code/Tools/TestImpactFramework/Runtime/Common/Code/Include/Static/Process/Scheduler/TestImpactProcessScheduler.h
+++ Code/Tools/TestImpactFramework/Runtime/Common/Code/Include/Static/Process/Scheduler/TestImpactProcessScheduler.h
@@ -52,68 +52,6 @@ namespace TestImpact
         Timeout //!< The scheduler aborted its run prematurely due to its runtime exceeding the scheduler timeout value.
     };
 
-    //!
-    class ProcessSchedulerNotifications
-        : public AZ::EBusTraits
-    {
-    public:
-        //////////////////////////////////////////////////////////////////////////
-        // EBusTraits overrides
-        static const AZ::EBusAddressPolicy AddressPolicy = AZ::EBusAddressPolicy::Single;
-        static const AZ::EBusHandlerPolicy HandlerPolicy = AZ::EBusHandlerPolicy::Multiple;
-        //////////////////////////////////////////////////////////////////////////
-
-        //! Callback for process launch attempt.
-        //! @param processId The id of the process that attempted to launch.
-        //! @param launchResult The result of the process launch attempt.
-        //! @param createTime The timestamp of the process launch attempt.
-        virtual ProcessCallbackResult OnProcessLaunch(
-            [[maybe_unused]] ProcessId processId,
-            [[maybe_unused]] LaunchResult launchResult,
-            [[maybe_unused]] AZStd::chrono::steady_clock::time_point createTime)
-        {
-            return ProcessCallbackResult::Continue;
-        }
-
-         //! Callback for process exit of successfully launched process.
-        //! @param processId The id of the process that attempted to launch.
-        //! @param exitCondition The circumstances upon which the processes exited.
-        //! @param returnCode The return code of the exited process.
-        //! @param std The standard output and standard error of the process.
-        //! @param createTime The timestamp of the process exit.
-        virtual ProcessCallbackResult OnProcessExit(
-            [[maybe_unused]] ProcessId processId,
-            [[maybe_unused]] ExitCondition exitCondition,
-            [[maybe_unused]] ReturnCode returnCode,
-            [[maybe_unused]] const StdContent& std,
-            [[maybe_unused]] AZStd::chrono::steady_clock::time_point exitTime)
-        {
-            return ProcessCallbackResult::Continue;
-        }
-
-        //! Callback for process standard output/error buffer consumption in real-time.
-        //! @note The full standard output/error data is available to all capturing processes at their end of life regardless of this
-        //! callback.
-        //! @param processId The id of the process that attempted to launch.
-        //! @param stdOutput The total accumulated standard output buffer.
-        //! @param stdError The total accumulated standard error buffer.
-        //! @param stdOutputDelta The standard output buffer data since the last callback.
-        //! @param stdErrorDelta The standard error buffer data since the last callback.
-        virtual void OnRealtimeStdContent(
-            [[maybe_unused]] ProcessId processId,
-            [[maybe_unused]] const AZStd::string& stdOutput,
-            [[maybe_unused]] const AZStd::string& stdError,
-            [[maybe_unused]] const AZStd::string& stdOutputDelta,
-            [[maybe_unused]] const AZStd::string& stdErrorDelta)
-        {
-        }
-    };
-
-    using ProcessSchedulerNotificationsBus = AZ::EBus<ProcessSchedulerNotifications>;
-
-    //! Returns the aggregate process callback result where if one or more aborts exists, the result is abort, otherwise continue. 
-    ProcessCallbackResult GetAggregateProcessCallbackResult(const AZ::EBusAggregateResults<ProcessCallbackResult>& results);
-
     //! Schedules a batch of processes for launch using a round robin approach to distribute the in-flight processes over
     //! the specified number of concurrent process slots.
     class ProcessScheduler
