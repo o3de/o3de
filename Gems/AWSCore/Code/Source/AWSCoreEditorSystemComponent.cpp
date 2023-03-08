ce6220cf88e05fc62b280b6c12b94864c9cf998d
Pip Potter <61438964+lmbr-pip@users.noreply.github.com>
2023-03-08 13:53:55 -0800

    [AWSCore Resource Mapping] Fix broken links, test level and tether tool lifetime to Editor
    Signed-off-by: Pip Potter <61438964+lmbr-pip@users.noreply.github.com>

--- Gems/AWSCore/Code/Source/AWSCoreEditorSystemComponent.cpp
+++ Gems/AWSCore/Code/Source/AWSCoreEditorSystemComponent.cpp
@@ -91,6 +91,8 @@ namespace AWSCore
         AzToolsFramework::ActionManagerRegistrationNotificationBus::Handler::BusDisconnect();
 
         AWSCoreEditorRequestBus::Handler::BusDisconnect();
+
+        m_awsCoreEditorMenu.reset();
     }
 
     void AWSCoreEditorSystemComponent::OnMenuBarRegistrationHook()
