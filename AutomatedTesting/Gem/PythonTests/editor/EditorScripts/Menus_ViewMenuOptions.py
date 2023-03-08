32b6ef6ae3628cbd9a5b511791f88577c7601929
amzn-ahmadkrm <105638312+amzn-ahmadkrm@users.noreply.github.com>
2023-03-08 21:02:44 +0000

    Helper visibility settings changes persist (#14724)
    * save settings to registry on change
    
    Signed-off-by: amzn-ahmadkrm <105638312+amzn-ahmadkrm@users.noreply.github.com>
    
    * add no helpers option
    
    Signed-off-by: amzn-ahmadkrm <105638312+amzn-ahmadkrm@users.noreply.github.com>
    
    * make helper settings work with new AM
    
    Signed-off-by: amzn-ahmadkrm <105638312+amzn-ahmadkrm@users.noreply.github.com>
    
    * Update EditorActionsHandler.cpp
    
    Signed-off-by: amzn-ahmadkrm <105638312+amzn-ahmadkrm@users.noreply.github.com>
    
    * fix python test with new menu
    
    Signed-off-by: amzn-ahmadkrm <105638312+amzn-ahmadkrm@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: amzn-ahmadkrm <105638312+amzn-ahmadkrm@users.noreply.github.com>

--- AutomatedTesting/Gem/PythonTests/editor/EditorScripts/Menus_ViewMenuOptions.py
+++ AutomatedTesting/Gem/PythonTests/editor/EditorScripts/Menus_ViewMenuOptions.py
@@ -35,9 +35,11 @@ def Menus_ViewMenuOptions_Work():
         ("Viewport", "Go to Position"),
         ("Viewport", "Center on Selection"),
         ("Viewport", "Go to Location"),
-        ("Viewport", "Show Helpers"),
-        ("Viewport", "Show Icons"),
-        ("Refresh Style",),
+        ("Viewport", "Viewport Helpers", "Show Icons"),
+        ("Viewport", "Viewport Helpers", "Show Helpers for all entities"),
+        ("Viewport", "Viewport Helpers", "Show Helpers for selected entities"),
+        ("Viewport", "Viewport Helpers", "Hide Helpers"),
+        ("Refresh Style",)
     ]
 
     # 1) Open an existing simple level
