16e855c373a65b6f32030bd7146223d911f4b86b
AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
2023-03-09 10:19:34 -0800

    Jobs with missing source dependencies are now run later, in case the dependencies resolve (#14862)
    * WIP test
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
    
    * First pass at automated test that demonstrates the not desired FBX processing failure.
    This is to make it easier to build the actual fix, after this test goes in then I can work on fixing this issue.
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
    
    * Cleaned up test
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
    
    * Cleaned up stale code
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
    
    * Cleaned up builder creation
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
    
    * PR feedback - added comments on processEvents
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
    
    * Jobs with missing dependencies are lower priority now, and when the dependency resolves (or goes away) the old job will be cleared from the queue and the new job replaces it.
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
    
    * Added comments, cleaned out stale code
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
    
    * work in progress alternate solution to dependency processing order. I'm going to clean this up and change my approach, but I didn't want to lose this commit.
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
    
    * Common platform jobs sort earlier now
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
    
    * Common jobs come earlier in the queue now. This fixes FBX, but there is still the potential for an edge case race condition, it's just much lower now.
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
    
    * Removed debugging output
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
    
    * Cleaned up more debug code
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
    
    * Added comments for TestingRCController class.
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
    
    * More code cleanup
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
    
    * Switched to AZ_Trace
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
    
    * Removed print and for loop that was causing unused variable issues in certain build configurations. This was just an edge case helper print out, it's cleaner to remove it than handle unused parameters.
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
    
    * Tightened up timing of events in the automated test, to make this test less frail to other tests running in parallel changing the timing.
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: AMZN-stankowi <4838196+AMZN-stankowi@users.noreply.github.com>

--- Code/Tools/AssetProcessor/native/tests/assetmanager/AssetManagerTestingBase.cpp
+++ Code/Tools/AssetProcessor/native/tests/assetmanager/AssetManagerTestingBase.cpp
@@ -145,7 +145,7 @@ namespace UnitTests
 
         AZ::Utils::WriteFile("unit test file", m_testFilePath);
 
-        m_rc = AZStd::make_unique<AssetProcessor::RCController>(1, 1);
+        m_rc = AZStd::make_unique<TestingRCController>(1, 1);
         m_rc->SetDispatchPaused(false);
 
         QObject::connect(
@@ -227,12 +227,16 @@ namespace UnitTests
     void AssetManagerTestingBase::ProcessJob(AssetProcessor::RCController& rcController, const AssetProcessor::JobDetails& jobDetails)
     {
         rcController.JobSubmitted(jobDetails);
+        UnitTests::JobSignalReceiver receiver;
+        WaitForNextJobToProcess(receiver);
+    }
 
-        JobSignalReceiver receiver;
-        QCoreApplication::processEvents(); // Once to get the job started
+    void AssetManagerTestingBase::WaitForNextJobToProcess(UnitTests::JobSignalReceiver &receiver)
+    {
+        QCoreApplication::processEvents(); // RCController::DispatchJobsImpl : Once to get the job started
         receiver.WaitForFinish(); // Wait for the RCJob to signal it has completed working
-        QCoreApplication::processEvents(); // Once more to trigger the JobFinished event
-        QCoreApplication::processEvents(); // Again to trigger the Finished event
+        QCoreApplication::processEvents(); // RCJob::Finished : Once more to trigger the JobFinished event
+        QCoreApplication::processEvents(); // RCController::FinishJob : Again to trigger the Finished event
     }
 
     AssetBuilderSDK::CreateJobFunction AssetManagerTestingBase::CreateJobStage(
@@ -347,6 +351,18 @@ namespace UnitTests
             CreateJobStage(name, createJobCommonPlatform),
             ProcessJobStage(outputExtension, outputFlags, outputExtraFile),
             "fingerprint");
+    }
+
+    void AssetManagerTestingBase::SetCatalogToUpdateOnJobCompletion()
+    {
+        using namespace AssetBuilderSDK;
+        QObject::connect(
+            m_rc.get(),
+            &AssetProcessor::RCController::FileCompiled,
+            [this](AssetProcessor::JobEntry entry, AssetBuilderSDK::ProcessJobResponse response)
+            {
+                QMetaObject::invokeMethod(m_rc.get(), "OnAddedToCatalog", Qt::QueuedConnection, Q_ARG(AssetProcessor::JobEntry, entry));
+            });
     }
 
     AZStd::string AssetManagerTestingBase::MakePath(const char* filename, bool intermediate)
