bd0d94139d53846f4de6b5edf7f602d7c3b3ec35
JLeclercq <99494374+CyaNinix@users.noreply.github.com>
2023-03-17 09:04:24 +0100

    Update Gems/EMotionFX/Code/Source/Integration/Components/ActorComponent.h
    Co-authored-by: Alex Peterson <26804013+AMZN-alexpete@users.noreply.github.com>
    Signed-off-by: JLeclercq <99494374+CyaNinix@users.noreply.github.com>

--- Gems/EMotionFX/Code/Source/Integration/Components/ActorComponent.h
+++ Gems/EMotionFX/Code/Source/Integration/Components/ActorComponent.h
@@ -94,7 +94,7 @@ namespace EMotionFX
                 bool m_forceUpdateJointsOOV = false;
                 BoundingBoxConfiguration m_bboxConfig; ///< Configuration for bounding box type and updates
 
-                bool m_excludeFromReflectionCubeMaps = false;
+                bool m_excludeFromReflectionCubeMaps = true;
 
                 static void Reflect(AZ::ReflectContext* context);
             };
