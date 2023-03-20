1f1423ccdd66130c810fafe6991ac11113d9f4c6
galibzon <66021303+galibzon@users.noreply.github.com>
2023-03-20 00:14:48 -0500

    Added support for custom external debuggers like vscode via a user customizable URI per the registry key: `/O3DE/Lua/Debugger/Uri
    If the above Uri is not specified,
    then the Editor will open LuaIDE.exe.
    
    For example, if using The Visual Studio Debugger Extension provided by lumbermixalot
    The value will be: "vscode://lumbermixalot.o3de-lua-debug/debug?"
    The following parameters will be added to the URI at runtime,
    as part of the query:
        - "projectPath". Absolute path of the game projec root.
        - "enginePath". Absolute path of the engine root. if not specified,
                        it will be assume to be one directory above the game project root.
        - "files[]". A list of files.
    
    Full example of a complete Uri Query that the Editor would
    dispatch to the OS to open two files with a particular vscode debugger:
    REMARK: The special characters would be properly escaped because
    the code uses QUrl and QUrlQuery classes for encoding.
    
    Example with two files to open:
    `vscode://lumbermixalot.o3de-lua-debug/debug?projectPath=D:\mydir\myproject&enginePath=C:\GIT\o3de&files[]=D:\mydir\myproject\scripts\something.lua&files[]=D:\mydir\myproject\scripts\utils\something2.lua`
    
    Exampled wit only one file to open (most common scenario).
    `vscode://lumbermixalot.o3de-lua-debug/debug?projectPath=D:\GIT\o3de\AutomatedTesting&files[]=D:\GIT\o3de\AutomatedTesting\Assets\Scripts\something.lua`
    
    Signed-off-by: galibzon <66021303+galibzon@users.noreply.github.com>

--- Code/Editor/Util/Platform/Windows/FileUtil_Windows.cpp
+++ Code/Editor/Util/Platform/Windows/FileUtil_Windows.cpp
@@ -14,20 +14,31 @@
 
 namespace Platform
 {
-    bool RunEditorWithArg(const QString editor, const QString arg)
+    bool RunCommandWithArguments(const QString& command, const QString& args)
     {
         bool success = false;
 
-        // Use the Win32API calls as they aren't limited to running items in the path.
-        QString fullTexturePathFixedForWindows = QString(arg.data()).replace('/', '\\');
+        HINSTANCE hInst = ShellExecuteW( nullptr,
+            L"open",
+            command.toStdWString().c_str(),
+            args.isEmpty() ? NULL : args.toStdWString().c_str(),
+            NULL, SW_SHOWNORMAL);
 
-        HINSTANCE hInst = ShellExecuteW(
-            nullptr, L"open", editor.toStdWString().c_str(), fullTexturePathFixedForWindows.toStdWString().c_str(), NULL,
-            SW_SHOWNORMAL);
-        
         success = ((DWORD_PTR)hInst > 32);
 
         return success;
+    }
+
+    bool OpenUri(const QUrl& uri)
+    {
+        return RunCommandWithArguments(uri.toString(), "");
+    }
+
+    bool RunEditorWithArg(const QString& editor, const QString& arg)
+    {
+        // Use the Win32API calls as they aren't limited to running items in the path.
+        QString fullTexturePathFixedForWindows = QString(arg.data()).replace('/', '\\');
+        return RunCommandWithArguments(editor, fullTexturePathFixedForWindows);
     }
 
     QString GetDefaultEditor(const Common::EditFileType fileType)
@@ -114,4 +125,5 @@ namespace Platform
     {
         return "LuaCompiler.exe";
     }
+
 }
