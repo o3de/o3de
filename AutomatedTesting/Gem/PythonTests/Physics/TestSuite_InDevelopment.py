ff3ee67c6ef6b9499a24dcbc1399f2619889713b
FuzzyCarterAWS <jscar@amazon.com>
2023-03-27 16:12:36 -0700

    Removing InDevelopment test entry for test file that doesn't exist in repository or in removed test commits from this PR.
    Signed-off-by: FuzzyCarterAWS <jscar@amazon.com>

--- AutomatedTesting/Gem/PythonTests/Physics/TestSuite_InDevelopment.py
+++ AutomatedTesting/Gem/PythonTests/Physics/TestSuite_InDevelopment.py
@@ -35,10 +35,6 @@ class TestAutomation(TestAutomationBase):
         from .tests import Physics_WorldBodyBusWorksOnEditorComponents as test_module
         self._run_test(request, workspace, editor, test_module)
 
-    def test_Collider_PxMeshErrorIfNoMesh(self, request, workspace, editor, launcher_platform):
-        from .tests.collider import Collider_PxMeshErrorIfNoMesh as test_module
-        self._run_test(request, workspace, editor, test_module)
-
 
 # Custom test spec, it provides functionality to override files
 class EditorSingleTest_WithFileOverrides(EditorSingleTest):
