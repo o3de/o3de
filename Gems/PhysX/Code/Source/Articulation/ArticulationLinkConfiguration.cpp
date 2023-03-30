c25a75fe28246d55a96d55a91379e1f31a70aaa7
amzn-ahmadkrm <105638312+amzn-ahmadkrm@users.noreply.github.com>
2023-03-30 12:41:40 +0100

    Add additional config when user selects hinge or prismatic joint type (#511)
    * add additional config when user selects hinge or prismatic joint type
    
    Signed-off-by: amzn-ahmadkrm <105638312+amzn-ahmadkrm@users.noreply.github.com>
    
    * fix typo
    
    Signed-off-by: amzn-ahmadkrm <105638312+amzn-ahmadkrm@users.noreply.github.com>
    
    * address PR comments
    
    Signed-off-by: amzn-ahmadkrm <105638312+amzn-ahmadkrm@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: amzn-ahmadkrm <105638312+amzn-ahmadkrm@users.noreply.github.com>

--- Gems/PhysX/Code/Source/Articulation/ArticulationLinkConfiguration.cpp
+++ Gems/PhysX/Code/Source/Articulation/ArticulationLinkConfiguration.cpp
@@ -27,31 +27,49 @@ namespace PhysX
                 ->Field("Fixed Base", &ArticulationLinkConfiguration::m_isFixedBase)
                 ->Field("Mass", &ArticulationLinkConfiguration::m_mass)
                 ->Field("Gravity Enabled", &ArticulationLinkConfiguration::m_gravityEnabled)
-                ->Field("Local Position", &ArticulationLinkConfiguration::m_localPosition)
-                ->Field("Local Rotation", &ArticulationLinkConfiguration::m_localRotation)
-                ->Field("Auto-calculate Leader frame", &ArticulationLinkConfiguration::m_autoCalculateLeaderFrame)
-                ->Field("Leader Local Position", &ArticulationLinkConfiguration::m_leaderLocalPosition)
-                ->Field("Leader Local Rotation", &ArticulationLinkConfiguration::m_LeaderLocalRotation)
-                ->Field("Initial linear velocity", &ArticulationLinkConfiguration::m_initialLinearVelocity)
-                ->Field("Initial angular velocity", &ArticulationLinkConfiguration::m_initialAngularVelocity)
                 ->Field("Linear damping", &ArticulationLinkConfiguration::m_linearDamping)
                 ->Field("Angular damping", &ArticulationLinkConfiguration::m_angularDamping)
                 ->Field("Sleep threshold", &ArticulationLinkConfiguration::m_sleepMinEnergy)
                 ->Field("Start Asleep", &ArticulationLinkConfiguration::m_startAsleep)
                 ->Field("Centre of mass offset", &ArticulationLinkConfiguration::m_centerOfMassOffset)
                 ->Field("Maximum Angular Velocity", &ArticulationLinkConfiguration::m_maxAngularVelocity)
-                ->Field("Articulation Joint Type", &ArticulationLinkConfiguration::m_articulationJointType)
-                ->Field("Motor configuration", &ArticulationLinkConfiguration::m_motorConfiguration)
-                ->Field("Fix Joint Location", &ArticulationLinkConfiguration::m_fixJointLocation)
-                ->Field("Select Lead on Snap", &ArticulationLinkConfiguration::m_selectLeadOnSnap)
-                ->Field("Self Collide", &ArticulationLinkConfiguration::m_selfCollide)
                 ->Field("SolverPositionIterations", &ArticulationLinkConfiguration::m_solverPositionIterations)
-                ->Field("SolverVelocityIterations", &ArticulationLinkConfiguration::m_solverVelocityIterations);
+                ->Field("SolverVelocityIterations", &ArticulationLinkConfiguration::m_solverVelocityIterations)
+                ->Field("Articulation Joint Type", &ArticulationLinkConfiguration::m_articulationJointType)
+                ->Field("Local Position", &ArticulationLinkConfiguration::m_localPosition)
+                ->Field("Local Rotation", &ArticulationLinkConfiguration::m_localRotation)
+                ->Field("Fix Joint Location", &ArticulationLinkConfiguration::m_fixJointLocation)
+                ->Field("Self Collide", &ArticulationLinkConfiguration::m_selfCollide)
+                ->Field("Leader Local Position", &ArticulationLinkConfiguration::m_leaderLocalPosition)
+                ->Field("Leader Local Rotation", &ArticulationLinkConfiguration::m_LeaderLocalRotation)
+                ->Field("Auto-calculate Leader frame", &ArticulationLinkConfiguration::m_autoCalculateLeaderFrame)
+                ->Field("Is Limited", &ArticulationLinkConfiguration::m_isLimited)
+                ->Field("Linear Limit Lower", &ArticulationLinkConfiguration::m_linearLimitLower)
+                ->Field("Linear Limit Upper", &ArticulationLinkConfiguration::m_linearLimitUpper)
+                ->Field("Angular Limit Negative", &ArticulationLinkConfiguration::m_angularLimitNegative)
+                ->Field("Angular Limit Positive", &ArticulationLinkConfiguration::m_angularLimitPositive)
+                ->Field("Motor configuration", &ArticulationLinkConfiguration::m_motorConfiguration)
+                ;
         }
     }
 
     bool ArticulationLinkConfiguration::IsNotRootArticulation() const
     {
         return !m_isRootArticulation;
+    }
+
+    bool ArticulationLinkConfiguration::HingePropertiesVisible() const
+    {
+        return m_articulationJointType == ArticulationJointType::Hinge && IsNotRootArticulation();
+    }
+
+    bool ArticulationLinkConfiguration::PrismaticPropertiesVisible() const
+    {
+        return m_articulationJointType == ArticulationJointType::Prismatic && IsNotRootArticulation();
+    }
+
+    bool ArticulationLinkConfiguration::IsSingleDofJointType() const
+    {
+        return HingePropertiesVisible() || PrismaticPropertiesVisible();
     }
 } // namespace PhysX
