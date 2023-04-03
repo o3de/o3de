71ba5ce5319e1a93a77c60c8f014a5609298292b
Luis Sempé <110437791+l-sempe@users.noreply.github.com>
2023-04-03 12:20:36 -0700

    Revert "Manually generated missing UUIDs"
    This reverts commit 89230b83e5dc88b3a7a9a9744087fabadc5ba296.

--- Gems/GraphicsGem_AR_Test/Code/Include/GraphicsGem_AR_Test/GraphicsGem_AR_TestTypeIds.h
+++ Gems/GraphicsGem_AR_Test/Code/Include/GraphicsGem_AR_Test/GraphicsGem_AR_TestTypeIds.h
@@ -11,12 +11,12 @@
 namespace GraphicsGem_AR_Test
 {
     // System Component TypeIds
-    inline constexpr const char* GraphicsGem_AR_TestSystemComponentTypeId = "{21A2EE32-B555-4E07-B9B5-02594F2DE041}";
-    inline constexpr const char* GraphicsGem_AR_TestEditorSystemComponentTypeId = "{6B7E0A84-9E8E-492F-83DE-ECD1EF298D60}";
+    inline constexpr const char* GraphicsGem_AR_TestSystemComponentTypeId = "${SysCompClassId}";
+    inline constexpr const char* GraphicsGem_AR_TestEditorSystemComponentTypeId = "${EditorSysCompClassId}";
 
     // Module derived classes TypeIds
     inline constexpr const char* GraphicsGem_AR_TestModuleInterfaceTypeId = "{E22C9D32-526E-481F-8440-29E489EEB349}";
-    inline constexpr const char* GraphicsGem_AR_TestModuleTypeId = "{14CC1350-97E5-46D4-8042-8DC6938D1B86}";
+    inline constexpr const char* GraphicsGem_AR_TestModuleTypeId = "${ModuleClassId}";
     // The Editor Module by default is mutually exclusive with the Client Module
     // so they use the Same TypeId
     inline constexpr const char* GraphicsGem_AR_TestEditorModuleTypeId = GraphicsGem_AR_TestModuleTypeId;
