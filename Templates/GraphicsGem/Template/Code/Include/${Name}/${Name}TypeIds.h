3b96474cfca307a5f1fbb045e3b9aa2203738ed6
Luis Sempé <110437791+l-sempe@users.noreply.github.com>
2023-04-04 09:15:10 -0700

    Fixed typeIds
    Signed-off-by: Luis Sempé <110437791+l-sempe@users.noreply.github.com>

--- Templates/GraphicsGem/Template/Code/Include/${Name}/${Name}TypeIds.h
+++ Templates/GraphicsGem/Template/Code/Include/${Name}/${Name}TypeIds.h
@@ -11,15 +11,15 @@
 namespace ${Name}
 {
     // System Component TypeIds
-    inline constexpr const char* ${Name}SystemComponentTypeId = "{${Random_Uuid}}";
-    inline constexpr const char* ${Name}EditorSystemComponentTypeId = "{${Random_Uuid}}";
+    inline constexpr const char* ${Name}SystemComponentTypeId = "${SysCompClassId}";
+    inline constexpr const char* ${Name}EditorSystemComponentTypeId = "${EditorSysCompClassId}";
 
     // Module derived classes TypeIds
     inline constexpr const char* ${Name}ModuleInterfaceTypeId = "{${Random_Uuid}}";
-    inline constexpr const char* ${Name}ModuleTypeId = "{${Random_Uuid}}";
+    inline constexpr const char* ${Name}ModuleTypeId = "${ModuleClassId}";
     // The Editor Module by default is mutually exclusive with the Client Module
     // so they use the Same TypeId
-    inline constexpr const char* ${Name}EditorModuleTypeId = ${Name}ModuleTypeId;
+    inline constexpr const char* ${Name}EditorModuleTypeId = ${SanitizedCppName}ModuleTypeId;
 
     // Interface TypeIds
     inline constexpr const char* ${Name}RequestsTypeId = "{${Random_Uuid}}";
