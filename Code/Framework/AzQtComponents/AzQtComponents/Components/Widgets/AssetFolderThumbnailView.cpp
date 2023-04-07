3f318b26755132b919f1e1e2b4fe6ad56a99dd21
Daniel Tamkin <jotamkin@amazon.com>
2023-04-07 15:24:08 -0500

    Fix merge conflict
    Signed-off-by: Daniel Tamkin <jotamkin@amazon.com>

--- Code/Framework/AzQtComponents/AzQtComponents/Components/Widgets/AssetFolderThumbnailView.cpp
+++ Code/Framework/AzQtComponents/AzQtComponents/Components/Widgets/AssetFolderThumbnailView.cpp
@@ -1063,29 +1063,6 @@ namespace AzQtComponents
     {
         m_showSearchResultsMode = searchMode;
     }
-
-    void AssetFolderThumbnailView::rowsInserted(const QModelIndex& parent, int start, int end)
-    {
-        scheduleDelayedItemsLayout();
-
-        QAbstractItemView::rowsInserted(parent, start, end);
-    }
-
-    void AssetFolderThumbnailView::rowsAboutToBeRemoved(const QModelIndex& parent, int start, int end)
-    {
-        scheduleDelayedItemsLayout();
-
-        QAbstractItemView::rowsAboutToBeRemoved(parent, start, end);
-    }
-
-    void AssetFolderThumbnailView::reset()
-    {
-        m_itemGeometry.clear();
-        m_expandedIndexes.clear();
-        m_childFrames.clear();
-
-        QAbstractItemView::reset();
-    }
 } // namespace AzQtComponents
 
 #include "Components/Widgets/moc_AssetFolderThumbnailView.cpp"
