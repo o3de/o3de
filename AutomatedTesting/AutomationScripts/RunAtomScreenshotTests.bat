8633776d91d4f70917d96a372c8022d520dcb09c
rgba16f <82187279+rgba16f@users.noreply.github.com>
2023-04-20 11:43:17 -0500

    Make windows batch script clearer, add a seedlist file for the image comparison settings data file
    Signed-off-by: rgba16f <82187279+rgba16f@users.noreply.github.com>

--- AutomatedTesting/AutomationScripts/RunAtomScreenshotTests.bat
+++ AutomatedTesting/AutomationScripts/RunAtomScreenshotTests.bat
@@ -7,8 +7,11 @@ REM SPDX-License-Identifier: Apache-2.0 OR MIT
 REM 
 REM --------------------------------------------------------------------------------------------------
 
-pushd %~dp0
+REM record current dir
+pushd .
+REM cd to o3de root dir, %~dp0 is the path to the folder that contains this script
+cd %~dp0/../..
 setlocal
-call ../../python/python.cmd -s -B -m pytest -v --tb=short --show-capture=stdout -c ../../pytest.ini --build-directory ../../AutomatedTesting/build/bin/profile ../../AutomatedTesting/Gem/PythonTests/Atom/TestSuite_Periodic_GPU.py --output-path ../../AutomatedTesting/build/Testing/LyTestTools/AutomatedTesting_Atom_TestSuite_Periodic_GPU --junitxml=../../AutomatedTesting/build/Testing/Pytest/AutomatedTesting_Atom_TestSuite_Periodic_GPU.xml
+call python/python.cmd -s -B -m pytest -v --tb=short --show-capture=stdout -c pytest.ini --build-directory AutomatedTesting/build/bin/profile AutomatedTesting/Gem/PythonTests/Atom/TestSuite_Periodic_GPU.py --output-path AutomatedTesting/build/Testing/LyTestTools/AutomatedTesting_Atom_TestSuite_Periodic_GPU --junitxml=AutomatedTesting/build/Testing/Pytest/AutomatedTesting_Atom_TestSuite_Periodic_GPU.xml
 endlocal
 popd
