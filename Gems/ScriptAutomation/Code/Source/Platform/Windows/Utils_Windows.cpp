c877cf3f36326828e48f56d8d429642e3f23aa28
rgba16f <82187279+rgba16f@users.noreply.github.com>
2023-05-08 13:26:38 -0500

    Add AreaLight screenshot test level (#15853)
    * Add ability to switch camera's within a level. Add new AreaLight test level that has 2 test area's.
    
    Signed-off-by: rgba16f <82187279+rgba16f@users.noreply.github.com>
    
    * Initial Implementation of launching an image diff app for failed screenshot compares
    
    Signed-off-by: rgba16f <82187279+rgba16f@users.noreply.github.com>
    
    * Add a linux implementation of LaunchApp, plus empty implementations for other platforms so they build
    
    Signed-off-by: rgba16f <82187279+rgba16f@users.noreply.github.com>
    
    * BCompare launching works on linux, plus update some expected compare images
    
    Signed-off-by: rgba16f <82187279+rgba16f@users.noreply.github.com>
    
    * Add comment explaining how to turn on local image diff launching
    
    Signed-off-by: rgba16f <82187279+rgba16f@users.noreply.github.com>
    
    * Update DepthOfField compare screenshots for the low end render pipeline
    
    Signed-off-by: rgba16f <82187279+rgba16f@users.noreply.github.com>
    
    * Update with PR feedback about whitespace.
    
    Signed-off-by: rgba16f <82187279+rgba16f@users.noreply.github.com>
    
    * Update the LaunchProcess code with advice from geds-dm
    
    Signed-off-by: rgba16f <82187279+rgba16f@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: rgba16f <82187279+rgba16f@users.noreply.github.com>

---
+++ Gems/ScriptAutomation/Code/Source/Platform/Windows/Utils_Windows.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+
+#include <AzCore/PlatformIncl.h>
+
+#include <AzCore/std/functional.h>
+#include <AzCore/std/string/string.h>
+#include <AzCore/std/containers/vector.h>
+#include <AzCore/StringFunc/StringFunc.h>
+
+#include <AzFramework/Process/ProcessWatcher.h>
+
+namespace AZ::Platform
+{
+    bool LaunchProgram(const AZStd::string& progPath, const AZStd::string& arguments)
+    {
+        AZ_Info("ScriptAutomation", "Attempting to launch \"%s %s\"", progPath.c_str(), arguments.c_str());
+
+        AzFramework::ProcessLauncher::ProcessLaunchInfo processLaunchInfo;
+
+        AZStd::vector<AZStd::string> launchCmd = { progPath };
+        auto SplitString = [&launchCmd](AZStd::string_view token)
+        {
+            launchCmd.emplace_back(token);
+        };
+        AZ::StringFunc::TokenizeVisitor(arguments, SplitString, " \t");
+
+        processLaunchInfo.m_commandlineParameters = AZStd::move(launchCmd);
+
+        return AzFramework::ProcessLauncher::LaunchUnwatchedProcess(processLaunchInfo);
+    }
+}
