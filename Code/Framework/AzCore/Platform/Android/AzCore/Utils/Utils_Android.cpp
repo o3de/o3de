314d8d72210973f258c3aa7acb395164f5466da9
lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
2023-05-18 10:46:53 -0500

    Adding missing implementation for Android GetEnv
    This is the same implementation as in Utils_UnixLike.cpp
    
    Signed-off-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>

--- Code/Framework/AzCore/Platform/Android/AzCore/Utils/Utils_Android.cpp
+++ Code/Framework/AzCore/Platform/Android/AzCore/Utils/Utils_Android.cpp
@@ -16,78 +16,113 @@
 #include <AzCore/Android/JNI/Internal/ClassName.h>
 #include <AzCore/Android/Utils.h>
 
-namespace AZ
+namespace AZ::Utils
 {
-    namespace Utils
+    void RequestAbnormalTermination()
     {
-        void RequestAbnormalTermination()
+        abort();
+    }
+
+    void NativeErrorMessageBox(const char*, const char*) {}
+
+    GetExecutablePathReturnType GetExecutablePath(char* exeStorageBuffer, size_t exeStorageSize)
+    {
+        GetExecutablePathReturnType result;
+        result.m_pathIncludesFilename = false;
+        const char* privateStoragePath  = AZ::Android::AndroidEnv::Get()->GetAppPrivateStoragePath();
+        if(exeStorageSize > strlen(privateStoragePath))
         {
-            abort();
+            azstrcpy(exeStorageBuffer, exeStorageSize, privateStoragePath);
+        }
+        else
+        {
+            result.m_pathStored = ExecutablePathResult::BufferSizeNotLargeEnough;
         }
 
-        void NativeErrorMessageBox(const char*, const char*) {}
+        return result;
+    }
 
-        GetExecutablePathReturnType GetExecutablePath(char* exeStorageBuffer, size_t exeStorageSize)
+    // Android attempts to find a bootstrap.cfg file within the public storage for an application
+    // in non-release builds.
+    // If a bootstrap.cfg file is not found in the public storage, it is then searched for
+    // within the APK itself
+    AZStd::optional<AZ::IO::FixedMaxPathString> GetDefaultAppRootPath()
+    {
+        const char* appRoot = AZ::Android::Utils::FindAssetsDirectory();
+        return appRoot ? AZStd::make_optional<AZ::IO::FixedMaxPathString>(appRoot) : AZStd::nullopt;
+    }
+
+    AZStd::optional<AZ::IO::FixedMaxPathString> GetDevWriteStoragePath()
+    {
+        const char* writeStorage = AZ::Android::Utils::GetAppPublicStoragePath();
+        return writeStorage ? AZStd::make_optional<AZ::IO::FixedMaxPathString>(writeStorage) : AZStd::nullopt;
+    }
+
+    bool ConvertToAbsolutePath(const char* path, char* absolutePath, AZ::u64 maxLength)
+    {
+        if (AZ::Android::Utils::IsApkPath(path))
         {
-            GetExecutablePathReturnType result;
-            result.m_pathIncludesFilename = false;
-            const char* privateStoragePath  = AZ::Android::AndroidEnv::Get()->GetAppPrivateStoragePath();
-            if(exeStorageSize > strlen(privateStoragePath))
+            azstrcpy(absolutePath, maxLength, path);
+            return true;
+        }
+
+#ifdef PATH_MAX
+        static constexpr size_t UnixMaxPathLength = PATH_MAX;
+#else
+        // Fallback to 4096 if the PATH_MAX macro isn't defined on the Unix System
+        static constexpr size_t UnixMaxPathLength = 4096;
+#endif
+        if (!AZ::IO::PathView(path).IsAbsolute())
+        {
+            // note that realpath fails if the path does not exist and actually changes the return value
+            // to be the actual place that FAILED, which we don't want.
+            // if we fail, we'd prefer to fall through and at least use the original path.
+            char absolutePathBuffer[UnixMaxPathLength];
+            if (const char* result = realpath(path, absolutePathBuffer); result != nullptr)
             {
-                azstrcpy(exeStorageBuffer, exeStorageSize, privateStoragePath);
+                azstrcpy(absolutePath, maxLength, absolutePathBuffer);
+                return true;
+            }
+        }
+        azstrcpy(absolutePath, maxLength, path);
+        return AZ::IO::PathView(absolutePath).IsAbsolute();
+    }
+
+    GetEnvOutcome GetEnv(AZStd::span<char> valueBuffer, const char* envname)
+    {
+        if (const char* envValue = std::getenv(envname);
+            envValue != nullptr)
+        {
+            if (AZStd::string_view utf8Value(envValue);
+                valueBuffer.size() >= utf8Value.size())
+            {
+                // copy the utf8 string value over to the value buffer
+                utf8Value.copy(valueBuffer.data(), valueBuffer.size());
+                // return a string that points the beginning of the span buffer
+                // with a size that is set to the environment variable value string
+                return AZStd::string_view(valueBuffer.data(), utf8Value.size());
             }
             else
             {
-                result.m_pathStored = ExecutablePathResult::BufferSizeNotLargeEnough;
+                return AZ::Failure(GetEnvErrorResult{ GetEnvErrorCode::BufferTooSmall, utf8Value.size() });
             }
-
-            return result;
         }
 
-        // Android attempts to find a bootstrap.cfg file within the public storage for an application
-        // in non-release builds.
-        // If a bootstrap.cfg file is not found in the public storage, it is then searched for
-        // within the APK itself
-        AZStd::optional<AZ::IO::FixedMaxPathString> GetDefaultAppRootPath()
-        {
-            const char* appRoot = AZ::Android::Utils::FindAssetsDirectory();
-            return appRoot ? AZStd::make_optional<AZ::IO::FixedMaxPathString>(appRoot) : AZStd::nullopt;
-        }
-
-        AZStd::optional<AZ::IO::FixedMaxPathString> GetDevWriteStoragePath()
-        {
-            const char* writeStorage = AZ::Android::Utils::GetAppPublicStoragePath();
-            return writeStorage ? AZStd::make_optional<AZ::IO::FixedMaxPathString>(writeStorage) : AZStd::nullopt;
-        }
-
-        bool ConvertToAbsolutePath(const char* path, char* absolutePath, AZ::u64 maxLength)
-        {
-            if (AZ::Android::Utils::IsApkPath(path))
-            {
-                azstrcpy(absolutePath, maxLength, path);
-                return true;
-            }
-
-#ifdef PATH_MAX
-            static constexpr size_t UnixMaxPathLength = PATH_MAX;
-#else
-            // Fallback to 4096 if the PATH_MAX macro isn't defined on the Unix System
-            static constexpr size_t UnixMaxPathLength = 4096;
-#endif
-            if (!AZ::IO::PathView(path).IsAbsolute())
-            {
-                // note that realpath fails if the path does not exist and actually changes the return value
-                // to be the actual place that FAILED, which we don't want.
-                // if we fail, we'd prefer to fall through and at least use the original path.
-                char absolutePathBuffer[UnixMaxPathLength];
-                if (const char* result = realpath(path, absolutePathBuffer); result != nullptr)
-                {
-                    azstrcpy(absolutePath, maxLength, absolutePathBuffer);
-                    return true;
-                }
-            }
-            azstrcpy(absolutePath, maxLength, path);
-            return AZ::IO::PathView(absolutePath).IsAbsolute();
-        }
+        return AZ::Failure(GetEnvErrorResult{ GetEnvErrorCode::EnvNotSet });
     }
-}
+
+    bool IsEnvSet(const char* envname)
+    {
+        return std::getenv(envname) != nullptr;
+    }
+
+    bool SetEnv(const char* envname, const char* envvalue, bool overwrite)
+    {
+        return setenv(envname, envvalue, overwrite) != -1;
+    }
+
+    bool UnsetEnv(const char* envname)
+    {
+        return unsetenv(envname) != -1;
+    }
+} // namespace AZ::Utils
