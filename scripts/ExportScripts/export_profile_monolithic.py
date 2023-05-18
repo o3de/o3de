81923ee74e7d8f3cd2c860452c701123ebace4e3
T.J. Kotha <112996779+tkothadev@users.noreply.github.com>
2023-05-18 19:06:24 -0400

    added fixes for the copy command on windows. New draft only focuses on creating monolithic binaries and copying them to dest folder. asset bundling will be handled in a separate PR
    Signed-off-by: T.J. Kotha <112996779+tkothadev@users.noreply.github.com>

--- scripts/ExportScripts/export_profile_monolithic.py
+++ scripts/ExportScripts/export_profile_monolithic.py
@@ -3,7 +3,7 @@ from o3de import manifest
 import argparse
 import pathlib
 import os
-
+import shutil
 #Note: project_path and engine_path are computed before this script runs, and can be accessed via o3de_context.project_path
 #for now this script assumes an engine-centric approach, and we assume to use the source built engine for the engine_path
 
@@ -22,6 +22,10 @@ projects_path_cmake_option = f"-DLY_PROJECTS={str(o3de_context.project_path)}"
 
 project_json_data = manifest.get_project_json_data(project_path = o3de_context.project_path)
 project_name= project_json_data.get('project_name')
+
+#clean the output folder if necessary
+if os.path.isdir(args.output_path):
+    shutil.rmtree(args.output_path)
 
 
 #build non-monolithic artifacts
@@ -43,13 +47,16 @@ process_command(['cmake', '-S', '.', '-B', args.mono_build_path, '-DLY_MONOLITHI
 process_command(['cmake', '--build',  args.mono_build_path, '--target', f'{project_name}.GameLauncher', '--target', f'{project_name}.ServerLauncher', '--target', f'{project_name}.UnifiedLauncher', '--config', 'profile'])
 
 #bundle content (WIP)
-# process_command([asset_bundler_exe, f'--project-path="{str(o3de_context.project_path)}"'])
+# process_command([asset_bundler_exe, 'assetList','--assetListFile {}',f'--project-path="{str(o3de_context.project_path)}"'])
 
 #copy artifacts to output directory
 
 pc_pak_path = args.output_path / 'Cache' / 'pc'
 
 os.makedirs(pc_pak_path, exist_ok=True)
+os.makedirs(args.output_path, exist_ok=True)
 # os.makedirs(args.output_path / 'Gems' / 'AWSCore')
-process_command(['copy', str(o3de_context.project_path / 'AssetBundling' / 'Bundles' / '*.pak'), str(pc_pak_path)])
-process_command(['copy', str(args.mono_build_path / 'bin' / 'profile' / '*.*'), str(args.output_path)])
+# process_command(['copy', str(o3de_context.project_path / 'AssetBundling' / 'Bundles' / '*.pak'), str(pc_pak_path)])
+source_path = str(args.mono_build_path / 'bin' / 'profile')
+output_path = str(args.output_path)
+process_command(f'xcopy {source_path} {output_path}', cwd=o3de_context.project_path)
