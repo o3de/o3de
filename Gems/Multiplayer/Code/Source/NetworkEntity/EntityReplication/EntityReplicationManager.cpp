a8dcc01bc4c0c7a2b8a9061ba48cb53049eeabde
AMZN-Gene <genewalt@amazon.com>
2023-05-18 17:44:45 -0700

    Adding safety net to make sure the entity is not null when printing AZ_WARN on Multiplayer EntityReplicationManager::HandleEntityDeleteMessage.
    Signed-off-by: AMZN-Gene <genewalt@amazon.com>

--- Gems/Multiplayer/Code/Source/NetworkEntity/EntityReplication/EntityReplicationManager.cpp
+++ Gems/Multiplayer/Code/Source/NetworkEntity/EntityReplication/EntityReplicationManager.cpp
@@ -559,7 +559,16 @@ namespace Multiplayer
         {
             if (entityReplicator->IsMarkedForRemoval())
             {
-                AZLOG_WARN("Entity replicator for id %llu is already marked for deletion on remote host %s", static_cast<AZ::u64>(updateMessage.GetEntityId()), GetRemoteHostId().GetString().c_str());
+                AZStd::string entityName = "<Entity Name Unknown>";
+                if (AZ::Entity* entity = entityReplicator->GetEntityHandle().GetEntity())
+                {
+                    entityName = entity->GetName();
+                }
+
+                AZLOG_WARN("Entity replicator for id %llu (%s) is already marked for deletion on remote host %s",
+                    static_cast<AZ::u64>(updateMessage.GetEntityId()),
+                    entityName.c_str(),
+                    GetRemoteHostId().GetString().c_str());
                 return true;
             }
             else if (entityReplicator->OwnsReplicatorLifetime())
