1204bb586d1538adb6febebeae13af3d63581bc8
Mike Chang <changml@amazon.com>
2023-05-25 13:43:07 -0700

    Remove hardcode to GCC in Linux configuration
    Signed-off-by: Mike Chang <changml@amazon.com>

--- cmake/Platform/Linux/Configurations_linux_x86_64.cmake
+++ cmake/Platform/Linux/Configurations_linux_x86_64.cmake
@@ -23,17 +23,13 @@ if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
                 -fpie
                 -Wl,-z,relro,-z,now
                 -Wl,-z,noexecstack
-                -L$ENV{SNAP}/usr/lib/gcc/x86_64-linux-gnu/12/
                 -L$ENV{SNAP}/lib/x86_64-linux-gnu
-                -L$ENV{SNAP}/lib32/x86_64-linux-gnu
                 -L$ENV{SNAP}/usr/lib/x86_64-linux-gnu
             LINK_EXE
                 -fpie
                 -Wl,-z,relro,-z,now
                 -Wl,-z,noexecstack
-                -L$ENV{SNAP}/usr/lib/gcc/x86_64-linux-gnu/12/
                 -L$ENV{SNAP}/lib/x86_64-linux-gnu
-                -L$ENV{SNAP}/lib32/x86_64-linux-gnu
                 -L$ENV{SNAP}/usr/lib/x86_64-linux-gnu
         )
     else()
