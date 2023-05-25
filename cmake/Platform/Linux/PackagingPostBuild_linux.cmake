0e54de1b4f7c524236025467c526b8cf1e0ae001
Mike Chang <changml@amazon.com>
2023-05-25 16:23:02 -0700

    Set postbuild filename to versioned name
    Signed-off-by: Mike Chang <changml@amazon.com>

--- cmake/Platform/Linux/PackagingPostBuild_linux.cmake
+++ cmake/Platform/Linux/PackagingPostBuild_linux.cmake
@@ -11,10 +11,10 @@ include(${LY_ROOT_FOLDER}/cmake/Platform/Common/PackagingPostBuild_common.cmake)
 include(${CPACK_CODESIGN_SCRIPT})
 
 if("$ENV{O3DE_PACKAGE_TYPE}" STREQUAL "SNAP")
-    set(snap_file "${CPACK_TOPLEVEL_DIRECTORY}/${CPACK_PACKAGE_NAME}_amd64.snap")
-    set(hash_file "${CPACK_TOPLEVEL_DIRECTORY}/${CPACK_PACKAGE_NAME}_amd64.snap.sha384")
+    set(snap_file "${CPACK_TOPLEVEL_DIRECTORY}/${CPACK_PACKAGE_FILE_NAME}_amd64.snap")
+    set(hash_file "${CPACK_TOPLEVEL_DIRECTORY}/${CPACK_PACKAGE_FILE_NAME}_amd64.snap.sha384")
     file(SHA384 ${snap_file} file_checksum) # Snap asserts use SHA284
-    file(WRITE ${hash_file} "${file_checksum}  ${CPACK_PACKAGE_NAME}_amd64.snap")
+    file(WRITE ${hash_file} "${file_checksum}  ${CPACK_PACKAGE_FILE_NAME}_amd64.snap")
 elseif("$ENV{O3DE_PACKAGE_TYPE}" STREQUAL "DEB")
     set(deb_file "${CPACK_TOPLEVEL_DIRECTORY}/${CPACK_PACKAGE_FILE_NAME}.deb")
     set(hash_file "${CPACK_TOPLEVEL_DIRECTORY}/${CPACK_PACKAGE_FILE_NAME}.deb.sha256")
@@ -73,7 +73,7 @@ if(CPACK_UPLOAD_URL)
             ly_upload_to_latest(${CPACK_UPLOAD_URL} "${latest_snap_package}")
 
             # Generate a checksum file for latest and upload it
-            set(latest_hash_file "${CPACK_UPLOAD_DIRECTORY}/${CPACK_PACKAGE_NAME}_latest.deb.sha384")
+            set(latest_hash_file "${CPACK_UPLOAD_DIRECTORY}/${CPACK_PACKAGE_NAME}_latest.snap.sha384")
             file(WRITE "${latest_hash_file}" "${file_checksum}  ${latest_snap_package}")
             ly_upload_to_latest(${CPACK_UPLOAD_URL} "${latest_hash_file}")
         elseif("$ENV{O3DE_PACKAGE_TYPE}" STREQUAL "DEB")
