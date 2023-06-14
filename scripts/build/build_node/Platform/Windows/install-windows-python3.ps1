6d392b2b829440bb4e6f7382d927f0d7e2946926
Mike Chang <changml@amazon.com>
2023-06-14 16:15:47 -0700

    Add symlink to python3 for Windows
    Signed-off-by: Mike Chang <changml@amazon.com>

--- scripts/build/build_node/Platform/Windows/install-windows-python3.ps1
+++ scripts/build/build_node/Platform/Windows/install-windows-python3.ps1
@@ -7,12 +7,15 @@ SPDX-License-Identifier: Apache-2.0 OR MIT
 
 Write-Host "Install Python"
 choco install -y python3 --version=3.10.5
+$pyroot = "C:\Python310"
 
 Write-Host "Ensure Python paths are set"
 [Environment]::SetEnvironmentVariable(
     "PATH",
-    [Environment]::GetEnvironmentVariable("PATH", [EnvironmentVariableTarget]::Machine) + ";C:\Python310;C:\Python310\Scripts",
+    [Environment]::GetEnvironmentVariable("PATH", [EnvironmentVariableTarget]::Machine) + ";$pyroot;$pyroot\Scripts",
     [EnvironmentVariableTarget]::Machine)
 
 Write-Host "Installing packages" # requirements.txt hould be in the "Platforms\Common" folder
 pip3 install -r "$pwd\requirements.txt"
+
+New-Item -ItemType SymbolicLink -Path "$pyroot\python3.exe" -Target "$pyroot\python.exe" # The Jenkins scripts use `python3` (as with linux) for python calls
