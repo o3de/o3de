f4dd7b5f5d1282b65314f9440df86a1dc83e520d
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2023-06-26 11:13:10 -0700

    Add missing 'libcpre2-16-0' dependency for perl regular expressions
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- cmake/Platform/Linux/Packaging_linux.cmake
+++ cmake/Platform/Linux/Packaging_linux.cmake
@@ -41,6 +41,7 @@ elseif("$ENV{O3DE_PACKAGE_TYPE}" STREQUAL "DEB")
         libxkbcommon-dev                        # For xcb keyboard input
         libxcb-xfixes0-dev                      # For mouse input
         libxcb-xinput-dev                       # For mouse input
+        libpcre2-16-0
         zlib1g-dev
         mesa-common-dev
         libunwind-dev
