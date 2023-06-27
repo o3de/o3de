4aa280bc4ff4090b746f217e0783c5b60ffd86b6
Martin Winter <102576959+martinwinter-huawei@users.noreply.github.com>
2023-06-27 19:00:32 +0200

    Multi-device base resources (#16202)
    Signed-off-by: Martin Winter <martin.winter@huawei.com>
    Co-authored-by: Joerg H. Mueller <joerg.mueller@huawei.com>

--- Gems/Atom/RHI/Code/atom_rhi_public_files.cmake
+++ Gems/Atom/RHI/Code/atom_rhi_public_files.cmake
@@ -51,10 +51,12 @@ set(FILES
     Include/Atom/RHI/Device.h
     Include/Atom/RHI/DeviceBusTraits.h
     Include/Atom/RHI/DeviceObject.h
+    Include/Atom/RHI/MultiDeviceObject.h
     Include/Atom/RHI/CommandQueue.h
     Include/Atom/RHI/ValidationLayer.h
     Source/RHI/Device.cpp
     Source/RHI/DeviceObject.cpp
+    Source/RHI/MultiDeviceObject.cpp
     Source/RHI/CommandQueue.cpp
     Source/RHI/ValidationLayer.cpp
     Include/Atom/RHI/Factory.h
@@ -133,13 +135,17 @@ set(FILES
     Source/RHI/QueryPool.cpp
     Source/RHI/QueryPoolSubAllocator.cpp
     Include/Atom/RHI/Resource.h
+    Include/Atom/RHI/MultiDeviceResource.h
     Include/Atom/RHI/ResourceInvalidateBus.h
     Include/Atom/RHI/ResourceView.h
     Source/RHI/Resource.cpp
+    Source/RHI/MultiDeviceResource.cpp
     Source/RHI/ResourceView.cpp
     Include/Atom/RHI/ResourcePool.h
+    Include/Atom/RHI/MultiDeviceResourcePool.h
     Include/Atom/RHI/ResourcePoolDatabase.h
     Source/RHI/ResourcePool.cpp
+    Source/RHI/MultiDeviceResourcePool.cpp
     Source/RHI/ResourcePoolDatabase.cpp
     Include/Atom/RHI/MemoryAllocation.h
     Include/Atom/RHI/MemorySubAllocator.h
