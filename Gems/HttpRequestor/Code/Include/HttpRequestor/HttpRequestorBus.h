4ef37ed3eb4106427ccd4bb1934936c6a01c4273
AMZN-Gene <genewalt@amazon.com>
2023-06-28 12:18:41 -0700

    Adding code comment to LastRoundTripTime
    Signed-off-by: AMZN-Gene <genewalt@amazon.com>

--- Gems/HttpRequestor/Code/Include/HttpRequestor/HttpRequestorBus.h
+++ Gems/HttpRequestor/Code/Include/HttpRequestor/HttpRequestorBus.h
@@ -75,6 +75,9 @@ namespace HttpRequestor
             const AZStd::string& body,
             const TextCallback& callback) = 0;
 
+        //! Receive the round trip time of the last RESTful call made to a HTTP(s) endpoint.
+        //! Call this method from inside the supplied callback to get the round trip time of the original request.
+        //! @return The round trip time in milliseconds.
         virtual AZStd::chrono::milliseconds LastRoundTripTime() const = 0;
     };
 
