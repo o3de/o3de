83a355db98d5579d49ce771e71b7d1d2455d422c
Akio Gaule <10719597+akioCL@users.noreply.github.com>
2023-06-28 17:36:35 -0400

    Minor typos
    Signed-off-by: Akio Gaule <10719597+akioCL@users.noreply.github.com>

--- Gems/Atom/RHI/Vulkan/Code/Source/RHI/Instance.cpp
+++ Gems/Atom/RHI/Vulkan/Code/Source/RHI/Instance.cpp
@@ -126,9 +126,9 @@ namespace AZ
             m_instanceCreateInfo.enabledExtensionCount = static_cast<uint32_t>(m_descriptor.m_requiredExtensions.size());
             m_instanceCreateInfo.ppEnabledExtensionNames = m_descriptor.m_requiredExtensions.data();
 
-            //For instance creation/destruction use nullptr for VkAllocationCallbacks* as using VkSystemAllocator::Get() crashes RenderDoc when used with openxr
-            //enabled projects. We think its because RenderDoc is maybe injecting something when doing allocations. Using nullptr when USE_RENDERDOC or enableRenderDoc
-            //is enabled is another option but it will not work for Android easily and will require further work, not to mention manually enabling this for Android renderdoc.
+            // For instance creation/destruction use nullptr for VkAllocationCallbacks* as using VkSystemAllocator::Get() crashes RenderDoc when used with openxr
+            // enabled projects. We think it's because RenderDoc is maybe injecting something when doing allocations. Using nullptr when USE_RENDERDOC or enableRenderDoc
+            // is enabled is another option but it will not work for Android easily and will require further work, not to mention manually enabling this for Android RenderDoc.
             VkResult result = m_context.CreateInstance(&m_instanceCreateInfo, nullptr, &m_instance);
 
             if (validation != RHI::ValidationMode::Disabled &&
