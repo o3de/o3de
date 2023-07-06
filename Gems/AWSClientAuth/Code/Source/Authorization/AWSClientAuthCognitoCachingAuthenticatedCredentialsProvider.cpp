5e246d695925b092edfb5016c7c6c8f3521f57fe
AMZN-Gene <genewalt@amazon.com>
2023-07-06 13:51:04 -0700

    AWS spam reduction. Avoid making AWS request if the Cognito identity pool was never set
    Signed-off-by: AMZN-Gene <genewalt@amazon.com>

--- Gems/AWSClientAuth/Code/Source/Authorization/AWSClientAuthCognitoCachingAuthenticatedCredentialsProvider.cpp
+++ Gems/AWSClientAuth/Code/Source/Authorization/AWSClientAuthCognitoCachingAuthenticatedCredentialsProvider.cpp
@@ -43,10 +43,17 @@ namespace AWSClientAuth
         if (!identityRepository.HasIdentityId())
         {
             auto accountId = identityRepository.GetAccountId();
-            auto identityPoolId = identityRepository.GetIdentityPoolId();
+            const Aws::String identityPoolId = identityRepository.GetIdentityPoolId();
 
             Aws::CognitoIdentity::Model::GetIdRequest getIdRequest;
-            getIdRequest.SetIdentityPoolId(identityPoolId);
+            // Only SetIdentityPoolId if there's actually a pool id.
+            // SetIdentityPoolId will cause AWS to think there's an id even it's empty.
+            // This leads AWS API calls to pass back a warning about an "invalid" pool id,
+            //     rather than (properly) passing back an error about not having a pool id.
+            if (!identityPoolId.empty())
+            {
+                getIdRequest.SetIdentityPoolId(identityPoolId);
+            }
 
             if (!accountId.empty()) // new check
             {
