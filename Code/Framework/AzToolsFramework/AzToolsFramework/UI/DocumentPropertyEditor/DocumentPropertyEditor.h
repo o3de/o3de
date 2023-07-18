2b878c4406a034a88ca0e890aba8c2e6e353fc99
Alex Montgomery <alexmont@amazon.com>
2023-07-17 22:44:14 -0700

    - apply expansion state in-place instead of doing a costly reset
    Signed-off-by: Alex Montgomery <alexmont@amazon.com>

--- Code/Framework/AzToolsFramework/AzToolsFramework/UI/DocumentPropertyEditor/DocumentPropertyEditor.h
+++ Code/Framework/AzToolsFramework/AzToolsFramework/UI/DocumentPropertyEditor/DocumentPropertyEditor.h
@@ -123,6 +123,7 @@ namespace AzToolsFramework
 
         void SetExpanded(bool expanded, bool recurseToChildRows = false);
         bool IsExpanded() const;
+        void ApplyExpansionState(const AZ::Dom::Path& rowPath, DocumentPropertyEditor* rowDPE);
 
         bool HasChildRows() const;
         int GetLevel() const;
@@ -206,6 +207,7 @@ namespace AzToolsFramework
         bool HasSavedExpanderStateForRow(const AZ::Dom::Path& rowPath) const;
         bool ShouldEraseExpanderStateWhenRowRemoved() const;
         void RemoveExpanderStateForRow(const AZ::Dom::Path& rowPath);
+        void ApplyExpansionStates();
         void ExpandAll();
         void CollapseAll();
 
