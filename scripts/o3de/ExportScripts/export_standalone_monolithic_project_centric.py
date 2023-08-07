333b9b4fde81ddb725250bd5752f4e8f2e39f0f0
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2023-08-07 09:44:53 -0700

    Additional linux fixes
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- scripts/o3de/ExportScripts/export_standalone_monolithic_project_centric.py
+++ scripts/o3de/ExportScripts/export_standalone_monolithic_project_centric.py
@@ -7,6 +7,7 @@
 #
 
 import argparse
+import fnmatch
 import glob
 import logging
 import os
@@ -49,54 +50,78 @@ $ <engine-path>\scripts\o3de.bat  export-project --export-scripts ExportScripts\
 # For more information on the available parameters for this script, check the parse_arguments function defined at the bottom of the file
 
 # Account for some windows-specific attributes
-if platform.system().lower()=='windows':
+if platform.system().lower() == 'windows':
     EXECUTABLE_EXTENSION = '.exe'
-    ADDITION_BUILD_ARGS = ['--', '/m', '/ nologo']
     O3DE_SCRIPT_NAME = 'o3de.bat'
+    GENERATOR = None
+    CMAKE_GENERATE_OPTIONS = None
 else:
     EXECUTABLE_EXTENSION = ""
-    ADDITION_BUILD_ARGS = []
     O3DE_SCRIPT_NAME = 'o3de.sh'
+    GENERATOR = "Ninja Multi-Config"
+    CMAKE_GENERATE_OPTIONS = ['-DLY_STRIP_DEBUG_SYMBOLS=ON']
 
-def build_non_monolithic_code_modules( project_path: pathlib.Path,
-                                        non_mono_build_path: pathlib.Path,
-                                        force_rebuild_all = False):
 
-    #build the pre_requisite tools if needed
+def build_non_monolithic_code_modules(project_path: pathlib.Path,
+                                      non_mono_build_path: pathlib.Path,
+                                      force_rebuild_all=False,
+                                      additional_cmake_build_args=[]):
+
+    # Build the pre_requisite tools if needed
     os.makedirs(non_mono_build_path, exist_ok=True)
 
     bundler_path = get_asset_bundler_batch_path(non_mono_build_path)
     processor_path = get_asset_processor_batch_path(non_mono_build_path)
 
-    process_command(["cmake", "-B", non_mono_build_path, "-S", project_path])
+    cmake_configure_command = ["cmake", "-B", non_mono_build_path, "-S", project_path]
+    if GENERATOR:
+        cmake_configure_command.extend(["-G", GENERATOR])
+    if CMAKE_GENERATE_OPTIONS:
+        cmake_configure_command.extend(CMAKE_GENERATE_OPTIONS)
+
+    process_command(cmake_configure_command)
 
     if not processor_path.exists() or force_rebuild_all:
-        process_command(["cmake", "--build", non_mono_build_path, "--target", "AssetProcessorBatch", "--config", "profile"] + ADDITION_BUILD_ARGS)
+        process_command(["cmake", "--build", non_mono_build_path, "--target", "AssetProcessorBatch", "--config", "profile"] + additional_cmake_build_args)
     if not bundler_path.exists() or force_rebuild_all:
-        process_command(["cmake", "--build", non_mono_build_path, "--target", "AssetBundlerBatch", "--config", "profile"] + ADDITION_BUILD_ARGS)
+        process_command(["cmake", "--build", non_mono_build_path, "--target", "AssetBundlerBatch", "--config", "profile"] + additional_cmake_build_args)
 
-def build_monolithic_code_modules( project_path: pathlib.Path,
-                        project_name: str,
-                        build_config: str,
-                        mono_build_path: pathlib.Path,
-                        should_build_game_launcher: bool,
-                        should_build_server_launcher: bool,
-                        should_build_unified_launcher: bool,
-                        allow_registry_overrides: bool):
-    
-    process_command(["cmake", "-B", mono_build_path, "-S", project_path, "-DLY_MONOLITHIC_GAME=1",
-                     f"-DALLOW_SETTINGS_REGISTRY_DEVELOPMENT_OVERRIDES={'0' if not allow_registry_overrides else '1'}"])
+
+def build_monolithic_code_modules(project_path: pathlib.Path,
+                                  project_name: str,
+                                  build_config: str,
+                                  mono_build_path: pathlib.Path,
+                                  should_build_game_launcher: bool,
+                                  should_build_server_launcher: bool,
+                                  should_build_unified_launcher: bool,
+                                  allow_registry_overrides: bool,
+                                  additional_cmake_build_args:List[str]):
+
+    if not (should_build_server_launcher or should_build_game_launcher or should_build_unified_launcher):
+        # logger.info("Skipping build for the launchers")
+        return
+
+    cmake_configure_command = ["cmake", "-B", mono_build_path, "-S", project_path]
+    if GENERATOR:
+        cmake_configure_command.extend(["-G", GENERATOR])
+    if CMAKE_GENERATE_OPTIONS:
+        cmake_configure_command.extend(CMAKE_GENERATE_OPTIONS)
+    cmake_configure_command.extend(["-DLY_MONOLITHIC_GAME=1",
+                                    f"-DALLOW_SETTINGS_REGISTRY_DEVELOPMENT_OVERRIDES={'0' if not allow_registry_overrides else '1'}"])
+    process_command(cmake_configure_command)
 
     os.makedirs(mono_build_path, exist_ok=True)
+    mono_build_args = ["cmake", "--build", mono_build_path, "--config", build_config, "--target"]
     if should_build_server_launcher:
-
-        process_command(["cmake", "--build", mono_build_path, "--target", f"{project_name}.ServerLauncher", "--config", build_config] + ADDITION_BUILD_ARGS)
-
+        mono_build_args.append(f"{project_name}.ServerLauncher")
+        mono_build_args.append(f"{project_name}.HeadlessServerLauncher")
     if should_build_game_launcher:
-        process_command(["cmake", "--build", mono_build_path, "--target", f"{project_name}.GameLauncher", "--config", build_config] + ADDITION_BUILD_ARGS)
-    
+        mono_build_args.append(f"{project_name}.GameLauncher")
     if should_build_unified_launcher:
-        process_command(["cmake", "--build", mono_build_path, "--target", f"{project_name}.UnifiedLauncher", "--config", build_config] + ADDITION_BUILD_ARGS)
+        mono_build_args.append(f"{project_name}.UnifiedLauncher")
+
+    mono_build_args.extend(additional_cmake_build_args)
+    process_command(mono_build_args)
 
 
 def kill_existing_processes(project_name: str):
@@ -122,8 +147,9 @@ def kill_existing_processes(project_name: str):
 
 def process_assets( project_name: str,
                     project_path: pathlib.Path,
-                    non_mono_build_path: pathlib.Path):
-    build_non_monolithic_code_modules(project_path, non_mono_build_path)
+                    non_mono_build_path: pathlib.Path,
+                    additional_cmake_build_args:List[str]):
+    build_non_monolithic_code_modules(project_path, non_mono_build_path, additional_cmake_build_args)
     process_command(["cmake", "--build", non_mono_build_path, "--target", f"{project_name}.Assets", "--config", "profile"])
 
 def get_asset_processor_batch_path(non_mono_build_path: pathlib.Path):
@@ -190,7 +216,8 @@ def setup_launcher_layout_directory(project_path: pathlib.Path,
                                      bundles_to_copy: List[pathlib.Path],
                                      project_file_patterns_to_copy: List[str],
                                      archive_output_format: str = "none",
-                                     logger: logging.Logger|None = None):
+                                     logger: logging.Logger|None = None,
+                                     ignore_file_patterns:List[str] = []):
     
     if output_path.exists():
         shutil.rmtree(output_path)
@@ -207,7 +234,13 @@ def setup_launcher_layout_directory(project_path: pathlib.Path,
         if file_path.is_dir():
             shutil.copytree(file, output_path / file_path.name, dirs_exist_ok=True)
         else:
-            shutil.copy(file, output_path)
+            skip_file=False
+            for ignore_file_pattern in ignore_file_patterns:
+                if fnmatch.fnmatch(file, ignore_file_pattern):
+                    skip_file=True
+                    break
+            if not skip_file:
+                shutil.copy(file, output_path)
 
     for project_file_pattern in project_file_patterns_to_copy:
         for file in glob.glob(str(pathlib.PurePath(project_path / project_file_pattern))):
@@ -240,7 +273,8 @@ def export_standalone_monolithic_project_centric(project_path: pathlib.Path,
                                                 non_mono_build_path: pathlib.Path|None =None,
                                                 mono_build_path: pathlib.Path|None =None,
                                                 archive_output_format: str = "none",
-                                                logger: logging.Logger|None = None):
+                                                logger: logging.Logger|None = None,
+                                                additional_cmake_build_args: List[str] = []):
     if not logger:
         logger = logging.getLogger()
         logger.setLevel(logging.ERROR)
@@ -257,7 +291,7 @@ def export_standalone_monolithic_project_centric(project_path: pathlib.Path,
 
     if should_build_all_code:
         if should_build_non_mono_tools:
-            build_non_monolithic_code_modules(project_path, non_mono_build_path, force_rebuild_all=True)
+            build_non_monolithic_code_modules(project_path, non_mono_build_path, force_rebuild_all=True, additional_cmake_build_args=additional_cmake_build_args)
         
         build_tools = [get_asset_bundler_batch_path(non_mono_build_path),get_asset_processor_batch_path(non_mono_build_path)]
         tools_missing = [b for b in build_tools if not b.exists()]
@@ -268,11 +302,11 @@ def export_standalone_monolithic_project_centric(project_path: pathlib.Path,
 
         build_monolithic_code_modules(project_path, project_name, build_config, mono_build_path,
                                       should_build_game_launcher, should_build_server_launcher, should_build_unified_launcher,
-                                      allow_registry_overrides)
+                                      allow_registry_overrides, additional_cmake_build_args)
     
     expected_bundles_path = None
     if should_build_all_assets:
-        process_assets(project_name, project_path, non_mono_build_path)
+        process_assets(project_name, project_path, non_mono_build_path, additional_cmake_build_args)
 
         expected_bundles_path = bundle_assets(project_path,
                                             selected_platform,
@@ -281,15 +315,21 @@ def export_standalone_monolithic_project_centric(project_path: pathlib.Path,
                                             preferred_asset_list_path,
                                             max_bundle_size)
     else:
-        expected_bundles_path = project_path / "AssetBundling/Bundles"    
-    for should_do, expected_output_dir, project_file_patterns in zip(
+        expected_bundles_path = project_path / "AssetBundling/Bundles"
+    zip_pattern_game_launcher = [f'{project_name}.GameLauncher{EXECUTABLE_EXTENSION}']
+    zip_pattern_server_launcher = [f'{project_name}.ServerLauncher{EXECUTABLE_EXTENSION}', f'{project_name}.ServerLauncher{EXECUTABLE_EXTENSION}']
+    zip_pattern_unified_launcher = [f'{project_name}.UnifiedLauncher{EXECUTABLE_EXTENSION}']
+    for should_do, expected_output_dir, project_file_patterns, ignore_patterns in zip(
                                               [should_build_game_launcher, should_build_server_launcher, should_build_unified_launcher],
                                               [output_path / f'{project_name}GamePackage',
                                                output_path / f'{project_name}ServerPackage',
                                                output_path / f'{project_name}UnifiedPackage'],
-                                              [project_file_patterns_to_copy + game_project_file_patterns_to_copy,
-                                               project_file_patterns_to_copy + server_project_file_patterns_to_copy,
-                                               project_file_patterns_to_copy + game_project_file_patterns_to_copy + server_project_file_patterns_to_copy]):
+                                              [project_file_patterns_to_copy + game_project_file_patterns_to_copy + zip_pattern_game_launcher,
+                                               project_file_patterns_to_copy + server_project_file_patterns_to_copy + zip_pattern_server_launcher,
+                                               project_file_patterns_to_copy + game_project_file_patterns_to_copy + server_project_file_patterns_to_copy + zip_pattern_unified_launcher],
+                                              [ ['*.ServerLauncher', '*.HeadlessServerLauncher', '*.UnifiedLauncher'],
+                                                ['*.GameLauncher', '*.UnifiedLauncher'],
+                                                ['*.ServerLauncher', '*.HeadlessServerLauncher', '*.GameLauncher']]):
         if should_do:
             setup_launcher_layout_directory(project_path,
                                          expected_output_dir,
@@ -299,7 +339,8 @@ def export_standalone_monolithic_project_centric(project_path: pathlib.Path,
                                          [expected_bundles_path / f'game_{selected_platform}.pak', expected_bundles_path / f'engine_{selected_platform}.pak'],
                                          project_file_patterns,
                                          archive_output_format,
-                                         logger)
+                                         logger,
+                                         ignore_patterns)
     
 
 
@@ -307,10 +348,27 @@ def export_standalone_monolithic_project_centric(project_path: pathlib.Path,
 #This code is only run by the 'export-project' O3DE CLI command
 if "o3de_context" in globals():
     def parse_args(unprocessed_args: List[str]):
+        # Check for any additional platform-specific cmake flags to pass down to the cmake build steps that are
+        # processed during the project export process
+        export_process_args = []
+        cmake_custom_build_args = []
+        additional_arg_marker_found = False
+        for arg in unprocessed_args:
+            if arg == '--' and not additional_arg_marker_found:
+                additional_arg_marker_found = True
+                continue
+            if additional_arg_marker_found:
+                cmake_custom_build_args.append(arg)
+            else:
+                export_process_args.append(arg)
+
         parser = argparse.ArgumentParser(
                     prog='Exporter for standalone builds',
                     description="Exports a project as standalone to the desired output directory with release layout. "
-                                "In order to use this script, the engine and project must be setup and registered beforehand. ")
+                                "In order to use this script, the engine and project must be setup and registered beforehand. ",
+                    epilog="Note: You can pass additional arguments to the cmake command to build the projects during the export"
+                           "process by adding a '--' between the arguments for this script and the additional arguments you would"
+                           "like to pass down to cmake build.")
         parser.add_argument('-out', '--output-path', type=pathlib.Path, required=True, help='Path that describes the final resulting Release Directory path location.')
         parser.add_argument('-cfg', '--config', type=str, default='profile', choices=['release', 'profile'],
                                 help='The CMake build configuration to use when building project binaries.  Tool binaries built with this script will always be built with the profile configuration.')
@@ -346,10 +404,12 @@ if "o3de_context" in globals():
         parser.add_argument('-nounified', '--no-unified-launcher', action='store_true', help='This flag skips building the Unified Launcher on a platform if not needed.')
         parser.add_argument('-pl', '--platform', type=str, default=exp.get_default_asset_platform(), choices=['pc', 'linux', 'mac'])
         parser.add_argument('-q', '--quiet', action='store_true', help='Suppresses logging information unless an error occurs.')
+
+        parsed_args, _ = parser.parse_known_args(unprocessed_args)
         
-        return parser.parse_args(unprocessed_args)
+        return parsed_args, cmake_custom_build_args
     
-    args = parse_args(o3de_context.args)
+    args, cmake_extra_build_args = parse_args(o3de_context.args)
     if args.quiet:
         o3de_logger.setLevel(logging.ERROR)
     export_standalone_monolithic_project_centric(o3de_context.project_path,
@@ -372,4 +432,5 @@ if "o3de_context" in globals():
                                                 args.non_mono_build_path,
                                                 args.mono_build_path, 
                                                 args.archive_output,
-                                                o3de_logger)
+                                                o3de_logger,
+                                                cmake_extra_build_args)
