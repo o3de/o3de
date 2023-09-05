3ff6710f7218a82a3069d488657ed3ad0938443a
Olex Lozitskiy <5432499+AMZN-Olex@users.noreply.github.com>
2023-09-05 14:38:04 -0500

    Added a license header for the bat packaging script
    Signed-off-by: Olex Lozitskiy <5432499+AMZN-Olex@users.noreply.github.com>

--- Templates/DefaultProject/Template/package.bat
+++ Templates/DefaultProject/Template/package.bat
@@ -1,3 +1,12 @@
+REM --------------------------------------------------------------------------------------------------
+REM 
+REM Copyright (c) Contributors to the Open 3D Engine Project.
+REM For complete copyright and license terms please see the LICENSE at the root of this distribution.
+REM 
+REM SPDX-License-Identifier: Apache-2.0 OR MIT
+REM 
+REM --------------------------------------------------------------------------------------------------
+
 set O3DE_PATH=${EnginePath}
 set O3DE_PROJECT_PATH=${ProjectPath}
 set O3DE_PROJECT_SEEDLIST=%O3DE_PROJECT_PATH%\AssetBundling\SeedLists\Example.seed
