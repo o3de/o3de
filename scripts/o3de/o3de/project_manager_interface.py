1c88c793d9fa1cc0b2d75b52e50c0230fe965816
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2023-09-08 00:12:38 +0000

    Update to use the registered `--this-engine` path as the engine path if none can be extracted from the project path, if any
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- scripts/o3de/o3de/project_manager_interface.py
+++ scripts/o3de/o3de/project_manager_interface.py
@@ -308,7 +308,14 @@ def get_all_gem_infos(project_path: pathlib.Path or None) -> list:
     # it's easier to determine which gems are engine gems here rather than in c++
     # because the project might be using a different engine than the one Project Manager is
     # running out of
-    engine_path = manifest.get_project_engine_path(project_path=project_path) if project_path else manifest.get_this_engine_path() 
+
+    # Attempt to get the engine path from the project path if possible. If not, then default
+    # to attempting to get engine path from the registered '--this-engine' value from the manifest
+    engine_path = None
+    if project_path:
+        engine_path = manifest.get_project_engine_path(project_path=project_path)
+    if not engine_path:
+        engine_path = manifest.get_this_engine_path()
     if not engine_path:
         logger.error("Failed to get engine path for gem info retrieval")
         return [] 
@@ -435,9 +442,16 @@ def get_gem_infos_from_all_repos(project_path:pathlib.Path = None, enabled_only:
     if not remote_gem_json_data_list:
         return list()
 
-    engine_path = manifest.get_project_engine_path(project_path=project_path) if project_path else manifest.get_this_engine_path() 
+    # Attempt to get the engine path from the project path if possible. If not, then default
+    # to attempting to get engine path from the registered '--this-engine' value from the manifest
+    engine_path = None
+    if project_path:
+        engine_path = manifest.get_project_engine_path(project_path=project_path)
     if not engine_path:
-        logger.error("Failed to get engine path for remote gem compatibility checks")
+        engine_path = manifest.get_this_engine_path()
+
+    if not engine_path:
+        logger.error("Failed to get engine path for remote gem compatibility checksi!")
         return list() 
 
     engine_json_data = manifest.get_engine_json_data(engine_path=engine_path)
