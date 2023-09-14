eee13a92da1689444f7ee9a76bce60ab8430bc9e
Mike Chang <changml@amazon.com>
2023-09-14 11:12:14 -0700

    Further cleanup on postbuild cmake
    Signed-off-by: Mike Chang <changml@amazon.com>

--- cmake/Platform/Linux/PackagingPostBuild_linux.cmake
+++ cmake/Platform/Linux/PackagingPostBuild_linux.cmake
@@ -10,26 +10,30 @@ file(REAL_PATH "${CPACK_SOURCE_DIR}/.." LY_ROOT_FOLDER)
 include(${LY_ROOT_FOLDER}/cmake/Platform/Common/PackagingPostBuild_common.cmake)
 include(${CPACK_CODESIGN_SCRIPT})
 
-# Set common variables by package type
+# Init common variables
 set(file_name "${CPACK_PACKAGE_FILE_NAME}")
 set(checksum ${CPACK_PACKAGE_CHECKSUM})
-set(pack_file "${CPACK_TOPLEVEL_DIRECTORY}/${file_name}.${ext}")
-if("$ENV{O3DE_PACKAGE_TYPE}" STREQUAL "SNAP")
+string(TOLOWER "$ENV{O3DE_PACKAGE_TYPE}" ext)
+
+# Set package variables for package types
+if(ext STREQUAL "snap")
     if(CPACK_SNAP_DISTRO)
         set(file_name "${CPACK_PACKAGE_FILE_NAME}_${CPACK_SNAP_DISTRO}")
     endif()
-    set(ext "snap")
     set(checksum SHA384) # Snap asserts use SHA384
-elseif("$ENV{O3DE_PACKAGE_TYPE}" STREQUAL "DEB")
-    set(ext "deb")
+elseif(ext STREQUAL "deb")
+    # Retain default variables, placeholder for future logic
 else()
      message(FATAL_ERROR "No valid packaging type defined in O3DE_PACKAGE_TYPE. Stopping build")
 endif()
 
+set(pack_file "${CPACK_TOPLEVEL_DIRECTORY}/${file_name}.${ext}")
+
 # Generate checksum file
-set(hash_file "${pack_file}.${checksum}")
+string(TOLOWER ${checksum} checkext)
+set(hash_file "${pack_file}.${checkext}")
 file(${checksum} ${pack_file} file_checksum) 
-file(WRITE ${hash_file} "${file_checksum} ${file_name}.${ext}")
+file(WRITE ${hash_file} "${file_checksum} ${pack_file}")
 
 if(CPACK_UPLOAD_URL)
     # use the internal default path if somehow not specified from cpack_configure_downloads
@@ -37,11 +41,11 @@ if(CPACK_UPLOAD_URL)
         set(CPACK_UPLOAD_DIRECTORY ${CPACK_PACKAGE_DIRECTORY}/CPackUploads)
     endif()
 
-    if("$ENV{O3DE_PACKAGE_TYPE}" STREQUAL "DEB")
+    if(ext STREQUAL "deb")
         # Sign and regenerate checksum
         ly_sign_binaries("${pack_file}" "")
         file(${checksum} ${pack_file} file_checksum)
-        file(WRITE ${hash_file} "${file_checksum}  ${pack_file}")
+        file(WRITE ${hash_file} "${file_checksum} ${pack_file}")
     endif()
 
     # Copy the artifacts intended to be uploaded to a remote server into the folder specified
@@ -51,7 +55,7 @@ if(CPACK_UPLOAD_URL)
     file(REMOVE_RECURSE ${CPACK_UPLOAD_DIRECTORY})
     file(GLOB _artifacts 
         "${CPACK_TOPLEVEL_DIRECTORY}/*.${ext}" 
-        "${CPACK_TOPLEVEL_DIRECTORY}/*.${checksum}"
+        "${CPACK_TOPLEVEL_DIRECTORY}/*.${checkext}"
         "${LY_ROOT_FOLDER}/scripts/signer/Platform/Linux/*.gpg"
         "${CPACK_3P_LICENSE_FILE}"
         "${CPACK_3P_MANIFEST_FILE}"
@@ -64,10 +68,10 @@ if(CPACK_UPLOAD_URL)
     ly_upload_to_url(
         ${CPACK_UPLOAD_URL}
         ${CPACK_UPLOAD_DIRECTORY}
-        ".*(.${ext}|.${checksum}|.gpg|.txt|.json)$"
+        ".*(.${ext}|.${checkext}|.gpg|.txt|.json)$"
     )
 
-    # for auto tagged builds, we will also upload a second copy of the package
+    # for auto tagged builds, we will also upload a second copy of the package/checksum
     # to a special "Latest" folder under the branch in place of the commit date/hash
     if(CPACK_AUTO_GEN_TAG)       
         set(latest_pack_file "${CPACK_UPLOAD_DIRECTORY}/${file_name}_latest.${ext}")
@@ -77,9 +81,11 @@ if(CPACK_UPLOAD_URL)
         )
         ly_upload_to_latest(${CPACK_UPLOAD_URL} "${latest_pack_file}")
 
-        # Generate a checksum file for latest and upload it
-        set(latest_hash_file "${latest_pack_file}.${checksum}")
-        file(WRITE "${latest_hash_file}" "${file_checksum}  ${latest_pack_file}")
+        set(latest_hash_file "${latest_pack_file}.${checkext}")
+        file(COPY_FILE
+            ${hash_file}
+            ${latest_hash_file}
+        )
         ly_upload_to_latest(${CPACK_UPLOAD_URL} "${latest_hash_file}")
     endif()
 endif()
