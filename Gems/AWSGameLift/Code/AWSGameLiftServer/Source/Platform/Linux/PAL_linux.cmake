fee43ea4dd08f0eb5f62ea4790d500fd23d9dd75
Gene Walters <genewalt@amazon.com>
2023-09-26 18:05:22 -0700

    WiP. Updating Windows and Linux GameLift Server SDK from 5.0 -> 5.1.1. Built windows and updated hash; still need to generate Linux
    Signed-off-by: Gene Walters <genewalt@amazon.com>

--- Gems/AWSGameLift/Code/AWSGameLiftServer/Source/Platform/Linux/PAL_linux.cmake
+++ Gems/AWSGameLift/Code/AWSGameLiftServer/Source/Platform/Linux/PAL_linux.cmake
@@ -10,11 +10,11 @@ set(PAL_TRAIT_AWSGAMELIFTSERVER_SUPPORTED TRUE)
 
 if(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "x86_64")
 
-    ly_associate_package(PACKAGE_NAME AWSGameLiftServerSDK-5.0.0-rev3-linux            TARGETS AWSGameLiftServerSDK        PACKAGE_HASH 2e51bbd06c67fe6f7dda99b8b59a742a0a6ca73151992be705de474d5253ed50)
+    ly_associate_package(PACKAGE_NAME AWSGameLiftServerSDK-5.1.1-rev1-linux            TARGETS AWSGameLiftServerSDK        PACKAGE_HASH 2e51bbd06c67fe6f7dda99b8b59a742a0a6ca73151992be705de474d5253ed50)
 
 elseif(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "aarch64")
 
-    ly_associate_package(PACKAGE_NAME AWSGameLiftServerSDK-5.0.0-rev3-linux-aarch64    TARGETS AWSGameLiftServerSDK        PACKAGE_HASH 7513a501f074544e05bc49c66849648c1cb8ae544a6c397d94b1d967e9f362ac)
+    ly_associate_package(PACKAGE_NAME AWSGameLiftServerSDK-5.1.1-rev1-linux-aarch64    TARGETS AWSGameLiftServerSDK        PACKAGE_HASH 7513a501f074544e05bc49c66849648c1cb8ae544a6c397d94b1d967e9f362ac)
 
 else()
 
