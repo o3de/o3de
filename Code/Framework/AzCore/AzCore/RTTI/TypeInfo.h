6d34518a3f585c93b4f765b55c835c64cb08889d
lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
2023-10-24 20:15:14 -0500

    Adding AZTypeInfo for AZStd::span
    Part of the change is to add a forwarding header to allow AZStd::span
    class to be referenced without needing to pull in the primary `span.h`
    header containing the class declaration/definition.
    
    Signed-off-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>

--- Code/Framework/AzCore/AzCore/RTTI/TypeInfo.h
+++ Code/Framework/AzCore/AzCore/RTTI/TypeInfo.h
@@ -10,6 +10,7 @@
 #include <AzCore/RTTI/TypeInfoSimple.h>
 #include <AzCore/RTTI/TemplateInfo.h>
 #include <AzCore/std/containers/array.h>
+#include <AzCore/std/containers/span_fwd.h>
 #include <AzCore/Preprocessor/Enum.h>
 #include <AzCore/std/typetraits/is_enum.h>
 #include <AzCore/std/typetraits/remove_cvref.h>
@@ -507,6 +508,10 @@ namespace AZStd
 
     // GetO3deTypeName/GetO3deTypeId overload for AZStd::function
     AZ_TYPE_INFO_INTERNAL_SPECIALIZED_TEMPLATE_PREFIX_UUID(AZStd::function, "AZStd::function", "{C9F9C644-CCC3-4F77-A792-F5B5DBCA746E}", AZ_TYPE_INFO_INTERNAL_TYPENAME);
+
+    // GetO3deTypeName/GetO3deTypeId overload for AZStd::span<T, Extent>
+    // Note the Extent is not taking into account for template paramater is not taking into account and only the type T argument
+    AZ_TYPE_INFO_INTERNAL_SPECIALIZED_TEMPLATE_PREFIX_UUID(AZStd::span, "AZStd::span", "{2FCDBAB3-45E0-4159-A91D-FD1D37056C0F}", AZ_TYPE_INFO_INTERNAL_TYPENAME);
 
     // Add declarations of GetO3deTypeName and GetO3deTypeId for the basic string templates
     // In TypeInfo.cpp the implementation for common string specializations are added
