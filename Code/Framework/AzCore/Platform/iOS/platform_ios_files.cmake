903936edc736ef3a0ba8ea1d7668a01c185d3841
lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
2023-10-30 19:30:52 -0500

    Adding implementation of a no-heap allocation Command Line Parser (#16965)
    * Adding implementation of a no-heap allocation Command Line Parser
    
    The Command Line Parser supports a user supplied callback function which
    is passed each parsed command line option paired with it's value or a
    positional parameter when the option field is empty.
    
    Signed-off-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
    
    * Fixed grammar and typos in the CommandLine parser comments.
    
    Added clarifying comment to the `ParseCommandLine` function to indicate
    that the first command line argument at index 0 is also parsed and not
    skipped.
    
    Signed-off-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
    
    * Fix parsing scenario when final CLI token is an option token
    
    When the final command line token is an option token, special case
    handling is needed to ensure the command line entry callback is invoked
    for it, instead of deferring until the next iteration of `ParseArgument` which will not happen in that case.
    
    Signed-off-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>

--- Code/Framework/AzCore/Platform/iOS/platform_ios_files.cmake
+++ Code/Framework/AzCore/Platform/iOS/platform_ios_files.cmake
@@ -57,6 +57,7 @@ set(FILES
     ../Common/UnixLike/AzCore/PlatformIncl_UnixLike.h
     ../Common/UnixLike/AzCore/Platform_UnixLike.cpp
     AzCore/PlatformIncl_Platform.h
+    ../Common/UnixLike/AzCore/Settings/CommandLineParser_UnixLike.cpp
     ../Common/UnixLike/AzCore/Socket/AzSocket_fwd_UnixLike.h
     ../Common/UnixLike/AzCore/Socket/AzSocket_UnixLike.cpp
     ../Common/UnixLike/AzCore/Socket/AzSocket_UnixLike.h
