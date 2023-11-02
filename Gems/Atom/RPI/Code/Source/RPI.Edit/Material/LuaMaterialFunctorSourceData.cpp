2b22a9d22c95f6680fa15ba879173a9f61c20574
lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
2023-11-02 00:30:02 -0500

    Updated the RPI Builder to use the global BehaviorContext.
    The RPI Builder now registers the Lua specific material functors and
    classes with the Global Behavior Context and uses the associated global
    ScriptContext to execute any Lua scripts.
    
    This removes all the BehaviorContext and ScriptContext instances in the
    Atom RPI gem.
    
    Signed-off-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>

--- Gems/Atom/RPI/Code/Source/RPI.Edit/Material/LuaMaterialFunctorSourceData.cpp
+++ Gems/Atom/RPI/Code/Source/RPI.Edit/Material/LuaMaterialFunctorSourceData.cpp
@@ -11,6 +11,7 @@
 #include <Atom/RPI.Reflect/Material/MaterialPropertiesLayout.h>
 #include <AzCore/Serialization/SerializeContext.h>
 #include <AzCore/Script/ScriptAsset.h>
+#include <AzCore/Script/ScriptSystemBus.h>
 #include <Atom/RPI.Edit/Common/AssetUtils.h>
 
 namespace AZ
@@ -163,10 +164,16 @@ namespace AZ
                 return Failure();
             }
 
-            AZ::ScriptContext scriptContext;
+            ScriptContext* scriptContext{};
+            ScriptSystemRequestBus::BroadcastResult(scriptContext, &ScriptSystemRequests::GetContext, ScriptContextIds::DefaultScriptContextId);
+            if (scriptContext == nullptr)
+            {
+                AZ_ErrorOnce("LuaMaterialFunctorSourceData", false, "Global script context is not available. Cannot execute script");
+                return Failure();
+            }
 
             auto scriptBuffer = functor->GetScriptBuffer();
-            if (!scriptContext.Execute(scriptBuffer.data(), functor->GetScriptDescription(), scriptBuffer.size()))
+            if (!scriptContext->Execute(scriptBuffer.data(), functor->GetScriptDescription(), scriptBuffer.size()))
             {
                 AZ_Error("LuaMaterialFunctorSourceData", false, "Error initializing script '%s'.", functor->m_scriptAsset.ToString<AZStd::string>().c_str());
                 return Failure();
@@ -174,8 +181,8 @@ namespace AZ
 
             // [GFX TODO][ATOM-6012]: Figure out how to make shader option dependencies and material property dependencies get automatically reported
 
-            auto materialPropertyDependencies = GetNameListFromLuaScript(scriptContext, "GetMaterialPropertyDependencies");
-            auto shaderOptionDependencies = GetNameListFromLuaScript(scriptContext, "GetShaderOptionDependencies");
+            auto materialPropertyDependencies = GetNameListFromLuaScript(*scriptContext, "GetMaterialPropertyDependencies");
+            auto shaderOptionDependencies = GetNameListFromLuaScript(*scriptContext, "GetShaderOptionDependencies");
 
             if (!materialPropertyDependencies.IsSuccess() || !shaderOptionDependencies.IsSuccess())
             {
