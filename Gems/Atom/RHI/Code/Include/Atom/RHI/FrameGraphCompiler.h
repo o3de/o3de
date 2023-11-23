3804e6e20eeeac8e410619ab0d0f997cfedcda41
Martin Winter <martin.winter@huawei.com>
2023-11-23 14:59:39 +0100

    Add multi-device TransientAttachmentPool
    Co-authored-by: Martin Winter <martin.winter@huawei.com>
    Co-authored-by: Joerg H. Mueller <joerg.mueller@huawei.com>
    Signed-off-by: Martin Winter <martin.winter@huawei.com>

--- Gems/Atom/RHI/Code/Include/Atom/RHI/FrameGraphCompiler.h
+++ Gems/Atom/RHI/Code/Include/Atom/RHI/FrameGraphCompiler.h
@@ -81,7 +81,7 @@ namespace AZ::RHI
     class FrameGraph;
     class FrameGraphAttachmentDatabase;
     class ResourcePoolFrameAttachment;
-    class SingleDeviceTransientAttachmentPool;
+    class MultiDeviceTransientAttachmentPool;
 
     /**
         * @brief Fill this request structure and pass to FrameGraphCompiler::Compile.
@@ -94,7 +94,7 @@ namespace AZ::RHI
         FrameGraph* m_frameGraph = nullptr;
 
         /// The transient attachment pool used for transient attachment allocations. Must be a valid instance.
-        SingleDeviceTransientAttachmentPool* m_transientAttachmentPool = nullptr;
+        MultiDeviceTransientAttachmentPool* m_transientAttachmentPool = nullptr;
 
         /// The verbosity requested for compilation. Logs are emitted using the AzCore logging functions.
         FrameSchedulerLogVerbosity m_logVerbosity = FrameSchedulerLogVerbosity::None;
@@ -213,7 +213,7 @@ namespace AZ::RHI
 
         void CompileTransientAttachments(
             FrameGraph& frameGraph,
-            SingleDeviceTransientAttachmentPool& transientAttachmentPool,
+            AZ::RHI::MultiDeviceTransientAttachmentPool& transientAttachmentPool,
             FrameSchedulerCompileFlags compileFlags,
             FrameSchedulerStatisticsFlags statisticsFlags);
 
