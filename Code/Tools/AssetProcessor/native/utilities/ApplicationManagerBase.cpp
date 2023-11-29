76e67b7fa026f4982f975c346665c85b9209830c
Alex Peterson <26804013+AMZN-alexpete@users.noreply.github.com>
2023-11-29 09:59:51 -0800

    Fix duplicate FileProcessor::AssessAddedFile call (#17110)
    When the threading gods are angry FileProcessor::AssessAddedFile() can get called on the Main thread and Qt thread from here and from the FileWatcher resulting in a lockup in sqlite3 trying to insert the same file.  This usually repros on the first run when there are lots of assets and a higher likelyhood of encountering the race condition.
    
    Signed-off-by: Alex Peterson <26804013+AMZN-alexpete@users.noreply.github.com>

--- Code/Tools/AssetProcessor/native/utilities/ApplicationManagerBase.cpp
+++ Code/Tools/AssetProcessor/native/utilities/ApplicationManagerBase.cpp
@@ -1535,9 +1535,8 @@ bool ApplicationManagerBase::Activate()
 
     auto notifyUuidManagerAndFileProcessor = [this](QString changedFile)
     {
-        // these are not necessarily time sensitive.
+        // this is not necessarily time sensitive.
         m_uuidManager->FileChanged(changedFile.toUtf8().constData());
-        m_fileProcessor->AssessAddedFile(changedFile);
     };
 
     QObject::connect(
