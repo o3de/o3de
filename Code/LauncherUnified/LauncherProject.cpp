16807396ee361b890f26bfca72ecbfb725fb1024
Nicholas Lawson <70027408+nick-l-o3de@users.noreply.github.com>
2023-12-05 11:32:56 -0800

    Fix for AR failure
    Signed-off-by: Nicholas Lawson <70027408+nick-l-o3de@users.noreply.github.com>

--- Code/LauncherUnified/LauncherProject.cpp
+++ Code/LauncherUnified/LauncherProject.cpp
@@ -9,6 +9,8 @@
 // note that this is the only file pulled into a launcher target that has the
 // defines set such as LY_CMAKE_TARGET, and LY_PROJECT_NAME, other files come from a static library
 // which do not provide those defines.
+// note that the tests use a mock implementation of this file, see Tests/Test.cpp
+// If you modify this file or the interface launcher.h, make sure to update the mock implementation as well.
 
 #include <AzCore/std/string/string_view.h>
 
