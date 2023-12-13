080e81c0111aef27228daf49eace2c7935701af6
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2023-12-13 09:21:31 -0800

    tab fixes
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- Templates/DefaultProject/Template/export.sh
+++ Templates/DefaultProject/Template/export.sh
@@ -51,11 +51,11 @@ O3DE_PROJECT_SEEDLIST=${O3DE_PROJECT_PATH}/AssetBundling/SeedLists/*.seed
 OUTPUT_PATH=${O3DE_PROJECT_PATH}/ProjectPackages
 
 if [[ "$OSTYPE" == *"darwin"* ]]; then
-	TOOLS_BUILD_PATH=${O3DE_PROJECT_PATH}/build/mac
+    TOOLS_BUILD_PATH=${O3DE_PROJECT_PATH}/build/mac
 elif [[ "$OSTYPE" == "msys" ]]; then
-	TOOLS_BUILD_PATH=${O3DE_PROJECT_PATH}/build/windows
+    TOOLS_BUILD_PATH=${O3DE_PROJECT_PATH}/build/windows
 else
-	TOOLS_BUILD_PATH=${O3DE_PROJECT_PATH}/build/linux
+    TOOLS_BUILD_PATH=${O3DE_PROJECT_PATH}/build/linux
 fi
 
 ${O3DE_PATH}/scripts/o3de.sh export-project -es ExportScripts/export_source_built_project.py --project-path ${O3DE_PROJECT_PATH} --log-level INFO -assets --build-tools --config release --archive-output zip --seedlist ${O3DE_PROJECT_SEEDLIST} -out ${OUTPUT_PATH}
