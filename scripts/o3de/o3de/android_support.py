8a100c54df591e8b0f2c7793ace9b0bc466e96dc
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2023-12-19 11:56:20 -0800

    Fix Android Project Generation with Pak mode (#17283)
    * Add missing cmake argument to specify pak mode search when creating android projects with pak assets
    * Fix hardcoded 'mergeProfileAssets' to 'merge{config}'Assets in android generator
    
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- scripts/o3de/o3de/android_support.py
+++ scripts/o3de/o3de/android_support.py
@@ -1602,6 +1602,9 @@ class AndroidProjectGenerator(object):
             if self._strip_debug_symbols:
                 cmake_argument_list.append('"-DLY_STRIP_DEBUG_SYMBOLS=ON"')
 
+            if self._asset_mode == ASSET_MODE_PAK:
+                cmake_argument_list.append('"-DLY_ARCHIVE_FILE_SEARCH_MODE=1"')
+
             cmake_argument_list.extend([
                 f'"-DANDROID_NATIVE_API_LEVEL={self._android_platform_sdk_api_level}"',
                 f'"-DLY_NDK_DIR={template_ndk_path}"',
