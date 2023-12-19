1ee83b770f33f0f08097220c4d6a99d14197e6fb
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2023-12-18 16:57:18 -0800

    Add missing cmake argument to specify pak mode search when creating android projects with pak assets
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- scripts/o3de/o3de/android_support.py
+++ scripts/o3de/o3de/android_support.py
@@ -1597,6 +1597,9 @@ class AndroidProjectGenerator(object):
             if self._strip_debug_symbols:
                 cmake_argument_list.append('"-DLY_STRIP_DEBUG_SYMBOLS=ON"')
 
+            if self._asset_mode == ASSET_MODE_PAK:
+                cmake_argument_list.append('"-DLY_ARCHIVE_FILE_SEARCH_MODE=2"')
+
             cmake_argument_list.extend([
                 f'"-DANDROID_NATIVE_API_LEVEL={self._android_platform_sdk_api_level}"',
                 f'"-DLY_NDK_DIR={template_ndk_path}"',
