0fb6c85fdf806f59c9d7d8998d73e08dae5f33e9
Joerg H. Mueller <joerg.mueller@huawei.com>
2024-01-10 09:04:16 +0100

    Revert "quick fix for truncation warning (#17326)"
    This reverts commit 7c11539a83b8474f584be4539306035544fb6f51.

--- Gems/Atom/RHI/Code/Include/Atom/RHI/MultiDeviceDrawPacketBuilder.h
+++ Gems/Atom/RHI/Code/Include/Atom/RHI/MultiDeviceDrawPacketBuilder.h
@@ -68,8 +68,7 @@ namespace AZ::RHI
 
             for (int deviceIndex = 0; deviceIndex < deviceCount; ++deviceIndex)
             {
-                // cast to u8 to prevent warning
-                if (RHI::CheckBit(AZStd::to_underlying(m_deviceMask), static_cast<AZ::u8>(deviceIndex)))
+                if (RHI::CheckBit(AZStd::to_underlying(m_deviceMask), deviceIndex))
                 {
                     m_deviceDrawPacketBuilders.emplace(deviceIndex, DrawPacketBuilder());
                 }
