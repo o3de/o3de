ffd1ace5fa20058d7b9e68840861cc01e4d66b00
Guthrie Adams <82461473+gadams3@users.noreply.github.com>
2024-01-12 18:00:17 -0600

    Fixed shader variant list generation which was not enumerating material types and materials that referenced a given shader Renamed SMC Python scripts according to what they were doing Updated generate shader variant lists for all shaders script to use the same function as generating for a single shader Removed unused functions and python bindings Added begin and end edit blocks where they were missing for some operations Registered generate all shader variants script with file script menu
    Signed-off-by: Guthrie Adams <82461473+gadams3@users.noreply.github.com>

--- Gems/Atom/Tools/ShaderManagementConsole/Scripts/GenerateShaderVariantListForAllShaders.py
+++ Gems/Atom/Tools/ShaderManagementConsole/Scripts/GenerateShaderVariantListForAllShaders.py
@@ -5,20 +5,53 @@ For complete copyright and license terms please see the LICENSE at the root of t
 SPDX-License-Identifier: Apache-2.0 OR MIT
 """
 
-import re
 import json
+import os
+import re
+import sys
+
+import azlmbr.bus
+import azlmbr.paths
+import collections
 import GenerateShaderVariantListUtil
 
-def main():
-    print("==== Begin generate shader variant list for all built material assets ==========")
-    
-    assetIds = azlmbr.shadermanagementconsole.ShaderManagementConsoleRequestBus(
-        azlmbr.bus.Broadcast, 
-        'GetAllMaterialAssetIds'
-    )
-    GenerateShaderVariantListUtil.save_shadervariantlists_for_materials(assetIds)
+from PySide2 import QtWidgets
 
-    print("==== Finish generate shader variant list for all built material assets ==========")
+PROJECT_SHADER_VARIANTS_FOLDER = "ShaderVariants"
+
+def main():
+    print("==== Begin generating shader variant lists for all shaders ==========")
+
+    paths = azlmbr.atomtools.util.GetPathsInSourceFoldersMatchingExtension('shader')
+
+    progressDialog = QtWidgets.QProgressDialog("Batch generating shader variant lists...", "Cancel", 0, len(paths))
+    progressDialog.setMaximumWidth(400)
+    progressDialog.setMaximumHeight(100)
+    progressDialog.setModal(True)
+    progressDialog.setWindowTitle("Batch generating shader variant lists")
+
+    for i, path in enumerate(paths):
+        shaderVariantList = GenerateShaderVariantListUtil.create_shadervariantlist_for_shader(path)
+        if len(shaderVariantList.shaderVariants) == 0:
+            continue
+
+        relativePath = azlmbr.shadermanagementconsole.ShaderManagementConsoleRequestBus(azlmbr.bus.Broadcast, 'GenerateRelativeSourcePath', path)
+
+        pre, ext = os.path.splitext(relativePath)
+        savePath = os.path.join(azlmbr.paths.projectroot, PROJECT_SHADER_VARIANTS_FOLDER, f'{pre}.shadervariantlist')
+
+        # clean previously generated shader variant list file so they don't clash.
+        if os.path.exists(savePath):
+            os.remove(savePath)
+            
+        azlmbr.shader.SaveShaderVariantListSourceData(savePath, shaderVariantList)
+
+        progressDialog.setValue(i)
+        if progressDialog.wasCanceled():
+            return
+    progressDialog.close()
+
+    print("==== Finish generating shader variant lists for all shaders ==========")
 
 if __name__ == "__main__":
     main()
