c70659b8280bf6e9829c35e6655de3b70b26b133
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2024-01-17 09:41:28 -0800

    Update ly_associate_package calls for PhysX - Remove AZ_USE_PHYSX5 cache variable from BuildInPackages_* (Windows/Android) - Remove ly_associate_package calls from BuildInPackages_* (Windows/Android) - Move ly_associate_package for PhysX4 to Platform subfolder under PhysX/PhysX4
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- Gems/PhysX/PhysX4/Source/Platform/Windows/PAL_windows.cmake
+++ Gems/PhysX/PhysX4/Source/Platform/Windows/PAL_windows.cmake
@@ -8,6 +8,8 @@
 
 set(PAL_TRAIT_PHYSX_SUPPORTED TRUE)
 
+ly_associate_package(PACKAGE_NAME PhysX-4.1.2.29882248-rev6-windows TARGETS PhysX4 PACKAGE_HASH 581ebdcb5eda90c57a51b3775f4f4a9643aed98325a2bebfe94877074e1426ec)
+
 if(PAL_TRAIT_BUILD_HOST_TOOLS)
      ly_associate_package(PACKAGE_NAME poly2tri-7f0487a-rev1-windows TARGETS poly2tri PACKAGE_HASH 5fea2bf294e5130e0654fbfa39f192e6369f3853901dde90bb9b3f3a11edcb1e) 
      ly_associate_package(PACKAGE_NAME v-hacd-2.3-1a49edf-rev1-windows TARGETS v-hacd PACKAGE_HASH c5826ec28aedc3b5931ddf655d055395872cd3e75cd829f5745a2e607deb7468)
