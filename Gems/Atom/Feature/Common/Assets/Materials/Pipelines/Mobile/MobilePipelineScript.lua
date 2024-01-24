1b2246d653d498d3505c84bd1fca4d070e75d9bb
moudgils <47460854+moudgils@users.noreply.github.com>
2024-01-24 15:18:14 -0800

    Disable Parallax from Mobile pipeline by default. (#156)
    Signed-off-by: moudgils <47460854+moudgils@users.noreply.github.com>

--- Gems/Atom/Feature/Common/Assets/Materials/Pipelines/Mobile/MobilePipelineScript.lua
+++ Gems/Atom/Feature/Common/Assets/Materials/Pipelines/Mobile/MobilePipelineScript.lua
@@ -24,7 +24,7 @@ function MaterialTypeSetup(context)
 
     if(lightingModel == "Standard" or lightingModel == "Enhanced") then
         if(lightingModel == "Enhanced") then
-            Warning("The multi view pipeline does not support the Enhanced lighting model. Will use Standard lighting as a fallback.")
+            Warning("The mobile pipeline does not support the Enhanced lighting model. Will use Standard lighting as a fallback.")
         end
         
         context:IncludeShader("ShadowmapPass_CustomZ")
