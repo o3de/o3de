6b5174ef6c2218a7ab4c6eabc733ca653dfab902
AMZN-Gene <genewalt@amazon.com>
2024-01-26 14:06:33 -0800

    WiP. Adding AssetPlatformComponentRemover spawnable processor.
    Signed-off-by: AMZN-Gene <genewalt@amazon.com>

---
+++ Code/Framework/AzToolsFramework/AzToolsFramework/Prefab/Spawnable/AssetPlatformComponentRemover.h
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+
+#pragma once
+
+#include <AzCore/Memory/SystemAllocator.h>
+#include <AzCore/Outcome/Outcome.h>
+#include <AzToolsFramework/Entity/EntityTypes.h>
+#include <AzToolsFramework/Prefab/Instance/Instance.h>
+#include <AzToolsFramework/Prefab/Spawnable/ComponentRequirementsValidator.h>
+#include <AzToolsFramework/Prefab/Spawnable/EditorOnlyEntityHandler/EditorOnlyEntityHandler.h>
+#include <AzToolsFramework/Prefab/Spawnable/EditorOnlyEntityHandler/UiEditorOnlyEntityHandler.h>
+#include <AzToolsFramework/Prefab/Spawnable/EditorOnlyEntityHandler/WorldEditorOnlyEntityHandler.h>
+#include <AzToolsFramework/Prefab/Spawnable/PrefabProcessor.h>
+
+namespace AZ
+{
+    class ReflectContext;
+}
+
+namespace AzToolsFramework::Components
+{
+    class EditorComponentBase;
+}
+
+namespace AzToolsFramework::Prefab::PrefabConversionUtils
+{
+    class AssetPlatformComponentRemover
+        : public PrefabProcessor
+    {
+    public:
+        AZ_CLASS_ALLOCATOR(AssetPlatformComponentRemover, AZ::SystemAllocator);
+        AZ_RTTI(AzToolsFramework::Prefab::PrefabConversionUtils::AssetPlatformComponentRemover,
+            "{25D9A8A6-908F-4B26-A752-EBAF7DC074F8}", PrefabProcessor);
+
+        ~AssetPlatformComponentRemover() override;
+
+        void Process(PrefabProcessorContext& prefabProcessorContext) override;
+
+        using RemoveComponentBasedOnAssetPlatformResult = AZ::Outcome<void, AZStd::string>;
+        RemoveComponentBasedOnAssetPlatformResult RemoveComponentBasedOnAssetPlatform(
+            PrefabDocument& prefab,
+            AZ::SerializeContext* serializeContext,
+            PrefabProcessorContext& prefabProcessorContext);
+
+        static void Reflect(AZ::ReflectContext* context);
+
+     protected:
+        static void GetEntitiesFromInstance(AzToolsFramework::Prefab::Instance& instance, EntityList& hierarchyEntities);
+
+        static bool ReadComponentAttribute(
+            AZ::Component* component,
+            AZ::Edit::Attribute* attribute,
+            AZStd::vector<AZ::Crc32>& attributeTags);
+
+        void SetEditorOnlyEntityHandlerFromCandidates(const EntityList& entities);
+
+        bool HasValidEditorOnlyHandler() const;
+
+        void ClearEditorOnlyEntityIds();
+
+        void AddEntityIdIfEditorOnly(AZ::Entity* entity);
+
+        using RemoveEditorOnlyEntitiesResult = AZ::Outcome<void, AZStd::string>;
+        RemoveEditorOnlyEntitiesResult RemoveEditorOnlyEntities(EntityList& entities);
+
+        using ExportEntityResult = AZ::Outcome<AZStd::unique_ptr<AZ::Entity>, AZStd::string>;
+        ExportEntityResult ExportEntity(AZ::Entity* sourceEntity, PrefabProcessorContext& context);
+
+        using ResolveExportedComponentResult = AZ::Outcome<AZ::ExportedComponent, AZStd::string>;
+        ResolveExportedComponentResult ResolveExportedComponent(
+            AZ::ExportedComponent& component, PrefabProcessorContext& prefabProcessorContext);
+
+        using ShouldExportResult = AZ::Outcome<bool, AZStd::string>;
+        ShouldExportResult ShouldExportComponent(
+            AZ::Component* component,
+            PrefabProcessorContext& prefabProcessorContext) const;
+
+        using BuildGameEntityResult = AZ::Outcome<void, AZStd::string>;
+        BuildGameEntityResult BuildGameEntity(
+            AzToolsFramework::Components::EditorComponentBase* editorComponent,
+            AZ::Entity* sourceEntity,
+            AZ::Entity* exportEntity
+        );
+
+        using ExportComponentResult = AZ::Outcome<void, AZStd::string>;
+        ExportComponentResult ExportComponent(
+            AZ::Component* component,
+            PrefabProcessorContext& prefabProcessorContext,
+            AZ::Entity* sourceEntity,
+            AZ::Entity* exportEntity);
+
+        AZ::SerializeContext* m_serializeContext{ nullptr };
+        EditorOnlyEntityHandler* m_editorOnlyEntityHandler{ nullptr };
+        EditorOnlyEntityHandlers m_editorOnlyEntityHandlerCandidates{
+            aznew WorldEditorOnlyEntityHandler(),
+            aznew UiEditorOnlyEntityHandler() };
+        ComponentRequirementsValidator m_componentRequirementsValidator;
+        EntityIdSet m_editorOnlyEntityIds;
+    };
+} // namespace AzToolsFramework::Prefab::PrefabConversionUtils
