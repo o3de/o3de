9ae90c67b6bec5f97db2b6477e77d13809093382
TJ Kotha <tankotha@amazon.com>
2024-01-31 22:12:09 -0500

    added more unit tests
    Signed-off-by: TJ Kotha <tankotha@amazon.com>

--- scripts/o3de/ExportScripts/export_source_ios_xcode.py
+++ scripts/o3de/ExportScripts/export_source_ios_xcode.py
@@ -42,13 +42,20 @@ def export_ios_xcode_project(ctx: exp.O3DEScriptExportContext,
     Note: In order to use this functionality, you must be running this script from a MacOS machine with a valid copy of Xcode. 
     This will also require setting up an AppleID and provisioning profile in Xcode when the resulting xcode-project file is generated before deploying to device.
 
-    Instructions to handle iOS projects in Xcode will be provided soon. This export function is currently experimental.
+    This export script produces an IPA file, but using a workaround to the traditional archive approach.
 
     :param ctx:                                     The O3DE Script context provided by the export-command
-    :param tools_build_path:                        Optional build path to build the tools. (see default_tools_path below)
-    :param ios_build_folder:                        The base output path of the generated Xcode Project file for iOS
+    :param target_ios_project_path:                 The base output path of the generated Xcode Project file for iOS
+    :param seedlist_paths:                          List of seed list files to optionally include in the asset bundling process
+    :param seedfile_paths:                          List of seed files to optionally include in the asset bundling process. These can be individual O3DE assets, such as levels or prefabs.
+    :param level_names:                             List of individual level names. These are assumed to follow standard O3DE level convention, and will be used in the asset bundler.
     :param should_build_tools:                      Option to build the export process dependent tools (AssetProcessor, AssetBundlerBatch, and dependencies)
     :param should_build_all_assets:                 Option to process all the assets for the game
+    :param build_config:                            The build configuration to use for the export package launchers
+    :param tool_config:                             The build configuration to refer to for tool binaries
+    :param tools_build_path:                        Optional build path to build the tools. (see default_tools_path below)
+    :param max_bundle_size:                         Maximum size to set for the archived bundle files
+    :param fail_on_asset_errors:                    Option to fail the process on any asset processing error
     :param logger:                                  Optional logger to use to log the process and errors
     """
     
@@ -105,6 +112,7 @@ def export_ios_xcode_project(ctx: exp.O3DEScriptExportContext,
     exp.process_command(xcode_build_common_args + ['build'], cwd=ctx.project_path)
     
     # Note: This workaround enables the creation of an IPA file, even if xcode archive doesn't work, but will require manual upload
+    logger.info(f"Creating IPA file for {ctx.project_name}")
     payload_path = target_ios_project_path / 'bin'/ build_config /'Payload'
     payload_path_str = str(payload_path)
     exp.process_command(['mkdir', payload_path_str], cwd=ctx.project_path)
@@ -118,24 +126,7 @@ def export_ios_xcode_project(ctx: exp.O3DEScriptExportContext,
     logger.info(f"iOS IPA file should be generated now. Please check {ipa_path_str}")
 
 
-# This code is only run by the 'export-project' O3DE CLI command
-if "o3de_context" in globals():
-    global o3de_context
-    global o3de_logger
-
-    # Resolve the export config
-    try:
-        if o3de_context is None:
-            project_name, project_path = command_utils.resolve_project_name_and_path()
-            export_config = exp.get_export_project_config(project_path=project_name)
-        else:
-            export_config = exp.get_export_project_config(project_path=o3de_context.project_path)
-    except command_utils.O3DEConfigError:
-        o3de_logger.debug(f"No project detected at {os.getcwd()}, default settings from global config.")
-        project_name = None
-        export_config = exp.get_export_project_config(project_path=None)
-
-    def parse_args(o3de_context: exp.O3DEScriptExportContext):
+def export_source_ios_parse_args(o3de_context: exp.O3DEScriptExportContext, export_config: command_utils.O3DEConfig):
         parser = argparse.ArgumentParser(
                     prog=f'o3de.py export-project -es {__file__}',
                     description="Exports a project as a generated iOS Xcode project in the project directory. "
@@ -162,8 +153,7 @@ if "o3de_context" in globals():
 
         return parsed_args
 
-    args = parse_args(o3de_context)
-
+def export_source_ios_run_command(o3de_context: exp.O3DEScriptExportContext, args, export_config: command_utils.O3DEConfig, o3de_logger):
     option_build_tools = export_config.get_parsed_boolean_option(parsed_args=args,
                                                                  key=exp.SETTINGS_OPTION_BUILD_TOOLS.key,
                                                                  enable_attribute='build_tools',
@@ -173,6 +163,12 @@ if "o3de_context" in globals():
                                                                   key=exp.SETTINGS_OPTION_BUILD_ASSETS.key,
                                                                   enable_attribute='build_assets',
                                                                   disable_attribute='skip_build_assets')
+    
+    fail_on_asset_errors = export_config.get_parsed_boolean_option(parsed_args=args,
+                                                                   key=exp.SETTINGS_OPTION_FAIL_ON_ASSET_ERR.key,
+                                                                   enable_attribute='fail_on_asset_errors',
+                                                                   disable_attribute='continue_on_asset_errors')
+
 
     if args.quiet:
         o3de_logger.setLevel(logging.ERROR)
@@ -188,8 +184,33 @@ if "o3de_context" in globals():
                                  tool_config=args.tool_config,
                                  tools_build_path=args.tools_build_path,
                                  max_bundle_size=args.max_bundle_size,
-                                 fail_on_asset_errors=args.fail_on_asset_errors,
+                                 fail_on_asset_errors=fail_on_asset_errors,
                                  logger=o3de_logger)
     except exp.ExportProjectError as err:
         print(err)
         sys.exit(1)
+
+# This code is only run by the 'export-project' O3DE CLI command
+if "o3de_context" in globals():
+    global o3de_context
+    global o3de_logger
+
+    # Resolve the export config
+    try:
+        if o3de_context is None:
+            project_name, project_path = command_utils.resolve_project_name_and_path()
+            export_config = exp.get_export_project_config(project_path=project_name)
+        else:
+            export_config = exp.get_export_project_config(project_path=o3de_context.project_path)
+    except command_utils.O3DEConfigError:
+        o3de_logger.debug(f"No project detected at {os.getcwd()}, default settings from global config.")
+        project_name = None
+        export_config = exp.get_export_project_config(project_path=None)
+
+    
+
+    args = export_source_ios_parse_args(o3de_context, export_config)
+
+    export_source_ios_run_command(o3de_context, args, export_config, o3de_logger)
+    o3de_logger.info("Finished exporting ios project")
+    sys.exit(0)
--- scripts/o3de/tests/ExportScripts/test_export_source_ios_xcode.py
+++ scripts/o3de/tests/ExportScripts/test_export_source_ios_xcode.py
@@ -10,7 +10,9 @@ import pytest
 import pathlib
 from unittest.mock import patch, create_autospec, MagicMock, Mock, PropertyMock
 from o3de.export_project import O3DEScriptExportContext
+import o3de.export_project as exp
 import shutil
+import logging
 
 # Because the export scripts are standalone, we have to manually import them to test
 import o3de.utils as utils
@@ -21,7 +23,7 @@ import export_utility as eutil
 
 from export_test_utility import setup_local_export_config_test
 
-
+from itertools import product
 
 
 @pytest.mark.parametrize("skip_assets_flag, should_build_tools_flag, expect_get_asset_path, expect_process_command_count", [
@@ -71,3 +73,199 @@ def test_asset_and_build_combinations(tmp_path, skip_assets_flag, should_build_t
         mock_asset_processor_path.assert_not_called()
         
         assert mock_process_command.call_count == expect_process_command_count
+
+
+@pytest.mark.parametrize("seedlists, seedfiles, levelnames, gamefile_patterns, serverfile_patterns, project_patterns",[
+    pytest.param([],[],[], [], [], []),
+    pytest.param(["/test/test.seedlist"],["/test2/seed.seed"],['main'], ['game.cfg'], ['server.cfg'], ['licensing/test*.txt']),
+    pytest.param(["/test/test.seedlist", "/test/test2.seedlist"],["/test2/seed.seed","/test2/seed2.seed"],['main', 'second', 'THIRD'], ['game.cfg', 'music/test*.mp3'], ['access_ctrls.xml','server.cfg'], ['licensing/test*.txt', 'dependency.graph']),
+])
+def test_export_standalone_multipart_args(tmpdir, seedlists, seedfiles, levelnames, gamefile_patterns, serverfile_patterns, project_patterns):
+
+    test_project_name, test_project_path, test_engine_path = setup_local_export_config_test(tmpdir)
+    
+    with patch('o3de.manifest.get_o3de_folder') as mock_get_o3de_folder,\
+         patch('o3de.export_project.get_default_asset_platform', return_value='mac') as mock_get_asset_platform,\
+         patch('export_source_ios_xcode.export_ios_xcode_project') as mock_export_func:
+        
+        mock_get_o3de_folder.return_value = pathlib.Path(tmpdir.join('.o3de').realpath())
+
+        mock_ctx = create_autospec(O3DEScriptExportContext)
+        mock_ctx.project_path = test_project_path
+        mock_ctx.engine_path = test_engine_path
+        mock_ctx.project_name = test_project_name
+        
+        mock_logger = create_autospec(logging.Logger)
+        test_export_config = exp.get_export_project_config(project_path=None)
+
+        mock_bundle_size = 777
+
+        mock_ctx.args = [ "--max-bundle-size", str(mock_bundle_size),
+                         '-bt', '-assets', '-cfg','release', '-tcfg','profile',
+                         '-tbp', str(tmpdir / 'tool_builds'),]
+        
+        for ln in levelnames:
+            mock_ctx.args += ['-lvl', ln]
+        
+        verify_seedlists = []
+        for sl in seedlists:
+            mock_ctx.args += ['-sl',  sl]
+            verify_seedlists += [pathlib.Path(sl)]
+        
+        verify_seedfiles = []
+        for sf in seedfiles:
+            mock_ctx.args += ['-sf', sf]
+            verify_seedfiles += [pathlib.Path(sf)]
+
+        args = exp_ios.export_source_ios_parse_args(mock_ctx, test_export_config)
+
+        assert 'level_names' in args and getattr(args, 'level_names')  == levelnames
+
+        assert 'seedlist_paths' in args and getattr(args, 'seedlist_paths') == verify_seedlists
+        assert 'seedfile_paths' in args and getattr(args, 'seedfile_paths') == verify_seedfiles
+
+        exp_ios.export_source_ios_run_command(mock_ctx, args, test_export_config, mock_logger)
+
+        mock_export_func.assert_called_once_with(ctx=mock_ctx,
+                                    target_ios_project_path=args.ios_build_path,
+                                    seedlist_paths=args.seedlist_paths,
+                                    seedfile_paths=args.seedfile_paths,
+                                    level_names=args.level_names,
+                                    should_build_tools=True,
+                                    should_build_all_assets=True,
+                                    build_config='release',
+                                    tool_config='profile',
+                                    tools_build_path=args.tools_build_path,
+                                    max_bundle_size=777,
+                                    fail_on_asset_errors=False,
+                                    logger=mock_logger)
+
+@pytest.mark.parametrize('dum_fail_asset_err', [True, False])
+@pytest.mark.parametrize('dum_build_tools', [True, False])
+@pytest.mark.parametrize('dum_build_assets', [True, False])
+def test_export_standalone_exhaustive(tmpdir, dum_fail_asset_err, dum_build_tools, dum_build_assets):
+    test_export_standalone_single(tmpdir, dum_fail_asset_err, dum_build_tools, dum_build_assets)
+
+
+@pytest.mark.parametrize("dum_fail_asset_err, dum_build_tools, dum_build_assets",[])
+def test_export_standalone_single(tmpdir, dum_fail_asset_err, dum_build_tools, dum_build_assets):
+    dummy_build_config = 'release'
+    dummy_tool_config = 'profile'
+    dummy_archive_format = 'none'
+
+    test_project_name, test_project_path, test_engine_path = \
+        setup_local_export_config_test(tmpdir, build_config=dummy_build_config, tool_config=dummy_tool_config, archive_format=dummy_archive_format,
+            fail_asset_errors=dum_fail_asset_err, build_assets=dum_build_assets, build_tools=dum_build_tools)
+    
+    with patch('o3de.manifest.get_o3de_folder') as mock_get_o3de_folder,\
+         patch('o3de.export_project.get_default_asset_platform', return_value='pc') as mock_get_asset_platform,\
+         patch('o3de.export_project.get_asset_processor_batch_path', return_value=tmpdir/'assetproc'),\
+         patch('o3de.export_project.get_asset_bundler_batch_path', return_value=tmpdir/'assetbundles'),\
+         patch('o3de.export_project.process_command', return_value=0) as mock_process_command,\
+         patch('export_utility.process_level_names'),\
+         patch('export_utility.build_and_bundle_assets'),\
+         patch('o3de.export_project.validate_project_artifact_paths'),\
+         patch('o3de.export_project.preprocess_seed_path_list'),\
+         patch('shutil.make_archive'),\
+         patch('export_source_ios_xcode.export_ios_xcode_project') as mock_export_func:
+        
+        mock_get_o3de_folder.return_value = pathlib.Path(tmpdir.join('.o3de').realpath())
+
+        mock_ctx = create_autospec(O3DEScriptExportContext)
+        mock_ctx.project_path = test_project_path
+        mock_ctx.engine_path = test_engine_path
+        mock_ctx.project_name = test_project_name
+        
+        mock_logger = create_autospec(logging.Logger)
+        test_export_config = exp.get_export_project_config(project_path=None)
+        
+        for use_asset_arg, use_build_tool_arg, use_asset_fail_arg, build_config in product(
+                    [True, False],
+                    [True, False],
+                    [True, False],
+                    ['release', 'profile']):
+                
+                tool_config = 'profile'
+
+                mock_ctx.args = ['-ibp', str(tmpdir/'output')]
+
+                #add all necessary arguments
+
+                check_build_config = build_config
+                if build_config:
+                    mock_ctx.args += ['-cfg', build_config]
+                if not check_build_config:
+                    check_build_config = test_export_config.get_value("project.build.config")
+                
+                check_tool_config = tool_config
+                if tool_config:
+                    mock_ctx.args += ['-tcfg', tool_config]
+                if not check_tool_config:
+                    check_tool_config = test_export_config.get_value("tool.build.config")
+
+                #now check for the boolean arguments
+
+                check_asset_build = dum_build_assets
+                if use_asset_arg:
+                    check_asset_build = not check_asset_build
+                    if dum_build_assets:
+                        mock_ctx.args += ['--skip-build-assets']
+                    else:
+                        mock_ctx.args += ['--build-assets']
+                
+                check_fail_asset_err = dum_fail_asset_err
+                if use_asset_fail_arg:
+                    check_fail_asset_err = not check_fail_asset_err
+                    if dum_fail_asset_err:
+                        mock_ctx.args += ['-coa']
+                    else:
+                        mock_ctx.args += ['-foa']
+
+                check_tool_build  = dum_build_tools
+                if use_build_tool_arg:
+                    check_tool_build = not check_tool_build
+                    if dum_build_tools:
+                        mock_ctx.args += ['--skip-build-tools']
+                    else:
+                        mock_ctx.args += ['--build-tools']
+
+                args = exp_ios.export_source_ios_parse_args(mock_ctx, test_export_config)
+
+                assert getattr(args, 'config') == check_build_config
+                assert getattr(args, 'tool_config') == check_tool_config
+
+                if use_asset_arg:
+                    if dum_build_assets:
+                        assert getattr(args, 'skip_build_assets')
+                    else:
+                        assert getattr(args, 'build_assets')
+                
+                if use_asset_fail_arg:
+                    if dum_fail_asset_err:
+                        assert getattr(args, 'continue_on_asset_errors')
+                    else:
+                        assert getattr(args, 'fail_on_asset_errors')
+                
+                if use_build_tool_arg:
+                    if dum_build_tools:
+                        assert getattr(args, 'skip_build_tools')
+                    else:
+                        assert getattr(args, 'build_tools')
+                
+                exp_ios.export_source_ios_run_command(mock_ctx, args, test_export_config, mock_logger)
+
+                mock_export_func.assert_called_once_with(ctx=mock_ctx,
+                                  target_ios_project_path=tmpdir/'output',
+                                  seedlist_paths=[],
+                                  seedfile_paths=[],
+                                  level_names=[],
+                                  should_build_all_assets=check_asset_build,
+                                  should_build_tools=check_tool_build,
+                                  build_config=check_build_config,
+                                  tool_config=check_tool_config,
+                                  tools_build_path=args.tools_build_path,
+                                  max_bundle_size=2048,
+                                  fail_on_asset_errors=check_fail_asset_err,
+                                  logger=mock_logger)
+                mock_export_func.reset_mock()
+
