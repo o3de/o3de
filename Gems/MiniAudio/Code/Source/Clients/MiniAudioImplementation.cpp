811d9cefcf309649f9b984193b180c2248e21243
TJ Kotha <tankotha@amazon.com>
2024-02-08 16:58:36 -0500

    added fixes for mini audio gem so that it compiles for ios. Template updated to remove AWSCore
    Signed-off-by: TJ Kotha <tankotha@amazon.com>

--- Gems/MiniAudio/Code/Source/Clients/MiniAudioImplementation.cpp
+++ Gems/MiniAudio/Code/Source/Clients/MiniAudioImplementation.cpp
@@ -19,10 +19,12 @@ extern "C" {
 
 #define STB_VORBIS_HEADER_ONLY
 #include <stb_vorbis.c>    // Enables Vorbis decoding.
+}
 
 #define MINIAUDIO_IMPLEMENTATION
 #include <miniaudio.h>
 
+extern "C" {
 // The stb_vorbis implementation must come after the implementation of miniaudio.
 #undef STB_VORBIS_HEADER_ONLY
 #include <stb_vorbis.c>
