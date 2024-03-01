9eb74df2b0cd88f612c5785f83dca95155d2c207
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2024-03-01 10:16:33 -0800

    Update snap packaging to pre-compile all .py files before packaging
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- cmake/Platform/Linux/Packaging_Snapcraft.cmake
+++ cmake/Platform/Linux/Packaging_Snapcraft.cmake
@@ -23,7 +23,16 @@ endif()
 
 execute_process (COMMAND lsb_release -a)
 
+# Download python before packaging. The python runtimes themselves will not be part of the packaging, but
+# the following script will also run through the scripts and create the necessary egg-link folders
+# which will be installed into the package
 execute_process (COMMAND bash ${CPACK_TEMPORARY_DIRECTORY}/O3DE/${CPACK_PACKAGE_VERSION}/python/get_python.sh)
+
+# Since snap is immutable, we will pre-compile all the python scripts that is packaged into python byte-codes
+file(GLOB_RECURSE pyfiles "${CPACK_TEMPORARY_DIRECTORY}/O3DE/${CPACK_PACKAGE_VERSION}/*.py")
+foreach(pyfile in ${pyfiles})
+    execute_process (COMMAND bash ${CPACK_TEMPORARY_DIRECTORY}/O3DE/${CPACK_PACKAGE_VERSION}/python/python.sh -m py_compile ${pyfile})
+endforeach()
 
 # make sure that all files have the correct permissions
 execute_process (COMMAND chmod -R 755 O3DE
