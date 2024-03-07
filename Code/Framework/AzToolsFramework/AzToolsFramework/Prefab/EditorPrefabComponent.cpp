d78d24bcc5c8bc31cad83ba9c895b775417d8a13
Danilo Aimini <82231674+AMZN-daimini@users.noreply.github.com>
2024-03-07 19:13:25 +0100

    WIP
    Signed-off-by: Danilo Aimini <82231674+AMZN-daimini@users.noreply.github.com>

--- Code/Framework/AzToolsFramework/AzToolsFramework/Prefab/EditorPrefabComponent.cpp
+++ Code/Framework/AzToolsFramework/AzToolsFramework/Prefab/EditorPrefabComponent.cpp
@@ -10,10 +10,13 @@
 
 #include <AzCore/Interface/Interface.h>
 #include <AzCore/Math/Uuid.h>
+#include <AzCore/IO/Path/Path.h>
 #include <AzCore/RTTI/BehaviorContext.h>
 #include <AzCore/Serialization/EditContext.h>
 #include <AzCore/Serialization/SerializeContext.h>
+#include <AzToolsFramework/Prefab/Instance/InstanceEntityMapperInterface.h>
 #include <AzToolsFramework/Prefab/PrefabPublicInterface.h>
+#include <AzToolsFramework/Prefab/PrefabSystemComponentInterface.h>
 #include <AzToolsFramework/UI/EditorEntityUi/EditorEntityUiInterface.h>
 #include <AzToolsFramework/UI/Prefab/PrefabIntegrationBus.h>
 
@@ -21,23 +24,59 @@ namespace AzToolsFramework
 {
     namespace Prefab
     {
-        void EditorPrefabComponent::Reflect(AZ::ReflectContext* context)
+        EditorPrefabOverride::EditorPrefabOverride(AZStd::string label, AZStd::string value)
+            : m_label(AZStd::move(label))
+            , m_value(AZStd::move(value))
+        {
+        }
+
+        void EditorPrefabOverride::Reflect(AZ::ReflectContext* context)
         {
             if (auto serializeContext = azrtti_cast<AZ::SerializeContext*>(context))
             {
-                serializeContext->Class<EditorPrefabComponent, EditorComponentBase>();
+                serializeContext->Class<EditorPrefabOverride>()
+                    ->Field("label", &EditorPrefabOverride::m_label)
+                    ->Field("value", &EditorPrefabOverride::m_value)
+                    ;
+
+                AZ::EditContext* editContext = serializeContext->GetEditContext();
+                if (editContext)
+                {
+                    editContext->Class<EditorPrefabOverride>("Prefab Override", "")
+                        ->ClassElement(AZ::Edit::ClassElements::EditorData, "")
+                        //->Attribute(AZ::Edit::Attributes::Visibility, AZ::Edit::PropertyVisibility::ShowChildrenOnly)
+                        ->DataElement(AZ::Edit::UIHandlers::Default, &EditorPrefabOverride::m_value, "Patch", "")
+                        ->Attribute(AZ::Edit::Attributes::NameLabelOverride, &EditorPrefabOverride::m_label)
+                        //->Attribute(AZ::Edit::Attributes::ReadOnly, true)
+                        ;
+                }
+            }
+        }
+
+        void EditorPrefabComponent::Reflect(AZ::ReflectContext* context)
+        {
+            EditorPrefabOverride::Reflect(context);
+
+            if (auto serializeContext = azrtti_cast<AZ::SerializeContext*>(context))
+            {
+                serializeContext->Class<EditorPrefabComponent, EditorComponentBase>()
+                    ->Field("patches", &EditorPrefabComponent::m_patches);
 
                 AZ::EditContext* editContext = serializeContext->GetEditContext();
                 if (editContext)
                 {
                     editContext->Class<EditorPrefabComponent>("Prefab Component", "")
                         ->ClassElement(AZ::Edit::ClassElements::EditorData, "")
-                        ->Attribute(AZ::Edit::Attributes::Visibility, AZ::Edit::PropertyVisibility::Hide)
-                        ->Attribute(AZ::Edit::Attributes::HideIcon, true)
+                        ->Attribute(AZ::Edit::Attributes::Visibility, AZ::Edit::PropertyVisibility::ShowChildrenOnly)
                         ->Attribute(
                             AZ::Edit::Attributes::SliceFlags,
                             AZ::Edit::SliceFlags::HideOnAdd | AZ::Edit::SliceFlags::PushWhenHidden |
-                                AZ::Edit::SliceFlags::DontGatherReference);
+                                AZ::Edit::SliceFlags::DontGatherReference)
+                        ->DataElement(AZ::Edit::UIHandlers::Default, &EditorPrefabComponent::m_patches, "Patches", "")
+                        ->Attribute(AZ::Edit::Attributes::Visibility, AZ::Edit::PropertyVisibility::ShowChildrenOnly)
+                        ->Attribute(AZ::Edit::Attributes::ContainerCanBeModified, false)
+                        ->Attribute(AZ::Edit::Attributes::ContainerReorderAllow, false)
+                        ->Attribute(AZ::Edit::Attributes::AutoExpand, true);
                 }
             }
 
@@ -63,6 +102,34 @@ namespace AzToolsFramework
         {
             PrefabInstanceContainerNotificationBus::Broadcast(
                 &PrefabInstanceContainerNotifications::OnPrefabComponentActivate, GetEntityId());
+
+            // Fetch all patches from direct parent (for now)
+            AZ::EntityId entityId = m_entity->GetId();
+
+            // Find LinkId to direct parent
+            auto instanceEntityMapperInterface = AZ::Interface<Prefab::InstanceEntityMapperInterface>::Get();
+            if (auto owningInstance = instanceEntityMapperInterface->FindOwningInstance(entityId); owningInstance.has_value())
+            {
+                auto linkId = owningInstance->get().GetLinkId();
+
+                // Get Link
+                auto prefabSystemComponentInterface = AZ::Interface<AzToolsFramework::Prefab::PrefabSystemComponentInterface>::Get();
+                auto findLinkResult = prefabSystemComponentInterface->FindLink(linkId);
+
+                if (findLinkResult.has_value())
+                {
+                    [[maybe_unused]] PrefabDom linkPatches;
+                    findLinkResult->get().GetLinkPatches(linkPatches, linkPatches.GetAllocator());
+
+                    for (const auto& patch : linkPatches.GetArray())
+                    {
+                        AZStd::string pathString = patch.GetObject().FindMember("path")->value.GetString();
+                        AZ::IO::Path path (pathString.c_str(), '/');
+
+                        m_patches.push_back({ path.RootName().String(), "123" });
+                    }
+                }
+            }
         }
 
         void EditorPrefabComponent::Deactivate()
--- Code/Framework/AzToolsFramework/AzToolsFramework/Prefab/EditorPrefabComponent.h
+++ Code/Framework/AzToolsFramework/AzToolsFramework/Prefab/EditorPrefabComponent.h
@@ -7,13 +7,30 @@
  */
 #pragma once
 
+#include <AzCore/Memory/SystemAllocator.h>
+#include <AzCore/std/string/string.h>
 #include <AzToolsFramework/ToolsComponents/EditorComponentBase.h>
 
 namespace AzToolsFramework
 {
     namespace Prefab
     {
-        class EditorPrefabComponent : public AzToolsFramework::Components::EditorComponentBase
+        struct EditorPrefabOverride final
+        {
+            AZ_RTTI(EditorPrefabOverride, "{348F250E-1EA9-4AA0-A02F-9D32D9B9B585}");
+            AZ_CLASS_ALLOCATOR(EditorPrefabOverride, AZ::SystemAllocator);
+
+            EditorPrefabOverride() = default;
+            EditorPrefabOverride(AZStd::string label, AZStd::string value);
+
+            static void Reflect(AZ::ReflectContext* context);
+
+            AZStd::string m_label;
+            AZStd::string m_value;
+        };
+
+        class EditorPrefabComponent
+            : public AzToolsFramework::Components::EditorComponentBase
         {
         public:
             static inline constexpr AZ::TypeId EditorPrefabComponentTypeId{ "{756E5F9C-3E08-4F8D-855C-A5AEEFB6FCDD}" };
@@ -27,6 +44,8 @@ namespace AzToolsFramework
         private:
             void Activate() override;
             void Deactivate() override;
+
+            AZStd::vector<EditorPrefabOverride> m_patches;
         };
     } // namespace Prefab
 } // namespace AzToolsFramework
