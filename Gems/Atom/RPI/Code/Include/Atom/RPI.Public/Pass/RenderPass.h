3ab24131378ba2f0d2560d8272b08cce0542bf99
Joerg H. Mueller <joerg.mueller@huawei.com>
2024-03-20 10:32:07 +0100

    Introduce deviceIndex in RenderPassData.
    Signed-off-by: Joerg H. Mueller <joerg.mueller@huawei.com>

--- Gems/Atom/RPI/Code/Include/Atom/RPI.Public/Pass/RenderPass.h
+++ Gems/Atom/RPI/Code/Include/Atom/RPI.Public/Pass/RenderPass.h
@@ -143,6 +143,9 @@ namespace AZ
             TimestampResult m_timestampResult;
             // Readback results from the PipelineStatistics queries
             PipelineStatisticsResult m_statisticsResult;
+
+            // The device index the pass should run on. Can be invalid if it doesn't matter.
+            int m_deviceIndex{RHI::MultiDevice::InvalidDeviceIndex};
             // The device index the pass ran on during the last frame, necessary to read the queries.
             int m_lastDeviceIndex = RHI::MultiDevice::DefaultDeviceIndex;
 
