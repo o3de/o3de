5762253b3ef5c92a4b160072c10bc382013cc681
Danilo Aimini <82231674+AMZN-daimini@users.noreply.github.com>
2024-03-26 11:53:10 +0100

    Ensure previous focused instance exists before accessing it
    Signed-off-by: Danilo Aimini <82231674+AMZN-daimini@users.noreply.github.com>

--- Code/Framework/AzToolsFramework/AzToolsFramework/Prefab/PrefabFocusHandler.cpp
+++ Code/Framework/AzToolsFramework/AzToolsFramework/Prefab/PrefabFocusHandler.cpp
@@ -288,7 +288,10 @@ namespace AzToolsFramework::Prefab
         m_rootAliasFocusPathLength = aznumeric_cast<int>(AZStd::distance(m_rootAliasFocusPath.begin(), m_rootAliasFocusPath.end()));
 
         // Unset the DOM caching for previous focus and enabled it in new focus to optimize editing.
-        previousFocusedInstance->get().EnableDomCaching(false);
+        if (previousFocusedInstance.has_value())
+        {
+            previousFocusedInstance->get().EnableDomCaching(false);
+        }
         focusedInstance->get().EnableDomCaching(true);
 
         // Focus on the container entity in the Editor, if the interface is initialized.
