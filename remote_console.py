d7eb9166c1bd4ffa14941ea3118b92340931aa96
jkim <junho@carbonated.com>
2024-04-04 15:42:43 -0700

    Added script for starting remote console
    Also prints out the log and saves the log

---
+++ remote_console.py
@@ -0,0 +1,35 @@
+import sys
+import signal
+import argparse
+import logging
+import Tools.RemoteConsole.ly_remote_console.ly_remote_console.remote_console_commands as remote_console_commands
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='remote_console.log', encoding='utf-8', filemode='w', level=logging.INFO)
+remoteConsole = None
+
+def main():
+	parser = argparse.ArgumentParser(description="Provides common dev functionality", add_help=False)
+	parser.add_argument('--addr', default='127.0.0.1', dest='address', help="Address for Remote Console to connect")
+	parser.add_argument('--port', default=4600, dest='port', help="Port for Remote Console to connect")
+
+	args, unknown = parser.parse_known_args(sys.argv[1:])
+
+	remoteConsole = remote_console_commands.RemoteConsole(addr=args.address,port=args.port,on_message_received=printLog)
+	remoteConsole.start()
+
+def printLog(raw):
+    # type: (str) -> None
+    refined = raw.strip()
+    if len(refined):
+        logger.info(raw)
+        print(refined)
+
+def handler(signal_received, frame):
+	print("Received Interrupt.  Exiting")
+	remoteConsole.stop()
+	sys.exit()
+
+if __name__ == "__main__":
+	signal.signal(signal.SIGINT, handler)
+	main()
