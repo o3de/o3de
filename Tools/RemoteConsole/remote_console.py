6040fb41b03bead72110b5915f5dc99c921ae6fb
vmednonogov <slava@tech-round.com>
2024-04-05 22:09:09 +0300

    Fix for failed authentication

--- Tools/RemoteConsole/remote_console.py
+++ Tools/RemoteConsole/remote_console.py
@@ -1,3 +1,10 @@
+"""
+Copyright (c) Contributors to the Open 3D Engine Project.
+For complete copyright and license terms please see the LICENSE at the root of this distribution.
+
+SPDX-License-Identifier: Apache-2.0 OR MIT
+"""
+
 import sys
 import signal
 import argparse
@@ -9,14 +16,14 @@ logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S
 remoteConsole = None
 
 def main():
-	parser = argparse.ArgumentParser(description="Provides common dev functionality", add_help=False)
-	parser.add_argument('--addr', default='127.0.0.1', dest='address', help="Address for Remote Console to connect")
-	parser.add_argument('--port', default=4600, dest='port', help="Port for Remote Console to connect")
+    parser = argparse.ArgumentParser(description="Provides common dev functionality", add_help=False)
+    parser.add_argument('--addr', default='127.0.0.1', dest='address', help="Address for Remote Console to connect")
+    parser.add_argument('--port', default=4600, dest='port', help="Port for Remote Console to connect")
 
-	args, unknown = parser.parse_known_args(sys.argv[1:])
+    args, unknown = parser.parse_known_args(sys.argv[1:])
 
-	remoteConsole = remote_console_commands.RemoteConsole(addr=args.address,port=args.port,on_message_received=printLog)
-	remoteConsole.start()
+    remoteConsole = remote_console_commands.RemoteConsole(addr=args.address,port=args.port,on_message_received=printLog)
+    remoteConsole.start()
 
 def printLog(raw):
     # type: (str) -> None
@@ -26,10 +33,10 @@ def printLog(raw):
         print(refined)
 
 def handler(signal_received, frame):
-	print("Received Interrupt.  Exiting")
-	remoteConsole.stop()
-	sys.exit()
+    print("Received Interrupt.  Exiting")
+    remoteConsole.stop()
+    sys.exit()
 
 if __name__ == "__main__":
-	signal.signal(signal.SIGINT, handler)
-	main()
+    signal.signal(signal.SIGINT, handler)
+    main()
