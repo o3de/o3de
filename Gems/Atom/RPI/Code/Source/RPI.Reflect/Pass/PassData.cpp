e808f20cf74d3a84a1ce71a42347768922ee0a29
galibzon <66021303+galibzon@users.noreply.github.com>
2024-04-11 07:59:11 -0500

    Fixed PassData.cpp
    Signed-off-by: galibzon <66021303+galibzon@users.noreply.github.com>

--- Gems/Atom/RPI/Code/Source/RPI.Reflect/Pass/PassData.cpp
+++ Gems/Atom/RPI/Code/Source/RPI.Reflect/Pass/PassData.cpp
@@ -92,9 +92,10 @@ namespace AZ
             if (auto* serializeContext = azrtti_cast<SerializeContext*>(context))
             {
                 serializeContext->Class<PassData>()
-                    ->Version(1)
+                    ->Version(2) // Added "MergeChildrenAsSubpasses"
                     ->Field("PipelineViewTag", &PassData::m_pipelineViewTag)
-                    ->Field("PipelineGlobalConnections", &PassData::m_pipelineGlobalConnections);
+                    ->Field("PipelineGlobalConnections", &PassData::m_pipelineGlobalConnections)
+                    ->Field("MergeChildrenAsSubpasses", &PassData::m_mergeChildrenAsSubpasses);
             }
         }
 
