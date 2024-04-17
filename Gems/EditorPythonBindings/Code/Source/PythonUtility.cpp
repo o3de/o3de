2c09990c555135d9941f8d24046128d53117c615
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2024-04-17 08:07:30 -0700

    Add more missing checks for signed/unsigned short/long. Add more detail to error message
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- Gems/EditorPythonBindings/Code/Source/PythonUtility.cpp
+++ Gems/EditorPythonBindings/Code/Source/PythonUtility.cpp
@@ -156,6 +156,10 @@ namespace EditorPythonBindings
                 AZ::s64 outboundPythonValue = 0;
 
                 bool converted = 
+                    ConvertPythonFromEnumClass<short>(underlyingTypeId, behaviorValue, outboundPythonValue) ||
+                    ConvertPythonFromEnumClass<unsigned short>(underlyingTypeId, behaviorValue, outboundPythonValue) ||
+                    ConvertPythonFromEnumClass<long>(underlyingTypeId, behaviorValue, outboundPythonValue) ||
+                    ConvertPythonFromEnumClass<unsigned long>(underlyingTypeId, behaviorValue, outboundPythonValue) ||
                     ConvertPythonFromEnumClass<AZ::u8>(underlyingTypeId, behaviorValue, outboundPythonValue) ||
                     ConvertPythonFromEnumClass<AZ::u16>(underlyingTypeId, behaviorValue, outboundPythonValue) ||
                     ConvertPythonFromEnumClass<AZ::u32>(underlyingTypeId, behaviorValue, outboundPythonValue) ||
@@ -207,6 +211,10 @@ namespace EditorPythonBindings
                     parameter.m_typeId = behaviorArgument.m_typeId;
 
                     bool handled = 
+                        ConvertBehaviorParameterEnum<short>(obj, underlyingTypeId, parameter) ||
+                        ConvertBehaviorParameterEnum<unsigned short>(obj, underlyingTypeId, parameter) ||
+                        ConvertBehaviorParameterEnum<long>(obj, underlyingTypeId, parameter) ||
+                        ConvertBehaviorParameterEnum<unsigned long>(obj, underlyingTypeId, parameter) ||
                         ConvertBehaviorParameterEnum<AZ::u8>(obj, underlyingTypeId, parameter) ||
                         ConvertBehaviorParameterEnum<AZ::u16>(obj, underlyingTypeId, parameter) ||
                         ConvertBehaviorParameterEnum<AZ::u32>(obj, underlyingTypeId, parameter) ||
@@ -356,7 +364,9 @@ namespace EditorPythonBindings
                     // so this code tries to pull out more type information about the typeId so that the user can get more human readable
                     // information than a UUID
                     LogSerializeTypeInfo(resultType->m_typeId);
-                    AZ_Error("python", behaviorClass, "A behavior class is missing for %s!",
+                    AZ_Error("python", behaviorClass, "A behavior class for method %s is missing for type '%s' (%s)!",
+                        behaviorMethod->m_name.c_str(),
+                        resultType->m_name,
                         resultType->m_typeId.ToString<AZStd::string>().c_str());
                 }
             }
@@ -845,6 +855,8 @@ namespace EditorPythonBindings
                     type = "bool";
                 }
                 else if (
+                    AZ::AzTypeInfo<short>::Uuid() == typeId || AZ::AzTypeInfo<unsigned short>::Uuid() == typeId ||
+                    AZ::AzTypeInfo<long>::Uuid() == typeId || AZ::AzTypeInfo<unsigned long>::Uuid() == typeId ||
                     AZ::AzTypeInfo<AZ::s8>::Uuid() == typeId || AZ::AzTypeInfo<AZ::u8>::Uuid() == typeId ||
                     AZ::AzTypeInfo<AZ::s16>::Uuid() == typeId || AZ::AzTypeInfo<AZ::u16>::Uuid() == typeId ||
                     AZ::AzTypeInfo<AZ::s32>::Uuid() == typeId || AZ::AzTypeInfo<AZ::u32>::Uuid() == typeId ||
