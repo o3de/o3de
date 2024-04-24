95bdea01fecb6007edab0bbae7d70e4048ca5a2c
Danilo Aimini <82231674+AMZN-daimini@users.noreply.github.com>
2024-04-24 11:07:41 +0200

    Fix permissions issue
    Signed-off-by: Danilo Aimini <82231674+AMZN-daimini@users.noreply.github.com>

--- Code/Editor/CryEdit.h
+++ Code/Editor/CryEdit.h
@@ -367,6 +367,8 @@ private:
 
     // @param files: A list of file paths, separated by '|';
     void OpenExternalLuaDebugger(AZStd::string_view luaDebuggerUri, AZStd::string_view enginePath, AZStd::string_view projectPath, const char * files);
+
+public:
     static bool Command_ExportToEngine();
 };
 
