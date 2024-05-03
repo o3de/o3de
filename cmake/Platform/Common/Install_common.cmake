1662c23230c8700742a71994f78e679d5ea9ca91
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2024-05-03 16:28:31 -0700

    Update to use the original svg
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- cmake/Platform/Common/Install_common.cmake
+++ cmake/Platform/Common/Install_common.cmake
@@ -1136,11 +1136,20 @@ function(ly_setup_o3de_install)
         COMPONENT ${CMAKE_INSTALL_DEFAULT_COMPONENT_NAME}
     )
 
-    ly_install(FILES
-        ${LY_ROOT_FOLDER}/snap/gui/o3de.svg
-        DESTINATION .
-        COMPONENT ${CMAKE_INSTALL_DEFAULT_COMPONENT_NAME}
-    )
+    if("$ENV{O3DE_PACKAGE_TYPE}" STREQUAL "SNAP")
+        ly_install(FILES
+            ${LY_ROOT_FOLDER}/snap/gui/o3de.desktop
+            ${LY_ROOT_FOLDER}/Code/Tools/ProjectManager/Resources/o3de.svg
+            DESTINATION snap/gui/
+            COMPONENT ${CMAKE_INSTALL_DEFAULT_COMPONENT_NAME}
+        )
+    elseif("$ENV{O3DE_PACKAGE_TYPE}" STREQUAL "DEB")
+        ly_install(FILES
+            ${LY_ROOT_FOLDER}/Code/Tools/ProjectManager/Resources/o3de.svg
+            DESTINATION .
+            COMPONENT ${CMAKE_INSTALL_DEFAULT_COMPONENT_NAME}
+        )
+    endif()
 
     # Inject other build directories
     foreach(external_dir ${LY_INSTALL_EXTERNAL_BUILD_DIRS})
