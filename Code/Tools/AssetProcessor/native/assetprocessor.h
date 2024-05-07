56b9572a0226741836c6a4f591245d09f5792cdf
Karl Haubenwallner <107414580+kh-huawei@users.noreply.github.com>
2024-05-08 00:56:27 +0200

    Set different max path lenghts for the different platforms (#17719)
    * Set different max path lenghts for the different platforms
    * added a warning if the asset path length exceeds 260 chars
    
    Signed-off-by: Karl Haubenwallner <karl.haubenwallner@huawei.com>

--- Code/Tools/AssetProcessor/native/assetprocessor.h
+++ Code/Tools/AssetProcessor/native/assetprocessor.h
@@ -41,9 +41,11 @@ namespace AssetProcessor
     const unsigned int g_RetriesForFenceFile = 5; // number of retries for fencing
     constexpr int RetriesForJobLostConnection = ASSETPROCESSOR_TRAIT_ASSET_BUILDER_LOST_CONNECTION_RETRIES; // number of times to retry a job when a network error due to network issues or a crashed AssetBuilder process is determined to have caused a job failure
     [[maybe_unused]] constexpr const char* IntermediateAssetsFolderName = "Intermediate Assets"; // name of the intermediate assets folder
+
     // Even though AP can handle files with path length greater than window's legacy path length limit, we have some 3rdparty sdk's
-    // which do not handle this case ,therefore we will make AP fail any jobs whose either source file or output file name exceeds the windows legacy path length limit
-#define AP_MAX_PATH_LEN 260
+    // which do not handle this case, therefore we will make AP warn about jobs whose either source file or output file name exceeds the windows legacy path length limit
+    // on all platforms
+#define ASSETPROCESSOR_WARN_PATH_LEN 260
 
     //! a shared convenience typedef for requests that have come over the network
     //! The first element is the connection id it came from and the second element is the serial number
