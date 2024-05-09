f5a31e4e89392f14b8942aa97779000f11ce7083
Michael Pollind <mpollind@gmail.com>
2024-05-08 19:49:56 -0700

    feat: remove unused Viewport cvar from ObjectManager (#17709)
    Signed-off-by: Michael Pollind <mpollind@gmail.com>

--- Code/Editor/Objects/ObjectManager.cpp
+++ Code/Editor/Objects/ObjectManager.cpp
@@ -727,16 +727,6 @@ void CObjectManager::LoadClassTemplates(const QString& path)
 }
 
 //////////////////////////////////////////////////////////////////////////
-void CObjectManager::RegisterCVars()
-{
-    REGISTER_CVAR2("AxisHelperHitRadius",
-        &m_axisHelperHitRadius,
-        20,
-        VF_DEV_ONLY,
-        "Adjust the hit radius used for axis helpers, like the transform gizmo.");
-}
-
-//////////////////////////////////////////////////////////////////////////
 void CObjectManager::InvalidateVisibleList()
 {
     if (m_isUpdateVisibilityList)
