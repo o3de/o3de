b5e9a04c713d551255fc56c5cea81afe8c179442
Alexander Efimov <>
2024-05-13 16:14:28 +0300

    Fix Mac OS SDK 14 deprecated items or suppress warnings

--- Code/Framework/AzQtComponents/AzQtComponents/Utilities/ScreenGrabber_mac.mm
+++ Code/Framework/AzQtComponents/AzQtComponents/Utilities/ScreenGrabber_mac.mm
@@ -57,8 +57,10 @@ namespace AzQtComponents
         QRect region({}, m_size);
         region.moveCenter(point);
         CGRect bounds = CGRectMake(region.x(), region.y(), region.width(), region.height());
-
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
         CGImageRef cgImage = CGWindowListCreateImageFromArray(bounds, windows, kCGWindowImageNominalResolution);
+#pragma clang diagnostic pop
         CFRelease(windows);
 
         QImage result = QtMac::fromCGImageRef(cgImage).toImage();
