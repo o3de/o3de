01049f8db67df62c3775c64dadd125b8657dcbbf
Martin Sattlecker <martin.sattlecker@huawei.com>
2024-05-22 14:55:24 +0200

    Add fence synchronization back to async upload queue fences
    Signed-off-by: Martin Sattlecker <martin.sattlecker@huawei.com>

--- Gems/Atom/RHI/Vulkan/Code/Source/RHI/AsyncUploadQueue.cpp
+++ Gems/Atom/RHI/Vulkan/Code/Source/RHI/AsyncUploadQueue.cpp
@@ -5,13 +5,14 @@
  * SPDX-License-Identifier: Apache-2.0 OR MIT
  *
  */
-#include <Atom/RHI/RHISystemInterface.h>
+#include <Atom/RHI.Reflect/ImageSubresource.h>
 #include <Atom/RHI.Reflect/PlatformLimitsDescriptor.h>
 #include <Atom/RHI/BufferPool.h>
+#include <Atom/RHI/RHISystemInterface.h>
 #include <Atom/RHI/StreamingImagePool.h>
-#include <Atom/RHI.Reflect/ImageSubresource.h>
 #include <AzCore/Component/TickBus.h>
 #include <AzCore/std/containers/vector.h>
+#include <AzCore/std/smart_ptr/make_shared.h>
 #include <RHI/AsyncUploadQueue.h>
 #include <RHI/Buffer.h>
 #include <RHI/BufferPool.h>
@@ -24,6 +25,7 @@
 #include <RHI/Image.h>
 #include <RHI/MemoryView.h>
 #include <RHI/Queue.h>
+#include <RHI/SignalEvent.h>
 
 namespace AZ
 {
@@ -93,6 +95,21 @@ namespace AZ
 
             RHI::Ptr<Fence> uploadFence = Fence::Create();
             uploadFence->Init(device, RHI::FenceState::Reset);
+
+            auto signalEvent = AZStd::make_shared<SignalEvent>();
+            uploadFence->SetSignalEvent(signalEvent);
+            uploadFence->SetSignalEventBitToSignal(0);
+            uploadFence->SetSignalEventDependencies(1);
+
+            if (request.m_fenceToSignal)
+            {
+                auto fenceToSignal = static_cast<Fence*>(request.m_fenceToSignal);
+                auto signalEvent = AZStd::make_shared<SignalEvent>();
+                fenceToSignal->SetSignalEvent(signalEvent);
+                fenceToSignal->SetSignalEventBitToSignal(0);
+                fenceToSignal->SetSignalEventDependencies(1);
+            }
+
             CommandQueue::Command command = [=, &device](void* queue)
             {
                 AZ_PROFILE_SCOPE(RHI, "Upload Buffer");
@@ -182,6 +199,11 @@ namespace AZ
 
             RHI::Ptr<Fence> uploadFence = Fence::Create();
             uploadFence->Init(device, RHI::FenceState::Reset);
+
+            auto signalEvent = AZStd::make_shared<SignalEvent>();
+            uploadFence->SetSignalEvent(signalEvent);
+            uploadFence->SetSignalEventBitToSignal(0);
+            uploadFence->SetSignalEventDependencies(1);
 
             CommandQueue::Command command = [=, &device](void* queue)
             {
@@ -463,6 +485,10 @@ namespace AZ
                 AZ_Assert(framePacket.m_stagingBuffer, "Failed to acquire staging buffer");
                 framePacket.m_fence = Fence::Create();
                 result = framePacket.m_fence->Init(device, RHI::FenceState::Signaled);
+                auto signalEvent = AZStd::make_shared<SignalEvent>();
+                framePacket.m_fence->SetSignalEvent(signalEvent);
+                framePacket.m_fence->SetSignalEventBitToSignal(0);
+                framePacket.m_fence->SetSignalEventDependencies(1);
                 RETURN_RESULT_IF_UNSUCCESSFUL(result);
             }
 
