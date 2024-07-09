3c8f9f46a72ca92c2c372412691e45d5c6aa56ad
Akio Gaule <10719597+akioCL@users.noreply.github.com>
2024-07-09 13:32:17 -0700

    Cherry pick mobile changes from stabilization 2409 (#18096)
    * Fix density map on Vulkan Android (#18022)
    
    Signed-off-by: Akio Gaule <10719597+akioCL@users.noreply.github.com>
    
    * Cherry pick mobile fixes (#18061)
    
    * Disable CopyToSwapchain pass for Mobile pipeline
    
    Signed-off-by: Akio Gaule <10719597+akioCL@users.noreply.github.com>
    (cherry picked from commit 5b743e444a4f1f0e754ef2501f6da07cd7bd8e6b)
    
    * Fix precision issue with cascade shadowmaps (#260)
    
    Signed-off-by: Akio Gaule <10719597+akioCL@users.noreply.github.com>
    
    * Add support to apply r_renderScale when r_resolutionMode is 0
    
    Signed-off-by: Akio Gaule <10719597+akioCL@users.noreply.github.com>
    
    * Fix NativeWindow size on iOS
    
    Signed-off-by: Akio Gaule <10719597+akioCL@users.noreply.github.com>
    
    * Fix per mesh shader options for instanced meshes (#278)
    
    Signed-off-by: Akio Gaule <10719597+akioCL@users.noreply.github.com>
    
    * Fix NANs showing up in DOF effect.
    
    Signed-off-by: moudgils <moudgils@amazon.com>
    
    * Fix pixelated shadows
    
    Signed-off-by: moudgils <moudgils@amazon.com>
    
    * Add Resize support for ios
    
    Signed-off-by: moudgils <moudgils@amazon.com>
    
    * Fix issues in case the pipeline wants to request and write to swapchain in the tonemapper pass.
    
    Signed-off-by: moudgils <moudgils@amazon.com>
    
    * Fix the issue whereby we had scopes from two different groups requesting swapchain drawable in parallel which is not allowed
    
    Signed-off-by: moudgils <moudgils@amazon.com>
    
    * Fixed the assert by ensuring that the scopes coming after the one requesting the swapchain drawable is not separated into a separate group based on commandlist cost.
    
    Signed-off-by: moudgils <moudgils@amazon.com>
    
    * Add deprecation to custom resolution functions
    
    Signed-off-by: Akio Gaule <10719597+akioCL@users.noreply.github.com>
    
    * Remove tabs
    
    Signed-off-by: Akio Gaule <10719597+akioCL@users.noreply.github.com>
    
    * Removed log
    
    Signed-off-by: Akio Gaule <10719597+akioCL@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Akio Gaule <10719597+akioCL@users.noreply.github.com>
    Signed-off-by: moudgils <moudgils@amazon.com>
    Co-authored-by: moudgils <moudgils@amazon.com>
    
    ---------
    
    Signed-off-by: Akio Gaule <10719597+akioCL@users.noreply.github.com>
    Signed-off-by: moudgils <moudgils@amazon.com>
    Co-authored-by: moudgils <moudgils@amazon.com>

--- Code/Framework/AzFramework/Platform/Common/Xcb/AzFramework/XcbNativeWindow.cpp
+++ Code/Framework/AzFramework/Platform/Common/Xcb/AzFramework/XcbNativeWindow.cpp
@@ -48,7 +48,7 @@ namespace AzFramework
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    void XcbNativeWindow::InitWindow(const AZStd::string& title, const WindowGeometry& geometry, const WindowStyleMasks& styleMasks)
+    void XcbNativeWindow::InitWindowInternal(const AZStd::string& title, const WindowGeometry& geometry, const WindowStyleMasks& styleMasks)
     {
         // Get the parent window
         const xcb_setup_t* xcbSetup = xcb_get_setup(m_xcbConnection);
@@ -432,10 +432,6 @@ namespace AzFramework
             {
                 WindowNotificationBus::Event(
                     reinterpret_cast<NativeWindowHandle>(m_xcbWindow), &WindowNotificationBus::Events::OnWindowResized, width, height);
-                if (!m_enableCustomizedResolution)
-                {
-                    WindowNotificationBus::Event(GetWindowHandle(), &WindowNotificationBus::Events::OnResolutionChanged, width, height);
-                }
             }
         }
     }
