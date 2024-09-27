6f27988ddcfa5953f4164e21b352ad41361bb6ab
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2024-09-27 17:41:13 +0000

    Remove unnecessary include
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- Code/Framework/AzFramework/AzFramework/Viewport/CameraInput.cpp
+++ Code/Framework/AzFramework/AzFramework/Viewport/CameraInput.cpp
@@ -13,7 +13,6 @@
 #include <AzFramework/AzFramework_Traits_Platform.h>
 #include <AzFramework/Input/Devices/Keyboard/InputDeviceKeyboard.h>
 #include <AzFramework/Input/Devices/Mouse/InputDeviceMouse.h>
-#include <AzFramework/Input/Buses/Requests/InputSystemCursorRequestBus.h>
 #include <AzFramework/Windowing/WindowBus.h>
 
 
