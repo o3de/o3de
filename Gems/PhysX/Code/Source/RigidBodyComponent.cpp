055c021edefb7ecd751b35f111d3611d06f3ee73
Niko <niko@carbonated.com>
2024-10-02 19:26:28 -0700

    Added guards

--- Gems/PhysX/Code/Source/RigidBodyComponent.cpp
+++ Gems/PhysX/Code/Source/RigidBodyComponent.cpp
@@ -405,7 +405,9 @@ namespace PhysX
         m_interpolator->Reset(transform.GetTranslation(), rotation);
 
         // set the transform to not update when the parent's transform changes, to avoid conflict with physics transform updates
+#if defined(CARBONATED)
         if (!m_configuration.m_kinematic)
+#endif
             GetEntity()->GetTransform()->SetOnParentChangedBehavior(AZ::OnParentChangedBehavior::DoNotUpdate);
 
         Physics::RigidBodyNotificationBus::Event(GetEntityId(), &Physics::RigidBodyNotificationBus::Events::OnPhysicsEnabled, GetEntityId());
