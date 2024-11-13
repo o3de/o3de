8f26dbf9967d58bb03b3ba21b4591293120678cd
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2024-11-13 08:07:19 -0800

    Legacy Android Script Deprecation (#17916)
    * Updates to support deprecation of legacy android generation scripts
    * Update gradle to version 8.7 for the new android project generation workflow
    * Update JDK to jdk17 for the new android project generation workflow to support the latest android_sdk
    * Add new 'NONE' option to 'ASSET_MODE' to support generating APK packages without assets (needed for gradle AR, but still a valid use case)
    * Update the gradle_windows.cmd to use the new android project generation scripts in the o3de command line tools
    * Update how AndroidSDK command line tools is downloaded
    * Update pipeline node label for new windows image as part of this update
    * Additional fixes
    * Fix ANDROID_SDK_ROOT
    * Fix AndroidSDK command path
    * Update scripts/build/Platform/Android/pipeline.json
    * Update scripts/build/build_node/Platform/Windows/install-windows-android.ps1
    
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>
    Co-authored-by: Mike Chang <changml@amazon.com>

--- scripts/build/build_node/Platform/Windows/install-windows-android.ps1
+++ scripts/build/build_node/Platform/Windows/install-windows-android.ps1
@@ -5,9 +5,21 @@ For complete copyright and license terms please see the LICENSE at the root of t
 SPDX-License-Identifier: Apache-2.0 OR MIT
 #>
 
-Write-Host "Installing Android SDK"
-choco install -y android-sdk
-[Environment]::SetEnvironmentVariable("ANDROID_HOME", "C:\Android\android-sdk", [EnvironmentVariableTarget]::Machine)
+Import-Module C:\ProgramData\chocolatey\helpers\chocolateyInstaller.psm1 
+
+
+
+#Android SDK Command line tools needs a custom installer due the latest command like version not being available in the Chocolatey repository
+$androidSdkPackageName = 'androidsdk'
+$androidSdkVersion = '11076708_latest'
+$androidSdkUrl = "https://dl.google.com/android/repository/commandlinetools-win-$androidSdkVersion.zip"
+$androidSdkChecksum = '4d6931209eebb1bfb7c7e8b240a6a3cb3ab24479ea294f3539429574b1eec862'
+$androidSdkInstallDir = "C:\AndroidSdk\cmdline-tools"
+$androidSdkLatestDir = "$androidSdkInstallDir\latest"
+Install-ChocolateyZipPackage $androidSdkPackageName $androidSdkUrl $androidSdkInstallDir -Checksum $androidSdkChecksum -ChecksumType 'sha256'
+[Environment]::SetEnvironmentVariable("ANDROID_SDK_ROOT", "C:\AndroidSdk", [EnvironmentVariableTarget]::Machine)
+# Rename the Android SDK folder 'cmdline-tools' to match what the command line tools looks like normally in a full AndroidSDK install
+Rename-Item -Path "$androidSdkInstallDir\cmdline-tools" "$androidSdkLatestDir"
 
 # Set package versions
 $android_packages = '"platforms;android-28" "platforms;android-29" "platforms;android-30"'
@@ -16,7 +28,8 @@ $build_tools = '"build-tools;30.0.2" "build-tools;34.0.0" "tools"'
 $ndk = '"ndk;21.4.7075529" "ndk;23.1.7779620" "ndk;25.1.8937393" "ndk;25.2.9519653"'
 
 Write-Host "Installing Android SDK packages"
-$sdkmanager = "C:\Android\android-sdk\tools\bin\sdkmanager.bat"
+$sdkmanager = "$androidSdkLatestDir\bin\sdkmanager.bat"
+Start-Process -FilePath $sdkmanager -ArgumentList "--licenses" -NoNewWindow -Wait # Accept all licenses manually
 Start-Process -FilePath $sdkmanager -ArgumentList $android_packages -NoNewWindow -Wait
 Start-Process -FilePath $sdkmanager -ArgumentList $googleplay_packages -NoNewWindow -Wait
 Start-Process -FilePath $sdkmanager -ArgumentList $build_tools -NoNewWindow -Wait
@@ -25,8 +38,8 @@ Start-Process -FilePath $sdkmanager -ArgumentList $ndk -NoNewWindow -Wait
 [Environment]::SetEnvironmentVariable("LY_NDK_DIR", "C:\AndroidSdk\ndk\25.1.8937393", [EnvironmentVariableTarget]::Machine)
 
 Write-Host "Installing Gradle"
-$gradle_version = '7.0'
-$gradle_checksum = '81003F83B0056D20EEDF48CDDD4F52A9813163D4BA185BCF8ABD34B8EEEA4CBD'
+$gradle_version = '8.7'
+$gradle_checksum = '194717442575a6f96e1c1befa2c30e9a4fc90f701d7aee33eb879b79e7ff05c0'
 
 #Gradle needs a custom installer due to being hardcoded to C:\Programdata in Chocolatey
 Import-Module C:\ProgramData\chocolatey\helpers\chocolateyInstaller.psm1 
