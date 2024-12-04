71af1fe8e734de249638388e04844d2ad5b0b3e9
galibzon <66021303+galibzon@users.noreply.github.com>
2024-12-04 13:47:18 -0600

    Fixes loading XInput9_1_0.dll. (#18535)
    * Fixes loading XInput9_1_0.dll.
    
    I noticed my gamepad stopped working.
    After some debugging I found that O3DE
    failed to load "XInput9_1_0.dll".
    The failured was introduced in PR #18525:
    https://github.com/o3de/o3de/pull/18525
    
    According to MSFT Docs:
    https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-loadlibraryexw
    When using `LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR`:
    ```
    The lpFileName parameter must specify a fully qualified path.
    ```
    
    The failure occured because "XInput9_1_0.dll" is NOT a fully qualified
    path and it is expected to be found in the SYSTEM32 directory.
    
    The solution is to only use `LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR` when
    the module path is fully qualified.
    
    Given that all DLL paths are fully qualified
    except for "XInput9_1_0.dll", it's better just to call
    LoadLibraryExW for a second time if the first time failed.
    The second time the flag LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR is removed.
    
    Signed-off-by: galibzon <66021303+galibzon@users.noreply.github.com>

--- Code/Framework/AzCore/Platform/Common/WinAPI/AzCore/Module/DynamicModuleHandle_WinAPI.cpp
+++ Code/Framework/AzCore/Platform/Common/WinAPI/AzCore/Module/DynamicModuleHandle_WinAPI.cpp
@@ -109,6 +109,13 @@ namespace AZ
                 {
                     // Note: Windows LoadLibrary has no concept of specifying that the module symbols are global or local
                     m_handle = LoadLibraryExW(fileNameW, NULL, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS | LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR);
+                    if (!m_handle)
+                    {
+                        // If the first time failed, most likely occurred because @fileNameW is not a fully qualified path.
+                        // Per API spec, LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR requires a fully qualified path.
+                        // Cases like "XInput9_1_0.dll" should be loaded without LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR.
+                        m_handle = LoadLibraryExW(fileNameW, NULL, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
+                    }
                 }
             }
             else
