f0d49adbcebfd4ebecd93ee6387a2682ac263162
niko <niko@carbonated.com>
2025-02-03 11:48:30 -0800

    Realized I missed an if def

--- Code/Legacy/CrySystem/LevelSystem/SpawnableLevelSystem.cpp
+++ Code/Legacy/CrySystem/LevelSystem/SpawnableLevelSystem.cpp
@@ -526,13 +526,14 @@ namespace LegacyLevelSystem
         AzFramework::LevelSystemLifecycleNotificationBus::Broadcast(
             &AzFramework::LevelSystemLifecycleNotifications::OnLoadingComplete, levelName);
 
-        #if defined(CARBONATED)
+#if AZ_LOADSCREENCOMPONENT_ENABLED
+    #if defined(CARBONATED)
         EBUS_EVENT(LoadScreenBus, Pause); //We will manually stop the loading screen to ensure it times well
-        #else
-    #if AZ_LOADSCREENCOMPONENT_ENABLED
+    #else
         EBUS_EVENT(LoadScreenBus, Stop);
-    #endif // if AZ_LOADSCREENCOMPONENT_ENABLED
-        #endif
+    #endif
+#endif // if AZ_LOADSCREENCOMPONENT_ENABLED
+
         AZ_TracePrintf("LevelSystem", "Level load complete: '%s'\n", levelName);
     }
 
