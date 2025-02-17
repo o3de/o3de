2cbc9f85814e78435ba65e6c1237c36f442808fe
Aleks Starykh <astarykh@tech-round.com>
2025-02-17 18:21:54 +0300

    Make m_trackMultiplier setters safe
    Signed-off-by: Aleks Starykh <astarykh@tech-round.com>

--- Gems/Maestro/Code/Source/Cinematics/AnimTrack.h
+++ Gems/Maestro/Code/Source/Cinematics/AnimTrack.h
@@ -365,7 +365,10 @@ namespace Maestro
 
         void SetMultiplier(float trackMultiplier) override
         {
-            m_trackMultiplier = trackMultiplier;
+            if (AZStd::abs(trackMultiplier) > AZ::Constants::Tolerance)
+            {
+                m_trackMultiplier = trackMultiplier;
+            }
         }
 
         void SetExpanded([[maybe_unused]] bool expanded) override
@@ -428,7 +431,7 @@ namespace Maestro
 
         IAnimNode* m_node;
 
-        float m_trackMultiplier;
+        float m_trackMultiplier = 1.0f;
 
         unsigned int m_id = 0;
     };
