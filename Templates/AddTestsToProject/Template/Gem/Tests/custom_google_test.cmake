51261b0a9923518ffa46dbfb33c7562ebabdee98
Nicholas Lawson <70027408+nick-l-o3de@users.noreply.github.com>
2025-03-18 07:59:58 -0700

    Adds license snippet to tests
    Signed-off-by: Nicholas Lawson <70027408+nick-l-o3de@users.noreply.github.com>

--- Templates/AddTestsToProject/Template/Gem/Tests/custom_google_test.cmake
+++ Templates/AddTestsToProject/Template/Gem/Tests/custom_google_test.cmake
@@ -1,3 +1,10 @@
+# {BEGIN_LICENSE}
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+# {END_LICENSE}
 
 # Include this file in your Project/Gem/CMakeLists.txt using "include(Tests/custom_google_test.cmake)" at the end.
 
