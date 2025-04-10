8fff607126c467e520620cf739b32645b329cbef
Abdullah Farwees <abdullahfarwees@users.noreply.github.com>
2025-04-10 06:57:44 +0530

    DCO Remediation Commit for Abdullah Farwees <abdullah.farwees@gmail.com> (#16538)
    I, Abdullah Farwees <abdullah.farwees@gmail.com>, hereby add my Signed-off-by to this commit: 62bba8a0c3fb18a054c627bb82cc6d935111cce6
    
    Signed-off-by: Abdullah Farwees <abdullah.farwees@gmail.com>
    Signed-off-by: Luis Sempé <58790905+lsemp3d@users.noreply.github.com>
    Co-authored-by: Luis Sempé <58790905+lsemp3d@users.noreply.github.com>

--- Gems/AtomLyIntegration/CommonFeatures/Code/Source/PostProcess/ChromaticAberration/EditorChromaticAberrationComponent.cpp
+++ Gems/AtomLyIntegration/CommonFeatures/Code/Source/PostProcess/ChromaticAberration/EditorChromaticAberrationComponent.cpp
@@ -35,7 +35,7 @@ namespace AZ
                         ->Attribute(Edit::Attributes::AutoExpand, true)
                         ->Attribute(
                             Edit::Attributes::HelpPageURL,
-                            "https://www.o3de.org/docs/user-guide/components/reference/atom/ChromaticAberration/") // [TODO ATOM-2672][PostFX] need create page for PostProcessing.
+                            "https://docs.o3de.org/docs/user-guide/components/reference/atom/chromatic-aberration/")
                         ;
 
                     editContext->Class<ChromaticAberrationComponentController>("ChromaticAberrationComponentController", "")
