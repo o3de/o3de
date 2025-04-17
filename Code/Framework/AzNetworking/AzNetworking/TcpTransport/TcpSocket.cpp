o3de/o3de

commit 59128cb8a5bc9b4e9f29b623106eefbc8c446d12

ScriptCanvas: Fix crash on SC editor close (#18734)
* Clear the tabwidget parent prior to destruction
* Additional fixes, remove complexity
* Fix accidental code removal
* Removed no longer needed hack
* Review feedback

---------

Signed-off-by: Luis Sempé <58790905+lsemp3d@users.noreply.github.com>

Gems/ScriptCanvas/Code/Editor/View/Widgets/LoggingPanel/LoggingWindow.cpp
@@ -25,8 +25,10 @@ namespace ScriptCanvasEditor
         , m_ui(new Ui::LoggingWindow)
     {
         m_ui->setupUi(this);
+        m_ui->emptyCapture->setParent(this);
 
         // Hack to hide the close button on the first tab. Since we always want it open.
+        
         m_ui->tabWidget->setTabsClosable(true);
         m_ui->tabWidget->tabBar()->setTabButton(0, QTabBar::ButtonPosition::RightSide, nullptr);
         m_ui->tabWidget->tabBar()->setTabButton(0, QTabBar::ButtonPosition::LeftSide, nullptr);
@@ -47,11 +49,11 @@ namespace ScriptCanvasEditor
 
         OnActiveTabChanged(m_ui->tabWidget->currentIndex());
         PivotOnEntities();
+
     }
 
     LoggingWindow::~LoggingWindow()
     {
-
     }
 
     void LoggingWindow::OnActiveTabChanged([[maybe_unused]] int index)

Gems/GraphCanvas/Code/StaticLib/GraphCanvas/Widgets/GraphOutliner/GraphOutlinerDockWidget.cpp
@@ -91,6 +91,7 @@ namespace GraphCanvas
         verticalLayout->addWidget(m_nodelistTable);
 
         this->setWindowTitle("Graph Outliner");
+        this->setObjectName("GraphOutlinerDockWidget");
         this->setWidget(dockWidgetContents);
 
     }

Gems/ScriptCanvas/Code/Editor/View/Widgets/LoggingPanel/LiveWindowSession/LiveLoggingDataAggregator.cpp
@@ -117,7 +117,7 @@ namespace ScriptCanvasEditor
         RemoveStaticRegistration(namedEntityId, oldGraphIdentifier);
     }
 
-    void LiveLoggingDataAggregator::Connected([[maybe_unused]] const ScriptCanvas::Debugger::Target& target)
+    void LiveLoggingDataAggregator::Connected([[maybe_unused]] ScriptCanvas::Debugger::Target& target)
     {
         AZStd::lock(m_notificationMutex);
         SetupExternalEntities();

Gems/ScriptCanvas/Code/Editor/View/Widgets/LoggingPanel/LiveWindowSession/LiveLoggingDataAggregator.h
@@ -30,7 +30,7 @@ namespace ScriptCanvasEditor
     public:
         AZ_CLASS_ALLOCATOR(LiveLoggingDataAggregator, AZ::SystemAllocator);
         LiveLoggingDataAggregator();
-        ~LiveLoggingDataAggregator();
+        ~LiveLoggingDataAggregator() override;
 
         // ClientUINotificationBus
         void OnCurrentTargetChanged() override;
@@ -50,7 +50,7 @@ namespace ScriptCanvasEditor
 
         // ServiceNotifications
         //// Logging Notifications
-        void Connected(const ScriptCanvas::Debugger::Target& target) override;
+        void Connected(ScriptCanvas::Debugger::Target& target) override;
         void GraphActivated(const ScriptCanvas::GraphActivation& activatedSignal) override;
         void GraphDeactivated(const ScriptCanvas::GraphDeactivation& deactivatedSignal) override;
         

Gems/ScriptCanvas/Code/Editor/View/Widgets/LoggingPanel/LiveWindowSession/LiveLoggingWindowSession.cpp
@@ -348,7 +348,7 @@ namespace ScriptCanvasEditor
         }
     }
 
-    void LiveLoggingWindowSession::Connected([[maybe_unused]] const ScriptCanvas::Debugger::Target& target)
+    void LiveLoggingWindowSession::Connected([[maybe_unused]] ScriptCanvas::Debugger::Target& target)
     {
         if (m_userSettings->IsAutoCaptureEnabled() && isVisible())
         {

Gems/ScriptCanvas/Code/Editor/View/Widgets/LoggingPanel/LiveWindowSession/LiveLoggingWindowSession.h
@@ -100,7 +100,7 @@ namespace ScriptCanvasEditor
         ////
 
         // ScriptCavnas::Debugger::ServiceNotificationsBus
-        void Connected(const ScriptCanvas::Debugger::Target& target) override;
+        void Connected(ScriptCanvas::Debugger::Target& target) override;
         ////
         
     protected:

Gems/ScriptCanvas/Code/Editor/View/Widgets/LoggingPanel/LoggingWindow.h
@@ -10,6 +10,7 @@
 
 #if !defined(Q_MOC_RUN)
 #include <QButtonGroup>
+#include <QScopedPointer>
 
 #include <AzCore/Memory/SystemAllocator.h>
 #include <AzQtComponents/Components/StyledDockWidget.h>
@@ -49,11 +50,11 @@ namespace ScriptCanvasEditor
 
         PivotTreeWidget* GetActivePivotWidget() const;
 
-        AZStd::unique_ptr<Ui::LoggingWindow> m_ui;
+        QScopedPointer<Ui::LoggingWindow> m_ui;
 
         QButtonGroup m_pivotGroup;
 
-        LoggingDataId m_activeDataId;        
+        LoggingDataId m_activeDataId;
 
         int m_entityPageIndex;
         int m_graphPageIndex;

Gems/ScriptCanvas/Code/Include/ScriptCanvas/Asset/ExecutionLogAsset.cpp
@@ -16,27 +16,17 @@ namespace ScriptCanvas
     {
         *this = source;
     }
-    
-    ExecutionLogData::ExecutionLogData(ExecutionLogData&& other)
-        : m_events(AZStd::move(other.m_events))
-    {
-    }
-        
+
     ExecutionLogData::~ExecutionLogData()
     {
         Clear();
     }
-    
+
     void ExecutionLogData::Clear()
     {
-        for (auto entry : m_events)
-        {
-            delete entry;
-        }
-
-        m_events.resize(0);
+        m_events.clear();
     }
-    
+
     ExecutionLogData& ExecutionLogData::operator=(const ExecutionLogData& source)
     {
         Clear();
@@ -45,18 +35,7 @@ namespace ScriptCanvas
     
         for (auto entry : source.m_events)
         {
-            AZ_Assert(entry, "there should never bee a nullptr entry in an event log");
-            m_events.push_back(entry->Duplicate());
-        }
-
-        return *this;
-    }
-    
-    ExecutionLogData& ExecutionLogData::operator=(ExecutionLogData&& other)
-    {
-        if (this != &other)
-        {
-            m_events = AZStd::move(other.m_events);
+            m_events.push_back(entry);
         }
 
         return *this;

commit f62e49032cf8461e1ebfd87dac20993d9cef63bf

Fixes spam when a TCP socket fails to bind in Remote tools. They try 100x a second, it is not helpful to show that.
Also fixes the Script Canvas window's live toolbar missing,
since it uses Remote Tools.  Note that it was broken by a previous
PR https://github.com/o3de/o3de/pull/18734 - and I retested
the broken conditions (closing SC and opening it) under various
conditions, in debug, and profile, and it does not crash with this change.

Other fixes, such as ASAN fixes have been made since then.

Signed-off-by: Nicholas Lawson <70027408+nick-l-o3de@users.noreply.github.com>

Code/Framework/AzNetworking/AzNetworking/TcpTransport/TcpSocket.cpp
@@ -157,7 +157,11 @@ namespace AzNetworking
         if (::bind(aznumeric_cast<int32_t>(m_socketFd), (const sockaddr*)&hints, sizeof(hints)) != 0)
         {
             const int32_t error = GetLastNetworkError();
-            AZLOG_WARN("Failed to bind TCP socket to port %u (%d:%s)", uint32_t(port), error, GetNetworkErrorDesc(error));
+            if (m_warnedBindForPortFailure != port)
+            {
+                m_warnedBindForPortFailure = port;
+                AZLOG_WARN("Failed to bind TCP listen socket to port %u (%d:%s)", uint32_t(port), error, GetNetworkErrorDesc(error));
+            }
             return false;
         }
 
@@ -183,8 +187,12 @@ namespace AzNetworking
 
             if (::bind(aznumeric_cast<int32_t>(m_socketFd), (const sockaddr*)&hints, sizeof(hints)) != 0)
             {
-                const int32_t error = GetLastNetworkError();
-                AZLOG_WARN("Failed to bind TCP socket to port %u (%d:%s)", uint32_t(localPort), error, GetNetworkErrorDesc(error));
+                if (m_warnedBindForPortFailure != localPort)
+                {
+                    m_warnedBindForPortFailure = localPort;
+                    const int32_t error = GetLastNetworkError();
+                    AZLOG_WARN("Failed to bind TCP connect socket to port %u (%d:%s)", uint32_t(localPort), error, GetNetworkErrorDesc(error));
+                }
                 return false;
             }
         }

Code/Framework/AzNetworking/AzNetworking/TcpTransport/TcpSocket.h
@@ -86,6 +86,12 @@ namespace AzNetworking
         bool SocketCreateInternal();
 
         SocketFd m_socketFd;
+
+        // prevent warning spam for bind failure, if something is sitting on the port.
+        // These threads usually try 100x a second or faster, so warning for every bind failure is bad
+        // We'll remember the last warned port so that if the app layer responds to a listen failure
+        // by trying another port, that will show up.
+        uint16_t m_warnedBindForPortFailure = 0; //! Which port have we most recently warned for?
     };
 }
 

Gems/ScriptCanvas/Code/Editor/View/Widgets/LoggingPanel/LoggingWindow.cpp
@@ -25,7 +25,6 @@ namespace ScriptCanvasEditor
         , m_ui(new Ui::LoggingWindow)
     {
         m_ui->setupUi(this);
-        m_ui->emptyCapture->setParent(this);
 
         // Hack to hide the close button on the first tab. Since we always want it open.
         

Gems/ScriptCanvas/Code/Include/ScriptCanvas/Utils/ScriptCanvasConstants.h
@@ -14,5 +14,5 @@ namespace ScriptCanvas
 {
     static const AZ::Name RemoteToolsName = AZ::Name::FromStringLiteral("ScriptCanvasRemoteTools", nullptr);
     static constexpr AZ::Crc32 RemoteToolsKey("ScriptCanvasRemoteTools");
-    static constexpr uint16_t RemoteToolsPort = 6787;
+    static constexpr uint16_t RemoteToolsPort = 45641;
 }