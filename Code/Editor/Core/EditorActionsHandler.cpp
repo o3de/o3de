472e8829fd51e53e2bdba3513fd76c02e22ca2b1
derikson-dev <85710361+derikson-dev@users.noreply.github.com>
2025-05-24 12:03:44 -0300

    Added shorcut for play game maximized.
    Signed-off-by: derikson-dev <85710361+derikson-dev@users.noreply.github.com>

--- Code/Editor/Core/EditorActionsHandler.cpp
+++ Code/Editor/Core/EditorActionsHandler.cpp
@@ -930,6 +930,8 @@ void EditorActionsHandler::OnActionRegistrationHook()
 
         // This action is only accessible outside of Component Modes
         m_actionManagerInterface->AssignModeToAction(AzToolsFramework::DefaultActionContextModeIdentifier, actionIdentifier);
+
+        m_hotKeyManagerInterface->SetActionHotKey(actionIdentifier, "Ctrl+Shift+G");
     }
 
     // Simulate
