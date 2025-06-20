240c61489ad77bef56b361ae019f33e1c6fbc32d
ExMatics HydrogenC <33123710+HydrogenC@users.noreply.github.com>
2025-06-20 11:56:54 +0800

    Fix compilation error
    Signed-off-by: ExMatics HydrogenC <33123710+HydrogenC@users.noreply.github.com>

--- Gems/Atom/Feature/Common/Code/Include/Atom/Feature/PostProcess/MotionBlur/MotionBlurConstants.h
+++ Gems/Atom/Feature/Common/Code/Include/Atom/Feature/PostProcess/MotionBlur/MotionBlurConstants.h
@@ -8,6 +8,8 @@
 
 #pragma once
 
+#include <AzCore/base.h>
+
 namespace AZ
 {
     namespace Render
--- GenerateVS.bat
+++
@@ -1,1 +0,0 @@
-cmake -B build/windows -S . -G "Visual Studio 17" -DLY_3RDPARTY_PATH=C:\Users\x1398\.o3de\3rdParty -DCMAKE_POLICY_VERSION_MINIMUM=3.5
