37eadff08a312daf9a34c467f82eadaa95b2dffb
kiotk <213088872+kiotk@users.noreply.github.com>
2025-06-26 22:33:54 +0200

    fix-descriptorBindingPartiallyBound-misassignment (#19059)
    Signed-off-by: kiotk <213088872+kiotk@users.noreply.github.com>

--- Gems/Atom/RHI/Vulkan/Code/Source/RHI/PhysicalDevice.h
+++ Gems/Atom/RHI/Vulkan/Code/Source/RHI/PhysicalDevice.h
@@ -95,7 +95,7 @@ namespace AZ
             const VkPhysicalDeviceProperties& GetPhysicalDeviceProperties() const;
             const VkPhysicalDeviceConservativeRasterizationPropertiesEXT& GetPhysicalDeviceConservativeRasterProperties() const;
             const VkPhysicalDeviceDepthClipEnableFeaturesEXT& GetPhysicalDeviceDepthClipEnableFeatures() const;
-            const VkPhysicalDeviceRobustness2FeaturesEXT& GetPhysicalDeviceRobutness2Features() const;
+            const VkPhysicalDeviceRobustness2FeaturesEXT& GetPhysicalDeviceRobustness2Features() const;
             const VkPhysicalDeviceShaderFloat16Int8FeaturesKHR& GetPhysicalDeviceFloat16Int8Features() const;
             const VkPhysicalDeviceDescriptorIndexingFeaturesEXT& GetPhysicalDeviceDescriptorIndexingFeatures() const;
             const VkPhysicalDeviceBufferDeviceAddressFeaturesEXT& GetPhysicalDeviceBufferDeviceAddressFeatures() const;
@@ -145,7 +145,7 @@ namespace AZ
             VkPhysicalDeviceProperties m_deviceProperties{};
             VkPhysicalDeviceConservativeRasterizationPropertiesEXT m_conservativeRasterProperties{};
             VkPhysicalDeviceDepthClipEnableFeaturesEXT m_dephClipEnableFeatures{};
-            VkPhysicalDeviceRobustness2FeaturesEXT m_robutness2Features{};
+            VkPhysicalDeviceRobustness2FeaturesEXT m_robustness2Features{};
             VkPhysicalDeviceShaderFloat16Int8FeaturesKHR m_float16Int8Features{};
             VkPhysicalDeviceDescriptorIndexingFeaturesEXT m_descriptorIndexingFeatures{};
             VkPhysicalDeviceBufferDeviceAddressFeaturesEXT m_bufferDeviceAddressFeatures{};
