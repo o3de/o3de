057e0963f4cbe750dfe3fdf61ae6299282e4f342
GaianHelmers <gaianhelmers@genomestudios.ca>
2025-07-03 17:18:26 -0600

    Annoying. The clang format didn't stage with the rest of my commit. Sorry for the extra commit.
    Signed-off-by: GaianHelmers <gaianhelmers@genomestudios.ca>

--- Gems/LyShine/Code/Source/UiElementGroupComponent.cpp
+++ Gems/LyShine/Code/Source/UiElementGroupComponent.cpp
@@ -7,9 +7,9 @@
  */
 #include "UiElementGroupComponent.h"
 
-#include <AzCore/Serialization/SerializeContext.h>
-#include <AzCore/Serialization/EditContext.h>
 #include <AzCore/RTTI/BehaviorContext.h>
+#include <AzCore/Serialization/EditContext.h>
+#include <AzCore/Serialization/SerializeContext.h>
 
 #include <LyShine/Bus/UiElementBus.h>
 #include <LyShine/Bus/UiInteractableBus.h>
@@ -33,9 +33,15 @@ void UiElementGroupComponent::Deactivate()
 void UiElementGroupComponent::InGamePostActivate()
 {
     UiInitializationBus::Handler::BusDisconnect();
-    
-    if(!m_isInteractionLocallyEnabled) { SetInteractivity(false); }
-    if(!m_isRenderingLocallyEnabled) { SetRendering(false); }
+
+    if (!m_isInteractionLocallyEnabled)
+    {
+        SetInteractivity(false);
+    }
+    if (!m_isRenderingLocallyEnabled)
+    {
+        SetRendering(false);
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -58,11 +64,11 @@ bool UiElementGroupComponent::SetParentInteractivity(bool parentEnabled)
 void UiElementGroupComponent::UpdateInteractiveState()
 {
     const bool effectiveState = GetInteractiveState();
-    
-    //Affect the current entity.
+
+    // Affect the current entity.
     UiInteractableBus::Event(GetEntityId(), &UiInteractableInterface::SetIsHandlingEvents, effectiveState);
     UiInteractableBus::Event(GetEntityId(), &UiInteractableInterface::SetIsHandlingMultiTouchEvents, effectiveState);
-    
+
     DoRecursiveSetInteractivityToChildren(GetEntityId(), effectiveState);
 }
 
@@ -71,20 +77,20 @@ void UiElementGroupComponent::DoRecursiveSetInteractivityToChildren(const AZ::En
 {
     AZStd::vector<AZ::EntityId> children;
     UiElementBus::EventResult(children, parentId, &UiElementInterface::GetChildEntityIds);
-    
+
     for (const AZ::EntityId& child : children)
     {
-        //If has ElementGroup, will return true;
+        // If has ElementGroup, will return true;
         bool hasGroup = false;
         UiElementGroupBus::EventResult(hasGroup, child, &UiElementGroupInterface::SetParentInteractivity, parentState);
-        
-        //Because no group found, affect child and recurse.
+
+        // Because no group found, affect child and recurse.
         if (!hasGroup)
         {
             // Affect interactable state directly
             UiInteractableBus::Event(child, &UiInteractableInterface::SetIsHandlingEvents, parentState);
             UiInteractableBus::Event(child, &UiInteractableInterface::SetIsHandlingMultiTouchEvents, parentState);
-            
+
             // Recurse into this child's children
             DoRecursiveSetInteractivityToChildren(child, parentState);
         }
@@ -103,8 +109,8 @@ bool UiElementGroupComponent::SetRendering(bool enabled)
 bool UiElementGroupComponent::GetRenderingState()
 {
     UiElementBus::EventResult(m_isRenderingLocallyEnabled, GetEntityId(), &UiElementInterface::IsRenderEnabled);
-    
-    return m_isRenderingLocallyEnabled; 
+
+    return m_isRenderingLocallyEnabled;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -113,32 +119,39 @@ void UiElementGroupComponent::Reflect(AZ::ReflectContext* context)
     if (auto serializeContext = azrtti_cast<AZ::SerializeContext*>(context))
     {
         serializeContext->Class<UiElementGroupComponent, AZ::Component>()
-        ->Version(1)
-        ->Field("LocalInteraction", &UiElementGroupComponent::m_isInteractionLocallyEnabled)
-        ->Field("LocalRendering", &UiElementGroupComponent::m_isRenderingLocallyEnabled)
-        ;
-        
+            ->Version(1)
+            ->Field("LocalInteraction", &UiElementGroupComponent::m_isInteractionLocallyEnabled)
+            ->Field("LocalRendering", &UiElementGroupComponent::m_isRenderingLocallyEnabled);
+
         if (AZ::EditContext* editContext = serializeContext->GetEditContext())
         {
-            editContext->Class<UiElementGroupComponent>("ElementGroup", "A grouping handler that allows interaction and rendering for the entire hierarchy of children.")
-            ->ClassElement(AZ::Edit::ClassElements::EditorData, "")
-            ->Attribute(AZ::Edit::Attributes::Category, "UI")
-            ->Attribute(AZ::Edit::Attributes::Icon, "Icons/Components/Component_Placeholder.svg")
-            ->Attribute(AZ::Edit::Attributes::AppearsInAddComponentMenu, AZ_CRC_CE("UI"))
-            ->DataElement(AZ::Edit::UIHandlers::Default, &UiElementGroupComponent::m_isInteractionLocallyEnabled, "Is Interactive", "Whether this group and children will be interactable.")
-            ->DataElement(AZ::Edit::UIHandlers::Default, &UiElementGroupComponent::m_isRenderingLocallyEnabled, "Is Visible", "Whether this group and children will be rendered.")
-            ;
+            editContext
+                ->Class<UiElementGroupComponent>(
+                    "ElementGroup", "A grouping handler that allows interaction and rendering for the entire hierarchy of children.")
+                ->ClassElement(AZ::Edit::ClassElements::EditorData, "")
+                ->Attribute(AZ::Edit::Attributes::Category, "UI")
+                ->Attribute(AZ::Edit::Attributes::Icon, "Icons/Components/Component_Placeholder.svg")
+                ->Attribute(AZ::Edit::Attributes::AppearsInAddComponentMenu, AZ_CRC_CE("UI"))
+                ->DataElement(
+                    AZ::Edit::UIHandlers::Default,
+                    &UiElementGroupComponent::m_isInteractionLocallyEnabled,
+                    "Is Interactive",
+                    "Whether this group and children will be interactable.")
+                ->DataElement(
+                    AZ::Edit::UIHandlers::Default,
+                    &UiElementGroupComponent::m_isRenderingLocallyEnabled,
+                    "Is Visible",
+                    "Whether this group and children will be rendered.");
         }
     }
-    
+
     if (AZ::BehaviorContext* behaviorContext = azrtti_cast<AZ::BehaviorContext*>(context))
     {
         behaviorContext->EBus<UiElementGroupBus>("UiElementGroupBus")
-        ->Event("Set Interactive State", &UiElementGroupInterface::SetInteractivity)
-        ->Event("Get Interactive State", &UiElementGroupInterface::GetInteractiveState)
-        ->Event("Set Rendering State", &UiElementGroupInterface::SetRendering)
-        ->Event("Get Rendering State", &UiElementGroupInterface::GetRenderingState)
-        ;
+            ->Event("Set Interactive State", &UiElementGroupInterface::SetInteractivity)
+            ->Event("Get Interactive State", &UiElementGroupInterface::GetInteractiveState)
+            ->Event("Set Rendering State", &UiElementGroupInterface::SetRendering)
+            ->Event("Get Rendering State", &UiElementGroupInterface::GetRenderingState);
     }
 }
 
