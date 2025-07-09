11323c190e2b9ef5f73767bb6ee07162390ee08c
Markus Prettner <markus.prettner@huawei.com>
2025-07-09 17:08:09 +0200

    Add option to disable deprecated enum-enum-conversion warnings for 3rd-party dependencies
    Signed-off-by: Markus Prettner <markus.prettner@huawei.com>

--- cmake/Platform/Common/MSVC/Configurations_clang.cmake
+++ cmake/Platform/Common/MSVC/Configurations_clang.cmake
@@ -25,6 +25,10 @@ set(O3DE_COMPILE_OPTION_EXPORT_SYMBOLS "")
 # those 3rd Party targets ONLY.
 set(O3DE_COMPILE_OPTION_DISABLE_WARNINGS PRIVATE /W0)
 
+# C++20 no longer allows to implicitly convert between enum values of different types or enum values and integral types.
+# This is problematic if 3rd-party libraries use such operations in header files.
+set(O3DE_COMPILE_OPTION_DISABLE_DEPRECATED_ENUM_ENUM_CONVERSION PRIVATE /Wv:18)
+
 ly_append_configurations_options(
     DEFINES_PROFILE
         _FORTIFY_SOURCE=2
