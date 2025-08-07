def43badd7072408182b22cc1394cdeefb9d278b
Nicholas Lawson <70027408+nick-l-o3de@users.noreply.github.com>
2025-08-07 16:11:04 -0700

    Fixes blendshapes ("morph targets") not working (#19151)
    Fixes https://github.com/o3de/o3de/issues/19058
    
    It turns out that the blend shape data was getting
    the number of vertices from the mesh by dividing
    the position buffer by 3 (3 floats per vertex),
    but the position buffer was being aligned to some larger
    multiple of 16 bytes.
    
    Instead, I get the number of vertices by using the
    m_numVertices value.
    
    Signed-off-by: Nicholas Lawson <70027408+nick-l-o3de@users.noreply.github.com>

--- Gems/Atom/RPI/Code/Source/RPI.Builders/Model/MorphTargetExporter.cpp
+++ Gems/Atom/RPI/Code/Source/RPI.Builders/Model/MorphTargetExporter.cpp
@@ -165,7 +165,7 @@ namespace AZ::RPI
 
         // Determine the vertex index range for the morph target.
         // Ignore any vertices that are associated with a later product mesh
-        uint32_t numVertices = aznumeric_cast<uint32_t>(productMesh.m_positions.size() / 3);
+        uint32_t numVertices = aznumeric_cast<uint32_t>(productMesh.m_vertexCount);
         uint32_t endVertex = startVertex + numVertices;
 
         if (blendShapeData->GetVertexCount() != sourceMesh.m_meshData->GetVertexCount()
