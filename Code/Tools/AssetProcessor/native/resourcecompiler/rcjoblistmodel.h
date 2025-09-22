ac6301fdf283caad1be0d636581d7ad4241d85b7
Nicholas Lawson <70027408+nick-l-o3de@users.noreply.github.com>
2025-09-22 08:34:30 -0700

    Update Code/Tools/AssetProcessor/native/resourcecompiler/rcjoblistmodel.h
    Co-authored-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>
    Signed-off-by: Nicholas Lawson <70027408+nick-l-o3de@users.noreply.github.com>

--- Code/Tools/AssetProcessor/native/resourcecompiler/rcjoblistmodel.h
+++ Code/Tools/AssetProcessor/native/resourcecompiler/rcjoblistmodel.h
@@ -104,7 +104,7 @@ namespace AssetProcessor
         void EraseJobs(const SourceAssetReference& sourceAssetReference, AZStd::vector<RCJob*>& pendingJobs);
 
         //! This happens when a new source file appears in the intermediate assets folder.
-        //! Returnst true if any jobs were unblocked by this.
+        //! Returns true if any jobs were unblocked by this.
         bool UpdateMissingSourceDependencies(const QString& sourceFileName);
 
     private:
