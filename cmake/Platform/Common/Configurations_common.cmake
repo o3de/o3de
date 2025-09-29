42ceffb1611cc6c69bdb0d8d7882efb91973e1e1
Nicholas Lawson <70027408+nick-l-o3de@users.noreply.github.com>
2025-09-29 11:11:22 -0700

    Fix situations where the config is imported multiple times.
    Signed-off-by: Nicholas Lawson <70027408+nick-l-o3de@users.noreply.github.com>

--- cmake/Platform/Common/Configurations_common.cmake
+++ cmake/Platform/Common/Configurations_common.cmake
@@ -79,6 +79,8 @@ check_pie_supported()
 # Determine if lld is installed to use as a default linker by supported platforms/configurations
 find_program(LLD_LINKER_INSTALLED lld)
 
-# Temporarily gets around the inclusion of <ciso646> in some 3rd party libraries.
-add_library(ciso646-include INTERFACE)
-target_include_directories(ciso646-include INTERFACE ${CMAKE_CURRENT_LIST_DIR}/ciso646-include)
+if (NOT TARGET ciso646-include)
+    # Temporarily gets around the inclusion of <ciso646> in some 3rd party libraries.
+    add_library(ciso646-include INTERFACE)
+    target_include_directories(ciso646-include INTERFACE ${CMAKE_CURRENT_LIST_DIR}/ciso646-include)
+endif()
