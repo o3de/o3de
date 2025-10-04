36c8b03cfe5d51232d4b24fdb1e03732530da558
Luis Sempé <58790905+lsemp3d@users.noreply.github.com>
2025-10-04 12:17:19 -0700

    Fixed error C4855: implicit capture of 'this' via '[=]' is deprecated in '/std:c++20'
    Signed-off-by: Luis Sempé <58790905+lsemp3d@users.noreply.github.com>

--- Code/Tools/AssetProcessor/native/ui/SourceAssetDetailsPanel.cpp
+++ Code/Tools/AssetProcessor/native/ui/SourceAssetDetailsPanel.cpp
@@ -139,7 +139,7 @@ namespace AssetProcessor
             connect(
                 m_ui->gotoAssetButton->m_ui->goToPushButton,
                 &QPushButton::clicked,
-                [=]
+                [this, upstreamSource]
                 {
                     GoToSource(SourceAssetReference(upstreamSource).AbsolutePath().c_str());
                 });
