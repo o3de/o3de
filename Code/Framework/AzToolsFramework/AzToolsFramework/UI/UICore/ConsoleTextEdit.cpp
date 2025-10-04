09002ee14ee2cf4d30a190c66c5f74e0bd4b256e
Luis Sempé <58790905+lsemp3d@users.noreply.github.com>
2025-10-04 11:53:16 -0700

    Fixed error C4855: implicit capture of 'this' via '[=]' is deprecated in '/std:c++20'
    Signed-off-by: Luis Sempé <58790905+lsemp3d@users.noreply.github.com>

--- Code/Framework/AzToolsFramework/AzToolsFramework/UI/UICore/ConsoleTextEdit.cpp
+++ Code/Framework/AzToolsFramework/AzToolsFramework/UI/UICore/ConsoleTextEdit.cpp
@@ -75,7 +75,7 @@ namespace AzToolsFramework
             this,
             &QPlainTextEdit::textChanged,
             selectAllAction,
-            [=]
+            [this, clearAction, selectAllAction]
             {
                 if (document() && !document()->isEmpty())
                 {
