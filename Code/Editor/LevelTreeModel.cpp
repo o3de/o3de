a247d8289b1c077ef630c313eaba1a113b9137e6
guillaume-haerinck <guillaume.haerinck@outlook.com>
2026-01-24 14:38:48 +0100

    Invalidate filter to beginEndFilter
    Signed-off-by: guillaume-haerinck <guillaume.haerinck@outlook.com>

--- Code/Editor/LevelTreeModel.cpp
+++ Code/Editor/LevelTreeModel.cpp
@@ -68,8 +68,9 @@ void LevelTreeModelFilter::setFilterText(const QString& text)
     QString lowerText = text.toLower();
     if (m_filterText != lowerText)
     {
+        beginFilterChange();
         m_filterText = lowerText;
-        invalidateFilter();
+        endFilterChange();
     }
 }
 
