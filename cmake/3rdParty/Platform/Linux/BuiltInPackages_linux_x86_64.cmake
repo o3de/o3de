0d110730d1ef9ff21db3c1fbfd3b5bba6832f36a
amzn-changml <amzn-changml@users.noreply.github.com>
2026-05-18 05:03:10 +0000

    Update 3P version and SHA256 hash for pyside6-6.10.2
    Signed-off-by: GitHub <noreply@github.com>

--- cmake/3rdParty/Platform/Linux/BuiltInPackages_linux_x86_64.cmake
+++ cmake/3rdParty/Platform/Linux/BuiltInPackages_linux_x86_64.cmake
@@ -43,3 +43,4 @@ ly_associate_package(PACKAGE_NAME pyside2-5.15.2.1-py3.10-rev7-linux            
 ly_associate_package(PACKAGE_NAME SQLite-3.37.2-rev1-linux                          TARGETS SQLite                      PACKAGE_HASH bee80d6c6db3e312c1f4f089c90894436ea9c9b74d67256d8c1fb00d4d81fe46)
 ly_associate_package(PACKAGE_NAME AwsIotDeviceSdkCpp-1.15.2-rev1-linux              TARGETS AwsIotDeviceSdkCpp          PACKAGE_HASH 83fc1711404d3e5b2faabb1134e97cc92b748d8b87ff4ea99599d8c750b8eff0)
 ly_associate_package(PACKAGE_NAME vulkan-validationlayers-1.2.198-rev1-linux        TARGETS vulkan-validationlayers     PACKAGE_HASH 9195c7959695bcbcd1bc1dc5c425c14639a759733b3abe2ffa87eb3915b12c71)
+ly_associate_package(PACKAGE_NAME pyside6-6.10.2-py3.10-rev1-linux                  TARGETS pyside6                     PACKAGE_HASH 70c81d45178c3898b2167d8fabf886c854d72f6ac84557b6b3a669d4d596437b
