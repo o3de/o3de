b95d68bbafe69c5aee3218d6489a0e710ddc9017
iamjbq <119447878+iamjbq@users.noreply.github.com>
2026-06-19 12:03:15 -0300

    Editor material asset converts older compliant contact mode
    Version 2 material assets are converted in editor to version 3 by checking for enabled compliant contact mode and sets restitution to the negative value of stiffness set.
    
    Signed-off-by: iamjbq <119447878+iamjbq@users.noreply.github.com>

--- Gems/PhysX/Core/Code/Editor/Source/Material/PhysXEditorMaterialAsset.cpp
+++ Gems/PhysX/Core/Code/Editor/Source/Material/PhysXEditorMaterialAsset.cpp
@@ -13,15 +13,69 @@
 
 namespace PhysX
 {
+    namespace Internal
+    {
+        // TODO: successfully converts older assets set in material slots during runtime, but does not convert when opened in Asset Editor
+        bool MaterialAssetDataConverter(AZ::SerializeContext& context, AZ::SerializeContext::DataElementNode& classElement)
+        {
+            // We're not going to try to convert legacy assets right now
+            // Just the latest materials before PhysX 5.6.1
+            if (classElement.GetVersion() != 3)
+            {
+                return false;
+            }
+
+            const int configurationIdx = classElement.FindElement(AZ_CRC_CE("MaterialConfiguration"));
+            if (configurationIdx >= 0)
+            {
+                auto& configurationElement = classElement.GetSubElement(configurationIdx);
+
+                // Explicit control of Compliant Contact was removed in PhysX 5.6.1 and is instead enabled by negative restitution
+                // This is the only change to material assets from version 2
+                const int compliantContactModeIdx = configurationElement.FindElement(AZ_CRC_CE("CompliantContactMode"));
+                const int restitutionIdx = configurationElement.FindElement(AZ_CRC_CE("Restitution"));
+                if (compliantContactModeIdx >= 0 && restitutionIdx >= 0)
+                {
+                    auto& compliantContactModeElement = configurationElement.GetSubElement(compliantContactModeIdx);
+                    auto& restitutionElement = configurationElement.GetSubElement(restitutionIdx);
+                    if (compliantContactModeElement.GetVersion() < 2)
+                    {
+                        const int enabledIdx = compliantContactModeElement.FindElement(AZ_CRC_CE("Enabled"));
+                        const int stiffnessIdx = compliantContactModeElement.FindElement(AZ_CRC_CE("Stiffness"));
+                        if (enabledIdx >= 0 && stiffnessIdx >= 0)
+                        {
+                            auto& enabledElement = compliantContactModeElement.GetSubElement(enabledIdx);
+                            auto& stiffnessElement = compliantContactModeElement.GetSubElement(stiffnessIdx);
+                            bool enabled = false;
+                            if (enabledElement.GetData(enabled)) // Set restitution to negative stiffness if it is enabled and remove switch
+                            {
+                                if (enabled)
+                                {
+                                    float stiffness = 0.0f;
+                                    if (stiffnessElement.GetData(stiffness))
+                                    {
+                                        restitutionElement.SetData(context, -stiffness); //TODO: not triggered in editor
+                                    }
+                                }
+                                compliantContactModeElement.RemoveElementByName(AZ_CRC_CE("Enabled"));
+                            }
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+
     void EditorMaterialAsset::Reflect(AZ::ReflectContext* context)
     {
         if (auto* serializeContext = azrtti_cast<AZ::SerializeContext*>(context))
         {
             serializeContext->Class<PhysX::EditorMaterialAsset, AZ::Data::AssetData>()
-                ->Version(3)
+                ->Version(3, Internal::MaterialAssetDataConverter)
                 ->Attribute(AZ::Edit::Attributes::EnableForAssetEditor, true)
-                ->Field("MaterialConfiguration", &EditorMaterialAsset::m_materialConfiguration)
-                ;
+                ->Field("MaterialConfiguration", &EditorMaterialAsset::m_materialConfiguration);
 
             if (auto* editContext = serializeContext->GetEditContext())
             {
