778152d9b01ff582a0dde6e30a83cc61b5d216cf
guillaume-haerinck <guillaume.haerinck@outlook.com>
2026-06-21 11:35:51 +0200

    Fix rpath command
    Signed-off-by: guillaume-haerinck <guillaume.haerinck@outlook.com>

--- Gems/LmbrCentral/Code/Platform/Linux/lrelease_linux.cmake
+++ Gems/LmbrCentral/Code/Platform/Linux/lrelease_linux.cmake
@@ -9,7 +9,7 @@
 add_custom_command(TARGET LmbrCentral.Editor POST_BUILD
     COMMAND "${CMAKE_COMMAND}" -P "${LY_ROOT_FOLDER}/cmake/Platform/Linux/RPathChange.cmake"
             "$<TARGET_FILE_DIR:LmbrCentral.Editor>/lrelease"
-            "$ORIGIN/../bin"
+            "$ORIGIN/../lib"
             "$ORIGIN"
     COMMENT "Patching lrelease..."
     VERBATIM
