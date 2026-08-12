2937b2f433641c0916b7ea119d6af08d115119fe
EnterTheArcane <96613937+EnterTheArcane@users.noreply.github.com>
2026-08-11 23:56:28 -0600

    Simplify pybind11 fetchcontent
    Signed-off-by: EnterTheArcane <96613937+EnterTheArcane@users.noreply.github.com>

--- cmake/3rdParty/Findpybind11.cmake
+++ cmake/3rdParty/Findpybind11.cmake
@@ -19,18 +19,14 @@ if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
 endif()
 
 if(INSTALLED_ENGINE)
-    ly_target_include_system_directories(TARGET 3rdParty::pybind11
+    ly_target_include_system_directories(
+        TARGET 3rdParty::pybind11
         INTERFACE "${LY_ROOT_FOLDER}/include/pybind11"
     )
     return()
 endif()
 
 block()
-    set(ADDITIONAL_FETCHCONTENT_FLAGS "")
-    if(CMAKE_VERSION VERSION_GREATER_EQUAL "3.25")
-        list(APPEND ADDITIONAL_FETCHCONTENT_FLAGS "SYSTEM")
-    endif()
-
     o3de_fetch_content(pybind11
         VERSION "v2.10.0"
         LICENSE "BSD-3-Clause"
@@ -38,32 +34,24 @@ block()
         URL_HASH "eacf582fa8f696227988d08cfc46121770823839fe9e301a20fbce67e7cd70ec"
         GIT "https://github.com/pybind/pybind11.git"
         GIT_HASH "aa304c9c7d725ffb9d10af08a3b34cb372307020"
-        EXCLUDE_FROM_ALL
-        ${ADDITIONAL_FETCHCONTENT_FLAGS}
+        SOURCE_SUBDIR "include"
     )
 
-    # FetchContent ignores CMAKE_ARGS, so configure pybind11 through the variables its option() calls consume.
-    # O3DE provides Python separately through 3rdParty::Python; only pybind11's headers are needed here.
-    set(PYBIND11_INSTALL OFF)
-    set(PYBIND11_NOPYTHON ON)
-    set(PYBIND11_TEST OFF)
-    set(CMAKE_POLICY_VERSION_MINIMUM "3.5")
-
-    set(OLD_LOG_LEVEL ${CMAKE_MESSAGE_LOG_LEVEL})
-    set(CMAKE_MESSAGE_LOG_LEVEL ${O3DE_FETCHCONTENT_MESSAGE_LEVEL})
-
     FetchContent_MakeAvailable(pybind11)
-
-    set(CMAKE_MESSAGE_LOG_LEVEL ${OLD_LOG_LEVEL})
-
-    target_link_libraries(3rdParty::pybind11 INTERFACE pybind11::pybind11)
-
     FetchContent_GetProperties(pybind11 SOURCE_DIR pybind11_source_dir)
-    ly_install(DIRECTORY "${pybind11_source_dir}/include/pybind11"
+
+    ly_target_include_system_directories(
+        TARGET 3rdParty::pybind11
+        INTERFACE "${pybind11_source_dir}/include"
+    )
+
+    ly_install(
+        DIRECTORY "${pybind11_source_dir}/include/pybind11"
         DESTINATION include/pybind11
         COMPONENT CORE
     )
-    ly_install(FILES "${pybind11_source_dir}/LICENSE"
+    ly_install(
+        FILES "${pybind11_source_dir}/LICENSE"
         DESTINATION include/pybind11
         COMPONENT CORE
     )
