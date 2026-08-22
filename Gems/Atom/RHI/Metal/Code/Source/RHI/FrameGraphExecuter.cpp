29f6e4cb24b36e49cbb5cff3dd27af8b64442cef
Arbiter <96613937+EnterTheArcane@users.noreply.github.com>
2026-08-22 09:44:33 -0600

    Fix zero-item FrameGraph command list count (#20031)
    Signed-off-by: EnterTheArcane <96613937+EnterTheArcane@users.noreply.github.com>

--- Gems/Atom/RHI/Metal/Code/Source/RHI/FrameGraphExecuter.cpp
+++ Gems/Atom/RHI/Metal/Code/Source/RHI/FrameGraphExecuter.cpp
@@ -174,7 +174,7 @@ namespace AZ
                     {
                         // And then create a new group for the current scope with dedicated [1, N] command lists.
                         const AZ::u32 commandListCount =
-                            AZStd::clamp(RHI::DivideByMultiple(totalScopeCost, CommandListCostThreshold), 1u, estimatedItemCount);
+                            AZStd::max(AZStd::min(RHI::DivideByMultiple(totalScopeCost, CommandListCostThreshold), estimatedItemCount), 1u);
 
                         FrameGraphExecuteGroupSecondary* scopeContextGroup = AddGroup<FrameGraphExecuteGroupSecondary>();
                         scopeContextGroup->Init(static_cast<Device&>(scope.GetDevice()), scope, commandListCount, GetJobPolicy());
