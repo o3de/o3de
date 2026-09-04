da61e022c41cc771678aa09b995d8a41f0252f53
nick-l-o3de <nick-l-o3de@users.noreply.github.com>
2026-09-04 02:20:24 +0000

    Update 3P version and SHA256 hash for pyside6-6.11.2
    Signed-off-by: GitHub <noreply@github.com>

--- cmake/3rdParty/Platform/Linux/BuiltInPackages_linux_aarch64.cmake
+++ cmake/3rdParty/Platform/Linux/BuiltInPackages_linux_aarch64.cmake
@@ -38,3 +38,4 @@ ly_associate_package(PACKAGE_NAME pyside2-5.15.2.1-py3.10-rev7-linux-aarch64    
 ly_associate_package(PACKAGE_NAME SQLite-3.37.2-rev1-linux-aarch64                           TARGETS SQLite                      PACKAGE_HASH 5cc1fd9294af72514eba60509414e58f1a268996940be31d0ab6919383f05118)
 ly_associate_package(PACKAGE_NAME AwsIotDeviceSdkCpp-1.15.2-rev1-linux-aarch64               TARGETS AwsIotDeviceSdkCpp          PACKAGE_HASH 0bac80fc09094c4fd89a845af57ebe4ef86ff8d46e92a448c6986f9880f9ee62)
 ly_associate_package(PACKAGE_NAME vulkan-validationlayers-1.2.198-rev1-linux-aarch64         TARGETS vulkan-validationlayers     PACKAGE_HASH e67a15a95e14397ccdffd70d17f61079e5720fea22b0d21e135497312419a23f)
+ly_associate_package(PACKAGE_NAME pyside6-6.11.2-py3.10-rev1-linux-aarch64                   TARGETS pyside6                     PACKAGE_HASH f0211df6ad1222af1cf17b4f0a9a6a0bcfe5d722ba178778fd31359b0175ac5a
