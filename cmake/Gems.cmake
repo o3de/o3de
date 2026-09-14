6f2a9a35f22d111d67091c82ffc5c35664f17840
EnterTheArcane <96613937+EnterTheArcane@users.noreply.github.com>
2026-09-14 10:35:03 -0600

    Warn when registered third-party roots do not exist
    Signed-off-by: EnterTheArcane <96613937+EnterTheArcane@users.noreply.github.com>

--- cmake/Gems.cmake
+++ cmake/Gems.cmake
@@ -87,7 +87,9 @@ macro(o3de_gem_setup) #default_gem_name)
 
     set(gem_json ${gem_path}/gem.json)
 
-    o3de_register_3rdparty_root("${gem_path}/3rdParty")
+    if(IS_DIRECTORY "${gem_path}/3rdParty")
+        o3de_register_3rdparty_root("${gem_path}/3rdParty")
+    endif()
 
     # Read the version field from the gem.json
     set(gem_version, "0.0.0")
